6739aa923a6c4d85eca93a6eedecb005b6bb85ea
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2013-11-08 18:09:56 +0900

    add Grunt task reload

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,19 @@ module.exports = function(grunt) {
       }
     },
     watch: {
-      files: 'src/*.js',
-      tasks: 'concat'
+      javascript: {
+        files: 'src/*.js',
+        tasks: 'concat'
+      },
+      static_files: {
+        files: ['textae.html', 'js/textae.js'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+    connect: {
+      uses_defaults: {}
     },
     qunit: {
       all: 'test/util.html'
@@ -17,5 +28,6 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-qunit');
 };
