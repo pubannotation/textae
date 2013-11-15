fe756aa069926416d36a1795de12a95c7b14d6ab
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2013-11-15 17:06:06 +0900

    move loadSaveDialog to editor.

--- js/textae.js
+++ js/textae.js
@@ -214,15 +214,15 @@
 //like a jQuery plugin
 (function(jQuery) {
     var editor = function() {
-        var $$textaeEditor = this;
+        var $textaeEditor = this;
 
         //cursor
         var setupWait = function(self) {
             var wait = function() {
-                $$textaeEditor.css('cursor', 'wait');
+                $textaeEditor.css('cursor', 'wait');
             };
             var endWait = function() {
-                $$textaeEditor.css('cursor', 'auto');
+                $textaeEditor.css('cursor', 'auto');
             }
             self.startWait = wait;
             self.endWait = endWait;
@@ -280,9 +280,130 @@
             }
         }();
 
-        //init
+        //load/saveDialog
+        var loadSaveDialog = function() {
+            var getLoadDialog = function() {
+                $body = $("body");
+                var $dialog = $body.find("#textae.dialog.load");
+
+                //make unless exists
+                if ($dialog.length === 0) {
+                    $dialog = $('<div id="textae.dialog.load" title="Load document with annotation.">')
+                        .append('<div>Sever :<input type="text" style="width:345px"/><input type="button" value="OK" /></div>')
+                        .append('<div>Local :<input type="file"　/></div>');
+
+                    //bind event handler
+                    var onFileChange = function() {
+                        var reader = new FileReader();
+                        reader.onload = function() {
+                            var annotation = JSON.parse(this.result);
+                            $("body").trigger("textae.dialog.localfile.load", annotation);
+                            $dialog.dialog("close");
+                        };
+                        reader.readAsText(this.files[0]);
+                    }
+
+                    $body.append($dialog);
+                    $dialog.hide()
+                    $dialog.find("input[type='file']").on("change", onFileChange);
+                    $dialog.find("input[type='button']")
+                        .on("click", function() {
+                            var url = $dialog.find("input[type='text']").val();
+                            $("body").trigger("textae.dialog.loadurl.select", url);
+                            $dialog.dialog("close");
+                        });
+                }
+
+                return $dialog;
+            };
+            return {
+                showAccess: function(targetUrl) {
+                    var $dialog = getLoadDialog();
+                    $dialog
+                        .find("input[type='text']")
+                        .val(targetUrl);
+                    $dialog
+                        .dialog({
+                            resizable: false,
+                            width: 550,
+                            height: 220,
+                            modal: true,
+                            buttons: {
+                                Cancel: function() {
+                                    $(this).dialog("close");
+                                }
+                            }
+                        });
+                },
+                showSave: function(url, content) {
+                    var getSaveDialog = function() {
+                        $body = $("body");
+                        var $dialog = $body.find("#textae.dialog.save");
+                        if ($dialog.length === 0) {
+                            $dialog = $('<div id="textae.dialog.save" title="Save document with annotation.">')
+                                .append('<div>Sever :<input type="text" style="width:345px"/><input type="button" value="OK" /></div>')
+                                .append('<div>Local :<span class="span_link_place"><a target="_blank"/></span></div>');
+
+                            $body.append($dialog);
+                            $dialog.hide();
+                            $dialog
+                                .on("click", "a", function() {
+                                    $("body").trigger("textae.dialog.localfile.save");
+                                    $dialog.dialog("close");
+                                })
+                                .on("click", "input[type='button']", function() {
+                                    $("body").trigger("textae.dialog.saveurl.select", $dialog.find("input[type='text']").val());
+                                    $dialog.dialog("close");
+                                });
+
+                        }
+
+                        return $dialog;
+                    };
+
+                    var createFileLink = function(contents, $save_dialog) {
+                        var $fileInput = getLoadDialog().find("input[type='file']");
+
+                        var file = $fileInput.prop("files")[0]
+                        var name = file ? file.name : "annotations.json";
+                        var blob = new Blob([contents], {
+                            type: 'application/json'
+                        });
+                        var link = $save_dialog.find('a')
+                            .text(name)
+                            .attr("href", URL.createObjectURL(blob))
+                            .attr("download", name);
+                    };
+
+                    var $dialog = getSaveDialog();
+
+                    //create local link
+                    createFileLink(content, $dialog);
+
+                    //open dialog
+                    $dialog
+                        .find("input[type='text']")
+                        .val(url);
+                    $dialog
+                        .dialog({
+                            resizable: false,
+                            width: 550,
+                            height: 220,
+                            modal: true,
+                            buttons: {
+                                Cancel: function() {
+                                    $(this).dialog("close");
+                                }
+                            }
+                        });
+                }
+            };
+        }();
+
+        //public to use on textae.js
         setupWait(this);
         this.entityTypes = entityTypes;
+        this.loadSaveDialog = loadSaveDialog;
 
         return this;
     };
@@ -649,123 +770,6 @@
         };
     })();
 })(jQuery);
-var loadSaveDialog = function() {
-    var getLoadDialog = function(){
-        $body = $("body");
-        var $dialog = $body.find("#textae.dialog.load");
-
-        //make unless exists
-        if($dialog.length === 0){
-            $dialog = $('<div id="textae.dialog.load" title="Load document with annotation.">')
-                .append('<div>Sever :<input type="text" style="width:345px"/><input type="button" value="OK" /></div>')
-                .append('<div>Local :<input type="file"　/></div>');
-
-            //bind event handler
-            var onFileChange = function(){
-                var reader = new FileReader();
-                reader.onload = function(){ 
-                    var annotation = JSON.parse(this.result);
-                    $("body").trigger("textae.dialog.localfile.load", annotation);
-                    $dialog.dialog("close");
-                };
-                reader.readAsText(this.files[0]);
-            }
-
-            $body.append($dialog);
-            $dialog.hide()
-            $dialog.find("input[type='file']").on("change", onFileChange);
-            $dialog.find("input[type='button']")
-                .on("click", function(){
-                    var url = $dialog.find("input[type='text']").val();
-                    $("body").trigger("textae.dialog.loadurl.select", url);
-                    $dialog.dialog("close");
-                });
-        }
-
-        return $dialog;
-    };
-    return {
-        showAccess : function(targetUrl) {
-            var $dialog = getLoadDialog();
-            $dialog
-                .find("input[type='text']")
-                .val(targetUrl);
-            $dialog
-                .dialog({
-                    resizable: false,
-                    width:550,
-                    height:220,
-                    modal: true,
-                    buttons: {
-                        Cancel: function() {
-                            $( this ).dialog( "close" );
-                        }
-                    }
-                });
-        },
-        showSave : function(url, content) {
-            var getSaveDialog = function(){
-                $body = $("body");
-                var $dialog = $body.find("#textae.dialog.save");
-                if($dialog.length === 0){
-                    $dialog = $('<div id="textae.dialog.save" title="Save document with annotation.">')
-                        .append('<div>Sever :<input type="text" style="width:345px"/><input type="button" value="OK" /></div>')
-                        .append('<div>Local :<span class="span_link_place"><a target="_blank"/></span></div>');
-                 
-                    $body.append($dialog);
-                    $dialog.hide();
-                    $dialog
-                        .on("click", "a", function(){
-                            $("body").trigger("textae.dialog.localfile.save");
-                            $dialog.dialog("close");
-                    }   )
-                        .on("click", "input[type='button']", function(){
-                            $("body").trigger("textae.dialog.saveurl.select", $dialog.find("input[type='text']").val());
-                            $dialog.dialog("close");
-                        });
-
-                    }
-
-                return $dialog;
-            };
-
-            var createFileLink= function(contents, $save_dialog){
-                var $fileInput = getLoadDialog().find("input[type='file']");
-
-                var file = $fileInput.prop("files")[0] 
-                var name = file ? file.name : "annotations.json";
-                var blob = new Blob([contents],{type:'application/json'});
-                var link = $save_dialog.find('a')
-                    .text(name)
-                    .attr("href",URL.createObjectURL(blob))
-                    .attr("download", name);
-            };
-
-            var $dialog = getSaveDialog();
-
-            //create local link
-            createFileLink(content, $dialog);
-
-            //open dialog
-            $dialog
-                .find("input[type='text']")
-                .val(url);
-            $dialog
-                .dialog({
-                    resizable: false,
-                    width:550,
-                    height:220,
-                    modal: true,
-                    buttons: {
-                        Cancel: function() {
-                            $( this ).dialog( "close" );
-                        }
-                    }
-                });
-        }
-    };
-}();
-
 // Application main
 $(document).ready(function() {
     var mode = 'span';  // screen mode: view | span(default) | relation
@@ -3047,8 +3051,8 @@ $(document).ready(function() {
         replicate: businessLogic.replicate,
         toggleReplicateAuto: businessLogic.toggleReplicateAuto,
         showPallet: presentationLogic.showPallet,
-        showAccess: function(){loadSaveDialog.showAccess(targetUrl);},
-        showSave: function(){loadSaveDialog.showSave(targetUrl, annotation_data.toJason());},
+        showAccess: function(){ $textaeEditor.loadSaveDialog.showAccess(targetUrl);},
+        showSave: function(){$textaeEditor.loadSaveDialog.showSave(targetUrl, annotation_data.toJason());},
         newLabe: businessLogic.newLabe,
         redo: function(){if (editHistory.hasAnythingToRedo()) { businessLogic.redo();}},
         undo: function(){if (editHistory.hasAnythingToUndo()) {businessLogic.undo();}},
--- src/jquery.textae.js
+++ src/jquery.textae.js
@@ -1,15 +1,15 @@
 //like a jQuery plugin
 (function(jQuery) {
     var editor = function() {
-        var $$textaeEditor = this;
+        var $textaeEditor = this;
 
         //cursor
         var setupWait = function(self) {
             var wait = function() {
-                $$textaeEditor.css('cursor', 'wait');
+                $textaeEditor.css('cursor', 'wait');
             };
             var endWait = function() {
-                $$textaeEditor.css('cursor', 'auto');
+                $textaeEditor.css('cursor', 'auto');
             }
             self.startWait = wait;
             self.endWait = endWait;
@@ -67,9 +67,130 @@
             }
         }();
 
-        //init
+        //load/saveDialog
+        var loadSaveDialog = function() {
+            var getLoadDialog = function() {
+                $body = $("body");
+                var $dialog = $body.find("#textae.dialog.load");
+
+                //make unless exists
+                if ($dialog.length === 0) {
+                    $dialog = $('<div id="textae.dialog.load" title="Load document with annotation.">')
+                        .append('<div>Sever :<input type="text" style="width:345px"/><input type="button" value="OK" /></div>')
+                        .append('<div>Local :<input type="file"　/></div>');
+
+                    //bind event handler
+                    var onFileChange = function() {
+                        var reader = new FileReader();
+                        reader.onload = function() {
+                            var annotation = JSON.parse(this.result);
+                            $("body").trigger("textae.dialog.localfile.load", annotation);
+                            $dialog.dialog("close");
+                        };
+                        reader.readAsText(this.files[0]);
+                    }
+
+                    $body.append($dialog);
+                    $dialog.hide()
+                    $dialog.find("input[type='file']").on("change", onFileChange);
+                    $dialog.find("input[type='button']")
+                        .on("click", function() {
+                            var url = $dialog.find("input[type='text']").val();
+                            $("body").trigger("textae.dialog.loadurl.select", url);
+                            $dialog.dialog("close");
+                        });
+                }
+
+                return $dialog;
+            };
+            return {
+                showAccess: function(targetUrl) {
+                    var $dialog = getLoadDialog();
+                    $dialog
+                        .find("input[type='text']")
+                        .val(targetUrl);
+                    $dialog
+                        .dialog({
+                            resizable: false,
+                            width: 550,
+                            height: 220,
+                            modal: true,
+                            buttons: {
+                                Cancel: function() {
+                                    $(this).dialog("close");
+                                }
+                            }
+                        });
+                },
+                showSave: function(url, content) {
+                    var getSaveDialog = function() {
+                        $body = $("body");
+                        var $dialog = $body.find("#textae.dialog.save");
+                        if ($dialog.length === 0) {
+                            $dialog = $('<div id="textae.dialog.save" title="Save document with annotation.">')
+                                .append('<div>Sever :<input type="text" style="width:345px"/><input type="button" value="OK" /></div>')
+                                .append('<div>Local :<span class="span_link_place"><a target="_blank"/></span></div>');
+
+                            $body.append($dialog);
+                            $dialog.hide();
+                            $dialog
+                                .on("click", "a", function() {
+                                    $("body").trigger("textae.dialog.localfile.save");
+                                    $dialog.dialog("close");
+                                })
+                                .on("click", "input[type='button']", function() {
+                                    $("body").trigger("textae.dialog.saveurl.select", $dialog.find("input[type='text']").val());
+                                    $dialog.dialog("close");
+                                });
+
+                        }
+
+                        return $dialog;
+                    };
+
+                    var createFileLink = function(contents, $save_dialog) {
+                        var $fileInput = getLoadDialog().find("input[type='file']");
+
+                        var file = $fileInput.prop("files")[0]
+                        var name = file ? file.name : "annotations.json";
+                        var blob = new Blob([contents], {
+                            type: 'application/json'
+                        });
+                        var link = $save_dialog.find('a')
+                            .text(name)
+                            .attr("href", URL.createObjectURL(blob))
+                            .attr("download", name);
+                    };
+
+                    var $dialog = getSaveDialog();
+
+                    //create local link
+                    createFileLink(content, $dialog);
+
+                    //open dialog
+                    $dialog
+                        .find("input[type='text']")
+                        .val(url);
+                    $dialog
+                        .dialog({
+                            resizable: false,
+                            width: 550,
+                            height: 220,
+                            modal: true,
+                            buttons: {
+                                Cancel: function() {
+                                    $(this).dialog("close");
+                                }
+                            }
+                        });
+                }
+            };
+        }();
+
+        //public to use on textae.js
         setupWait(this);
         this.entityTypes = entityTypes;
+        this.loadSaveDialog = loadSaveDialog;
 
         return this;
     };
