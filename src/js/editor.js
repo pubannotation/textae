3fa06b198881644b39c09e0c020531eb3ae48122
shigeru nakajima <shigeru.nakajima@luxiar.com>
2014-03-26 18:46:30 +0900

    Display short name instead of URL. 1. The supported url scheme is only http and https. 2. Display the file name and the extention if exists. 3. Display the last directory name unless the file name exists. 4. Display the domain name unless the file name and the directory name exists. 5. The regular-expression came from http://someweblog.com/url-regular-expression-javascript-link-shortener/

--- src/js/editor.js
+++ src/js/editor.js
@@ -1268,10 +1268,30 @@
                                             .attr('id', 'P-' + typeId)
                                             .addClass('textae-editor__entity-pane');
 
-                                        // Label over the span.
+                                        // Display short name for URL(http or https);
+                                        var displayName = type;
+                                        // For tunning, search the scheme before execute a regular-expression.
+                                        if (String(type).indexOf('http') > -1) {
+                                            // The regular-expression to parse URL.
+                                            // See detail:
+                                            // http://someweblog.com/url-regular-expression-javascript-link-shortener/
+                                            var urlRegex = /\(?(?:(http|https|ftp):\/\/)?(?:((?:[^\W\s]|\.|-|[:]{1})+)@{1})?((?:www.)?(?:[^\W\s]|\.|-)+[\.][^\W\s]{2,4}|localhost(?=\/)|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::(\d*))?([\/]?[^\s\?]*[\/]{1})*(?:\/?([^\s\n\?\[\]\{\}\#]*(?:(?=\.)){1}|[^\s\n\?\[\]\{\}\.\#]*)?([\.]{1}[^\s\?\#]*)?)?(?:\?{1}([^\s\n\#\[\]]*))?([\#][^\s\n]*)?\)?/gi;
+                                            var matches = urlRegex.exec(type);
+                                            // Order to dispaly.
+                                            // 1. The file name with the extention.
+                                            // 2. The last directory name.
+                                            // 3. The domain name.
+                                            displayName = matches[6] ? matches[6] + (matches[7] || '') :
+                                                matches[5] ? matches[5].split('/').filter(function(s) {
+                                                    return s !== '';
+                                                }).pop() :
+                                                matches[3];
+                                        }
+
+                                        // The label over the span.
                                         var $typeLabel = $('<div>')
                                             .addClass('textae-editor__type-label')
-                                            .text(type)
+                                            .text(displayName)
                                             .css({
                                                 'background-color': view.viewModel.typeContainer.entity.getColor(type),
                                             });
