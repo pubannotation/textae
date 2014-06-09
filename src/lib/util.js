d7a4b1d5336633532db38f30ab69642926603ab7
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2014-06-09 12:09:03 +0900

    Extract the selection model.

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,215 +1,253 @@
     // utility functions
-    var textAeUtil = {
-        // ajax wrapper
-        ajaxAccessor: function() {
-            var isEmpty = function(str) {
-                return !str || str === "";
-            };
+    var textAeUtil = function() {
+        // A mixin for the separeted presentation by the observer pattern.
+        var bindable = function() {
+            var callbacks = {};
+
             return {
-                getSync: function(url) {
-                    if (isEmpty(url)) {
-                        return;
-                    }
+                bind: function(event, callback) {
+                    if (!_.isFunction(callback)) throw new Error('Only a function is bindable!');
 
-                    var result = null;
-                    $.ajax({
-                        type: "GET",
-                        url: url,
-                        async: false
-                    }).done(function(data) {
-                        result = data;
+                    callbacks[event] = callbacks[event] || [];
+                    callbacks[event].push(callback);
+                    return this;
+                },
+                unbind: function(event, callback) {
+                    if (!callbacks[event]) return this;
+
+                    callbacks[event] = _.reject(callbacks[event], function(existsCallback) {
+                        return existsCallback === callback;
                     });
-                    return result;
+                    return this;
                 },
-
-                getAsync: function(url, dataHandler, finishHandler) {
-                    if (isEmpty(url)) {
-                        return;
-                    }
-
-                    $.ajax({
-                        type: "GET",
-                        url: url,
-                        cache: false
-                    })
-                        .done(function(data) {
-                            if (dataHandler !== undefined) {
-                                dataHandler(data);
-                            }
-                        })
-                        .fail(function(res, textStatus, errorThrown) {
-                            alert("connection failed.");
-                        })
-                        .always(function(data) {
-                            if (finishHandler !== undefined) {
-                                finishHandler();
-                            }
+                trigger: function(event, data) {
+                    if (callbacks[event]) {
+                        callbacks[event].forEach(function(callback) {
+                            callback(data);
                         });
-                },
-
-                post: function(url, data, successHandler, failHandler, finishHandler) {
-                    if (isEmpty(url)) {
-                        return;
                     }
-
-                    console.log("POST data", data);
-
-                    $.ajax({
-                        type: "post",
-                        url: url,
-                        contentType: "application/json",
-                        data: data,
-                        crossDomain: true,
-                        xhrFields: {
-                            withCredentials: true
-                        }
-                    })
-                        .done(successHandler)
-                        .fail(failHandler)
-                        .always(finishHandler);
+                    return data;
                 }
             };
-        }(),
+        };
 
-        // Usage sample: textAeUtil.getUrlParameters(location.search). 
-        getUrlParameters: function(urlQuery) {
-            // Remove ? at top.
-            var queryString = urlQuery ? String(urlQuery).replace(/^\?(.*)/, '$1') : '';
-
-            // Convert to array if exists
-            var querys = queryString.length > 0 ? queryString.split('&') : [];
-
-            return querys
-                .map(function(param) {
-                    // Convert string "key=value" to object.
-                    var vals = param.split('=');
-                    return {
-                        key: vals[0],
-                        val: vals[1]
-                    };
-                }).reduce(function(a, b) {
-                    // Convert [{key: 'abc', val: '123'},...] to { abc: 123 ,...}
-                    // Set value true if val is not set.
-                    a[b.key] = b.val ? b.val : true;
-                    return a;
-                }, {});
-        },
-
-        makeInformationModal: function() {
-            //this is bound object.
-            var showModal = function(className, obj) {
-                var getModal = function() {
-                    var $modal = $('.' + this.className);
-                    // add modal unless exists
-                    if ($modal.length === 0) {
-                        $modal = $('<div>')
-                            .addClass('textae__information-modal')
-                            .addClass(this.className)
-                            .hide();
-                        this.addContentsFunc.call($modal);
-                        $('body').append($modal);
-                    }
-                    return $modal;
+        return {
+            // ajax wrapper
+            ajaxAccessor: function() {
+                var isEmpty = function(str) {
+                    return !str || str === "";
                 };
-
-                var setPositionCenter = function($modal) {
-                    var $window = $(window);
-                    $modal.css({
-                        'position': 'absolute',
-                        'top': ($window.height() - $modal.height()) / 2 + $window.scrollTop(),
-                        'left': ($window.width() - $modal.width()) / 2 + $window.scrollLeft()
-                    });
-                };
-
-                //close other dialogs
-                $('.textae__information-modal').hide();
-
-                //show at center
-                var $modal = getModal.call(this);
-                setPositionCenter($modal);
-                $modal.show();
-            };
-
-            //this is bound object.
-            var hideModal = function(className) {
-                $('.' + this.className).hide();
-            };
-
-            //expected param has className and addContentsFunc.
-            var bindObject = function(param) {
                 return {
-                    show: showModal.bind(param),
-                    hide: hideModal.bind(param)
-                };
-            };
+                    getSync: function(url) {
+                        if (isEmpty(url)) {
+                            return;
+                        }
 
-            //close modal when modal clicked.
-            $(function() {
-                $('body').on('mouseup', '.textae__information-modal', function() {
-                    $(this).hide();
-                });
-            });
+                        var result = null;
+                        $.ajax({
+                            type: "GET",
+                            url: url,
+                            async: false
+                        }).done(function(data) {
+                            result = data;
+                        });
+                        return result;
+                    },
 
-            return bindObject;
-        }(),
+                    getAsync: function(url, dataHandler, finishHandler) {
+                        if (isEmpty(url)) {
+                            return;
+                        }
 
-        getDialog: function() {
-            // Cash a div for dialog by self, because $('#dialog_id') cannot find exists div element.
-            var cash = {};
-
-            return function(editorId, id, title, $content, noCancelButton) {
-                var makeDialog = function(id) {
-                    var $dialog = $('<div>')
-                        .attr('id', id)
-                        .attr('title', title)
-                        .hide()
-                        .append($content);
-
-                    $.extend($dialog, {
-                        open: function() {
-                            this.dialog({
-                                resizable: false,
-                                width: 550,
-                                height: 220,
-                                modal: true,
-                                buttons: noCancelButton ? {} : {
-                                    Cancel: function() {
-                                        $(this).dialog('close');
-                                    }
+                        $.ajax({
+                            type: "GET",
+                            url: url,
+                            cache: false
+                        })
+                            .done(function(data) {
+                                if (dataHandler !== undefined) {
+                                    dataHandler(data);
+                                }
+                            })
+                            .fail(function(res, textStatus, errorThrown) {
+                                alert("connection failed.");
+                            })
+                            .always(function(data) {
+                                if (finishHandler !== undefined) {
+                                    finishHandler();
                                 }
                             });
-                        },
-                        close: function() {
-                            this.dialog('close');
-                        },
-                    });
+                    },
 
-                    return $dialog;
+                    post: function(url, data, successHandler, failHandler, finishHandler) {
+                        if (isEmpty(url)) {
+                            return;
+                        }
+
+                        console.log("POST data", data);
+
+                        $.ajax({
+                            type: "post",
+                            url: url,
+                            contentType: "application/json",
+                            data: data,
+                            crossDomain: true,
+                            xhrFields: {
+                                withCredentials: true
+                            }
+                        })
+                            .done(successHandler)
+                            .fail(failHandler)
+                            .always(finishHandler);
+                    }
+                };
+            }(),
+
+            // Usage sample: textAeUtil.getUrlParameters(location.search). 
+            getUrlParameters: function(urlQuery) {
+                // Remove ? at top.
+                var queryString = urlQuery ? String(urlQuery).replace(/^\?(.*)/, '$1') : '';
+
+                // Convert to array if exists
+                var querys = queryString.length > 0 ? queryString.split('&') : [];
+
+                return querys
+                    .map(function(param) {
+                        // Convert string "key=value" to object.
+                        var vals = param.split('=');
+                        return {
+                            key: vals[0],
+                            val: vals[1]
+                        };
+                    }).reduce(function(a, b) {
+                        // Convert [{key: 'abc', val: '123'},...] to { abc: 123 ,...}
+                        // Set value true if val is not set.
+                        a[b.key] = b.val ? b.val : true;
+                        return a;
+                    }, {});
+            },
+
+            makeInformationModal: function() {
+                //this is bound object.
+                var showModal = function(className, obj) {
+                    var getModal = function() {
+                        var $modal = $('.' + this.className);
+                        // add modal unless exists
+                        if ($modal.length === 0) {
+                            $modal = $('<div>')
+                                .addClass('textae__information-modal')
+                                .addClass(this.className)
+                                .hide();
+                            this.addContentsFunc.call($modal);
+                            $('body').append($modal);
+                        }
+                        return $modal;
+                    };
+
+                    var setPositionCenter = function($modal) {
+                        var $window = $(window);
+                        $modal.css({
+                            'position': 'absolute',
+                            'top': ($window.height() - $modal.height()) / 2 + $window.scrollTop(),
+                            'left': ($window.width() - $modal.width()) / 2 + $window.scrollLeft()
+                        });
+                    };
+
+                    //close other dialogs
+                    $('.textae__information-modal').hide();
+
+                    //show at center
+                    var $modal = getModal.call(this);
+                    setPositionCenter($modal);
+                    $modal.show();
                 };
 
-                var dialogId = editorId + '.' + id;
+                //this is bound object.
+                var hideModal = function(className) {
+                    $('.' + this.className).hide();
+                };
 
-                if (cash.hasOwnProperty(dialogId)) {
-                    return cash[dialogId];
-                } else {
-                    // Make unless exists
-                    var $dialog = makeDialog(dialogId);
+                //expected param has className and addContentsFunc.
+                var bindObject = function(param) {
+                    return {
+                        show: showModal.bind(param),
+                        hide: hideModal.bind(param)
+                    };
+                };
 
-                    $.extend($content, {
-                        dialogClose: function() {
-                            $dialog.close();
-                        }
+                //close modal when modal clicked.
+                $(function() {
+                    $('body').on('mouseup', '.textae__information-modal', function() {
+                        $(this).hide();
                     });
+                });
 
-                    $('body').append($dialog);
-                    cash[dialogId] = $dialog;
-                    return $dialog;
-                }
-            };
-        }(),
+                return bindObject;
+            }(),
 
-        // A util function to fallent array with reduce. 
-        flatten: function(a, b) {
-            return a.concat(b);
-        }
-    };
+            getDialog: function() {
+                // Cash a div for dialog by self, because $('#dialog_id') cannot find exists div element.
+                var cash = {};
+
+                return function(editorId, id, title, $content, noCancelButton) {
+                    var makeDialog = function(id) {
+                        var $dialog = $('<div>')
+                            .attr('id', id)
+                            .attr('title', title)
+                            .hide()
+                            .append($content);
+
+                        $.extend($dialog, {
+                            open: function() {
+                                this.dialog({
+                                    resizable: false,
+                                    width: 550,
+                                    height: 220,
+                                    modal: true,
+                                    buttons: noCancelButton ? {} : {
+                                        Cancel: function() {
+                                            $(this).dialog('close');
+                                        }
+                                    }
+                                });
+                            },
+                            close: function() {
+                                this.dialog('close');
+                            },
+                        });
+
+                        return $dialog;
+                    };
+
+                    var dialogId = editorId + '.' + id;
+
+                    if (cash.hasOwnProperty(dialogId)) {
+                        return cash[dialogId];
+                    } else {
+                        // Make unless exists
+                        var $dialog = makeDialog(dialogId);
+
+                        $.extend($content, {
+                            dialogClose: function() {
+                                $dialog.close();
+                            }
+                        });
+
+                        $('body').append($dialog);
+                        cash[dialogId] = $dialog;
+                        return $dialog;
+                    }
+                };
+            }(),
+
+            // A util function to fallent array with reduce. 
+            flatten: function(a, b) {
+                return a.concat(b);
+            },
+
+            // Append event emittion to the object.
+            extendBindable: function(obj) {
+                return _.extend({}, obj, bindable());
+            }
+        };
+    }();
