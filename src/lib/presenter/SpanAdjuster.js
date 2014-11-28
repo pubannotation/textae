fd0ff48c9007c1c9236a4c9382b9406a127ab309
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2014-11-28 20:44:35 +0900

    Refactor the SpanAdjuster module.

--- src/lib/presenter/SpanAdjuster.js
+++ src/lib/presenter/SpanAdjuster.js
@@ -1,48 +1,54 @@
 // adjust the beginning position of a span
-var adjustSpanBeginLong = function(spanConfig, sourceDoc, beginPosition) {
-        var pos = beginPosition;
-
-        while (
-            spanConfig.isNonEdgeCharacter(sourceDoc.charAt(pos))
-        ) {
-            pos++;
+var getChar = function(sourceDoc, offset, pos) {
+        return sourceDoc.charAt(pos + offset);
+    },
+    skipCharacters = function(predicate, startPositios, step) {
+        while (predicate(startPositios)) {
+            startPositios += step;
         }
+        return startPositios;
+    },
+    bind = function(obj, functionName) {
+        return obj[functionName].bind(obj);
+    },
+    IsCharNonEdgeChar = function(spanConfig, sourceDoc, offset) {
+        var getOffsetChar = _.partial(getChar, sourceDoc, offset);
 
-        while (
-            pos > 0 &&
-            !spanConfig.isDelimiter(sourceDoc.charAt(pos)) &&
-            !spanConfig.isDelimiter(sourceDoc.charAt(pos - 1))
-        ) {
-            pos--;
-        }
-        return pos;
+        return function(position) {
+            return spanConfig.isNonEdgeCharacter(getOffsetChar(position));
+        };
+    },
+    adjustSpanBeginLong = function(spanConfig, sourceDoc, beginPosition) {
+        var isPosCharNonEdgeChar = new IsCharNonEdgeChar(spanConfig, sourceDoc, 0),
+            isPosPreNonDelimChar = function(pos) {
+                return !spanConfig.isDelimiter(getChar(sourceDoc, 0, pos)) &&
+                    !spanConfig.isDelimiter(getChar(sourceDoc, -1, pos));
+            },
+            nonEdgePos = skipCharacters(isPosCharNonEdgeChar, beginPosition, 1),
+            nonDelimPos = skipCharacters(isPosPreNonDelimChar, nonEdgePos, -1);
+
+        return nonDelimPos;
     },
     // adjust the end position of a span
     adjustSpanEndLong = function(spanConfig, sourceDoc, endPosition) {
-        var pos = endPosition;
+        var isNonEdgeCharacter = bind(spanConfig, 'isNonEdgeCharacter'),
+            isPreCharNonEdgeChar = new IsCharNonEdgeChar(spanConfig, sourceDoc, -1),
+            isPosCharNonDelimiChar = function(pos) {
+                return !spanConfig.isDelimiter(sourceDoc.charAt(pos));
+            },
+            nonEdgePos = skipCharacters(isPreCharNonEdgeChar, endPosition, -1),
+            nonDelimPos = skipCharacters(isPosCharNonDelimiChar, nonEdgePos, 1);
 
-        while (
-            spanConfig.isNonEdgeCharacter(sourceDoc.charAt(pos - 1))
-        ) {
-            pos--;
-        }
-
-        while (!spanConfig.isDelimiter(sourceDoc.charAt(pos)) &&
-            pos < sourceDoc.length
-        ) {
-            pos++;
-        }
-        return pos;
+        return nonDelimPos;
     },
     // adjust the beginning position of a span for shortening
     adjustSpanBeginShort = function(spanConfig, sourceDoc, beginPosition) {
         var pos = beginPosition;
-        while (
-            pos < sourceDoc.length &&
-            (
-                spanConfig.isNonEdgeCharacter(sourceDoc.charAt(pos)) ||
-                !spanConfig.isDelimiter(sourceDoc.charAt(pos - 1))
-            )
+
+        // Proceed the position between two characters as (nonEdge || delimiter)(!delimiter). 
+        while (!spanConfig.isNonEdgeCharacter(sourceDoc.charAt(pos - 1)) &&
+            !spanConfig.isDelimiter(sourceDoc.charAt(pos - 1)) ||
+            spanConfig.isDelimiter(sourceDoc.charAt(pos))
         ) {
             pos++;
         }
@@ -51,12 +57,11 @@ var adjustSpanBeginLong = function(spanConfig, sourceDoc, beginPosition) {
     // adjust the end position of a span for shortening
     adjustSpanEndShort = function(spanConfig, sourceDoc, endPosition) {
         var pos = endPosition;
-        while (
-            pos > 0 &&
-            (
-                spanConfig.isNonEdgeCharacter(sourceDoc.charAt(pos - 1)) ||
-                !spanConfig.isDelimiter(sourceDoc.charAt(pos))
-            )
+
+        // Proceed the position between two characters as (nonEdge || delimiter)(!delimiter). 
+        while (!spanConfig.isNonEdgeCharacter(sourceDoc.charAt(pos)) &&
+            !spanConfig.isDelimiter(sourceDoc.charAt(pos)) ||
+            spanConfig.isDelimiter(sourceDoc.charAt(pos - 1))
         ) {
             pos--;
         }
