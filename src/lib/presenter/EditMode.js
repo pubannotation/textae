ec42e9980da1711dd79734ca5bcc147093f2f7f0
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-01-23 19:47:42 +0900

    Fix indent.

--- src/lib/presenter/EditMode.js
+++ src/lib/presenter/EditMode.js
@@ -1,135 +1,135 @@
 var typeGapCache = function() {
-	var seed = {
-			instanceHide: 0,
-			instanceShow: 2
-		},
-		set = function(mode, val) {
-			typeGapCache[mode] = val;
-			return val;
-		},
-		api = _.extend({}, seed),
-		capitalize = require('../util/capitalize');
+    var seed = {
+            instanceHide: 0,
+            instanceShow: 2
+        },
+        set = function(mode, val) {
+            typeGapCache[mode] = val;
+            return val;
+        },
+        api = _.extend({}, seed),
+        capitalize = require('../util/capitalize');
 
-	_.each(seed, function(val, key) {
-		api['set' + capitalize(key)] = _.partial(set, key);
-	});
+    _.each(seed, function(val, key) {
+        api['set' + capitalize(key)] = _.partial(set, key);
+    });
 
-	return api;
+    return api;
 }();
 
 module.exports = function(model, viewMode, typeEditor, typeGap) {
-	var api = {
-			init: function() {
-				_.extend(api, state.init);
-			},
-			get lineHeight() {
-				return Math.floor(viewMode.getLineHeight());
-			},
-			changeLineHeight: viewMode.changeLineHeight
-		},
-		resetView = function() {
-			typeEditor.hideDialogs();
-			model.selectionModel.clear();
-		},
-		transition = {
-			toTerm: function() {
-				resetView();
+    var api = {
+            init: function() {
+                _.extend(api, state.init);
+            },
+            get lineHeight() {
+                return Math.floor(viewMode.getLineHeight());
+            },
+            changeLineHeight: viewMode.changeLineHeight
+        },
+        resetView = function() {
+            typeEditor.hideDialogs();
+            model.selectionModel.clear();
+        },
+        transition = {
+            toTerm: function() {
+                resetView();
 
-				typeEditor.editEntity();
-				viewMode.setTerm();
-				viewMode.setEditable(true);
-				typeGap.set(typeGapCache.instanceHide);
+                typeEditor.editEntity();
+                viewMode.setTerm();
+                viewMode.setEditable(true);
+                typeGap.set(typeGapCache.instanceHide);
 
-				_.extend(api, state.termCentric);
-			},
-			toInstance: function() {
-				resetView();
+                _.extend(api, state.termCentric);
+            },
+            toInstance: function() {
+                resetView();
 
-				typeEditor.editEntity();
-				viewMode.setInstance();
-				viewMode.setEditable(true);
-				typeGap.set(typeGapCache.instanceShow);
+                typeEditor.editEntity();
+                viewMode.setInstance();
+                viewMode.setEditable(true);
+                typeGap.set(typeGapCache.instanceShow);
 
-				_.extend(api, state.instanceRelation);
-			},
-			toRelation: function() {
-				resetView();
+                _.extend(api, state.instanceRelation);
+            },
+            toRelation: function() {
+                resetView();
 
-				typeEditor.editRelation();
-				viewMode.setRelation();
-				viewMode.setEditable(true);
-				typeGap.set(typeGapCache.instanceShow);
+                typeEditor.editRelation();
+                viewMode.setRelation();
+                viewMode.setEditable(true);
+                typeGap.set(typeGapCache.instanceShow);
 
-				_.extend(api, state.relationEdit);
-			},
-			toViewTerm: function() {
-				resetView();
+                _.extend(api, state.relationEdit);
+            },
+            toViewTerm: function() {
+                resetView();
 
-				typeEditor.noEdit();
-				viewMode.setTerm();
-				viewMode.setEditable(false);
-				typeGap.set(typeGapCache.instanceHide);
+                typeEditor.noEdit();
+                viewMode.setTerm();
+                viewMode.setEditable(false);
+                typeGap.set(typeGapCache.instanceHide);
 
-				_.extend(api, state.viewTerm);
-			},
-			toViewInstance: function() {
-				resetView();
+                _.extend(api, state.viewTerm);
+            },
+            toViewInstance: function() {
+                resetView();
 
-				typeEditor.noEdit();
-				viewMode.setInstance();
-				viewMode.setEditable(false);
-				typeGap.set(typeGapCache.instanceShow);
+                typeEditor.noEdit();
+                viewMode.setInstance();
+                viewMode.setEditable(false);
+                typeGap.set(typeGapCache.instanceShow);
 
-				_.extend(api, state.viewInstance);
-			}
-		},
-		// Calculate the line-height when the view-mode set
-		notTransit = function() {
-			console.log('no mode transition.', this.name);
-		},
-		changeTypeGapInstanceHide = _.compose(typeGap.set, typeGapCache.setInstanceHide),
-		changeTypeGapInstanceShow = _.compose(typeGap.set, typeGapCache.setInstanceShow),
-		state = {
-			init: _.extend({}, transition, {
-				name: 'Init'
-			}),
-			termCentric: _.extend({}, transition, {
-				name: 'Term Centric',
-				toTerm: notTransit,
-				changeTypeGap: changeTypeGapInstanceHide,
-				showInstance: false
-			}),
-			instanceRelation: _.extend({}, transition, {
-				name: 'Instance / Relation',
-				toInstance: notTransit,
-				changeTypeGap: changeTypeGapInstanceShow,
-				showInstance: true
-			}),
-			relationEdit: _.extend({}, transition, {
-				name: 'Relation Edit',
-				toRelation: notTransit,
-				changeTypeGap: changeTypeGapInstanceShow,
-				showInstance: true
-			}),
-			viewTerm: _.extend({}, transition, {
-				name: 'View Only',
-				toTerm: notTransit,
-				toInstance: transition.toViewInstance,
-				toRelation: notTransit,
-				toViewTerm: notTransit,
-				changeTypeGap: changeTypeGapInstanceHide,
-				showInstance: false
-			}),
-			viewInstance: _.extend({}, transition, {
-				name: 'View Only',
-				toTerm: transition.toViewTerm,
-				toInstance: notTransit,
-				toRelation: notTransit,
-				toViewInstance: notTransit,
-				changeTypeGap: changeTypeGapInstanceShow,
-				showInstance: true
-			})
-		};
+                _.extend(api, state.viewInstance);
+            }
+        },
+        // Calculate the line-height when the view-mode set
+        notTransit = function() {
+            console.log('no mode transition.', this.name);
+        },
+        changeTypeGapInstanceHide = _.compose(typeGap.set, typeGapCache.setInstanceHide),
+        changeTypeGapInstanceShow = _.compose(typeGap.set, typeGapCache.setInstanceShow),
+        state = {
+            init: _.extend({}, transition, {
+                name: 'Init'
+            }),
+            termCentric: _.extend({}, transition, {
+                name: 'Term Centric',
+                toTerm: notTransit,
+                changeTypeGap: changeTypeGapInstanceHide,
+                showInstance: false
+            }),
+            instanceRelation: _.extend({}, transition, {
+                name: 'Instance / Relation',
+                toInstance: notTransit,
+                changeTypeGap: changeTypeGapInstanceShow,
+                showInstance: true
+            }),
+            relationEdit: _.extend({}, transition, {
+                name: 'Relation Edit',
+                toRelation: notTransit,
+                changeTypeGap: changeTypeGapInstanceShow,
+                showInstance: true
+            }),
+            viewTerm: _.extend({}, transition, {
+                name: 'View Only',
+                toTerm: notTransit,
+                toInstance: transition.toViewInstance,
+                toRelation: notTransit,
+                toViewTerm: notTransit,
+                changeTypeGap: changeTypeGapInstanceHide,
+                showInstance: false
+            }),
+            viewInstance: _.extend({}, transition, {
+                name: 'View Only',
+                toTerm: transition.toViewTerm,
+                toInstance: notTransit,
+                toRelation: notTransit,
+                toViewInstance: notTransit,
+                changeTypeGap: changeTypeGapInstanceShow,
+                showInstance: true
+            })
+        };
 
-	return api;
+    return api;
 };
--- src/lib/presenter/SettingDialog.js
+++ src/lib/presenter/SettingDialog.js
@@ -1,160 +1,160 @@
 var debounce300 = function(func) {
-		return _.debounce(func, 300);
-	},
-	sixteenTimes = function(val) {
-		return val * 16;
-	},
-	// Redraw all editors in tha windows.
-	redrawAllEditor = function() {
-		$(window).trigger('resize');
-	},
-	createContent = function() {
-		return jQuerySugar.Div('textae-editor__setting-dialog');
-	},
-	// Open the dialog.
-	open = function($dialog) {
-		return $dialog.open();
-	},
-	jQuerySugar = require('../util/jQuerySugar'),
-	// Update the checkbox state, because it is updated by the button on control too.
-	updateViewMode = function(editMode, $content) {
-		return jQuerySugar.setChecked(
-			$content,
-			'.mode',
-			editMode.showInstance ? 'checked' : null
-		);
-	},
-	updateLineHeight = function(editMode, $content) {
-		return jQuerySugar.setValue(
-			$content,
-			'.line-height',
-			editMode.lineHeight
-		);
-	},
-	updateTypeGapValue = function(typeGap, $content) {
-		return jQuerySugar.setValue(
-			$content,
-			'.type-gap',
-			typeGap.get
-		);
-	},
-	toTypeGap = function($content) {
-		return $content.find('.type-gap');
-	},
-	updateTypeGapEnable = function(editMode, $content) {
-		jQuerySugar.enabled(toTypeGap($content), editMode.showInstance);
-		return $content;
-	},
-	changeMode = function(editMode, typeGap, $content, checked) {
-		if (checked) {
-			editMode.toInstance();
-		} else {
-			editMode.toTerm();
-		}
-		updateTypeGapEnable(editMode, $content);
-		updateTypeGapValue(typeGap, $content);
-		updateLineHeight(editMode, $content);
-	},
-	SettingDialogLabel = _.partial(jQuerySugar.Label, 'textae-editor__setting-dialog__label');
+        return _.debounce(func, 300);
+    },
+    sixteenTimes = function(val) {
+        return val * 16;
+    },
+    // Redraw all editors in tha windows.
+    redrawAllEditor = function() {
+        $(window).trigger('resize');
+    },
+    createContent = function() {
+        return jQuerySugar.Div('textae-editor__setting-dialog');
+    },
+    // Open the dialog.
+    open = function($dialog) {
+        return $dialog.open();
+    },
+    jQuerySugar = require('../util/jQuerySugar'),
+    // Update the checkbox state, because it is updated by the button on control too.
+    updateViewMode = function(editMode, $content) {
+        return jQuerySugar.setChecked(
+            $content,
+            '.mode',
+            editMode.showInstance ? 'checked' : null
+        );
+    },
+    updateLineHeight = function(editMode, $content) {
+        return jQuerySugar.setValue(
+            $content,
+            '.line-height',
+            editMode.lineHeight
+        );
+    },
+    updateTypeGapValue = function(typeGap, $content) {
+        return jQuerySugar.setValue(
+            $content,
+            '.type-gap',
+            typeGap.get
+        );
+    },
+    toTypeGap = function($content) {
+        return $content.find('.type-gap');
+    },
+    updateTypeGapEnable = function(editMode, $content) {
+        jQuerySugar.enabled(toTypeGap($content), editMode.showInstance);
+        return $content;
+    },
+    changeMode = function(editMode, typeGap, $content, checked) {
+        if (checked) {
+            editMode.toInstance();
+        } else {
+            editMode.toTerm();
+        }
+        updateTypeGapEnable(editMode, $content);
+        updateTypeGapValue(typeGap, $content);
+        updateLineHeight(editMode, $content);
+    },
+    SettingDialogLabel = _.partial(jQuerySugar.Label, 'textae-editor__setting-dialog__label');
 
 module.exports = function(editor, editMode, typeGap) {
-	var addInstanceRelationView = function($content) {
-			var onModeChanged = debounce300(function() {
-				changeMode(editMode, typeGap, $content, $(this).is(':checked'));
-			});
+    var addInstanceRelationView = function($content) {
+            var onModeChanged = debounce300(function() {
+                changeMode(editMode, typeGap, $content, $(this).is(':checked'));
+            });
 
-			return $content
-				.append(jQuerySugar.Div()
-					.append(
-						new SettingDialogLabel('Instance/Relation View')
-					)
-					.append(
-						jQuerySugar.Checkbox('textae-editor__setting-dialog__term-centric-view mode')
-					)
-				)
-				.on(
-					'click',
-					'.mode',
-					onModeChanged
-				);
-		},
-		addTypeGap = function($content) {
-			var onTypeGapChange = debounce300(
-				function() {
-					editMode.changeTypeGap($(this).val());
-					updateLineHeight(editMode, $content);
-				}
-			);
+            return $content
+                .append(jQuerySugar.Div()
+                    .append(
+                        new SettingDialogLabel('Instance/Relation View')
+                    )
+                    .append(
+                        jQuerySugar.Checkbox('textae-editor__setting-dialog__term-centric-view mode')
+                    )
+                )
+                .on(
+                    'click',
+                    '.mode',
+                    onModeChanged
+                );
+        },
+        addTypeGap = function($content) {
+            var onTypeGapChange = debounce300(
+                function() {
+                    editMode.changeTypeGap($(this).val());
+                    updateLineHeight(editMode, $content);
+                }
+            );
 
-			return $content
-				.append(jQuerySugar.Div()
-					.append(
-						new SettingDialogLabel('Type Gap')
-					)
-					.append(
-						jQuerySugar.Number('textae-editor__setting-dialog__type-gap type-gap')
-						.attr({
-							step: 1,
-							min: 0,
-							max: 5
-						})
-					)
-				).on(
-					'change',
-					'.type-gap',
-					onTypeGapChange
-				);
-		},
-		addLineHeight = function($content) {
-			var changeLineHeight = _.compose(redrawAllEditor, editMode.changeLineHeight, sixteenTimes),
-				onLineHeightChange = debounce300(
-					function() {
-						changeLineHeight($(this).val());
-					}
-				);
+            return $content
+                .append(jQuerySugar.Div()
+                    .append(
+                        new SettingDialogLabel('Type Gap')
+                    )
+                    .append(
+                        jQuerySugar.Number('textae-editor__setting-dialog__type-gap type-gap')
+                        .attr({
+                            step: 1,
+                            min: 0,
+                            max: 5
+                        })
+                    )
+                ).on(
+                    'change',
+                    '.type-gap',
+                    onTypeGapChange
+                );
+        },
+        addLineHeight = function($content) {
+            var changeLineHeight = _.compose(redrawAllEditor, editMode.changeLineHeight, sixteenTimes),
+                onLineHeightChange = debounce300(
+                    function() {
+                        changeLineHeight($(this).val());
+                    }
+                );
 
-			return $content
-				.append(jQuerySugar.Div()
-					.append(
-						new SettingDialogLabel('Line Height')
-					)
-					.append(
-						jQuerySugar.Number('textae-editor__setting-dialog__line-height line-height')
-						.attr({
-							'step': 1,
-							'min': 3,
-							'max': 50
-						})
-					))
-				.on(
-					'change',
-					'.line-height',
-					onLineHeightChange
-				);
-		},
-		appendToDialog = function($content) {
-			return require('../util/dialog/GetEditorDialog')(editor)(
-				'textae.dialog.setting',
-				'Setting',
-				$content,
-				true
-			);
-		},
-		partialEditMode = function(func) {
-			return _.partial(func, editMode);
-		};
+            return $content
+                .append(jQuerySugar.Div()
+                    .append(
+                        new SettingDialogLabel('Line Height')
+                    )
+                    .append(
+                        jQuerySugar.Number('textae-editor__setting-dialog__line-height line-height')
+                        .attr({
+                            'step': 1,
+                            'min': 3,
+                            'max': 50
+                        })
+                    ))
+                .on(
+                    'change',
+                    '.line-height',
+                    onLineHeightChange
+                );
+        },
+        appendToDialog = function($content) {
+            return require('../util/dialog/GetEditorDialog')(editor)(
+                'textae.dialog.setting',
+                'Setting',
+                $content,
+                true
+            );
+        },
+        partialEditMode = function(func) {
+            return _.partial(func, editMode);
+        };
 
-	// Update values after creating a dialog because the dialog is re-used.
-	return _.compose(
-		open,
-		partialEditMode(updateLineHeight),
-		_.partial(updateTypeGapValue, typeGap),
-		partialEditMode(updateTypeGapEnable),
-		partialEditMode(updateViewMode),
-		appendToDialog,
-		addLineHeight,
-		addTypeGap,
-		addInstanceRelationView,
-		createContent
-	);
+    // Update values after creating a dialog because the dialog is re-used.
+    return _.compose(
+        open,
+        partialEditMode(updateLineHeight),
+        _.partial(updateTypeGapValue, typeGap),
+        partialEditMode(updateTypeGapEnable),
+        partialEditMode(updateViewMode),
+        appendToDialog,
+        addLineHeight,
+        addTypeGap,
+        addInstanceRelationView,
+        createContent
+    );
 };
