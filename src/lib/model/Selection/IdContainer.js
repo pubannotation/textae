490efe8f0257922547193dcb200856d18b5f51b2
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-01-29 16:58:06 +0900

    Fix indent.

--- src/lib/model/Selection/IdContainer.js
+++ src/lib/model/Selection/IdContainer.js
@@ -1,50 +1,50 @@
 module.exports = function(kindName) {
-	var extendBindable = require('../../util/extendBindable'),
-		selected = {},
-		triggerChange = function() {
-			api.trigger(kindName + '.change');
-		};
+    var extendBindable = require('../../util/extendBindable'),
+        selected = {},
+        triggerChange = function() {
+            api.trigger(kindName + '.change');
+        };
 
-	var api = extendBindable({
-		name: kindName,
-		add: function(id) {
-			selected[id] = id;
-			api.trigger(kindName + '.add', id);
-			triggerChange();
-		},
-		all: function() {
-			return _.toArray(selected);
-		},
-		has: function(id) {
-			return _.contains(selected, id);
-		},
-		some: function() {
-			return _.some(selected);
-		},
-		single: function() {
-			var array = api.all();
-			return array.length === 1 ? array[0] : null;
-		},
-		toggle: function(id) {
-			if (api.has(id)) {
-				api.remove(id);
-			} else {
-				api.add(id);
-			}
-		},
-		remove: function(id) {
-			delete selected[id];
-			api.trigger(kindName + '.remove', id);
-			triggerChange();
-		},
-		clear: function() {
-			if (!api.some()) return;
+    var api = extendBindable({
+        name: kindName,
+        add: function(id) {
+            selected[id] = id;
+            api.trigger(kindName + '.add', id);
+            triggerChange();
+        },
+        all: function() {
+            return _.toArray(selected);
+        },
+        has: function(id) {
+            return _.contains(selected, id);
+        },
+        some: function() {
+            return _.some(selected);
+        },
+        single: function() {
+            var array = api.all();
+            return array.length === 1 ? array[0] : null;
+        },
+        toggle: function(id) {
+            if (api.has(id)) {
+                api.remove(id);
+            } else {
+                api.add(id);
+            }
+        },
+        remove: function(id) {
+            delete selected[id];
+            api.trigger(kindName + '.remove', id);
+            triggerChange();
+        },
+        clear: function() {
+            if (!api.some()) return;
 
-			_.each(api.all(), api.remove);
-			selected = {};
-			triggerChange();
-		}
-	});
+            _.each(api.all(), api.remove);
+            selected = {};
+            triggerChange();
+        }
+    });
 
-	return api;
+    return api;
 };
--- src/lib/model/Selection/index.js
+++ src/lib/model/Selection/index.js
@@ -1,50 +1,50 @@
 var clearAll = function(containerList) {
-		_.each(containerList, function(container) {
-			container.clear();
-		});
-	},
-	someAll = function(containerList) {
-		return containerList
-			.map(function(container) {
-				return container.some();
-			})
-			.reduce(function(a, b) {
-				return a || b;
-			});
+        _.each(containerList, function(container) {
+            container.clear();
+        });
+    },
+    someAll = function(containerList) {
+        return containerList
+            .map(function(container) {
+                return container.some();
+            })
+            .reduce(function(a, b) {
+                return a || b;
+            });
 
-	},
-	relayEventsOfEachContainer = function(api) {
-		_.each(api, function(container) {
-			container
-				.bind(container.name + '.change', function() {
-					api.trigger(container.name + '.change');
-				})
-				.bind(container.name + '.add', function(id) {
-					api.trigger(container.name + '.select', id);
-				})
-				.bind(container.name + '.remove', function(id) {
-					api.trigger(container.name + '.deselect', id);
-				});
-		});
-	},
-	extendUtilFunctions = function(containerList, api) {
-		return _.extend(api, {
-			clear: _.partial(clearAll, containerList),
-			some: _.partial(someAll, containerList)
-		});
-	};
+    },
+    relayEventsOfEachContainer = function(api) {
+        _.each(api, function(container) {
+            container
+                .bind(container.name + '.change', function() {
+                    api.trigger(container.name + '.change');
+                })
+                .bind(container.name + '.add', function(id) {
+                    api.trigger(container.name + '.select', id);
+                })
+                .bind(container.name + '.remove', function(id) {
+                    api.trigger(container.name + '.deselect', id);
+                });
+        });
+    },
+    extendUtilFunctions = function(containerList, api) {
+        return _.extend(api, {
+            clear: _.partial(clearAll, containerList),
+            some: _.partial(someAll, containerList)
+        });
+    };
 
 module.exports = function(kinds) {
-	var containerList = kinds.map(require('./IdContainer')),
-		api = require('../../util/extendBindable')(
-			containerList
-			.reduce(function(a, b) {
-				a[b.name] = b;
-				return a;
-			}, {})
-		);
+    var containerList = kinds.map(require('./IdContainer')),
+        api = require('../../util/extendBindable')(
+            containerList
+            .reduce(function(a, b) {
+                a[b.name] = b;
+                return a;
+            }, {})
+        );
 
-	relayEventsOfEachContainer(api);
+    relayEventsOfEachContainer(api);
 
-	return extendUtilFunctions(containerList, api);
+    return extendUtilFunctions(containerList, api);
 };
