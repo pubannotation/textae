2df4ac164bd0a8853881c4d3c6963672c802a939
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-02-20 13:12:36 +0900

    Rewrite in ES6.

--- src/lib/model/AnnotationData/parseAnnotation/importAnnotation/index.js
+++ src/lib/model/AnnotationData/parseAnnotation/importAnnotation/index.js
@@ -1,11 +1,12 @@
-var importSource = require('./importSource'),
-    translateDenotation = require('./translateDenotation'),
-    translateRelation = require('./translateRelation'),
-    translateModification = require('./translateModification'),
-    importAnnotations = function(span, entity, relation, modification, denotations, relations, modifications, prefix) {
-        importSource([span, entity], _.partial(translateDenotation, prefix), denotations);
-        importSource([relation], _.partial(translateRelation, prefix), relations);
-        importSource([modification], _.partial(translateModification, prefix), modifications);
-    };
+import importSource from './importSource';
+import translateDenotation from './translateDenotation';
+import translateRelation from './translateRelation';
+import translateModification from './translateModification';
 
-module.exports = importAnnotations;
+export default importAnnotations;
+
+function importAnnotations(span, entity, relation, modification, denotations, relations, modifications, prefix) {
+    importSource([span, entity], _.partial(translateDenotation, prefix), denotations);
+    importSource([relation], _.partial(translateRelation, prefix), relations);
+    importSource([modification], _.partial(translateModification, prefix), modifications);
+}
