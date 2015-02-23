14abe690c33e9b9736f9e6aeed47f5dcab49aadd
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-02-23 15:48:46 +0900

    Fix indent.

--- src/lib/model/isBoundaryCrossingWithOtherSpans.js
+++ src/lib/model/isBoundaryCrossingWithOtherSpans.js
@@ -1,16 +1,16 @@
 // A span its range is coross over with other spans are not able to rendered.
 // Because spans are renderd with span tag. Html tags can not be cross over.
 module.exports = function(spans, candidateSpan) {
-	return spans.filter(function(existSpan) {
-		return (
-				existSpan.begin < candidateSpan.begin &&
-				candidateSpan.begin < existSpan.end &&
-				existSpan.end < candidateSpan.end
-			) ||
-			(
-				candidateSpan.begin < existSpan.begin &&
-				existSpan.begin < candidateSpan.end &&
-				candidateSpan.end < existSpan.end
-			);
-	}).length > 0;
+    return spans.filter(function(existSpan) {
+        return (
+                existSpan.begin < candidateSpan.begin &&
+                candidateSpan.begin < existSpan.end &&
+                existSpan.end < candidateSpan.end
+            ) ||
+            (
+                candidateSpan.begin < existSpan.begin &&
+                existSpan.begin < candidateSpan.end &&
+                candidateSpan.end < existSpan.end
+            );
+    }).length > 0;
 };
