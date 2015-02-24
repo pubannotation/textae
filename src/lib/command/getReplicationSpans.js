dca8a7a85bb2a6f336ac41e299726023d93cf473
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-02-24 12:15:58 +0900

    Fix indent.

--- src/lib/command/getReplicationSpans.js
+++ src/lib/command/getReplicationSpans.js
@@ -1,62 +1,62 @@
 // Get spans their stirng is same with the originSpan from sourceDoc.
 var getSpansTheirStringIsSameWith = function(sourceDoc, originSpan) {
-		var getNextStringIndex = String.prototype.indexOf.bind(
-				sourceDoc,
-				sourceDoc.substring(originSpan.begin, originSpan.end)
-			),
-			length = originSpan.end - originSpan.begin,
-			findStrings = [],
-			offset = 0;
+        var getNextStringIndex = String.prototype.indexOf.bind(
+                sourceDoc,
+                sourceDoc.substring(originSpan.begin, originSpan.end)
+            ),
+            length = originSpan.end - originSpan.begin,
+            findStrings = [],
+            offset = 0;
 
-		for (var index = getNextStringIndex(offset); index !== -1; index = getNextStringIndex(offset)) {
-			findStrings.push({
-				begin: index,
-				end: index + length
-			});
+        for (var index = getNextStringIndex(offset); index !== -1; index = getNextStringIndex(offset)) {
+            findStrings.push({
+                begin: index,
+                end: index + length
+            });
 
-			offset = index + length;
-		}
+            offset = index + length;
+        }
 
-		return findStrings;
-	},
-	// The preceding charactor and the following of a word charactor are delimiter.
-	// For example, 't' ,a part of 'that', is not same with an origin span when it is 't'.
-	isWord = function(sourceDoc, detectBoundaryFunc, candidateSpan) {
-		var precedingChar = sourceDoc.charAt(candidateSpan.begin - 1);
-		var followingChar = sourceDoc.charAt(candidateSpan.end);
+        return findStrings;
+    },
+    // The preceding charactor and the following of a word charactor are delimiter.
+    // For example, 't' ,a part of 'that', is not same with an origin span when it is 't'.
+    isWord = function(sourceDoc, detectBoundaryFunc, candidateSpan) {
+        var precedingChar = sourceDoc.charAt(candidateSpan.begin - 1);
+        var followingChar = sourceDoc.charAt(candidateSpan.end);
 
-		return detectBoundaryFunc(precedingChar) && detectBoundaryFunc(followingChar);
-	},
-	not = function(val) {
-		return !val;
-	},
-	isAlreadySpaned = require('../model/isAlreadySpaned'),
-	isBoundaryCrossingWithOtherSpans = require('../model/isBoundaryCrossingWithOtherSpans');
+        return detectBoundaryFunc(precedingChar) && detectBoundaryFunc(followingChar);
+    },
+    not = function(val) {
+        return !val;
+    },
+    isAlreadySpaned = require('../model/isAlreadySpaned'),
+    isBoundaryCrossingWithOtherSpans = require('../model/isBoundaryCrossingWithOtherSpans');
 
 // Check replications are word or not if spanConfig is set.
 module.exports = function(dataStore, originSpan, detectBoundaryFunc) {
-	var allSpans = dataStore.span.all(),
-		wordFilter = detectBoundaryFunc ?
-		_.partial(isWord, dataStore.sourceDoc, detectBoundaryFunc) :
-		_.identity;
+    var allSpans = dataStore.span.all(),
+        wordFilter = detectBoundaryFunc ?
+        _.partial(isWord, dataStore.sourceDoc, detectBoundaryFunc) :
+        _.identity;
 
-	return getSpansTheirStringIsSameWith(dataStore.sourceDoc, originSpan)
-		.filter(function(span) {
-			// The candidateSpan is a same span when begin is same.
-			// Because string of each others are same. End of them are same too.
-			return span.begin !== originSpan.begin;
-		})
-		.filter(wordFilter)
-		.filter(
-			_.compose(
-				not,
-				_.partial(isAlreadySpaned, allSpans)
-			)
-		)
-		.filter(
-			_.compose(
-				not,
-				_.partial(isBoundaryCrossingWithOtherSpans, allSpans)
-			)
-		);
+    return getSpansTheirStringIsSameWith(dataStore.sourceDoc, originSpan)
+        .filter(function(span) {
+            // The candidateSpan is a same span when begin is same.
+            // Because string of each others are same. End of them are same too.
+            return span.begin !== originSpan.begin;
+        })
+        .filter(wordFilter)
+        .filter(
+            _.compose(
+                not,
+                _.partial(isAlreadySpaned, allSpans)
+            )
+        )
+        .filter(
+            _.compose(
+                not,
+                _.partial(isBoundaryCrossingWithOtherSpans, allSpans)
+            )
+        );
 };
