cb23a2a81dfc1751cd33e0691db6f3f3a9f92c35
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-03-06 18:48:52 +0900

    Set the line-height button enable at edit-mode.

--- src/lib/presenter/EditMode/ViewMode.js
+++ src/lib/presenter/EditMode/ViewMode.js
@@ -59,6 +59,7 @@ module.exports = function(editor, model, buttonStateHelper, modeAccordingToButto
             if (isEditable) {
                 editor.addClass('textae-editor_editable');
                 buttonStateHelper.enabled('relation-edit-mode', true);
+                buttonStateHelper.enabled('line-height', true);
             } else {
                 editor.removeClass('textae-editor_editable');
                 buttonStateHelper.enabled('replicate-auto', false);
