4366170ed9b115e65e288f935b42a2a68f2a488d
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-03-17 15:30:17 +0900

    Bug fix: Failure to position grids in the Firefox.

--- src/lib/editor/View/DomPositionCache/SpanAndEntityPosition.js
+++ src/lib/editor/View/DomPositionCache/SpanAndEntityPosition.js
@@ -3,7 +3,7 @@ import CachedGetterFactory from './CachedGetterFactory';
 
 let factory = new CachedGetterFactory();
 
-export default function (editor, entityModel, gridPositionCache) {
+export default function(editor, entityModel, gridPositionCache) {
     // The cache for span positions.
     // Getting the postion of spans is too slow about 5-10 ms per a element in Chrome browser. For example offsetTop property.
     // This cache is big effective for the initiation, and little effective for resize.
@@ -23,9 +23,14 @@ function getSpan(editor, spanId) {
         throw new Error("span is not renderd : " + spanId);
     }
 
+    // An element.offsetTop and element.offsetLeft does not work in the Firefox,
+    // when much spans are loaded like http://pubannotation.org/docs/sourcedb/PMC/sourceid/1315279/divs/10/annotations.json.
+    let spanBox = span.getBoundingClientRect(),
+        textBox = span.offsetParent.getBoundingClientRect();
+
     return {
-        top: span.offsetTop,
-        left: span.offsetLeft,
+        top: spanBox.top - textBox.top,
+        left: spanBox.left - textBox.left,
         width: span.offsetWidth,
         height: span.offsetHeight,
         center: span.offsetLeft + span.offsetWidth / 2
