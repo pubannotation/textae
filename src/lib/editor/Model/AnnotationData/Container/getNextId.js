084c616b3fae0da02a466c085538fef2513403e2
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-03-24 15:51:35 +0900

    'T1x' is not generated id.

--- src/lib/editor/Model/AnnotationData/Container/getNextId.js
+++ src/lib/editor/Model/AnnotationData/Container/getNextId.js
@@ -1,18 +1,21 @@
-var hasPrefix = function(prefix, id) {
-        return id[0] === prefix;
-    },
-    withoutPrefix = function(id) {
-        return id.slice(1);
-    },
-    getNextId = function(prefix, existsIds) {
-        var numbers = existsIds
-            .filter(_.partial(hasPrefix, prefix))
-            .map(withoutPrefix),
-            // The Math.max retrun -Infinity when the second argument array is empty.
-            max = numbers.length === 0 ? 0 : Math.max.apply(null, numbers),
-            nextNumber = max + 1;
+export default function(prefix, existsIds) {
+    let numbers = existsIds
+        .filter(id => hasPrefix(prefix, id))
+        .map(withoutPrefix),
+        // The Math.max retrun -Infinity when the second argument array is empty.
+        max = numbers.length === 0 ? 0 : Math.max.apply(null, numbers),
+        nextNumber = max + 1;
 
-        return prefix + nextNumber;
-    };
+    return prefix + nextNumber;
+}
 
-module.exports = getNextId;
+function hasPrefix(prefix, id) {
+    // Exclude an id have other than prefix and number, for example 'T1a'.
+    let reg = new RegExp(`${prefix}\\d+$`);
+
+    return reg.test(id);
+}
+
+function withoutPrefix(id) {
+    return id.slice(1);
+}
