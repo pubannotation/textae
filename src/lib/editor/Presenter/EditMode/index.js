a5660ede141d7af710c351fa468c605783616cf9
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-05-26 18:09:45 +0900

    Refactor EditMode.

--- src/lib/editor/Presenter/EditMode/index.js
+++ src/lib/editor/Presenter/EditMode/index.js
@@ -3,10 +3,10 @@ import {
 }
 from 'events';
 import Transition from './Transition';
+import toStateMachine from './toStateMachine';
+import resetView from './resetView';
 import setEditModeApi from './setEditModeApi';
 import setViewModeApi from './setViewModeApi';
-import resetView from './resetView';
-import toStateMachine from './toStateMachine';
 
 export default function(editor, model, typeEditor, buttonStateHelper, modeAccordingToButton) {
     let emitter = new EventEmitter(),
