41522cdf59f533bf364ef33bebf4897d8d8755fa
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-05-26 16:13:37 +0900

    Remove the event listener when the mode is changed to the view.

--- src/lib/editor/Presenter/setMode/index.js
+++ src/lib/editor/Presenter/setMode/index.js
@@ -3,24 +3,32 @@ import {
 }
 from '../../model/AnnotationData/parseAnnotation/validateAnnotation';
 
+let showForEdit;
+
 export default function(annotationData, editMode, mode, writable) {
     let isEditable = mode === 'edit';
 
     editMode.setModeApi(isEditable);
 
+    if (!showForEdit)
+        showForEdit = (annotationData, multitrack, reject) => showMessageForEditMode(annotationData, multitrack, reject, writable);
+
     if (isEditable) {
-        annotationData
-            .on('all.change', (annotationData, multitrack, reject) => {
-                writable.forceModified(false);
+        annotationData.on('all.change', showForEdit);
+    } else {
+        annotationData.removeListener('all.change', showForEdit);
+    }
+}
 
-                if (multitrack) {
-                    toastr.success("track annotations have been merged to root annotations.");
-                    writable.forceModified(true);
-                }
+function showMessageForEditMode(annotationData, multitrack, reject, writable) {
+    writable.forceModified(false);
 
-                if (hasError(reject)) {
-                    writable.forceModified(true);
-                }
-            });
+    if (multitrack) {
+        toastr.success("track annotations have been merged to root annotations.");
+        writable.forceModified(true);
+    }
+
+    if (hasError(reject)) {
+        writable.forceModified(true);
     }
 }
