a5e579a3f21f0afac3395ae68c1ff49ee72f4994
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-05-27 16:03:20 +0900

    Refactor the DisplayInstance.

--- src/lib/editor/Presenter/DisplayInstance.js
+++ src/lib/editor/Presenter/DisplayInstance.js
@@ -1,64 +1,64 @@
-var TypeGapCache = function() {
-        var seed = {
-                instanceHide: 0,
-                instanceShow: 2
-            },
-            api = _.extend({}, seed),
-            set = function(mode, val) {
-                api[mode] = val;
-                return val;
-            },
-            capitalize = require('capitalize');
+import capitalize from 'capitalize';
 
-        _.each(seed, function(val, key) {
-            api['set' + capitalize(key)] = _.partial(set, key);
+const SEED = {
+    instanceHide: 0,
+    instanceShow: 2
+};
+
+export default function(typeGap, instanceMode) {
+    let showInstance = true,
+        typeGapCache = new TypeGapCache(),
+        setShowInstance = function(val) {
+            showInstance = val;
+            updateTypeGap(showInstance, typeGap, typeGapCache);
+        };
+
+    instanceMode
+        .on('show', function(argument) {
+            setShowInstance(true);
+        })
+        .on('hide', function(argument) {
+            setShowInstance(false);
         });
 
-        return api;
-    },
-    DisplayInstance = function(typeGap, instanceMode) {
-        var showInstance = true,
-            typeGapCache = new TypeGapCache(),
-            updateTypeGap = function() {
-                if (showInstance) {
-                    typeGap.set(typeGapCache.instanceShow);
-                } else {
-                    typeGap.set(typeGapCache.instanceHide);
-                }
-            },
-            setShowInstance = function(val) {
-                showInstance = val;
-                updateTypeGap();
-            };
-
-        instanceMode
-            .on('show', function(argument) {
-                setShowInstance(true);
-            })
-            .on('hide', function(argument) {
-                setShowInstance(false);
-            });
-
-        return {
-            showInstance: function() {
-                return showInstance;
-            },
-            changeTypeGap: function(val) {
-                if (showInstance) {
-                    typeGapCache.setInstanceShow(val);
-                } else {
-                    typeGapCache.setInstanceHide(val);
-                }
-
-                updateTypeGap();
-            },
-            getTypeGap:function () {
-                return typeGap();
-            },
-            notifyNewInstance: function() {
-                if (!showInstance) toastr.success("an instance is created behind.");
+    return {
+        showInstance: () => showInstance,
+        changeTypeGap: (val) => {
+            if (showInstance) {
+                typeGapCache.setInstanceShow(val);
+            } else {
+                typeGapCache.setInstanceHide(val);
             }
-        };
-    };
 
-module.exports = DisplayInstance;
+            updateTypeGap();
+        },
+        getTypeGap: () => typeGap(),
+        notifyNewInstance: () => {
+            if (!showInstance) toastr.success("an instance is created behind.");
+        }
+    };
+}
+
+function updateTypeGap (showInstance, typeGap, typeGapCache) {
+    if (showInstance) {
+        typeGap.set(typeGapCache.instanceShow);
+    } else {
+        typeGap.set(typeGapCache.instanceHide);
+    }
+}
+
+function TypeGapCache() {
+    let api = _.extend({}, SEED),
+        set = (mode, val) => updateHash(api, mode, val);
+
+    _.each(SEED, (val, key) => {
+        api['set' + capitalize(key)] = (val) => set(key, val);
+    });
+
+    return api;
+}
+
+function updateHash(hash, key, val) {
+    hash[key] = val;
+    return val;
+}
