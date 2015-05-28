b5bc833783e63a2471f5f983e5f8d8516a568f93
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-05-28 17:30:39 +0900

    Add triggers to transit state of the mode.

--- src/lib/editor/Presenter/EditMode/Trigger.js
+++ src/lib/editor/Presenter/EditMode/Trigger.js
@@ -16,6 +16,62 @@ export default function(stateMachine) {
         },
         toViewInstance: function() {
             stateMachine.setState(state.VIEW_INSTANCE);
+        },
+        pushView: function() {
+            switch (stateMachine.currentState) {
+                case state.TERM:
+                    stateMachine.setState(state.VIEW_TERM);
+                    break;
+                case state.INSTANCE:
+                case state.RELATION:
+                    stateMachine.setState(state.VIEW_INSTANCE);
+                    break;
+                default:
+                    throw new Error(`Invalid state: ${stateMachine.currentState}`);
+            }
+        },
+        upView: function() {
+            switch (stateMachine.currentState) {
+                case state.VIEW_TERM:
+                    stateMachine.setState(state.TERM);
+                    break;
+                case state.VIEW_INSTANCE:
+                    stateMachine.setState(state.INSTANCE);
+                    break;
+                default:
+                    throw new Error(`Invalid state: ${stateMachine.currentState}`);
+            }
+        },
+        pushRelation: function() {
+            stateMachine.setState(state.RELATION);
+        },
+        upRelation: function() {
+            stateMachine.setState(state.INSTANCE);
+        },
+        pushSimple: function() {
+            switch (stateMachine.currentState) {
+                case state.INSTANCE:
+                case state.RELATION:
+                    stateMachine.setState(state.TERM);
+                    break;
+                case state.VIEW_INSTANCE:
+                    stateMachine.setState(state.VIEW_TERM);
+                    break;
+                default:
+                    throw new Error(`Invalid state: ${stateMachine.currentState}`);
+            }
+        },
+        upSimple: function() {
+            switch (stateMachine.currentState) {
+                case state.TERM:
+                    stateMachine.setState(state.INSTANCE);
+                    break;
+                case state.VIEW_TERM:
+                    stateMachine.setState(state.VIEW_INSTANCE);
+                    break;
+                default:
+                    throw new Error(`Invalid state: ${stateMachine.currentState}`);
+            }
         }
     };
 }
