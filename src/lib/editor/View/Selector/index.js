f60d841d6a1286a9728b693552243ce175abf2c9
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-08-28 17:07:23 +0900

    Fix indent.

--- src/lib/editor/View/Selector/index.js
+++ src/lib/editor/View/Selector/index.js
@@ -5,52 +5,52 @@ import selectRelation from './selectRelation';
 import deselectRelation from './deselectRelation';
 
 export default function(editor, model) {
-    let domPositionCache = new DomPositionCache(editor, model.annotationData.entity);
+  let domPositionCache = new DomPositionCache(editor, model.annotationData.entity);
 
-    return {
-        span: {
-            select: (id) => {
-              modifyStyle(editor, 'span', 'add', id)
-              document.querySelector(`#${id}`).focus()
-            },
-            deselect: (id) => {
-              modifyStyle(editor, 'span', 'remove', id)
-              let dom = document.querySelector(`#${id}`)
+  return {
+    span: {
+      select: (id) => {
+        modifyStyle(editor, 'span', 'add', id)
+        document.querySelector(`#${id}`).focus()
+      },
+      deselect: (id) => {
+        modifyStyle(editor, 'span', 'remove', id)
+        let dom = document.querySelector(`#${id}`)
 
-              // A dom does not exist when it is deleted.
-              if(dom){
-                dom.blur()
-              }
-            }
-        },
-        entity: {
-            select: (id) => modifyStyle(editor, 'entity', 'add', id),
-            deselect: (id) => modifyStyle(editor, 'entity', 'remove', id)
-        },
-        relation: {
-            select: (id) => selectRelation(domPositionCache, id),
-            deselect: (id) => deselectRelation(domPositionCache, id)
-        },
-        entityLabel: {
-            update: (id) => updateEntityLabel(editor, id)
+        // A dom does not exist when it is deleted.
+        if (dom) {
+          dom.blur()
         }
-    };
+      }
+    },
+    entity: {
+      select: (id) => modifyStyle(editor, 'entity', 'add', id),
+      deselect: (id) => modifyStyle(editor, 'entity', 'remove', id)
+    },
+    relation: {
+      select: (id) => selectRelation(domPositionCache, id),
+      deselect: (id) => deselectRelation(domPositionCache, id)
+    },
+    entityLabel: {
+      update: (id) => updateEntityLabel(editor, id)
+    }
+  };
 }
 
 function modifyStyle(editor, type, handle, id) {
-    var $elment = domUtil.selector[type].get(id, editor);
-    selectionClass[handle + 'Class']($elment);
+  var $elment = domUtil.selector[type].get(id, editor);
+  selectionClass[handle + 'Class']($elment);
 }
 
 // Select the typeLabel if all entities is selected.
 function updateEntityLabel(editor, entityId) {
-    var $entity = domUtil.selector.entity.get(entityId, editor),
-        $typePane = $entity.parent(),
-        $typeLabel = $typePane.prev();
+  var $entity = domUtil.selector.entity.get(entityId, editor),
+    $typePane = $entity.parent(),
+    $typeLabel = $typePane.prev();
 
-    if ($typePane.children().length === $typePane.find('.ui-selected').length) {
-        selectionClass.addClass($typeLabel);
-    } else {
-        selectionClass.removeClass($typeLabel);
-    }
+  if ($typePane.children().length === $typePane.find('.ui-selected').length) {
+    selectionClass.addClass($typeLabel);
+  } else {
+    selectionClass.removeClass($typeLabel);
+  }
 }
