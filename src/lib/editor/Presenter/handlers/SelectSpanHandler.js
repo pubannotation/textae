01e854db03528147082153f86d46ef1f20b2144d
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-08-28 16:56:43 +0900

    Select a entry by the left or the right key.

--- src/lib/editor/Presenter/handlers/SelectSpanHandler.js
+++ src/lib/editor/Presenter/handlers/SelectSpanHandler.js
@@ -15,5 +15,44 @@ function selectNextSpan(annotationData, selectionModel, direction) {
       selectionModel.clear()
       selectionModel.span.add(span[direction].id)
     }
+  } else {
+    selectNextEntity(annotationData, selectionModel, direction)
   }
+}
+
+function selectNextEntity(annotationData, selectionModel, direction) {
+  let entityId = selectionModel.entity.single()
+  if (entityId) {
+    let entity = annotationData.entity.get(entityId),
+      span = annotationData.span.get(entity.span),
+      brothers = span.getEntities(),
+      index = brothers.indexOf(entityId)
+
+    if (direction === 'left') {
+      if (brothers[index - 1]) {
+        selectEntity(selectionModel, brothers[index - 1])
+      } else {
+        if (span[direction]) {
+          let cousins = span[direction].getEntities()
+          selectEntity(selectionModel, cousins[cousins.length - 1])
+        }
+      }
+    }
+
+    if (direction === 'right') {
+      if (brothers[index + 1]) {
+        selectEntity(selectionModel, brothers[index + 1])
+      } else {
+        if (span[direction]) {
+          let cousins = span[direction].getEntities()
+          selectEntity(selectionModel, cousins[0])
+        }
+      }
+    }
+  }
+}
+
+function selectEntity(selectionModel, id) {
+  selectionModel.clear()
+  selectionModel.entity.add(id)
 }
