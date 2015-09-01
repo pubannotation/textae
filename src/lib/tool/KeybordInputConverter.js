87fd6e54017757d328299263ddd5f505b5eb6596
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-09-01 14:37:02 +0900

    Refactor.

--- src/lib/tool/KeybordInputConverter.js
+++ src/lib/tool/KeybordInputConverter.js
@@ -3,30 +3,28 @@ import {
 }
 from 'events'
 
-let controlKeyEventMap = {
-  27: 'ESC',
-  46: 'DEL',
-  37: 'LEFT',
-  39: 'RIGHT'
-}
+const controlKeyEventMap = new Map([
+  [27, 'ESC'],
+  [46, 'DEL'],
+  [37, 'LEFT'],
+  [39, 'RIGHT']
+])
+
 
 // Observe key-input events and convert events to readable code.
 export default function(keyInputHandler) {
   let emitter = new EventEmitter(),
     eventHandler = (e) => emitter.emit('input', convertKeyEvent(getKeyCode(e))),
-    onKeyup = eventHandler // Overwrite by the noop when daialogs are opened. 
+    noop = () => {},
+    onKeyup = eventHandler // Overwrite by the noop when daialogs are opened.
 
   // Observe key-input
-  $(document).on('keyup', function(event) {
-    onKeyup(event)
-  })
+  document.addEventListener('keyup', (event) => onKeyup(event))
 
   // Disable/Enable key-input When a jquery-ui dialog is opened/closeed
-  $('body').on('dialogopen', '.ui-dialog', function() {
-    onKeyup = () => {}
-  }).on('dialogclose', '.ui-dialog', function() {
-    onKeyup = eventHandler
-  })
+  $('body')
+    .on('dialogopen', '.ui-dialog', () => onKeyup = noop)
+    .on('dialogclose', '.ui-dialog', () => onKeyup = eventHandler)
 
   return emitter
 }
@@ -35,9 +33,9 @@ function convertKeyEvent(keyCode) {
   if (65 <= keyCode && keyCode <= 90) {
     // From a to z, convert 'A' to 'Z'
     return String.fromCharCode(keyCode)
-  } else if (controlKeyEventMap[keyCode]) {
+  } else if (controlKeyEventMap.has(keyCode)) {
     // Control keys, like ESC, DEL ...
-    return controlKeyEventMap[keyCode]
+    return controlKeyEventMap.get(keyCode)
   }
 }
 
