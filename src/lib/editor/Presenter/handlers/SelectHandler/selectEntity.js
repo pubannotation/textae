5634f2c41f78280a12174a8aa7490b9e31b2f017
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-09-07 17:20:08 +0900

    Fix English.

--- src/lib/editor/Presenter/handlers/SelectHandler/selectEntity.js
+++ src/lib/editor/Presenter/handlers/SelectHandler/selectEntity.js
@@ -1,6 +1,6 @@
 export default function(selectionModel, id) {
-  console.assert(selectionModel, 'selectionModel MUST not undefined.')
-  console.assert(id, 'id MUST not undefined.')
+  console.assert(selectionModel, 'selectionModel MUST not be undefined.')
+  console.assert(id, 'id MUST not be undefined.')
 
   selectionModel.clear()
   selectionModel.entity.add(id)
