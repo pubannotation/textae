c6a0d99104781943ae7aa54ea17a9c33cd0ad020
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-09-16 17:55:42 +0900

    Refactor the RemoveCommandsFromSelection.

--- src/lib/editor/Presenter/handlers/removeSelectedElements/RemoveCommandsFromSelection.js
+++ src/lib/editor/Presenter/handlers/removeSelectedElements/RemoveCommandsFromSelection.js
@@ -1,27 +1,28 @@
-var toRomeveSpanCommands = function(spanIds, command) {
-        return spanIds.map(command.factory.spanRemoveCommand);
-    },
-    toRemoveEntityCommands = function(entityIds, command) {
-        return command.factory.entityRemoveCommand(entityIds);
-    },
-    toRemoveRelationCommands = function(relationIds, command) {
-        return relationIds.map(command.factory.relationRemoveCommand);
-    },
-    getAll = function(command, spanIds, entityIds, relationIds) {
-        return []
-            .concat(
-                toRemoveRelationCommands(relationIds, command),
-                toRemoveEntityCommands(entityIds, command),
-                toRomeveSpanCommands(spanIds, command)
-            );
-    },
-    RemoveCommandsFromSelection = function(command, selectionModel) {
-        var spanIds = _.uniq(selectionModel.span.all()),
-            entityIds = _.uniq(selectionModel.entity.all()),
-            relationIds = _.uniq(selectionModel.relation.all());
+export default function(command, selectionModel) {
+  let spanIds = selectionModel.span.all(),
+    entityIds = selectionModel.entity.all(),
+    relationIds = selectionModel.relation.all()
 
-        return getAll(command, spanIds, entityIds, relationIds);
-    };
+  return getAll(command, spanIds, entityIds, relationIds)
+}
 
+function getAll(command, spanIds, entityIds, relationIds) {
+  return []
+    .concat(
+      toRemoveRelationCommands(relationIds, command),
+      toRemoveEntityCommands(entityIds, command),
+      toRomeveSpanCommands(spanIds, command)
+    )
+}
 
-module.exports = RemoveCommandsFromSelection;
+function toRomeveSpanCommands(spanIds, command) {
+  return spanIds.map(command.factory.spanRemoveCommand)
+}
+
+function toRemoveEntityCommands(entityIds, command) {
+  return command.factory.entityRemoveCommand(entityIds)
+}
+
+function toRemoveRelationCommands(relationIds, command) {
+  return relationIds.map(command.factory.relationRemoveCommand)
+}
