47b142a857253348d5c930f206ce04d1793899af
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-09-24 14:17:34 +0900

    Fix indent and delete semicolon to handelers.

--- src/lib/editor/Presenter/handlers/ToggleButtonHandler.js
+++ src/lib/editor/Presenter/handlers/ToggleButtonHandler.js
@@ -1,23 +1,23 @@
 export default function(modeAccordingToButton, editMode) {
-    return {
-        toggleSimpleMode: () => toggleSimpleMode(modeAccordingToButton, editMode),
-        toggleDetectBoundaryMode: () => toggleDetectBoundaryMode(modeAccordingToButton),
-        toggleInstaceRelation: () => toggleInstaceRelation(editMode)
-    };
+  return {
+    toggleSimpleMode: () => toggleSimpleMode(modeAccordingToButton, editMode),
+    toggleDetectBoundaryMode: () => toggleDetectBoundaryMode(modeAccordingToButton),
+    toggleInstaceRelation: () => toggleInstaceRelation(editMode)
+  }
 }
 
 function toggleSimpleMode(modeAccordingToButton, editMode) {
-    if (modeAccordingToButton.simple.value()) {
-        editMode.upSimple();
-    } else {
-        editMode.pushSimple();
-    }
+  if (modeAccordingToButton.simple.value()) {
+    editMode.upSimple()
+  } else {
+    editMode.pushSimple()
+  }
 }
 
 function toggleDetectBoundaryMode(modeAccordingToButton) {
-    modeAccordingToButton['boundary-detection'].toggle();
+  modeAccordingToButton['boundary-detection'].toggle()
 }
 
 function toggleInstaceRelation(editMode) {
-    editMode.toggleInstaceRelation();
+  editMode.toggleInstaceRelation()
 }
