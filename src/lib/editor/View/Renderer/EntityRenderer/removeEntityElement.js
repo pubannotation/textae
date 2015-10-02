055395680a058b8b27ebf6b29ab7cae7b57eb9cb
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2015-10-02 11:19:37 +0900

    Bug fix: An error occurs when push up key on the selected span after changing that span from normal span to block span. - Remove a grid unless type. - Refactor the removeEntityElement.

--- src/lib/editor/View/Renderer/EntityRenderer/removeEntityElement.js
+++ src/lib/editor/View/Renderer/EntityRenderer/removeEntityElement.js
@@ -1,22 +1,30 @@
 import getEntityDom from '../../../getEntityDom'
-import getTypeDom from '../getTypeDom'
 import arrangePositionOfPane from './arrangePositionOfPane'
 
 export default function(editor, annotationData, entity) {
-  // Get old type from Dom, Because the entity may have new type when changing type of the entity.
-  var oldType = $(getEntityDom(editor[0], entity.id)).remove().attr('type')
+  let entityDom = getEntityDom(editor[0], entity.id)
 
-  // Delete type if no entity.
-  if (doesTypeHasNoEntity(annotationData, entity, oldType)) {
-    getTypeDom(entity.span, oldType).remove()
-  } else {
-    // Arrage the position of TypePane, because number of entities decrease.
-    arrangePositionOfPane(getTypeDom(entity.span, oldType).find('.textae-editor__entity-pane')[0])
+  // The entity is removed already.
+  if (!entityDom) {
+    return
   }
-}
 
-function doesTypeHasNoEntity(annotationData, entity, typeName) {
-  return annotationData.span.get(entity.span).getTypes().filter(function(type) {
-    return type.name === typeName
-  }).length === 0
+  let paneDom = entityDom.parentNode
+  entityDom.remove()
+
+  if (paneDom.children.length) {
+    // Arrage the position of TypePane, because number of entities decrease.
+    arrangePositionOfPane(paneDom)
+  } else {
+    let typeDom = paneDom.parentNode,
+      gridDom = typeDom.parentNode
+
+    // Remove type unlese entity.
+    typeDom.remove()
+
+    // Remove grid unless type.
+    if (!gridDom.children.length) {
+      gridDom.remove()
+    }
+  }
 }
