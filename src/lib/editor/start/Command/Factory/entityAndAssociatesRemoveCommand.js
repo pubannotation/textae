14884d5bbae670782c8527c5c73645c2b825f348
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2016-04-01 21:12:56 +0900

    Bug fix: Modifications of relations are left in the model when their are removed by removing entities that is the parent of their parent relation

--- src/lib/editor/start/Command/Factory/entityAndAssociatesRemoveCommand.js
+++ src/lib/editor/start/Command/Factory/entityAndAssociatesRemoveCommand.js
@@ -3,12 +3,14 @@ import {
 }
 from './commandTemplate'
 import executeCompositCommand from './executeCompositCommand'
+import relationAndAssociatesRemoveCommand from './relationAndAssociatesRemoveCommand'
+
 
 export default function(model, id) {
   const entityRemoveCommand = (entity) => new RemoveCommand(model.annotationData, model.selectionModel, 'entity', entity),
     removeEntity = entityRemoveCommand(id),
     removeRelation = model.annotationData.entity.assosicatedRelations(id)
-      .map((id) => new RemoveCommand(model.annotationData, model.selectionModel, 'relation', id)),
+      .map((id) => relationAndAssociatesRemoveCommand(model, id)),
       removeModification = model.annotationData.getModificationOf(id)
       .map((modification) => modification.id)
       .map((id) => new RemoveCommand(model.annotationData, model.selectionModel, 'modification', id)),
