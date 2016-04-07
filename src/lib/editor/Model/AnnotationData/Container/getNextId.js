44f5fa5e4c7988b07684ca2a6235aeb67d279fae
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2016-04-07 16:51:39 +0900

    Bug fix : New ids are broken on annotations have multi tracks

--- src/lib/editor/Model/AnnotationData/Container/getNextId.js
+++ src/lib/editor/Model/AnnotationData/Container/getNextId.js
@@ -1,21 +1,31 @@
 export default function(prefix, existsIds) {
-  let numbers = existsIds
-    .filter(id => hasPrefix(prefix, id))
-    .map(withoutPrefix),
-    // The Math.max retrun -Infinity when the second argument array is empty.
-    max = numbers.length === 0 ? 0 : Math.max.apply(null, numbers),
-    nextNumber = max + 1
+  const numbers = getIssuedNumbers(existsIds, prefix),
+    nextNumber = getNextNumber(numbers)
 
   return prefix + nextNumber
 }
 
-function hasPrefix(prefix, id) {
-  // Exclude an id have other than prefix and number, for example 'T1a'.
-  let reg = new RegExp(`${prefix}\\d+$`)
+function getNextNumber(numbers) {
+  // The Math.max retrun -Infinity when the second argument array is empty.
+  const max = numbers.length === 0 ? 0 : Math.max(...numbers),
+    nextNumber = max + 1
+
+  return nextNumber
+}
+
+function getIssuedNumbers(ids, prefix) {
+  return ids
+    .filter((id) => isWellFormed(prefix, id))
+    .map(onlyNumber)
+}
+
+function isWellFormed(prefix, id) {
+  // The format of id is a prefix and a number, for exapmle 'T1'.
+  const reg = new RegExp(`^${prefix}\\d+$`)
 
   return reg.test(id)
 }
 
-function withoutPrefix(id) {
+function onlyNumber(id) {
   return id.slice(1)
 }
