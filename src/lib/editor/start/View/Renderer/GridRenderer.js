16f6d1828da2e0fd4509e0af4f72dc47c0afa659
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2016-04-13 19:07:55 +0900

    Bug fix: The position of grids is broken when a new span is inserted before block spans

--- src/lib/editor/start/View/Renderer/GridRenderer.js
+++ src/lib/editor/start/View/Renderer/GridRenderer.js
@@ -29,13 +29,47 @@ function createGrid(editorDom, domPositionCache, container, spanId) {
   element.classList.add('hidden')
   element.style.width = spanPosition.width + 'px'
 
-  let rightSpan = getRightElement(editorDom, document.querySelector(`#${spanId}`))
-  if (rightSpan) {
-    container.insertBefore(element, document.querySelector(`#G${rightSpan.id}`))
+  const rightGrid = getRightGrid(editorDom, spanId)
+  if (rightGrid) {
+    container.insertBefore(element, rightGrid)
   } else {
     // append to the annotation area.
     container.appendChild(element)
   }
 
   return element
+}
+
+// Block spans have no grid.
+// Travers the next span if the right span is a block span.
+function getRightGrid(editorDom, spanId) {
+  let [rightSpan, grid] = getRightSpanAndGrid(editorDom, spanId)
+  if (!rightSpan) {
+    return null
+  }
+
+  if (grid) {
+    return grid
+  }
+
+  while (rightSpan) {
+    [rightSpan, grid] = getRightSpanAndGrid(editorDom, rightSpan.id)
+    if (grid) {
+      return grid
+    }
+  }
+}
+
+function getRightSpanAndGrid(editorDom, spanId) {
+  const rightSpan = getRightElement(editorDom, document.querySelector(`#${spanId}`))
+  if (!rightSpan) {
+    return [null, null]
+  }
+
+  const grid = document.querySelector(`#G${rightSpan.id}`)
+  if (grid) {
+    return [rightSpan, grid]
+  }
+
+  return [rightSpan, null]
 }
