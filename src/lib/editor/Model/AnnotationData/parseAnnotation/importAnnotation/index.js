3a85311ede209c92b662ebfa3853f3308642a94d
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-07-14 14:34:57 +0900

    Remove an unnecessary file

--- src/lib/editor/Model/AnnotationData/parseAnnotation/importAnnotation/index.js
+++
@@ -1,8 +0,0 @@
-import importSource from './importSource'
-import translateModification from './translateModification'
-
-export default importAnnotations
-
-function importAnnotations(span, entity, relation, modification, denotations, relations, modifications, prefix) {
-  importSource([modification], _.partial(translateModification, prefix), modifications)
-}
