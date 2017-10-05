1a33d305bbc5b8017368e09d308db01b9e2c7b97
terada_youworks <terada@youworks.co.jp>
2017-10-05 14:40:25 +0900

    ADD: can save attributes to the JSON.

--- src/lib/editor/Model/AnnotationData/toJson.js
+++ src/lib/editor/Model/AnnotationData/toJson.js
@@ -1,6 +1,7 @@
 export default function(dataStore) {
   return {
     denotations: toDenotation(dataStore),
+    attributions: toAttribution(dataStore),
     relations: toRelation(dataStore),
     modifications: dataStore.modification.all()
   }
@@ -22,6 +23,17 @@ function toDenotation(dataStore) {
         obj: entity.type
       }
     })
+}
+
+function toAttribution(dataStore) {
+  return dataStore.attribute.all().map((attribute) => {
+    return {
+      id: attribute.id,
+      pred: attribute.pred,
+      subj: attribute.subj,
+      obj: attribute.type,
+    }
+  })
 }
 
 function toRelation(dataStore) {
