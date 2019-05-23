1e4fe98a4b78f56e3636049d0200aa654d4dc65b
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2019-05-23 17:54:54 +0900

    refactor: Fix code style

--- src/lib/util/CursorChanger.js
+++ src/lib/util/CursorChanger.js
@@ -1,17 +1,12 @@
-import _ from 'underscore'
-
-var changeCursor = function(editor, action) {
-  // Add jQuery Ui dialogs to targets because they are not in the editor.
-  editor = editor.add('.ui-dialog, .ui-widget-overlay')
-  editor[action + 'Class']('textae-editor--wait')
+module.exports = function(editor) {
+  return {
+    startWait: () => changeCursor(editor, 'add'),
+    endWait: () => changeCursor(editor, 'remove')
+  }
 }
 
-module.exports = function(editor) {
-  var wait = _.partial(changeCursor, editor, 'add'),
-    endWait = _.partial(changeCursor, editor, 'remove')
-
-  return {
-    startWait: wait,
-    endWait: endWait,
-  }
+function changeCursor(editor, action) {
+  // Add jQuery Ui dialogs to targets because they are not in the editor.
+  editor = editor.add('.ui-dialog, .ui-widget-overlay')
+  editor[`${action}Class`]('textae-editor--wait')
 }
