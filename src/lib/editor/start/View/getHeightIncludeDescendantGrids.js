ddb7a223da582d795e0157afa0f493bc08141a51
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2019-05-28 17:34:22 +0900

    refactor: Fix code style

--- src/lib/editor/start/View/getHeightIncludeDescendantGrids.js
+++ src/lib/editor/start/View/getHeightIncludeDescendantGrids.js
@@ -2,13 +2,14 @@ import _ from 'underscore'
 import calcAttributeHeightOfGrid from './calcAttributeHeightOfGrid'
 
 export default function getHeightIncludeDescendantGrids(span, typeContainer, typeGapValue) {
-  var descendantsMaxHeight = span.children.length === 0 ? 0 :
+  const descendantsMaxHeight = span.children.length === 0 ? 0 :
     _.max(
       span.children.map(childSpan => getHeightIncludeDescendantGrids(childSpan, typeContainer, typeGapValue))
-    ),
-    gridHeight = span.getTypes()
+    )
+
+  const gridHeight = span.getTypes()
     .filter(type => !typeContainer.entity.isBlock(type.name))
-    .length * (typeGapValue * 18 + calcAttributeHeightOfGrid(span.id) + 18)// first 18px is margin between grids, second 18px is pane height.
+    .length * (typeGapValue * 18 + calcAttributeHeightOfGrid(span.id) + 18) // first 18px is margin between grids, second 18px is pane height.
 
   return gridHeight + descendantsMaxHeight
 }
