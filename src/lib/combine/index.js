ba0b09e09cfd36748159876aea977b746911ce2c
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2019-05-28 11:48:45 +0900

    bugfix: In Firefox, pressing the right mouse button opens a context menu and releasing it immediately closes the context menu

--- src/lib/combine/index.js
+++ src/lib/combine/index.js
@@ -39,7 +39,18 @@ module.exports = function combine(editor, controlBar, contextMenu) {
   })
 
   // Close ContextMenu when another editor is clicked
-  window.addEventListener('click', () => contextMenu.hide())
+  window.addEventListener('click', (e) => {
+    // In Firefox, the right button of mouse fires a 'click' event.
+    // https://stackoverflow.com/questions/43144995/mouse-right-click-on-firefox-triggers-click-event
+    // In Fireforx, MoesueEvent has a 'which' property, which is 3 when the right button is clicked.
+    // https://stackoverflow.com/questions/2405771/is-right-click-a-javascript-event
+    if (e.which === 3) {
+      return
+    }
+
+    contextMenu.hide()
+  })
+
   window.addEventListener('contextmenu', (e) => {
     // Close ContextMenu when another editor is clicked.
     contextMenu.hide()
