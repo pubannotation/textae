126cb0f62d3ec691bee7b620f447f51e6a44893c
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2019-06-05 14:17:48 +0900

    refactor: Fix code style

--- src/lib/editor/start/Command/Factory/entityChangeTypeRemoveRelationCommand.js
+++ src/lib/editor/start/Command/Factory/entityChangeTypeRemoveRelationCommand.js
@@ -5,10 +5,10 @@ from './commandTemplate'
 import ChangeTypeCommand from './ChangeTypeCommand'
 import executeCompositCommand from './executeCompositCommand'
 
-export default function(editor, annotationData, selectionModel, id, newType, isRemoveRelations) {
-  // isRemoveRelations is set true when newType is block.
-  const changeType = new ChangeTypeCommand(editor, annotationData, 'entity', id, newType),
-    subCommands = isRemoveRelations ?
+export default function(editor, annotationData, selectionModel, id, newType, isBlockType) {
+  const changeType = new ChangeTypeCommand(editor, annotationData, 'entity', id, newType)
+  // Block types do not have relations. If there is a relation, delete it.
+  const subCommands = isBlockType ?
     annotationData.entity.assosicatedRelations(id)
     .map((id) => new RemoveCommand(editor, annotationData, selectionModel, 'relation', id))
     .concat(changeType) : [changeType]
