c4f71a70517969eb8537f96d28605863a60d53c0
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2019-06-06 16:21:59 +0900

    bugfix: An unnecessary type property was added to attribute

--- src/lib/editor/start/View/Renderer/AttributeRenderer/create.js
+++ src/lib/editor/start/View/Renderer/AttributeRenderer/create.js
@@ -3,9 +3,6 @@ import createAttributeElement from './createAttributeElement'
 import $ from 'jquery'
 
 export default function(editor, namspace, typeContainer, gridRenderer, attributeModel) {
-  // Replace null to 'null' if type is null and undefined too.
-  attributeModel.type = String(attributeModel.type)
-
   let entityDom = getEntityDom(editor[0], attributeModel.subj)
   if (!entityDom) {
     throw new Error("entity is not rendered : " + attributeModel.subj)
