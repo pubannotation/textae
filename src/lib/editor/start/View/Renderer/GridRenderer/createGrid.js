a053a45dca27cfac38889195b6a9de35a8813a62
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2019-06-13 12:39:50 +0900

    refactor: Use html template

--- src/lib/editor/start/View/Renderer/GridRenderer/createGrid.js
+++ src/lib/editor/start/View/Renderer/GridRenderer/createGrid.js
@@ -1,24 +1,19 @@
 import getRightGrid from "./getRightGrid"
+import createGridHtml from "./createGridHtml"
 
 export default function(editorDom, domPositionCache, container, spanId) {
   const spanPosition = domPositionCache.getSpan(spanId)
-  const element = createGridElement(spanId, spanPosition.width)
   const rightGrid = getRightGrid(editorDom, spanId)
+
   if (rightGrid) {
-    container.insertBefore(element, rightGrid)
+    // insert before the right grid.
+    const html = createGridHtml(spanId, spanPosition.width)
+    rightGrid.insertAdjacentHTML('beforebegin', html)
+    return rightGrid.previousElementSibling
   } else {
     // append to the annotation area.
-    container.appendChild(element)
+    const html = createGridHtml(spanId, spanPosition.width)
+    container.insertAdjacentHTML('beforeend', html)
+    return container.lastElementChild
   }
-  return element
 }
-
-function createGridElement(spanId, spanWidth) {
-  const element = document.createElement('div')
-  element.setAttribute('id', `G${spanId}`)
-  element.classList.add('textae-editor__grid')
-  element.classList.add('hidden')
-  element.style.width = `${spanWidth}px`
-  return element
-}
-
---
+++ src/lib/editor/start/View/Renderer/GridRenderer/createGridHtml.js
@@ -0,0 +1,11 @@
+import Handlebars from 'handlebars'
+
+const source = `
+  <div id={{id}} class="textae-editor__grid hidden" style="width: {{spanWidth}}px">
+  </div>
+  `
+const template = Handlebars.compile(source)
+
+export default function(spanId, spanWidth) {
+  return template({id: `G${spanId}`, spanWidth})
+}
