b4a30a2112a267644daedfa3232180783d2b69a9
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2019-06-24 12:26:43 +0900

    refactor: Move extendRelationId into the hoverize.js

--- src/lib/editor/start/View/Renderer/RelationRenderer/setRenderLazy/render/hoverize.js
+++ src/lib/editor/start/View/Renderer/RelationRenderer/setRenderLazy/render/hoverize.js
@@ -2,15 +2,24 @@ import LabelOverlay from '../../LabelOverlay'
 import extendPointup from './extendPointup'
 
 // Set hover action.
-export default function(editor, annotationData, typeContainer, connect) {
+export default function(editor, annotationData, typeContainer, connect, relationId) {
+  extendRelationId(connect, relationId)
   extendPointup(editor, annotationData, typeContainer, connect)
   bindConnect(connect)
   bindLabel(connect)
   return connect
 }
 
-function bindHoverAction(jsPlumbElement, onMouseOver, onMouseRemove) {
-  jsPlumbElement.bind('mouseenter', onMouseOver).bind('mouseexit', onMouseRemove)
+// The relation id needs to be embedded in the jsPlumb connector instance.
+//
+// When hovering the relation, the connector of the relationship is broadened.
+// At that time, the color of the connector is required.
+// Colors are obtained by type from the type container.
+// Type is obtained from the model by the relation ID.
+function extendRelationId(connect, relationId) {
+  return Object.assign(connect, {
+    relationId
+  })
 }
 
 function bindConnect(connect) {
@@ -19,4 +28,8 @@ function bindConnect(connect) {
 
 function bindLabel(connect) {
   bindHoverAction(new LabelOverlay(connect), (label) => label.component.pointup(), (label) => label.component.pointdown())
+}
+
+function bindHoverAction(jsPlumbElement, onMouseOver, onMouseRemove) {
+  jsPlumbElement.bind('mouseenter', onMouseOver).bind('mouseexit', onMouseRemove)
 }
--- src/lib/editor/start/View/Renderer/RelationRenderer/setRenderLazy/render/index.js
+++ src/lib/editor/start/View/Renderer/RelationRenderer/setRenderLazy/render/index.js
@@ -9,24 +9,10 @@ export default function(jsPlumbInstance, editor, annotationData, typeContainer, 
 
   const connect = createJsPlumbConnect(jsPlumbInstance, editor, relation, annotationData, typeContainer, modificationRenderer)
 
-  extendRelationId(connect, relation)
-
-  hoverize(editor, annotationData, typeContainer, connect)
+  hoverize(editor, annotationData, typeContainer, connect, relation.id)
   Object.assign(connect, new Api())
 
   // Notify to controller that a new jsPlumbConnection is added.
   editor.trigger('textae.editor.jsPlumbConnection.add', connect)
   cache(connect, editor, annotationData)
-}
-
-// The relation id needs to be embedded in the jsPlumb connector instance.
-//
-// When hovering the relation, the connector of the relationship is broadened.
-// At that time, the color of the connector is required.
-// Colors are obtained by type from the type container.
-// Type is obtained from the model by the relation ID.
-function extendRelationId(connect, relation) {
-  return Object.assign(connect, {
-    relationId: relation.id
-  })
 }
