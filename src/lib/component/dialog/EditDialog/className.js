4b77bc81aa9a5cbb9d34306d46a71f66be1f1127
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-06-27 18:36:14 +0900

    refactor: Remove unused css class

--- src/lib/component/dialog/EditDialog/className.js
+++ src/lib/component/dialog/EditDialog/className.js
@@ -1,8 +1,8 @@
 const CLASS_NAMES = {
   container: 'textae-editor__edit-value-and-pred-dialog__container',
   inputBox: 'textae-editor__edit-value-and-pred-dialog__input-box',
-  predicate: 'textae-editor__edit-dialog textae-editor__edit-value-and-pred-dialog textae-editor__edit-value-and-pred-dialog--predicate',
-  value: 'textae-editor__edit-dialog textae-editor__edit-value-and-pred-dialog textae-editor__edit-value-and-pred-dialog--value'
+  predicate: 'textae-editor__edit-value-and-pred-dialog--predicate',
+  value: 'textae-editor__edit-value-and-pred-dialog--value'
 }
 
 export default CLASS_NAMES
