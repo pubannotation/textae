f26ab53be126b4c66a739972a3b4c387c7d03b01
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-06-27 12:31:07 +0900

    refactor: Fix code style

--- src/lib/editor/start/Presenter/TypeEditor/changeAttributeHandler.js
+++ src/lib/editor/start/Presenter/TypeEditor/changeAttributeHandler.js
@@ -3,31 +3,31 @@ import EditAttributeDialog from '../../../../component/EditAttributeDialog'
 // An handler is get on runtime, because it is changed by the edit mode.
 export default function(editor, selectionModel, editAttributeHandler, autocompletionWs) {
   if (editAttributeHandler.getSelectedIdEditable().length > 0) {
-    const handler = editAttributeHandler,
-      value1 = handler.getSelectedPred(),
-      value2 = handler.getSelectedValue(),
-      done = (pred, value, label) => {
-        let commands = []
+    const value1 = editAttributeHandler.getSelectedPred()
+    const value2 = editAttributeHandler.getSelectedValue()
 
-        if (label) {
-          const command = editAttributeHandler.changeLabelOfId(value, label)
+    const done = (pred, value, label) => {
+      let commands = []
 
-          if (command) {
-            commands.push(command)
-          }
+      if (label) {
+        const command = editAttributeHandler.changeLabelOfId(value, label)
+
+        if (command) {
+          commands.push(command)
         }
-
-        if (value) {
-          commands = commands.concat(editAttributeHandler.changeSelectedElement(pred, value))
-          editAttributeHandler.command.invoke(commands, ['annotation'])
-        }
-
-        // Cancel selections here.
-        // Because attributes are selected only during processing click events of add, edit and delete buttons.
-        selectionModel.clear()
       }
 
-    let dialog = new EditAttributeDialog(editor, editAttributeHandler.typeContainer, done, autocompletionWs)
+      if (value) {
+        commands = commands.concat(editAttributeHandler.changeSelectedElement(pred, value))
+        editAttributeHandler.command.invoke(commands, ['annotation'])
+      }
+
+      // Cancel selections here.
+      // Because attributes are selected only during processing click events of add, edit and delete buttons.
+      selectionModel.clear()
+    }
+
+    const dialog = new EditAttributeDialog(editor, editAttributeHandler.typeContainer, done, autocompletionWs)
     dialog.update(value1, value2)
     dialog.open()
   }
