ff4b1af243e6c9bb8452397e5bca583de9caeae2
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-06-27 14:53:51 +0900

    refactor: Remove unused value

--- src/lib/editor/start/Presenter/TypeEditor/ElementEditor/initiateEditAttribute/deleteButtonClicked.js
+++ src/lib/editor/start/Presenter/TypeEditor/ElementEditor/initiateEditAttribute/deleteButtonClicked.js
@@ -1,8 +1,7 @@
 import getAttributeIdByClickedButton from './getAttributeIdByClickedButton'
 
 export default function(annotationData, selectionModel, command, e) {
-  let attributeId = getAttributeIdByClickedButton(e),
-    attribute = annotationData.attribute.get(attributeId)
+  const attributeId = getAttributeIdByClickedButton(e)
 
   command.invoke([command.factory.attributeRemoveCommand(attributeId)], ['annotation'])
   selectionModel.attribute.remove(attributeId)
