3344c89bdb22bd2d04abff5005500f1d9701250c
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-06-28 19:04:59 +0900

    refactor: Fix code style

--- src/lib/editor/start/View/Renderer/EntityRenderer/getTypeElement/createEmptyTypeDomElement.js
+++ src/lib/editor/start/View/Renderer/EntityRenderer/getTypeElement/createEmptyTypeDomElement.js
@@ -4,24 +4,28 @@ import $ from 'jquery'
 
 // A Type element has an entity_pane elment that has a label and will have entities.
 export default function(namespace, typeContainer, spanId, type) {
-  let typeId = idFactory.makeTypeId(spanId, type)
+  const typeId = idFactory.makeTypeId(spanId, type)
 
   // The EntityPane will have entities.
-  let $entityPane = $('<div>')
+  const $entityPane = $('<div>')
     .attr('id', 'P-' + typeId)
     .addClass('textae-editor__entity-pane')
-  // The label over the span.
-  let $typeLabel = $('<div>')
+
+
+    // The label over the span.
+  const $typeLabel = $('<div>')
     .addClass('textae-editor__type-label')
     .css({
       'background-color': typeContainer.getColor(type),
     })
-  let addButtonElement = document.createElement('div')
+
+  const addButtonElement = document.createElement('div')
   addButtonElement.classList.add('textae-editor__attribute-button')
   addButtonElement.classList.add('textae-editor__attribute-button--add')
   addButtonElement.setAttribute('title', 'Add a new attribute to this entity.')
   $typeLabel[0].setAttribute('tabindex', 0)
   setLabelToTypeLabel($typeLabel[0], namespace, typeContainer, type)
+
   return $('<div>')
     .attr('id', typeId)
     .addClass('textae-editor__type')
