a044e5bd4f388dd6afa7c6f1531e87eae16e6b95
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-01 16:20:17 +0900

    bugfix: updating type labels causes attributes to be rendered doubly

--- src/lib/editor/start/View/Renderer/AttributeRenderer/appendAttribute.js
+++ src/lib/editor/start/View/Renderer/AttributeRenderer/appendAttribute.js
@@ -2,6 +2,12 @@ import idFactory from '../../../../idFactory'
 import createAttributeHtml from './createAttributeHtml'
 
 export default function(editor, pane, attribute) {
+  const label = pane.nextElementSibling
   const id = idFactory.makeAttributeDomId(editor, attribute.id)
-  pane.nextElementSibling.insertAdjacentHTML('beforeend', createAttributeHtml(id, attribute))
+
+  if (label.querySelector(`#${id}`)) {
+    return
+  }
+
+  label.insertAdjacentHTML('beforeend', createAttributeHtml(id, attribute))
 }
