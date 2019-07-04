83c8f05fef72fa2b0faa422df93848655b6be23f
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-04 17:36:29 +0900

    refactor: Fix code style

--- src/lib/component/Pallet/Component.js
+++ src/lib/component/Pallet/Component.js
@@ -3,68 +3,16 @@ import EditTypeDialog from "./EditTypeDialog"
 import CLASS_NAMES from './className'
 
 export default function(editor, annotationData, command, typeContainer, autocompletionWs, elementEditor) {
-  const pallet = document.createElement('div')
-  pallet.classList.add(CLASS_NAMES.base)
-  pallet.style.display = 'none'
+  const pallet = createPallet()
 
-  let title = document.createElement('p')
-  title.classList.add(CLASS_NAMES.title)
+  bindEventHandlers(pallet, elementEditor, typeContainer, editor, autocompletionWs, command)
 
-  let titleText = document.createElement('span')
-  titleText.classList.add(CLASS_NAMES.titleText)
+  return pallet
+}
 
-  let lockIcon = document.createElement('span')
-  lockIcon.classList.add(CLASS_NAMES.lockIcon)
-  lockIcon.innerText = 'locked'
-
-  let buttonContainer = document.createElement('div')
-  buttonContainer.classList.add(CLASS_NAMES.buttons)
-  buttonContainer.classList.add(CLASS_NAMES.hideWhenLocked)
-
-  let addButton = document.createElement('span')
-  addButton.classList.add(CLASS_NAMES.button)
-  addButton.classList.add(CLASS_NAMES.buttonAdd)
-  addButton.setAttribute('title', 'Add new type')
-
-  let readButton = document.createElement('span')
-  readButton.classList.add(CLASS_NAMES.button)
-  readButton.classList.add(CLASS_NAMES.buttonRead)
-  readButton.setAttribute('title', 'Import')
-
-  let writeButton = document.createElement('span')
-  writeButton.classList.add(CLASS_NAMES.button)
-  writeButton.classList.add(CLASS_NAMES.buttonWrite)
-  writeButton.setAttribute('title', 'Upload')
-
-  title.appendChild(titleText)
-  title.appendChild(lockIcon)
-  buttonContainer.appendChild(addButton)
-  buttonContainer.appendChild(readButton)
-  buttonContainer.appendChild(writeButton)
-  pallet.appendChild(title)
-  pallet.appendChild(buttonContainer)
-  pallet.appendChild(document.createElement('table'))
-
+function bindEventHandlers(pallet, elementEditor, typeContainer, editor, autocompletionWs, command) {
   delegate(pallet, '.' + CLASS_NAMES.buttonAdd, 'click', (e) => {
-    let handler = elementEditor.getHandlerForPallet(),
-      target = e.delegateTarget,
-      id = target.getAttribute('data-id'),
-      defaultColor = typeContainer[handler.modelType].getDefaultColor(),
-      done = (newId, newLabel, newColor, newDefault) => {
-        if (newId === '') return
-
-        let newType = {id: newId}
-        if (newLabel !== '') newType.label = newLabel
-        if (newColor !== defaultColor) newType.color = newColor
-        if (newDefault) newType.default = newDefault
-
-        handler.command.invoke([handler.addType(newType)], ['configuration'])
-        triggerUpdatePallet(editor)
-      },
-      dialog = new EditTypeDialog(editor, handler.typeContainer, done, autocompletionWs, 'Please create a new type')
-
-    dialog.update(id, '', defaultColor, false)
-    dialog.open()
+    openCreateTypeDialog(elementEditor, e, typeContainer, editor, autocompletionWs)
   })
 
   delegate(pallet, '.' + CLASS_NAMES.buttonRead, 'click', (e) => {
@@ -82,50 +30,141 @@ export default function(editor, annotationData, command, typeContainer, autocomp
   })
 
   delegate(pallet, '.' + CLASS_NAMES.selectAll, 'click', (e) => {
-    if (!checkButtonEnable(e.target)) return
+    if (!checkButtonEnable(e.target)) {
+      return
+    }
 
     elementEditor.getHandlerForPallet().selectAllByLabel(e.delegateTarget.getAttribute('data-id'))
   })
 
   delegate(pallet, '.' + CLASS_NAMES.editType, 'click', (e) => {
-    let handler = elementEditor.getHandlerForPallet(),
-      target = e.delegateTarget,
-      id = target.getAttribute('data-id'),
-      label = handler.typeContainer.getLabel(id) || '',
-      color = target.getAttribute('data-color').toLowerCase(),
-      isDefault = target.getAttribute('data-is-default') === 'true',
-      done = (newId, newLabel, newColor, newDefault) => {
-        let changeValues = {}
-
-        if (newId !== id) changeValues.id = newId
-        if (newLabel !== label) {
-          changeValues.label = newLabel === '' ? null : newLabel
-        }
-        if (newColor !== color) {
-          changeValues.color = newColor === '' ? null : newColor
-        }
-        if (newDefault !== isDefault) {
-          changeValues.default = isDefault ? null : true
-        }
-
-        if (Object.keys(changeValues).length) {
-          handler.command.invoke([handler.changeType(id, changeValues)], ['annotation', 'configuration'])
-          triggerUpdatePallet(editor)
-        }
-      },
-      dialog = new EditTypeDialog(editor, handler.typeContainer, done, autocompletionWs, 'Please edit the type')
-
-    dialog.update(id, label, color, isDefault)
-    dialog.open()
+    openEditTypeDialog(elementEditor, e, editor, autocompletionWs)
   })
 
   delegate(pallet, '.' + CLASS_NAMES.remove, 'click', (e) => {
-    if (!checkButtonEnable(e.target)) return
+    if (!checkButtonEnable(e.target)) {
+      return
+    }
 
     const commands = elementEditor.getHandlerForPallet().removeType(e.delegateTarget.getAttribute('data-id'), e.delegateTarget.getAttribute('data-short-label'))
     command.invoke(commands, ['configuration'])
     triggerUpdatePallet(editor)
   })
+}
+
+function openEditTypeDialog(elementEditor, e, editor, autocompletionWs) {
+  const handler = elementEditor.getHandlerForPallet()
+  const target = e.delegateTarget
+  const id = target.getAttribute('data-id')
+  const label = handler.typeContainer.getLabel(id) || ''
+  const color = target.getAttribute('data-color').toLowerCase()
+  const isDefault = target.getAttribute('data-is-default') === 'true'
+  const done = (newId, newLabel, newColor, newDefault) => {
+    invokeChangeTypeCommand(newId, id, newLabel, label, newColor, color, newDefault, isDefault, handler, editor)
+  }
+  const dialog = new EditTypeDialog(editor, handler.typeContainer, done, autocompletionWs, 'Please edit the type')
+  dialog.update(id, label, color, isDefault)
+  dialog.open()
+}
+
+function invokeChangeTypeCommand(newId, id, newLabel, label, newColor, color, newDefault, isDefault, handler, editor) {
+  const changeValues = {}
+
+  if (newId !== id) {
+    changeValues.id = newId
+  }
+
+  if (newLabel !== label) {
+    changeValues.label = newLabel === '' ? null : newLabel
+  }
+
+  if (newColor !== color) {
+    changeValues.color = newColor === '' ? null : newColor
+  }
+
+  if (newDefault !== isDefault) {
+    changeValues.default = isDefault ? null : true
+  }
+
+  if (Object.keys(changeValues).length) {
+    handler.command.invoke([handler.changeType(id, changeValues)], ['annotation', 'configuration'])
+    triggerUpdatePallet(editor)
+  }
+}
+
+function openCreateTypeDialog(elementEditor, e, typeContainer, editor, autocompletionWs) {
+  const handler = elementEditor.getHandlerForPallet()
+  const target = e.delegateTarget
+  const id = target.getAttribute('data-id')
+  const defaultColor = typeContainer[handler.modelType].getDefaultColor()
+  const done = (newId, newLabel, newColor, newDefault) => {
+    if (newId === '') {
+      return
+    }
+
+    const newType = {
+      id: newId,
+      color: newColor
+    }
+
+    if (newLabel !== '') {
+      newType.label = newLabel
+    }
+
+    if (newDefault) {
+      newType.default = newDefault
+    }
+
+    handler.command.invoke([handler.addType(newType)], ['configuration'])
+    triggerUpdatePallet(editor)
+  }
+  const dialog = new EditTypeDialog(editor, handler.typeContainer, done, autocompletionWs, 'Please create a new type')
+  dialog.update(id, '', defaultColor, false)
+  dialog.open()
+}
+
+function createPallet() {
+  const pallet = document.createElement('div')
+  pallet.classList.add(CLASS_NAMES.base)
+  pallet.style.display = 'none'
+
+  const title = document.createElement('p')
+  title.classList.add(CLASS_NAMES.title)
+  pallet.appendChild(title)
+
+  const titleText = document.createElement('span')
+  titleText.classList.add(CLASS_NAMES.titleText)
+  title.appendChild(titleText)
+
+  const lockIcon = document.createElement('span')
+  lockIcon.classList.add(CLASS_NAMES.lockIcon)
+  lockIcon.innerText = 'locked'
+  title.appendChild(lockIcon)
+
+  const buttonContainer = document.createElement('div')
+  buttonContainer.classList.add(CLASS_NAMES.buttons)
+  buttonContainer.classList.add(CLASS_NAMES.hideWhenLocked)
+  pallet.appendChild(buttonContainer)
+
+  const addButton = document.createElement('span')
+  addButton.classList.add(CLASS_NAMES.button)
+  addButton.classList.add(CLASS_NAMES.buttonAdd)
+  addButton.setAttribute('title', 'Add new type')
+  buttonContainer.appendChild(addButton)
+
+  const readButton = document.createElement('span')
+  readButton.classList.add(CLASS_NAMES.button)
+  readButton.classList.add(CLASS_NAMES.buttonRead)
+  readButton.setAttribute('title', 'Import')
+  buttonContainer.appendChild(readButton)
+
+  const writeButton = document.createElement('span')
+  writeButton.classList.add(CLASS_NAMES.button)
+  writeButton.classList.add(CLASS_NAMES.buttonWrite)
+  writeButton.setAttribute('title', 'Upload')
+  buttonContainer.appendChild(writeButton)
+
+  pallet.appendChild(document.createElement('table'))
 
   return pallet
 }
