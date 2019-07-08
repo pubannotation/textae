f049879aa0c49007560385c91c08d1c389f35cc5
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-08 20:06:17 +0900

    refactor: Make TypeContainer/Container class

--- src/lib/editor/start/TypeContainer/Container/index.js
+++ src/lib/editor/start/TypeContainer/Container/index.js
@@ -10,76 +10,110 @@ import getDefaultTypeAutomatically from './getDefaultTypeAutomatically'
 import getLabelOrColor from './getLabelOrColor'
 import getSortedIds from './getSortedIds'
 
-export default function(getAllInstanceFunc, defaultColor) {
-  let definedTypes = new Map(),
-    defaultType = null,
-    isSetDefaultTypeManually = false
+export default class extends EventEmitter {
+  constructor(getAllInstanceFunc, defaultColor) {
+    super()
+    this.definedTypes = new Map()
+    this.defaultType = null
+    this.isSetDefaultTypeManually = false
+    this.getAllInstanceFunc = getAllInstanceFunc
+    this.defaultColor = defaultColor
+  }
 
-  const emitter = new EventEmitter(),
-    api = {
-      setDefinedType: (newType) => {
-        if (typeof newType.color === 'undefined') {
-          let forwardMatchColor = api.getColor(newType.id)
-          if (forwardMatchColor !== defaultColor) {
-            newType.color = forwardMatchColor
-          }
-        }
-
-        if (typeof newType.label === 'undefined') {
-          let forwardMatchLabel = api.getLabel(newType.id)
-          if (forwardMatchLabel) {
-            newType.label = forwardMatchLabel
-          }
-        }
-
-        definedTypes.set(newType.id, newType)
-        emitter.emit('type.change', newType.id)
-      },
-      setDefinedTypes: (newDefinedTypes) => definedTypes = new Map(newDefinedTypes.reduce((a, b) => {
-        a.push([b.id, b])
-        return a
-      }, [])),
-      getDefinedType: (id) => Object.assign({}, definedTypes.get(id)),
-      getDefinedTypes: () => {
-        const ret = []
-        for (let id of definedTypes.keys()) {
-          ret.push(definedTypes.get(id))
-        }
-
-        return ret
-      },
-      changeDefinedType: (id, newType) => {
-        definedTypes.set(newType.id, newType)
-        emitter.emit('type.change', newType.id)
-      },
-      setDefaultType: (id) => {
-        console.assert(id, 'id is necessary!')
-        defaultType = id
-        setDefaultTypeToDefinedTypes(definedTypes, id)
-        isSetDefaultTypeManually = true
-      },
-      countTypeUse: () => countTypeUse(getAllInstanceFunc),
-      getDefaultType: () => {
-        if (!isSetDefaultTypeManually) {
-          const id = getDefaultTypeAutomatically(getAllInstanceFunc)
-          defaultType = id
-          setDefaultTypeToDefinedTypes(definedTypes, id)
-        }
-
-        return defaultType
-      },
-      getDefaultPred: () => DEFAULTS.pred,
-      getDefaultValue: () => DEFAULTS.value,
-      getDefaultColor: () => defaultColor,
-      getColor: (id, dismissForwardMatch = false) => getLabelOrColor('color', definedTypes, id, defaultColor, dismissForwardMatch),
-      getLabel: (id, dismissForwardMatch = false) => getLabelOrColor('label', definedTypes, id, undefined, dismissForwardMatch),
-      getUri: (id) => uri.getUrlMatches(id) ? id : undefined,
-      getSortedIds: () => getSortedIds(countTypeUse(getAllInstanceFunc)),
-      remove: (id) => {
-        definedTypes.delete(id)
-        emitter.emit('type.change', id)
+  setDefinedType(newType) {
+    if (typeof newType.color === 'undefined') {
+      let forwardMatchColor = this.getColor(newType.id)
+      if (forwardMatchColor !== this.defaultColor) {
+        newType.color = forwardMatchColor
       }
     }
 
-  return Object.assign(emitter, api)
+    if (typeof newType.label === 'undefined') {
+      let forwardMatchLabel = this.getLabel(newType.id)
+      if (forwardMatchLabel) {
+        newType.label = forwardMatchLabel
+      }
+    }
+
+    this.definedTypes.set(newType.id, newType)
+    super.emit('type.change', newType.id)
+  }
+
+  setDefinedTypes(newDefinedTypes) {
+    this.definedTypes = new Map(newDefinedTypes.reduce((a, b) => {
+      a.push([b.id, b])
+      return a
+    }, []))
+  }
+
+  getDefinedType(id) {
+    return Object.assign({}, this.definedTypes.get(id))
+  }
+
+  getDefinedTypes() {
+    const ret = []
+    for (let id of this.definedTypes.keys()) {
+      ret.push(this.definedTypes.get(id))
+    }
+
+    return ret
+  }
+
+  changeDefinedType(id, newType) {
+    this.definedTypes.set(newType.id, newType)
+    super.emit('type.change', newType.id)
+  }
+
+  setDefaultType(id) {
+    console.assert(id, 'id is necessary!')
+    this.defaultType = id
+    setDefaultTypeToDefinedTypes(this.definedTypes, id)
+    this.isSetDefaultTypeManually = true
+  }
+
+  countTypeUse() {
+    return countTypeUse(this.getAllInstanceFunc)
+  }
+
+  getDefaultType() {
+    if (!this.isSetDefaultTypeManually) {
+      const id = getDefaultTypeAutomatically(this.getAllInstanceFunc)
+      this.defaultType = id
+      setDefaultTypeToDefinedTypes(this.definedTypes, id)
+    }
+
+    return this.defaultType
+  }
+
+  getDefaultPred() {
+    return DEFAULTS.pred
+  }
+
+  getDefaultValue() {
+    return DEFAULTS.value
+  }
+
+  getDefaultColor() {
+    return this.defaultColor
+  }
+  getColor(id, dismissForwardMatch = false) { 
+    return getLabelOrColor('color', this.definedTypes, id, this.defaultColor, dismissForwardMatch)
+  }
+
+  getLabel(id, dismissForwardMatch = false) {
+    return getLabelOrColor('label', this.definedTypes, id, undefined, dismissForwardMatch)
+  }
+
+  getUri(id) {
+    return uri.getUrlMatches(id) ? id : undefined
+  }
+
+  getSortedIds() {
+    return getSortedIds(countTypeUse(this.getAllInstanceFunc))
+  }
+
+  remove(id) {
+    this.definedTypes.delete(id)
+    super.emit('type.change', id)
+  }
 }
