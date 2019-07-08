5207e32eab0accb7c85a7366201028d6f59b8b6a
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-08 11:50:00 +0900

    refactor: Fix code style

--- src/lib/component/Pallet/Component/bindEventHandlers.js
+++ src/lib/component/Pallet/Component/bindEventHandlers.js
@@ -6,37 +6,37 @@ import triggerUpdatePallet from './triggerUpdatePallet'
 import openEditTypeDialog from './openEditTypeDialog'
 
 export default function(pallet, elementEditor, typeContainer, editor, autocompletionWs, command) {
-  delegate(pallet, '.' + CLASS_NAMES.buttonAdd, 'click', (e) => {
+  delegate(pallet, `.${CLASS_NAMES.buttonAdd}`, 'click', (e) => {
     openCreateTypeDialog(elementEditor, e, typeContainer, editor, autocompletionWs)
   })
 
-  delegate(pallet, '.' + CLASS_NAMES.buttonRead, 'click', (e) => {
+  delegate(pallet, `.${CLASS_NAMES.buttonRead}`, 'click', (e) => {
     editor.api.handlePalletClick('textae.pallet.button.read.click')
   })
 
-  delegate(pallet, '.' + CLASS_NAMES.buttonWrite, 'click', (e) => {
+  delegate(pallet, `.${CLASS_NAMES.buttonWrite}`, 'click', (e) => {
     editor.api.handlePalletClick('textae.pallet.button.write.click')
   })
 
-  delegate(pallet, '.' + CLASS_NAMES.label, 'click', (e) => {
+  delegate(pallet, `.${CLASS_NAMES.label}`, 'click', (e) => {
     console.log('hi')
     const commands = elementEditor.getHandlerForPallet().changeTypeOfSelectedElement(e.delegateTarget.id)
     command.invoke(commands, ['annotation'])
     triggerUpdatePallet(editor)
   })
 
-  delegate(pallet, '.' + CLASS_NAMES.selectAll, 'click', (e) => {
+  delegate(pallet, `.${CLASS_NAMES.selectAll}`, 'click', (e) => {
     if (!checkButtonEnable(e.target)) {
       return
     }
     elementEditor.getHandlerForPallet().selectAllByLabel(e.delegateTarget.getAttribute('data-id'))
   })
 
-  delegate(pallet, '.' + CLASS_NAMES.editType, 'click', (e) => {
+  delegate(pallet, `.${CLASS_NAMES.editType}`, 'click', (e) => {
     openEditTypeDialog(elementEditor, e, editor, autocompletionWs)
   })
 
-  delegate(pallet, '.' + CLASS_NAMES.remove, 'click', (e) => {
+  delegate(pallet, `.${CLASS_NAMES.remove}`, 'click', (e) => {
     if (!checkButtonEnable(e.target)) {
       return
     }
