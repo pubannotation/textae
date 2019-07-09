6f2b61c22db3c81e263078038004e45fe49bbfe8
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-09 21:28:10 +0900

    refactor: Fix code style

--- src/lib/editor/start/Command/Factory/TypeCreateCommand.js
+++ src/lib/editor/start/Command/Factory/TypeCreateCommand.js
@@ -5,16 +5,15 @@ import TypeRemoveCommand from './TypeRemoveCommand'
 class TypeCreateCommand extends BaseCommand {
   constructor(editor, typeContainer, newType) {
     super(function() {
-      let revertDefaultTypeId
-
       Object.keys(newType).forEach((key) => {
-        if (newType[key] === '' || (key === 'default' && !newType[key])) {
+        if (newType[key] === '' || key === 'default' && !newType[key]) {
           delete newType[key]
         }
       })
       typeContainer.setDefinedType(newType)
 
       // manage default type
+      let revertDefaultTypeId
       if (newType.default) {
         // remember the current default, because revert command will not understand what type was it.
         revertDefaultTypeId = typeContainer.getDefaultType()
@@ -24,8 +23,7 @@ class TypeCreateCommand extends BaseCommand {
       this.revert = () => new TypeRemoveCommand(editor, typeContainer, newType, revertDefaultTypeId)
 
       editor.eventEmitter.emit('textae.pallet.update')
-      commandLog('create a new type:' + JSON.stringify(newType)
-        + ', default is `' + typeContainer.getDefaultType() + '`')
+      commandLog(`create a new type:${JSON.stringify(newType)}, default is ${ typeContainer.getDefaultType() }`)
     })
   }
 }
