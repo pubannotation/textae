4bafd5819af3171d620c277c8c771a70a99be0fb
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-09 19:32:40 +0900

    refactor: Fix code style

--- src/lib/editor/start/View/Renderer/RelationRenderer/makeJsPlumbInstance.js
+++ src/lib/editor/start/View/Renderer/RelationRenderer/makeJsPlumbInstance.js
@@ -1,7 +1,3 @@
-// global.jQuery = require("jquery");
-// jsPlumb = require("js-plumb");
-// require("jsplumb");
-
 export default function(container) {
   const newInstance = jsPlumb.getInstance({
     ConnectionsDetachable: false,
