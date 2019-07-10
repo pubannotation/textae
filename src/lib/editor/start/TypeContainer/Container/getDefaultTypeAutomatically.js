62db94b04716fd228f3050a91fa51cbdbd74dced
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-10 18:04:20 +0900

    refactor: Fix code style

--- src/lib/editor/start/TypeContainer/Container/getDefaultTypeAutomatically.js
+++ src/lib/editor/start/TypeContainer/Container/getDefaultTypeAutomatically.js
@@ -1,22 +1,22 @@
-import countTypeUse from './createCountMap'
+import createCountMap from './createCountMap'
 
 export default function(instances) {
-  const countMap = countTypeUse(instances)
+  const countMap = createCountMap(instances)
 
-  if (!countMap || countMap.size === 0) {
+  if (countMap.size === 0) {
     return 'something'
-  } else {
-    const countMapEntries = [...countMap.entries()].sort()
-
-    const {mostCommonlyUsedType} = countMapEntries.reduce(({max, mostCommonlyUsedType}, [type, useCount]) => {
-      if (useCount > max) {
-        max = useCount
-        mostCommonlyUsedType = type
-      }
-
-      return {max, mostCommonlyUsedType}
-    }, {max: 0, mostCommonlyUsedType: [countMapEntries][0]})
-
-    return mostCommonlyUsedType
   }
+
+  const countMapEntries = [...countMap.entries()].sort()
+
+  const {mostCommonlyUsedType} = countMapEntries.reduce(({max, mostCommonlyUsedType}, [type, useCount]) => {
+    if (useCount > max) {
+      max = useCount
+      mostCommonlyUsedType = type
+    }
+
+    return {max, mostCommonlyUsedType}
+  }, {max: 0, mostCommonlyUsedType: [countMapEntries][0]})
+
+  return mostCommonlyUsedType
 }
