b977b9ef29d7a55a2abe75f74adf80c98e1fd1c4
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-10 15:31:34 +0900

    refactor: Fix code style

--- src/lib/component/SettingDialog/create.js
+++ src/lib/component/SettingDialog/create.js
@@ -4,29 +4,29 @@ import $ from 'jquery'
 import _ from 'underscore'
 
 const CONTENT = `
-    <div class="textae-editor__setting-dialog">
-        <div>
-            <label class="textae-editor__setting-dialog__label">Type Gap</label>
-            <input type="number" class="textae-editor__setting-dialog__type-gap type-gap" step="1" min="0" max="5">
-        </div>
-        <div>
-            <label class="textae-editor__setting-dialog__label">Line Height</label>
-            <input type="number" class="textae-editor__setting-dialog__line-height line-height" step="1" min="50" max="500">
-            px
-        </div>
-        <div>
-            <label class="textae-editor__setting-dialog__label">Lock Edit Config</label>
-            <input type="checkbox" class="textae-editor__setting-dialog__lock-config lock-config">
-        </div>
-        <div>
-             <label class="textae-editor__setting-dialog__label">Reset Hidden Message Boxes</label>
-            <input type="button" class="textae-editor__setting-dialog__reset-hidden-message-boxes reset-hidden-message-boxes" value="Reset">
-        </div>
-    </div>
+  <div class="textae-editor__setting-dialog">
+      <div>
+          <label class="textae-editor__setting-dialog__label">Type Gap</label>
+          <input type="number" class="textae-editor__setting-dialog__type-gap type-gap" step="1" min="0" max="5">
+      </div>
+      <div>
+          <label class="textae-editor__setting-dialog__label">Line Height</label>
+          <input type="number" class="textae-editor__setting-dialog__line-height line-height" step="1" min="50" max="500">
+          px
+      </div>
+      <div>
+          <label class="textae-editor__setting-dialog__label">Lock Edit Config</label>
+          <input type="checkbox" class="textae-editor__setting-dialog__lock-config lock-config">
+      </div>
+      <div>
+            <label class="textae-editor__setting-dialog__label">Reset Hidden Message Boxes</label>
+          <input type="button" class="textae-editor__setting-dialog__reset-hidden-message-boxes reset-hidden-message-boxes" value="Reset">
+      </div>
+  </div>
 `
 
 export default function(editor, displayInstance) {
-  let $content = $(CONTENT)
+  const $content = $(CONTENT)
 
   bind($content, editor, displayInstance)
 
@@ -57,7 +57,7 @@ function bind($content, editor, displayInstance) {
 }
 
 function bindChangeTypeGap($content, editor, displayInstance) {
-  let onTypeGapChange = debounce300(
+  const onTypeGapChange = debounce300(
     (e) => {
       displayInstance.changeTypeGap(Number(e.target.value))
       updateLineHeight(editor, $content)
@@ -73,7 +73,7 @@ function bindChangeTypeGap($content, editor, displayInstance) {
 }
 
 function bindChangeLineHeight($content, editor) {
-  let onLineHeightChange = debounce300(
+  const onLineHeightChange = debounce300(
     (e) => {
       lineHeight.set(editor[0], e.target.value)
       redrawAllEditor()
@@ -89,7 +89,7 @@ function bindChangeLineHeight($content, editor) {
 }
 
 function bindChangeLockConfig($content, editor) {
-  let onChangeLockConfig = debounce300(
+  const onChangeLockConfig = debounce300(
     (e) => {
       if (e.target.checked) {
         editor.eventEmitter.emit('textae.config.lock')
@@ -108,7 +108,7 @@ function bindChangeLockConfig($content, editor) {
 }
 
 function bindClickResetHiddenMessageBoxes($content, editor) {
-  let onClickResetHiddenMessageBoxes = () => {
+  const onClickResetHiddenMessageBoxes = () => {
     editor.eventEmitter.emit('textae.message-box.show')
   }
 
@@ -123,8 +123,4 @@ function redrawAllEditor() {
 
 function debounce300(func) {
   return _.debounce(func, 300)
-}
-
-function sixteenTimes(val) {
-  return val * 16
 }
