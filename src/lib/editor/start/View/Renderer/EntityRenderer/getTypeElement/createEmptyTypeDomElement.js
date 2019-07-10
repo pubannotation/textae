b4d7039ad1c8997ce31084489ce391692dfa15ca
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-10 20:42:30 +0900

    refactor: Remove ineffective function call

--- src/lib/editor/start/View/Renderer/EntityRenderer/getTypeElement/createEmptyTypeDomElement.js
+++ src/lib/editor/start/View/Renderer/EntityRenderer/getTypeElement/createEmptyTypeDomElement.js
@@ -1,5 +1,4 @@
 import idFactory from '../../../../../idFactory'
-import setLabelToTypeLabel from '../setLabelToTypeLabel'
 import $ from 'jquery'
 
 // A Type element has an entity_pane elment that has a label and will have entities.
@@ -11,20 +10,15 @@ export default function(namespace, typeContainer, spanId, type) {
     .attr('id', 'P-' + typeId)
     .addClass('textae-editor__entity-pane')
 
-
-    // The label over the span.
+  // The label over the span.
   const $typeLabel = $('<div>')
     .addClass('textae-editor__type-label')
-    .css({
-      'background-color': typeContainer.getColor(type),
-    })
+    .attr({tabindex: 0})
 
   const addButtonElement = document.createElement('div')
   addButtonElement.classList.add('textae-editor__attribute-button')
   addButtonElement.classList.add('textae-editor__attribute-button--add')
   addButtonElement.setAttribute('title', 'Add a new attribute to this entity.')
-  $typeLabel[0].setAttribute('tabindex', 0)
-  setLabelToTypeLabel($typeLabel[0], namespace, typeContainer, type)
 
   return $('<div>')
     .attr('id', typeId)
