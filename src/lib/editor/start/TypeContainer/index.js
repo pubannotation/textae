3894d593beec31e70cb737147ce7e63563972743
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-11 17:16:25 +0900

    refactor: Use ES6 class syntax

--- src/lib/editor/start/TypeContainer/index.js
+++ src/lib/editor/start/TypeContainer/index.js
@@ -3,31 +3,44 @@ import EntityContainer from './EntityContainer'
 import Container from './Container'
 import setContainerDefinedTypes from './setContainerDefinedTypes'
 
-export default function(annotationData) {
-  const lockStateObservable = new Observable(false)
+export default class {
+  constructor(annotationData) {
+    this.lockStateObservable = new Observable(false)
+    this.entityContainer = new EntityContainer(() => annotationData.entity.all(), '#77DDDD', this.lockStateObservable)
+    this.relationContaier = new Container(() => annotationData.relation.all(), '#555555', this.lockStateObservable)
+  }
 
-  const entityContainer = new EntityContainer(() => annotationData.entity.all(), '#77DDDD', lockStateObservable)
-  const relationContaier = new Container(() => annotationData.relation.all(), '#555555', lockStateObservable)
+  isLock() {
+    return this.lockStateObservable()
+  }
 
-  return {
-    isLock: lockStateObservable,
-    lockEdit() {
-      lockStateObservable.set(true)
-    },
-    unlockEdit() {
-      lockStateObservable.set(false)
-    },
-    entity: entityContainer,
-    setDefinedEntityTypes: (newDefinedTypes) => setContainerDefinedTypes(entityContainer, newDefinedTypes),
-    relation: relationContaier,
-    setDefinedRelationTypes: (newDefinedTypes) => setContainerDefinedTypes(relationContaier, newDefinedTypes),
-    getConfig: () => {
-      return {
-        'entity types': entityContainer.getDefinedTypes(),
-        // 'attribute types': attributeContainer.getDefinedTypes(),
-        'relation types': relationContaier.getDefinedTypes()
-      }
+  lockEdit() {
+    this.lockStateObservable.set(true)
+  }
+  unlockEdit() {
+    this.lockStateObservable.set(false)
+  }
+
+  get entity() {
+    return this.entityContainer
+  }
+
+  setDefinedEntityTypes(newDefinedTypes) {
+    return setContainerDefinedTypes(this.entityContainer, newDefinedTypes)
+  }
+
+  get relation() {
+    return this.relationContaier
+  }
+
+  setDefinedRelationTypes(newDefinedTypes) {
+    return setContainerDefinedTypes(this.relationContaier, newDefinedTypes)
+  }
+
+  getConfig() {
+    return {
+      'entity types': this.entityContainer.getDefinedTypes(),
+      'relation types': this.relationContaier.getDefinedTypes()
     }
   }
 }
-
