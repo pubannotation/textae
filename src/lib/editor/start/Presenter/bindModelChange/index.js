e2cbaa3126b7beb205e9ce3ad4ec9209a5dc89b4
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-16 17:53:05 +0900

    refactor: Fix code style

--- src/lib/editor/start/Presenter/bindModelChange/index.js
+++ src/lib/editor/start/Presenter/bindModelChange/index.js
@@ -4,7 +4,7 @@ import updateWritable from './updateWritable'
 
 export default function(annotationData, writable, editMode, mode) {
   annotationData
-    .on('all.change', (annotationData, multitrack, reject) => updateWritable(multitrack, reject, writable))
+    .on('all.change', (_, multitrack, reject) => updateWritable(multitrack, reject, writable))
     .on('all.change', (annotationData, multitrack) => {
       if (mode !== 'edit') {
         changeEditMode.forView(editMode, annotationData)
