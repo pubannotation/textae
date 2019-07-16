25af8284ec2d62c58c5d03739e22d049d25230a9
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-16 14:02:01 +0900

    refactor: Fix code style

--- src/lib/buttonModel/ButtonController/UpdateButtonState.js
+++ src/lib/buttonModel/ButtonController/UpdateButtonState.js
@@ -1,23 +1,12 @@
 export default function(selectionModel, buttonEnableStates, clipBoard) {
   // Short cut name
-  const hasCopy = () => clipBoard.clipBoard.length > 0,
-    eOrR = () => selectionModel.entity.some() || selectionModel.relation.some()
+  const hasCopy = () => clipBoard.clipBoard.length > 0
+  const eOrR = () => selectionModel.entity.some() || selectionModel.relation.some()
 
   // Check all associated anntation elements.
   // For exapmle, it should be that buttons associate with entitis is enable,
   // when deselect the span after select a span and an entity.
-  const predicates = new Map(
-    [
-      ['replicate', () => Boolean(selectionModel.span.single())],
-      ['entity', () => selectionModel.span.some()],
-      ['delete', () => selectionModel.some()], // It works well on relation-edit-mode if relations are deselect brefore an entity is select.
-      ['copy', () => selectionModel.span.some() || selectionModel.entity.some()],
-      ['paste', () => hasCopy() && selectionModel.span.some()],
-      ['change-label', eOrR],
-      ['negation', eOrR],
-      ['speculation', eOrR]
-    ]
-  )
+  const predicates = createPredicates(selectionModel, hasCopy, eOrR)
 
   return function(buttons) {
     for (let buttonName of buttons) {
@@ -25,4 +14,17 @@ export default function(selectionModel, buttonEnableStates, clipBoard) {
       buttonEnableStates.set(buttonName, predicate())
     }
   }
+}
+
+function createPredicates(selectionModel, hasCopy, eOrR) {
+  return new Map([
+    ['replicate', () => Boolean(selectionModel.span.single())],
+    ['entity', () => selectionModel.span.some()],
+    ['delete', () => selectionModel.some()],
+    ['copy', () => selectionModel.span.some() || selectionModel.entity.some()],
+    ['paste', () => hasCopy() && selectionModel.span.some()],
+    ['change-label', eOrR],
+    ['negation', eOrR],
+    ['speculation', eOrR]
+  ])
 }
