6b87010b10eadfb4879e8df628b3bdd2a080c16e
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-18 18:35:48 +0900

    refactor: Fix code style

--- src/lib/editor/start/Presenter/createEventMap/handlers/SelectHandler/index.js
+++ src/lib/editor/start/Presenter/createEventMap/handlers/SelectHandler/index.js
@@ -4,11 +4,11 @@ import {
 }
 from '../../../../getNextElement'
 
-const SPAN_CLASS = 'textae-editor__span',
-  GRID_CLASS = 'textae-editor__grid',
-  LABEL_CLASS = 'textae-editor__type-label',
-  TYPE_CLASS = 'textae-editor__type',
-  ENTINY_CLASS = 'textae-editor__entity'
+const SPAN_CLASS = 'textae-editor__span'
+const GRID_CLASS = 'textae-editor__grid'
+const LABEL_CLASS = 'textae-editor__type-label'
+const TYPE_CLASS = 'textae-editor__type'
+const ENTINY_CLASS = 'textae-editor__entity'
 
 export default function(editorDom, selectionModel) {
   return {
@@ -28,25 +28,25 @@ export default function(editorDom, selectionModel) {
 }
 
 function selectLeft(editorDom, selectionModel, shiftKey) {
-  let selectNext = selectLeftFunc(editorDom, selectionModel, shiftKey)
+  const selectNext = selectLeftFunc(editorDom, selectionModel, shiftKey)
   selectNext()
 }
 
 function selectRight(editorDom, selectionModel, shiftKey) {
-  let selectNext = selectRightFunc(editorDom, selectionModel, shiftKey)
+  const selectNext = selectRightFunc(editorDom, selectionModel, shiftKey)
   selectNext()
 }
 
 function selectUpperLayer(editorDom, selectionModel) {
   // When one span is selected.
-  let spanId = selectionModel.span.single()
+  const spanId = selectionModel.span.single()
   if (spanId) {
     selectFirstEntityLabelOfSpan(selectionModel, spanId)
     return
   }
 
   // When one entity label is selected.
-  let labels = selectSelected(editorDom, LABEL_CLASS)
+  const labels = selectSelected(editorDom, LABEL_CLASS)
   if (labels.length === 1) {
     selectFirstEntityOfEntityLabel(selectionModel, labels[0])
     return
@@ -55,14 +55,14 @@ function selectUpperLayer(editorDom, selectionModel) {
 
 function selectLowerLayer(editorDom, selectionModel) {
   // When one entity label is selected.
-  let labels = selectSelected(editorDom, LABEL_CLASS)
+  const labels = selectSelected(editorDom, LABEL_CLASS)
   if (labels.length === 1) {
     selectSpanOfEntityLabel(selectionModel, labels[0])
     return
   }
 
   // When one entity is selected.
-  let selectedEnities = selectSelected(editorDom, ENTINY_CLASS)
+  const selectedEnities = selectSelected(editorDom, ENTINY_CLASS)
   if (selectedEnities.length === 1) {
     selectLabelOfEntity(selectionModel, selectedEnities[0])
     return
@@ -70,37 +70,37 @@ function selectLowerLayer(editorDom, selectionModel) {
 }
 
 function selectLeftFunc(editorDom, selectionModel, shiftKey) {
-  let getNextFunc = (selected) => getLeftElement(editorDom, selected[0])
+  const getNextFunc = (selected) => getLeftElement(editorDom, selected[0])
 
   return selectNextFunc(editorDom, selectionModel, shiftKey, getNextFunc)
 }
 
 function selectRightFunc(editorDom, selectionModel, shiftKey) {
-  let getNextFunc = (selected) => getRightElement(editorDom, selected[selected.length - 1])
+  const getNextFunc = (selected) => getRightElement(editorDom, selected[selected.length - 1])
 
   return selectNextFunc(editorDom, selectionModel, shiftKey, getNextFunc)
 }
 
 function selectNextFunc(editorDom, selectionModel, shiftKey, getNextFunc) {
-  let selectedSpans = selectSelected(editorDom, SPAN_CLASS)
+  const selectedSpans = selectSelected(editorDom, SPAN_CLASS)
 
   if (selectedSpans.length) {
-    let nextElement = getNextFunc(selectedSpans)
+    const nextElement = getNextFunc(selectedSpans)
     return () => selectionModel.selectSpan(nextElement, shiftKey)
   }
 
-  let selectedEntityLabel = selectSelected(editorDom, LABEL_CLASS)
+  const selectedEntityLabel = selectSelected(editorDom, LABEL_CLASS)
 
   if (selectedEntityLabel.length) {
-    let nextElement = getNextFunc(selectedEntityLabel)
+    const nextElement = getNextFunc(selectedEntityLabel)
     return () => selectionModel.selectEntityLabel(nextElement, shiftKey)
   }
 
-  let selectedEntities = selectSelected(editorDom, ENTINY_CLASS)
+  const selectedEntities = selectSelected(editorDom, ENTINY_CLASS)
 
   if (selectedEntities.length) {
     // return a select entity function
-    let nextElement = getNextFunc(selectedEntities)
+    const nextElement = getNextFunc(selectedEntities)
     return () => selectionModel.selectEntity(nextElement, shiftKey)
   }
 
@@ -112,19 +112,19 @@ function selectSelected(dom, className) {
 }
 
 function selectFirstEntityLabelOfSpan(selectionModel, spanId) {
-  let grid = document.querySelector(`#G${spanId}`)
+  const grid = document.querySelector(`#G${spanId}`)
 
   // Block span has no grid.
   if (grid) {
-    let type = grid.querySelector(`.${TYPE_CLASS}`),
-      label = type.querySelector(`.${LABEL_CLASS}`)
+    const type = grid.querySelector(`.${TYPE_CLASS}`)
+    const label = type.querySelector(`.${LABEL_CLASS}`)
 
     selectionModel.selectEntityLabel(label)
   }
 }
 
 function selectFirstEntityOfEntityLabel(selectionModel, label) {
-  let pane = label.previousElementSibling
+  const pane = label.previousElementSibling
 
   selectionModel.selectEntity(pane.children[0])
 }
@@ -132,7 +132,7 @@ function selectFirstEntityOfEntityLabel(selectionModel, label) {
 function selectSpanOfEntityLabel(selectionModel, label) {
   console.assert(label, 'A label MUST exists.')
 
-  let spanId = label.closest(`.${GRID_CLASS}`).id.substring(1)
+  const spanId = label.closest(`.${GRID_CLASS}`).id.substring(1)
   selectionModel.selectSingleSpanById(spanId)
 }
 
