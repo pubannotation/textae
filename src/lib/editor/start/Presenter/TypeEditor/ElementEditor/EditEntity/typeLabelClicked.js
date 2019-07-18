a10a5cd618f172077d0b3b9d0b4f1c608f45cd7e
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-18 19:05:09 +0900

    refactor: Fix code style

--- src/lib/editor/start/Presenter/TypeEditor/ElementEditor/EditEntity/typeLabelClicked.js
+++ src/lib/editor/start/Presenter/TypeEditor/ElementEditor/EditEntity/typeLabelClicked.js
@@ -1,11 +1,11 @@
 import selectEntities from './selectEntities'
 
 export default function(selectionModel, e) {
-  let typeLabel = e.target,
-    isTypeLabel = typeLabel.classList.contains('textae-editor__type-label')
+  const typeLabel = e.target
+  const isTypeLabel = typeLabel.classList.contains('textae-editor__type-label')
 
   if (isTypeLabel) {
-    let entities = e.target.previousElementSibling.children
+    const entities = e.target.previousElementSibling.children
 
     return selectEntities(selectionModel, e.ctrlKey || e.metaKey, typeLabel, entities)
   }
