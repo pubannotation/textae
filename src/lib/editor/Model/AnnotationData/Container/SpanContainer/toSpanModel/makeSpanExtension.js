dae8ac62852a489b91450ce894440c9961cf7a78
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-19 16:27:28 +0900

    refactor: Fix code style

--- src/lib/editor/Model/AnnotationData/Container/SpanContainer/toSpanModel/makeSpanExtension.js
+++ src/lib/editor/Model/AnnotationData/Container/SpanContainer/toSpanModel/makeSpanExtension.js
@@ -1,62 +1,62 @@
-import _ from 'underscore'
 import idFactory from '../../../../../idFactory'
 import toStringOnlyThis from './toStringOnlyThis'
 
 export default function(emitter) {
   return {
     // for debug. print myself only.
-    toStringOnlyThis: function() {
+    toStringOnlyThis() {
       return toStringOnlyThis(this, emitter)
     },
     // for debug. print with children.
-    toString: function(depth) {
+    toString(depth) {
       depth = depth || 1 // default depth is 1
-      let childrenString = this.children && this.children.length > 0 ?
+
+      const childrenString = this.children && this.children.length > 0 ?
         "\n" + this.children.map(function(child) {
           return new Array(depth + 1).join("\t") + child.toString(depth + 1)
         }).join("\n") : ""
+
       return this.toStringOnlyThis() + childrenString
     },
     // Get online for update is not grantieed.
-    getTypes: function() {
-      let spanId = this.id
+    getTypes() {
+      const spanId = this.id
+
       // Return an array of type like { id : "editor2__S1741_1755-1", name: "Negative_regulation", entities: ["E16", "E17"], attributes: ["A16", "A17"] }.
       return emitter.entity.all()
-        .filter(function(entity) {
-          return spanId === entity.span
-        })
-        .reduce(function(a, b) {
-          let typeId = idFactory.makeTypeId(b.span, b.type), type = a.filter(function(type) {
-            return type.id === typeId
-          }), attributes = emitter.attribute.all()
-            .filter(function(attribute) {
-              return attribute.subj === b.id
-            })
-            .map(function(attribute) {
-              return attribute.id
-            })
+        .filter((entity) => spanId === entity.span)
+        .reduce((array, entity) => {
+          const id = idFactory.makeTypeId(entity.span, entity.type)
+
+          const type = array.filter((type) => type.id === id)
+
+          const attributes = emitter.attribute.all()
+            .filter((attribute) => attribute.subj === entity.id)
+            .map((attribute) => attribute.id)
+
           if (type.length > 0) {
-            type[0].entities.push(b.id)
+            type[0].entities.push(entity.id)
           } else {
-            a.push({
-              id: typeId,
-              name: b.type,
-              entities: [b.id],
-              attributes: attributes
+            array.push({
+              id,
+              name: entity.type,
+              entities: [entity.id],
+              attributes
             })
           }
-          return a
+
+          return array
         }, [])
     },
-    getEntities: function() {
-      return _.flatten(this.getTypes().map(function(type) {
-        return type.entities
-      }))
+    getEntities() {
+      return this.getTypes()
+        .map((type) => type.entities)
+        .reduce((prev, current) => prev.concat(current), [])
     },
-    getAttributes: function() {
-      return _.flatten(this.getTypes().map(function(type) {
-        return type.attributes
-      }))
+    getAttributes() {
+      return this.getTypes()
+        .map((type) => type.attributes)
+        .reduce((prev, current) => prev.concat(current), [])
     }
   }
 }
