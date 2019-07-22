8bd655c6297e7d621e31f675327b3dc4f4b40446
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-22 17:52:40 +0900

    refactor: Fix code style

--- src/lib/editor/start/Command/Factory/spanRemoveCommand.js
+++ src/lib/editor/start/Command/Factory/spanRemoveCommand.js
@@ -8,13 +8,9 @@ import entityAndAssociatesRemoveCommand from './entityAndAssociatesRemoveCommand
 import executeCompositCommand from './executeCompositCommand'
 
 export default function(editor, annotationData, selectionModel, id) {
-  var removeSpan = new RemoveCommand(editor, annotationData, selectionModel, 'span', id),
-    removeEntity = _.flatten(annotationData.span.get(id).getTypes().map(function(type) {
-      return type.entities.map(function(entityId) {
-        return entityAndAssociatesRemoveCommand(editor, annotationData, selectionModel, entityId)
-      })
-    })),
-    subCommands = removeEntity.concat(removeSpan)
+  const removeEntity = _.flatten(annotationData.span.get(id).getTypes().map((type) => type.entities.map((entityId) => entityAndAssociatesRemoveCommand(editor, annotationData, selectionModel, entityId))))
+  const removeSpan = new RemoveCommand(editor, annotationData, selectionModel, 'span', id)
+  const subCommands = removeEntity.concat(removeSpan)
 
   return {
     execute: function() {
