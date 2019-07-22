30b94b3f3c869b5db93dda2204655d843c983050
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-22 20:26:40 +0900

    feature: can Edit and remove attributes when selecting entities

--- src/lib/editor/start/Selector/updateEntityTypeValues.js
+++ src/lib/editor/start/Selector/updateEntityTypeValues.js
@@ -3,7 +3,6 @@ import getEntitiesDomOfType from '../getEntitiesDomOfType'
 import getPaneDomOfType from '../../getPaneDomOfType'
 import SELECTED from "./SELECTED"
 
-// Select the typeValues if all entities is selected.
 export default function(editor, entityId) {
   console.assert(entityId, 'An entity id is necessary.')
   const entity = getEntityDom(editor[0], entityId)
@@ -13,12 +12,21 @@ export default function(editor, entityId) {
     const typePane = getPaneDomOfType(entity)
     const typeValues = typePane.closest('.textae-editor__type').querySelector('.textae-editor__type-values')
     const entities = getEntitiesDomOfType(entity)
+
+    // Select the typeValues if all entities is selected.
     if (entities.length === typePane.querySelectorAll(`.${SELECTED}`).length) {
       typeValues.classList.add(SELECTED)
       typePane.classList.add(SELECTED)
     } else {
       typeValues.classList.remove(SELECTED)
       typePane.classList.remove(SELECTED)
+    }
+
+    // You can click the attribute button when selecting at least one entity.
+    if (typePane.querySelectorAll(`.${SELECTED}`).length) {
+      typeValues.classList.add('textae-editor__type-values--some_entity_selected')
+    } else {
+      typeValues.classList.remove('textae-editor__type-values--some_entity_selected')
     }
   }
 }
