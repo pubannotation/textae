9dc822eb689bb38f6f97c8f788249a55444d5200
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-22 17:57:59 +0900

    refactor: Fix code style

--- src/lib/editor/start/Presenter/TypeEditor/ElementEditor/EditEntity/SelectSpan.js
+++ src/lib/editor/start/Presenter/TypeEditor/ElementEditor/EditEntity/SelectSpan.js
@@ -1,39 +1,32 @@
 import _ from 'underscore'
 
 export default function(editor, annotationData, selectionModel, typeDefinition) {
-  let getBlockEntities = function(spanId) {
-      return _.flatten(
-        annotationData.span.get(spanId)
-        .getTypes()
-        .filter(function(type) {
-          return typeDefinition.entity.isBlock(type.name)
-        })
-        .map(function(type) {
-          return type.entities
-        })
-      )
-    },
-    operateSpanWithBlockEntities = function(method, spanId) {
-      selectionModel.span[method](spanId)
-      if (editor.find('#' + spanId).hasClass('textae-editor__span--block')) {
-        getBlockEntities(spanId).forEach(selectionModel.entity[method])
-      }
-    },
-    selectSpanWithBlockEnities = _.partial(operateSpanWithBlockEntities, 'add'),
-    toggleSpanWithBlockEnities = _.partial(operateSpanWithBlockEntities, 'toggle')
+  const getBlockEntities = (spanId) => _.flatten(
+    annotationData.span.get(spanId)
+      .getTypes()
+      .filter((type) => typeDefinition.entity.isBlock(type.name))
+      .map((type) => type.entities)
+  )
 
-  return function(event) {
-    let firstId = selectionModel.span.single(),
-      target = event.target,
-      id = target.id
+  const operateSpanWithBlockEntities = (method, spanId) => {
+    selectionModel.span[method](spanId)
+    if (editor.find(`#${spanId}`).hasClass('textae-editor__span--block')) {
+      getBlockEntities(spanId).forEach(selectionModel.entity[method])
+    }
+  }
+
+  const selectSpanWithBlockEnities = _.partial(operateSpanWithBlockEntities, 'add')
+  const toggleSpanWithBlockEnities = _.partial(operateSpanWithBlockEntities, 'toggle')
+
+  return (event) => {
+    const firstId = selectionModel.span.single()
+    const target = event.target
+    const id = target.id
 
     if (event.shiftKey && firstId) {
       // select reange of spans.
       selectionModel.clear()
-      annotationData.span.range(firstId, id)
-        .forEach(function(spanId) {
-          selectSpanWithBlockEnities(spanId)
-        })
+      annotationData.span.range(firstId, id).forEach((spanId) => selectSpanWithBlockEnities(spanId))
     } else if (event.ctrlKey || event.metaKey) {
       toggleSpanWithBlockEnities(id)
     } else {
