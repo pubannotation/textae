db0b28a617d8b681db2aa2cbbc3b24889463b6c1
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-23 18:10:02 +0900

    refactor: Fix code style

---
+++ src/lib/editor/start/View/Renderer/RelationRenderer/connectorStrokeStyle/index.js
@@ -0,0 +1,11 @@
+import converseHEXinotRGBA from "./converseHEXinotRGBA"
+
+export default function(annotationData, typeDefinition, relationId) {
+    const type = annotationData.relation.get(relationId).type
+    const colorHex = typeDefinition.relation.getColor(type)
+
+    return {
+      lineWidth: 1,
+      strokeStyle: converseHEXinotRGBA(colorHex)
+    }
+  }
