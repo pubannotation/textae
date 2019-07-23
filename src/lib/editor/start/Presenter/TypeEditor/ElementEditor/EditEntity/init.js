78a970fa8f36f59c0e06d577a76baf9a46a5b7a2
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-23 13:09:54 +0900

    feature: Select all entities of type when clicking on a label or attributes

--- src/lib/editor/start/Presenter/TypeEditor/ElementEditor/EditEntity/init.js
+++ src/lib/editor/start/Presenter/TypeEditor/ElementEditor/EditEntity/init.js
@@ -1,6 +1,6 @@
 import spanClicked from './spanClicked'
 import bodyClicked from './bodyClicked'
-import typeLabelClicked from './typeLabelClicked'
+import typeValeusClicked from './typeValuesClicked'
 import entityClicked from './entityClicked'
 
 export default function(editor, cancelSelect, selectEnd, spanConfig, selectSpan, selectionModel) {
@@ -10,7 +10,7 @@ export default function(editor, cancelSelect, selectEnd, spanConfig, selectSpan,
   editor
     .on('click', '.textae-editor__body', (e) => bodyClicked(cancelSelect, selectEnd, spanConfig, e))
     .on('click', '.textae-editor__type', () => editor.focus())
-    .on('click', '.textae-editor__type-label', (e) => typeLabelClicked(selectionModel, e))
+    .on('click', '.textae-editor__type-values', (e) => typeValeusClicked(selectionModel, e))
     .on('click', '.textae-editor__entity', (e) => entityClicked(selectionModel, e))
 
   // To shrink a span listen the mouseup event.
---
+++ src/lib/editor/start/Presenter/TypeEditor/ElementEditor/EditEntity/typeValuesClicked.js
@@ -0,0 +1,7 @@
+import selectEntities from './selectEntities'
+import getEntitiesDomOfType from '../../../../getEntitiesDomOfType'
+
+export default function(selectionModel, e) {
+  const entities = getEntitiesDomOfType(e.target)
+  return selectEntities(selectionModel, e.ctrlKey || e.metaKey, e.target.closest('.textae-editor__type-values'), entities)
+}
