1d1677976d6957eabae96db971ae45e611a7c1e2
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-23 14:06:24 +0900

    refactor: Fix code style

--- src/lib/editor/start/Presenter/TypeEditor/changeLabelHandler.js
+++ src/lib/editor/start/Presenter/TypeEditor/changeLabelHandler.js
@@ -3,27 +3,26 @@ import EditLabelDialog from '../../../../component/EditLabelDialog'
 // An handler is get on runtime, because it is changed by the edit mode.
 export default function(editor, getHandler, autocompletionWs) {
   if (getHandler().getSelectedIdEditable().length > 0) {
-    const handler = getHandler(),
-      predicate = 'type',
-      value = handler.getSelectedType(),
-      done = (_, value, label) => {
-        let commands = []
+    const handler = getHandler()
+    const predicate = 'type'
+    const value = handler.getSelectedType()
+    const done = (_, value, label) => {
+      const commands = []
 
-        if (label) {
-          const command = handler.changeLabelOfId(value, label)
+      if (label) {
+        const command = handler.changeLabelOfId(value, label)
 
-          if (command) {
-            commands.push(command)
-          }
-        }
-
-        if (value) {
-          commands = commands.concat(handler.changeTypeOfSelectedElement(value))
-          handler.command.invoke(commands, ['annotation'])
+        if (command) {
+          commands.push(command)
         }
       }
 
-    let dialog = new EditLabelDialog(editor, handler.typeContainer, done, autocompletionWs)
+      if (value) {
+        handler.command.invoke(commands.concat(handler.changeTypeOfSelectedElement(value)), ['annotation'])
+      }
+    }
+
+    const dialog = new EditLabelDialog(editor, handler.typeContainer, done, autocompletionWs)
     dialog.update(predicate, value)
     dialog.open()
   }
