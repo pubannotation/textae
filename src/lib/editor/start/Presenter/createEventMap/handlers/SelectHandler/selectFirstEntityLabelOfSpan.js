b5af0e25b248776213c6f7b3f47dfa9e190d339d
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-23 12:00:33 +0900

    refactor: Remove unnecessary DOM element selection

--- src/lib/editor/start/Presenter/createEventMap/handlers/SelectHandler/selectFirstEntityLabelOfSpan.js
+++ src/lib/editor/start/Presenter/createEventMap/handlers/SelectHandler/selectFirstEntityLabelOfSpan.js
@@ -1,4 +1,4 @@
-import {TYPE_CLASS, LABEL_CLASS} from "./const"
+import {TYPE_CLASS} from "./const"
 
 export default function(selectionModel, spanId) {
   const grid = document.querySelector(`#G${spanId}`)
@@ -6,7 +6,6 @@ export default function(selectionModel, spanId) {
   // Block span has no grid.
   if (grid) {
     const type = grid.querySelector(`.${TYPE_CLASS}`)
-    const label = type.querySelector(`.${LABEL_CLASS}`)
-    selectionModel.selectEntityLabel(label)
+    selectionModel.selectEntityLabel(type)
   }
 }
