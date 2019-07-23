9649d80c6b9b60e0c49bc4489b2c21a36887be77
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-23 14:41:58 +0900

    refactor: Inline arguments into the function that do not need to be initialized outside the function

--- src/lib/component/dialog/EditDialog/create.js
+++ src/lib/component/dialog/EditDialog/create.js
@@ -2,7 +2,26 @@ import delegate from 'delegate'
 import EditorDialog from '../EditorDialog'
 import CLASS_NAMES from './className'
 
-export default function(editor, el, input1, input2, label, done) {
+const HTML = `
+<div class="${CLASS_NAMES.inputBox}">
+<label>Predicate:</label><br>
+<input class="${CLASS_NAMES.predicate}">
+</div>
+<div class="${CLASS_NAMES.inputBox}">
+<label>Value:<span></span></label><br>
+<input class="${CLASS_NAMES.value}">
+</div>`
+
+export default function(editor, done) {
+  const el = document.createElement('div')
+  el.classList.add(CLASS_NAMES.container)
+  el.innerHTML = HTML
+
+  const label = el.querySelector('span')
+  const input = el.querySelectorAll('input')
+  const input1 = input[0]
+  const input2 = input[1]
+
   const okHandler = () => {
     done(input1.value, input2.value, label.innerText)
     $dialog.close()
--- src/lib/component/dialog/EditDialog/index.js
+++ src/lib/component/dialog/EditDialog/index.js
@@ -1,25 +1,9 @@
-import CLASS_NAMES from './className'
 import create from './create'
 import update from './update'
 
 export default class {
   constructor(editor, done) {
-    const el = document.createElement('div')
-    el.classList.add(CLASS_NAMES.container)
-    el.innerHTML = `
-<div class="${CLASS_NAMES.inputBox}">
-  <label>Predicate:</label><br>
-  <input class="${CLASS_NAMES.predicate}">
-</div>
-<div class="${CLASS_NAMES.inputBox}">
-  <label>Value:<span></span></label><br>
-  <input class="${CLASS_NAMES.value}">
-</div>`
-
-    const label = el.querySelector('span')
-    const input = el.querySelectorAll('input')
-
-    this.$dialog = create(editor, el, input[0], input[1], label, done)
+    this.$dialog = create(editor, done)
   }
 
   update(predicate, value) {
