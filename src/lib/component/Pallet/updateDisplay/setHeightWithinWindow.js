008153117be6b59c0b8540d74192f0b0281752f8
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-24 13:27:39 +0900

    refactor: Remove debug print

--- src/lib/component/Pallet/updateDisplay/setHeightWithinWindow.js
+++ src/lib/component/Pallet/updateDisplay/setHeightWithinWindow.js
@@ -1,7 +1,6 @@
 export default function(pallet) {
   if (window.innerHeight - 2 <= pallet.offsetHeight) {
     const height = window.innerHeight - 2
-    console.log('hi',height)
     pallet.style.height = `${height}px`
   }
 }
