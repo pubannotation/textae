b5d5339adcfa3bef7635983a2fe055b3a061f0eb
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-07-29 13:08:14 +0900

    code-style: Fix code style

--- src/lib/editor/start/Presenter/createEventMap/handlers/ClipBoardHandler.js
+++ src/lib/editor/start/Presenter/createEventMap/handlers/ClipBoardHandler.js
@@ -1,40 +1,42 @@
 import _ from 'underscore'
 
-module.exports = function(command, annotationData, selectionModel, clipBoard) {
-  const copyEntities = function() {
-    // Unique Entities. Because a entity is deplicate When a span and thats entity is selected.
-    clipBoard.clipBoard = _.uniq(
-      (function getEntitiesFromSelectedSpan() {
-        return _.flatten(
-          selectionModel.span.all().map((spanId) => {
-            return annotationData.span.get(spanId).getEntities().map((e) => e.id)
-          })
-        )
-      })().concat(selectionModel.entity.all())
-    ).map((entityId) => {
-      // Map entities to types, because entities may be delete.
-      return annotationData.entity.get(entityId).type
-    })
+export default function(command, annotationData, selectionModel, clipBoard) {
+  return {
+    copyEntities: () => copyEntities(clipBoard, selectionModel, annotationData),
+    pasteEntities: () => pasteEntities(selectionModel, clipBoard, command)
   }
+}
 
-  const pasteEntities = function() {
-    // Make commands per selected spans from types in clipBoard.
-    const commands = _.flatten(
-      selectionModel.span.all().map((spanId) => {
-        return clipBoard.clipBoard.map((type) => {
-          return command.factory.entityCreateCommand({
-            span: spanId,
-            type
-          })
+// Unique Entities. Because a entity is deplicate When a span and thats entity is selected.
+function copyEntities(clipBoard, selectionModel, annotationData) {
+  clipBoard.clipBoard = _.uniq(
+    (function getEntitiesFromSelectedSpan() {
+      return _.flatten(
+        selectionModel.span.all().map((spanId) => {
+          return annotationData.span
+            .get(spanId)
+            .getEntities()
+            .map((e) => e.id)
+        })
+      )
+    })().concat(selectionModel.entity.all())
+  ).map((entityId) => {
+    // Map entities to types, because entities may be delete.
+    return annotationData.entity.get(entityId).type
+  })
+}
+
+// Make commands per selected spans from types in clipBoard.
+function pasteEntities(selectionModel, clipBoard, command) {
+  const commands = _.flatten(
+    selectionModel.span.all().map((spanId) => {
+      return clipBoard.clipBoard.map((type) => {
+        return command.factory.entityCreateCommand({
+          span: spanId,
+          type
         })
       })
-    )
-
-    command.invoke(commands, ['annotation'])
-  }
-
-  return {
-    copyEntities,
-    pasteEntities
-  }
+    })
+  )
+  command.invoke(commands, ['annotation'])
 }
