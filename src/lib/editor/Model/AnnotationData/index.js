de52198db2dcc7a1d0269e07a3f932d9a89ce3c8
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-08-13 16:20:49 +0900

    refactor: Refactor the AnnotationData

--- src/lib/editor/Model/AnnotationData/index.js
+++ src/lib/editor/Model/AnnotationData/index.js
@@ -2,14 +2,24 @@ import reset from './reset'
 import toJson from './toJson'
 import Container from './Container'
 
-export default function(editor) {
-  const dataStore = new Container(editor)
+export default class extends Container {
+  constructor(editor) {
+    super(editor)
+  }
 
-  return Object.assign(dataStore, {
-    reset: (annotation) => reset(dataStore, annotation),
-    resetOnlyConfig: () => dataStore.emit('config.change', dataStore),
-    toJson: () => toJson(dataStore),
-    getModificationOf: (objectId) =>
-      dataStore.modification.all().filter((m) => m.obj === objectId)
-  })
+  reset(annotation) {
+    reset(this, annotation)
+  }
+
+  resetOnlyConfig() {
+    super.emit('config.change', this)
+  }
+
+  toJson() {
+    return toJson(this)
+  }
+
+  getModificationOf(objectId) {
+    return this.modification.all().filter((m) => m.obj === objectId)
+  }
 }
