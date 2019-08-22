ec35b9b69ea10b628ce50b74dc6037b60cc23efa
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-08-22 14:20:38 +0900

    refactor: Redactor the SpanRenderer.create

--- src/lib/editor/start/View/Renderer/bindAnnotationDataEvents/SpanRenderer/create/index.js
+++ src/lib/editor/start/View/Renderer/bindAnnotationDataEvents/SpanRenderer/create/index.js
@@ -5,20 +5,19 @@ import renderEntitiesOfSpan from './renderEntitiesOfSpan'
 import destroyChildrenSpan from './destroyChildrenSpan'
 import renderChildresnSpan from './renderChildresnSpan'
 
-export default create
-
 // Destroy children spans to wrap a TextNode with <span> tag when new span over exists spans.
-function create(annotationData, span, isBlockFunc, renderEntityFunc) {
+export default function create(
+  annotationData,
+  span,
+  isBlockFunc,
+  renderEntityFunc
+) {
   destroyChildrenSpan(span)
 
   const bigBrother = getBigBrother(span, annotationData.span.topLevel())
-
   renderSingleSpan(span, bigBrother)
-
   renderClassOfSpan(span, isBlockFunc)
-
   renderEntitiesOfSpan(span, renderEntityFunc)
-
   renderChildresnSpan(span, (span) =>
     create(annotationData, span, isBlockFunc, renderEntityFunc)
   )
