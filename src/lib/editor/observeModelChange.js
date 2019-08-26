77fe3cb03df2944160098f44e6007f299d5f3986
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-08-26 18:30:18 +0900

    refactor: Fix typo

--- src/lib/editor/observeModelChange.js
+++ src/lib/editor/observeModelChange.js
@@ -1,8 +1,8 @@
-import showVilidationDialog from '../component/showVilidationDialog'
+import showValidationDialog from '../component/showValidationDialog'
 
 export default function(annotationData, history) {
   annotationData.on('all.change', (_, __, reject) => {
     history.resetAllHistories()
-    showVilidationDialog(self, reject)
+    showValidationDialog(self, reject)
   })
 }
