96b3c8b0ea0616a48842723bb497372274bd3cb6
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-09-25 18:32:04 +0900

    refactor: Extract a module

---
+++ src/lib/editor/start/View/Renderer/EntityRenderer/setLabelToTypeLabel/getLabelTag.js
@@ -0,0 +1,12 @@
+import getLabel from '../getLabel'
+import getUri from '../getUri'
+
+export default function(namespace, typeContainer, type) {
+  const label = getLabel(namespace, typeContainer, type)
+  const href = getUri(namespace, typeContainer, type)
+  if (href) {
+    return `<a target="_blank"/ href="${href}">${label}</a>`
+  } else {
+    return label
+  }
+}
--- src/lib/editor/start/View/Renderer/EntityRenderer/setLabelToTypeLabel/index.js
+++ src/lib/editor/start/View/Renderer/EntityRenderer/setLabelToTypeLabel/index.js
@@ -1,17 +1,10 @@
-import getLabel from '../getLabel'
-import getUri from '../getUri'
 import getChildAttributesHtml from './getChildAttributesHtml'
+import getLabelTag from './getLabelTag'
 
 export default function(typeLabel, namespace, typeContainer, type) {
-  const label = getLabel(namespace, typeContainer, type)
-  const href = getUri(namespace, typeContainer, type)
-
   // Keep the attributes html not to be overwrote the following 'innerHTML'.
   const childAttributesHtml = getChildAttributesHtml(typeLabel)
 
-  if (href) {
-    typeLabel.innerHTML = `<a target="_blank"/ href="${href}">${label}</a>${childAttributesHtml}`
-  } else {
-    typeLabel.innerHTML = label + childAttributesHtml
-  }
+  typeLabel.innerHTML =
+    getLabelTag(namespace, typeContainer, type) + childAttributesHtml
 }
