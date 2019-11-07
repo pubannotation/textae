920d334490912f06a563ae4caa394f9fa44d14f5
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-11-07 15:14:03 +0900

    bugfix: If you update a type that includes a wildcard, update the label other than the target type

--- src/lib/editor/start/View/Renderer/bindTypeDefinitionEvents.js
+++ src/lib/editor/start/View/Renderer/bindTypeDefinitionEvents.js
@@ -1,6 +1,10 @@
 export default function(typeDefinition, entityRenderer, relationRenderer) {
   typeDefinition.entity
-    .on('type.change', (id) => entityRenderer.updateLabel(id))
+    .on('type.change', () => {
+      // If you update a type that includes a wildcard, update the label other than the target type.
+      entityRenderer.updateLabelAll()
+    })
     .on('type.reset', () => entityRenderer.updateLabelAll())
+
   typeDefinition.relation.on('type.reset', () => relationRenderer.changeAll())
 }
