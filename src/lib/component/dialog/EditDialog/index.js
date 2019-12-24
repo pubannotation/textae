216e1e65d5cad10546065a5c88169c3bb91b3019
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-12-24 15:54:25 +0900

    refactor: You do not need to do the same thing as the default behavior of the jQueryUI dialog

--- src/lib/component/dialog/EditDialog/index.js
+++ src/lib/component/dialog/EditDialog/index.js
@@ -7,15 +7,5 @@ export default class {
 
   open() {
     this.$dialog.open()
-
-    // I do not know the reason, but the input element can not be focused without inserting a timeout.
-    setTimeout(
-      () =>
-        this.$dialog
-          .find('input')
-          .eq(1)
-          .focus(),
-      0
-    )
   }
 }
