57133cac019e257878f2e14d374c4490b9c81ddb
shigeru.nakajima <shigeru.nakajima@gmail.com>
2019-12-27 16:36:38 +0900

    bugfix: When checking for duplicate predicates, it was determined that only the predicate and neighbor were duplicated.
    For example, if the first and third predicates were the same, duplicates could not be detected.

--- src/lib/component/EditTypeDialog/observeDuplicatedPredicateValidation/validateDuplicatedPredicate.js
+++ src/lib/component/EditTypeDialog/observeDuplicatedPredicateValidation/validateDuplicatedPredicate.js
@@ -4,19 +4,11 @@ import valid from './valid'
 
 export default function(element) {
   const { attributes } = getValues(element)
-  if (attributes.length < 2) {
+  const uniqPred = [...new Set(attributes.map((a) => a.pred))]
+
+  if (attributes.length === uniqPred.length) {
     valid(element)
-    return
+  } else {
+    invalid(element)
   }
-  let prevValue = undefined
-  for (const input of attributes) {
-    if (prevValue !== undefined) {
-      if (prevValue === input.pred) {
-        invalid(element)
-        return
-      }
-    }
-    prevValue = input.pred
-  }
-  valid(element)
 }
