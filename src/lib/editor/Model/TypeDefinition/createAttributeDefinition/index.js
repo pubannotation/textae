6d3a44d3a60955e512c984f324905161c695b94a
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-02-17 17:05:49 +0900

    feature: Throw more verbose error

--- src/lib/editor/Model/TypeDefinition/createAttributeDefinition/index.js
+++ src/lib/editor/Model/TypeDefinition/createAttributeDefinition/index.js
@@ -2,8 +2,15 @@ import FlagAttributeDefinition from './FlagAttributeDefinition'
 import NumericAttributeDefinition from './NumericAttributeDefinition'
 import SelectionAttributeDefinition from './SelectionAttributeDefinition'
 import StringAttributeDefinition from './StringAttributeDefinition'
+import AttributeDefinition from './AttributeDefinition'
 
 export default function(hash) {
+  if (hash instanceof AttributeDefinition) {
+    throw new Error(
+      `Give me hash object instead of an AttributeDefinition instance`
+    )
+  }
+
   switch (hash['value type']) {
     case 'flag':
       return new FlagAttributeDefinition(hash)
@@ -14,6 +21,8 @@ export default function(hash) {
     case 'string':
       return new StringAttributeDefinition(hash)
     default:
-      throw `hash['value type'] is Uknown Attribute`
+      throw new Error(
+        `${hash['value type']} of ${JSON.stringify(hash)} is Uknown Attribute`
+      )
   }
 }
