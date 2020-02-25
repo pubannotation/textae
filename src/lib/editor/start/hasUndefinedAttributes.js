590e5061bc985646d3a55046663a744500b54d22
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-02-25 20:53:11 +0900

    feature: Check if annotation does not include any undefined selection attribute values

--- src/lib/editor/start/hasUndefinedAttributes.js
+++ src/lib/editor/start/hasUndefinedAttributes.js
@@ -1,15 +1,42 @@
 export default function(annotation, config) {
   if (annotation.attributes) {
+    const atrributeTypes = config['attribute types'] || []
+
     const attributesWithoutDefenition = annotation.attributes.filter(
-      (attr) =>
-        !(config['attribute types'] || []).some(
-          (attrDef) => attrDef.pred === attr.pred
-        )
+      (attr) => !atrributeTypes.some((attrDef) => attrDef.pred === attr.pred)
     )
+
     if (attributesWithoutDefenition.length) {
       console.warn('attributes without definition', attributesWithoutDefenition)
       return `attribute definition for "${attributesWithoutDefenition
         .map((a) => a.pred)
+        .join(', ')}" in configuration is missing. `
+    }
+
+    const selectionAttributeValueWithoutDefinition = annotation.attributes
+      .filter((attr) =>
+        atrributeTypes.some(
+          (attrDef) =>
+            attrDef.pred === attr.pred && attrDef['value type'] === 'selection'
+        )
+      )
+      .filter(
+        (attr) =>
+          !atrributeTypes.some(
+            (attrDef) =>
+              attrDef.pred === attr.pred &&
+              attrDef.values.some((v) => v.id === attr.obj)
+          )
+      )
+
+    if (selectionAttributeValueWithoutDefinition.length) {
+      console.warn(
+        'selection attribute values without definition',
+        selectionAttributeValueWithoutDefinition
+      )
+
+      return `selection attribute value definition for "${selectionAttributeValueWithoutDefinition
+        .map((a) => a.obj)
         .join(', ')}" in configuration is missing. `
     }
   }
