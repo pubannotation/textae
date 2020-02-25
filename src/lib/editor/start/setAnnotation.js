874b62362f32b84fcab6d692c594c4a7b0833afd
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-02-25 16:48:03 +0900

    feature: Validate configure file when loading annotation file

--- src/lib/editor/start/setAnnotation.js
+++ src/lib/editor/start/setAnnotation.js
@@ -1,5 +1,7 @@
+import alertifyjs from 'alertifyjs'
 import getConfigFromServer from './getConfigFromServer'
 import setSpanAndTypeConfig from './setSpanAndTypeConfig'
+import validateConfiguration from '../Model/AnnotationData/validateConfiguration'
 
 export default function(
   spanConfig,
@@ -9,10 +11,18 @@ export default function(
   configUrl
 ) {
   if (annotation.config) {
+    if (!validateConfiguration(annotation.config)) {
+      alertifyjs.error(`configuration in anntotaion file is invalid.`)
+      return
+    }
     setSpanAndTypeConfig(spanConfig, typeDefinition, annotation.config)
     annotationData.reset(annotation)
   } else {
     getConfigFromServer(configUrl, (configFromServer) => {
+      if (configFromServer && !validateConfiguration(configFromServer)) {
+        alertifyjs.error(`a configuration file from ${configUrl} is invalid.`)
+        return
+      }
       setSpanAndTypeConfig(spanConfig, typeDefinition, configFromServer)
       annotationData.reset(annotation)
     })
