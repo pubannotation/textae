b875ffc3a41af324bf590cd3dde4ff6c01ee5471
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-03-24 16:08:18 +0900

    bugifx: When the value of line-height is normal, integer conversion fails

--- src/lib/editor/start/View/pixelToInt.js
+++ src/lib/editor/start/View/pixelToInt.js
@@ -1,3 +1,6 @@
 export default function(str) {
-  return str === '' ? 0 : parseInt(str, 10)
+  // The default value of google chrome line-height is "normal".
+  // "normal" cannot be converted to an integer.
+  const i = parseInt(str, 10)
+  return isNaN(i) ? 0 : i
 }
