813c0d30bffd6c4c3b48cfc34ed60d26730ee214
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-03-24 15:54:31 +0900

    refactor: Replace duplicate logic using getLineHeight

--- src/lib/editor/start/View/setLineHeightToTypeGap.js
+++ src/lib/editor/start/View/setLineHeightToTypeGap.js
@@ -1,23 +1,21 @@
 import getHeightIncludeDescendantGrids from './getHeightIncludeDescendantGrids'
-import pixelToInt from './pixelToInt'
 import setLineHeight from './lineHeight/setLineHeight'
+import getLineHeight from './lineHeight/getLineHeight'
 
 const TEXT_HEIGHT = 23
 const MARGIN_TOP = 30
 
 export default function(editor, annotationData, typeGap) {
-  let maxHeight
   if (annotationData.span.all.length === 0) {
-    const style = window.getComputedStyle(editor)
-    const n = pixelToInt(style.lineHeight)
-    maxHeight = n
+    // You need to adjust the padding-top according to the line-height.
+    // Call setLineHeight even if there is no span.
+    setLineHeight(editor, getLineHeight(editor))
   } else {
-    maxHeight = Math.max(
+    const maxHeight = Math.max(
       ...annotationData.span.all.map((span) =>
         getHeightIncludeDescendantGrids(span, typeGap, annotationData)
       )
     )
-    maxHeight += TEXT_HEIGHT + MARGIN_TOP
+    setLineHeight(editor, maxHeight + TEXT_HEIGHT + MARGIN_TOP)
   }
-  setLineHeight(editor, maxHeight)
 }
