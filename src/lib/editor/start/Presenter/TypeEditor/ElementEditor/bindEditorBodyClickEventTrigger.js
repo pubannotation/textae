0588400b875fc583b576f80eddbf710eb1c7d467
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-04-20 10:52:04 +0900

    refactor: Remove default function name

--- src/lib/editor/start/Presenter/TypeEditor/ElementEditor/bindEditorBodyClickEventTrigger.js
+++ src/lib/editor/start/Presenter/TypeEditor/ElementEditor/bindEditorBodyClickEventTrigger.js
@@ -2,7 +2,7 @@ import delegate from 'delegate'
 
 // Trigger body click event
 // The blank area on the editor is textae-editor__body__text-box or textae-editor__body__text-box__paragraph-margin.
-export default function bindEditorBodyClickEventTrigger(editor) {
+export default function(editor) {
   return delegate(editor[0], '.textae-editor__body__text-box', 'click', (e) => {
     // The delegate also fires events for child elements of the selector.
     // Ignores events that occur in child elements.
