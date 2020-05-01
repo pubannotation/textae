302cc641d5dd79731d2688de082009d6aacc1899
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-05-01 14:57:12 +0900

    bugfix: Error when reading HTML file as a configuration file

--- src/lib/editor/start/validateConfigurationAndAlert/index.js
+++ src/lib/editor/start/validateConfigurationAndAlert/index.js
@@ -4,6 +4,13 @@ import validateConfiguration from './validateConfiguration'
 import hasUndefinedAttributes from './hasUndefinedAttributes'
 
 export default function(annotation, config, defaultErrorMessage) {
+  // When config is specified, it must be JSON.
+  // For example, when we load an HTML file, we treat it as text here.
+  if (config && typeof config !== 'object') {
+    alertifyjs.error(defaultErrorMessage)
+    return [false]
+  }
+
   const patchedConfig = patchConfiguration(annotation, config)
   if (patchedConfig) {
     const [isValid, errorMessage] = validateConfiguration(patchedConfig)
