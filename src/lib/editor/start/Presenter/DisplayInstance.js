fae55186af3398f5cbb116106e2bbdde6ca4a078
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-06-17 22:55:36 +0900

    refactor: Refactor the DisplayInstance

---
+++ src/lib/editor/start/Presenter/DisplayInstance.js
@@ -0,0 +1,50 @@
+import alertifyjs from 'alertifyjs'
+
+export default class {
+  constructor(typeGap) {
+    // The typeGap is changed from the Setting dialog or
+    // by changing mode showing or not showing instances.
+    this._typeGap = typeGap
+    this._isModeShowingInstance = true
+    this._valueForModeShowingInstance = 2
+  }
+
+  show() {
+    this._isModeShowingInstance = true
+    this._typeGap.set({
+      value: this._valueForModeShowingInstance,
+      showInstance: true
+    })
+  }
+
+  hide() {
+    this._isModeShowingInstance = false
+    this._typeGap.set({
+      value: 0,
+      showInstance: false
+    })
+  }
+
+  get showInstance() {
+    return this._isModeShowingInstance
+  }
+
+  get typeGap() {
+    return this._isModeShowingInstance ? this._valueForModeShowingInstance : 0
+  }
+
+  // The typeGap be able to be changed when mode showing instances.
+  set typeGap(val) {
+    this._valueForModeShowingInstance = val
+    this._typeGap.set({
+      value: val,
+      showInstance: true
+    })
+  }
+
+  notifyNewInstance() {
+    if (!this._isModeShowingInstance) {
+      alertifyjs.success('an instance is created behind.')
+    }
+  }
+}
