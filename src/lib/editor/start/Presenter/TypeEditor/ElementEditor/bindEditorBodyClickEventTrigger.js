6a7cf23f61c416eef863b541a94f9ec253e85a6a
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-06-19 05:15:24 +0900

    feature: Deselect by clicking on the blank space next to the control bar

--- src/lib/editor/start/Presenter/TypeEditor/ElementEditor/bindEditorBodyClickEventTrigger.js
+++ src/lib/editor/start/Presenter/TypeEditor/ElementEditor/bindEditorBodyClickEventTrigger.js
@@ -3,11 +3,14 @@ import delegate from 'delegate'
 // Trigger body click event
 // The blank area on the editor is textae-editor__body__text-box.
 export default function(editor) {
-  return delegate(editor[0], '.textae-editor__body__text-box', 'click', (e) => {
+  return delegate(editor[0], '.textae-editor', 'click', (e) => {
     // The delegate also fires events for child elements of the selector.
     // Ignores events that occur in child elements.
     // Otherwise, you cannot select child elements.
-    if (e.target.classList.contains('textae-editor__body__text-box')) {
+    if (
+      e.target.classList.contains('textae-editor') ||
+      e.target.classList.contains('textae-editor__body__text-box')
+    ) {
       editor.eventEmitter.emit('textae.editor.body.click')
     }
   })
