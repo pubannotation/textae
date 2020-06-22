5815d3adb407c5a2a5bdb3ea0744075c8d635e8d
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-06-22 17:44:18 +0900

    refactor: Refactor the validate module

--- src/lib/editor/Model/AnnotationData/parseDenotation/parseAnnotation/validateAnnotation/validate/index.js
+++ src/lib/editor/Model/AnnotationData/parseDenotation/parseAnnotation/validateAnnotation/validate/index.js
@@ -3,19 +3,13 @@ import Reject from './Reject'
 export default function(values, predicate, predicateOption) {
   if (!values) return new Reject()
 
-  return values.reduce(
-    (result, target, index, array) =>
-      acceptIf(predicate, predicateOption, result, target, index, array),
-    new Reject()
-  )
-}
+  return values.reduce((result, target, index, array) => {
+    if (predicate(target, predicateOption, index, array)) {
+      result.accept.push(target)
+    } else {
+      result.reject.push(target)
+    }
 
-function acceptIf(predicate, predicateOption, result, target, index, array) {
-  if (predicate(target, predicateOption, index, array)) {
-    result.accept.push(target)
-  } else {
-    result.reject.push(target)
-  }
-
-  return result
+    return result
+  }, new Reject())
 }
