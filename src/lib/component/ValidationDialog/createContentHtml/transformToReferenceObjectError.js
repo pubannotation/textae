f8af9231155992414eb6b005ceae31dc7290c041
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-06-22 23:08:03 +0900

    feature: Show reference error of subject of attribute in the VaildationDialog

--- src/lib/component/ValidationDialog/createContentHtml/transformToReferenceObjectError.js
+++ src/lib/component/ValidationDialog/createContentHtml/transformToReferenceObjectError.js
@@ -1,10 +1,17 @@
 export default function(reject) {
   // Combine rejects for referenced object errer.
-  reject.referencedItems = reject.relationObj
-    .map((relation) => {
-      relation.alertObj = true
-      return relation
+  // Mark the column you want to highlight.
+  reject.referencedItems = reject.attributeSubj
+    .map((attribute) => {
+      attribute.alertSubj = true
+      return attribute
     })
+    .concat(
+      reject.relationObj.map((relation) => {
+        relation.alertObj = true
+        return relation
+      })
+    )
     .concat(
       reject.relationSubj.map((relation) => {
         relation.alertSubj = true
