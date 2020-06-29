1c2ddcb46fa28a3d3c2c26818f18f963d11efa33
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-06-29 22:19:58 +0900

    refactor: Remove a no effect function

--- src/lib/editor/start/View/lineHeight/setLineHeight.js
+++ src/lib/editor/start/View/lineHeight/setLineHeight.js
@@ -1,11 +1,9 @@
-import getTextBox from '../../getTextBox'
-import updateTextBoxHeight from '../../updateTextBoxHeight'
-import suppressScrollJump from './suppressScrollJump'
+import getTextBox from '../getTextBox'
+import updateTextBoxHeight from '../updateTextBoxHeight'
 
 export default function(editor, heightValue) {
   const textBox = getTextBox(editor)
   textBox.style.lineHeight = `${heightValue}px`
   textBox.style.paddingTop = `${heightValue / 2}px`
-  suppressScrollJump(textBox, heightValue)
   updateTextBoxHeight(editor)
 }
--- src/lib/editor/start/View/lineHeight/setLineHeight/suppressScrollJump.js
+++
@@ -1,9 +0,0 @@
-import pixelToInt from '../../pixelToInt'
-
-export default function(textBox, heightValue) {
-  const beforeLineHeight = textBox.style.lineHeight
-  const b = pixelToInt(beforeLineHeight)
-  if (b) {
-    window.scroll(window.scrollX, (window.scrollY * heightValue) / b)
-  }
-}
