0eb999f59166787138b5c1274f5fd0e0e10f3ddb
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-07-02 06:47:03 +0900

    refactor: Shorten imports

--- src/lib/editor/start/Presenter/TypeEditor/ElementEditor/EditEntity/SpanEditor/validateOnSpan.js
+++ src/lib/editor/start/Presenter/TypeEditor/ElementEditor/EditEntity/SpanEditor/validateOnSpan.js
@@ -1,4 +1,4 @@
-import SelectionWrapper from '../../../ElementEditor/SelectionWrapper'
+import SelectionWrapper from '../../SelectionWrapper'
 import hasCharacters from './hasCharacters'
 
 export default function(annotationData, spanConfig, selection) {
