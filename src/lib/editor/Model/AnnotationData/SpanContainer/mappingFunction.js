fff6ec4235e344a2eff469efb679ab82f984da6f
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-08-12 16:31:51 +0900

    refactor: Fix imported name

--- src/lib/editor/Model/AnnotationData/SpanContainer/mappingFunction.js
+++ src/lib/editor/Model/AnnotationData/SpanContainer/mappingFunction.js
@@ -1,4 +1,4 @@
-import SpanModel from './ObjectSpanModel'
+import ObjectSpanModel from './ObjectSpanModel'
 import StyleSpanModel from './StyleSpanModel'
 
 export default function(denotations, editor, entityContainer) {
@@ -8,7 +8,12 @@ export default function(denotations, editor, entityContainer) {
     if (entity.style) {
       return new StyleSpanModel(editor, entity.span, entity.style)
     } else {
-      return new SpanModel(editor, entity.span, entityContainer, entity.style)
+      return new ObjectSpanModel(
+        editor,
+        entity.span,
+        entityContainer,
+        entity.style
+      )
     }
   })
 }
