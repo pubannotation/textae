170da7144c93dc028b762082d73d07753fb74890
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-08-18 15:21:01 +0900

    refactor: Fix typo of an arguments

--- src/lib/editor/start/View/Renderer/EntityRenderer/create/createEntity/index.js
+++ src/lib/editor/start/View/Renderer/EntityRenderer/create/createEntity/index.js
@@ -6,9 +6,15 @@ import createEntityElement from './createEntityElement'
 // An entity is a circle on Type that is an endpoint of a relation.
 // A span have one grid and a grid can have multi types and a type can have multi entities.
 // A grid is only shown when at least one entity is owned by a correspond span.
-export default function(editor, namspace, typeContainer, gridRenderer, entity) {
+export default function(
+  editor,
+  namespace,
+  typeContainer,
+  gridRenderer,
+  entity
+) {
   // Append a new entity to the type
-  const typeDom = getTypeElement(namspace, typeContainer, gridRenderer, entity)
+  const typeDom = getTypeElement(namespace, typeContainer, gridRenderer, entity)
   const pane = typeDom.querySelector('.textae-editor__entity-pane')
   const entityDomId = idFactory.makeEntityDomId(editor, entity.id)
 
