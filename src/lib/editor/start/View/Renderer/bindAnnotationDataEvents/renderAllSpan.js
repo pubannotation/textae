bceb77277d18e5c6341a57f1d158be1f09efb12a
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-08-21 04:43:45 +0900

    refactor: Delete useless comment

--- src/lib/editor/start/View/Renderer/bindAnnotationDataEvents/renderAllSpan.js
+++ src/lib/editor/start/View/Renderer/bindAnnotationDataEvents/renderAllSpan.js
@@ -1,10 +1,5 @@
 export default function(annotationData, spanRenderer) {
-  // For tuning
-  // var startTime = new Date();
   annotationData.span.topLevel().forEach((span) => {
     spanRenderer.render(span)
   })
-  // For tuning
-  // var endTime = new Date();
-  // console.log('render all span : ', endTime.getTime() - startTime.getTime() + 'ms');
 }
