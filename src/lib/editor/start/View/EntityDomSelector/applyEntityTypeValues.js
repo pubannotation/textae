86a1a5567715957bf03c02463d13954cdd338f17
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-08-20 17:26:18 +0900

    refactor: Delete useless guard statements

--- src/lib/editor/start/View/EntityDomSelector/applyEntityTypeValues.js
+++ src/lib/editor/start/View/EntityDomSelector/applyEntityTypeValues.js
@@ -4,18 +4,15 @@ import areAllSibilingEntitiesHasClass from '../areAllSibilingEntitiesHasClass'
 export default function(entity, cssClass) {
   console.assert(entity, 'An entity is necessary.')
 
-  // Entities of block span hos no dom elements.
-  if (entity) {
-    const type = getTypeDomOfEntityDom(entity)
-    const typeValues = type.querySelector('.textae-editor__type-values')
+  const type = getTypeDomOfEntityDom(entity)
+  const typeValues = type.querySelector('.textae-editor__type-values')
 
-    // Set class to the typeValues if all entities of its has class.
-    if (areAllSibilingEntitiesHasClass(entity, cssClass)) {
-      type.classList.add(cssClass)
-      typeValues.classList.add(cssClass)
-    } else {
-      type.classList.remove(cssClass)
-      typeValues.classList.remove(cssClass)
-    }
+  // Set class to the typeValues if all entities of its has class.
+  if (areAllSibilingEntitiesHasClass(entity, cssClass)) {
+    type.classList.add(cssClass)
+    typeValues.classList.add(cssClass)
+  } else {
+    type.classList.remove(cssClass)
+    typeValues.classList.remove(cssClass)
   }
 }
--- src/lib/editor/start/View/applyTypeValues.js
+++ src/lib/editor/start/View/applyTypeValues.js
@@ -2,17 +2,16 @@ import getTypeDomOfEntityDom from '../getTypeDomOfEntityDom'
 import areAllSibilingEntitiesHasClass from './areAllSibilingEntitiesHasClass'
 
 export default function(entity, cssClass) {
-  // Entities of block span hos no dom elements.
-  if (entity) {
-    const typeValues = getTypeDomOfEntityDom(entity).querySelector(
-      '.textae-editor__type-values'
-    )
+  console.assert(entity, 'An entity is necessary.')
 
-    // Set class to the typeValues if all entities of its has class.
-    if (areAllSibilingEntitiesHasClass(entity, cssClass)) {
-      typeValues.classList.add(cssClass)
-    } else {
-      typeValues.classList.remove(cssClass)
-    }
+  const typeValues = getTypeDomOfEntityDom(entity).querySelector(
+    '.textae-editor__type-values'
+  )
+
+  // Set class to the typeValues if all entities of its has class.
+  if (areAllSibilingEntitiesHasClass(entity, cssClass)) {
+    typeValues.classList.add(cssClass)
+  } else {
+    typeValues.classList.remove(cssClass)
   }
 }
