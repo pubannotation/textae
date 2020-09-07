df7a81fc71640e8d704fd95f84c1c728d95bee88
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-09-07 10:30:28 +0900

    misc: Update the jsondiffpatch

--- src/lib/component/SaveConfigurationDialog/jsonDiff.js
+++ src/lib/component/SaveConfigurationDialog/jsonDiff.js
@@ -1,7 +1,8 @@
-import { diff } from 'jsondiffpatch'
-import formatters from 'jsondiffpatch/src/formatters'
+import { create, formatters } from 'jsondiffpatch'
+
+const jsond = create()
 
 export default function(originalConfig, editedConfig) {
-  const delta = diff(originalConfig, editedConfig)
+  const delta = jsond.diff(originalConfig, editedConfig)
   return formatters.html.format(delta, originalConfig)
 }
