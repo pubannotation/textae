c8668812595cae9eb6d9343d700f2ad7939f69c8
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-09-07 12:29:14 +0900

    refactor: Rename an argument

--- src/lib/editor/AnnotationData/parseDenotation/parseAnnotation/validateAnnotation/validate/index.js
+++ src/lib/editor/AnnotationData/parseDenotation/parseAnnotation/validateAnnotation/validate/index.js
@@ -1,11 +1,11 @@
 import ValidationResults from './ValidationResults'
 
-export default function(values, predicate) {
-  if (!values) return new ValidationResults()
+export default function(nodes, predicate) {
+  if (!nodes) return new ValidationResults()
 
-  return values.reduce((results, target, index) => {
+  return nodes.reduce((results, target, index) => {
     // This variable only for isNotSpanCrossing.
-    const nodesBeforeTarget = values.slice(0, index)
+    const nodesBeforeTarget = nodes.slice(0, index)
 
     if (predicate(target, nodesBeforeTarget)) {
       results.accept(target)
