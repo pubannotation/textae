84c56031c0ea2150f281663f362e38ba3cead98f
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-09-07 12:05:21 +0900

    refactor: Rename an argument

--- src/lib/editor/AnnotationData/parseDenotation/parseAnnotation/validateAnnotation/validateAttribute.js
+++ src/lib/editor/AnnotationData/parseDenotation/parseAnnotation/validateAnnotation/validateAttribute.js
@@ -1,8 +1,8 @@
 import validate from './validate'
 import isContains from './isContains'
 
-export default function(denotations, annotations) {
-  const resultAttributeSubj = validate(annotations, isContains, {
+export default function(denotations, attributes) {
+  const resultAttributeSubj = validate(attributes, isContains, {
     property: 'subj',
     dictionary: denotations
   })
