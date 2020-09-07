18f3aa15df4c55c3ba807368e6926ea6f30c3224
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-09-07 11:27:11 +0900

    feature: Smart array diff of configuration

--- src/lib/component/SaveConfigurationDialog/jsonDiff.js
+++ src/lib/component/SaveConfigurationDialog/jsonDiff.js
@@ -1,6 +1,10 @@
 import { create, formatters } from 'jsondiffpatch'
 
-const jsond = create()
+const jsond = create({
+  objectHash(obj, index) {
+    return obj.id || `$$index:${index}`
+  }
+})
 
 export default function(originalConfig, editedConfig) {
   const delta = jsond.diff(originalConfig, editedConfig)
