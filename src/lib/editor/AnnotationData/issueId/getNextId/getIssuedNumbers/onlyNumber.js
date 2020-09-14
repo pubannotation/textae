5a710a99ab17cd56af2080b5ea2ea5c6f7180319
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-09-14 16:58:00 +0900

    refactor: Refactor the getNextId

---
+++ src/lib/editor/AnnotationData/issueId/getNextId/getIssuedNumbers/onlyNumber.js
@@ -0,0 +1,3 @@
+export default function(id) {
+  return id.slice(1)
+}
---
+++ src/lib/editor/AnnotationData/issueId/getNextId/index.js
@@ -0,0 +1,9 @@
+import getIssuedNumbers from './getIssuedNumbers/inedx'
+import getNextNumber from './getNextNumber'
+
+export default function(prefix, existsIds) {
+  const numbers = getIssuedNumbers(existsIds, prefix)
+  const nextNumber = getNextNumber(numbers)
+
+  return prefix + nextNumber
+}
