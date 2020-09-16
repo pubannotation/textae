3b4cfcb9f45d99d2f5b65a93c5bd7332b3869fdc
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-09-16 09:47:24 +0900

    bugfix: The style span that has the same range as the object span does not appear

--- src/lib/editor/AnnotationData/SpanContainer/index.js
+++ src/lib/editor/AnnotationData/SpanContainer/index.js
@@ -49,6 +49,7 @@ export default class extends ContainerWithSubContainer {
 
   addSource(spans) {
     super.addSource(spans)
+
     updateSpanTree(this.all)
   }
 
@@ -59,13 +60,7 @@ export default class extends ContainerWithSubContainer {
 
   get(spanId) {
     if (this._container.has(spanId)) {
-      const span = super.get(spanId)
-
-      // Merges a span and a typeset so that it can be rendered as a single DOM element.
-      if (this._typeSets.has(spanId)) {
-        span.styles = this._typeSets.get(spanId).styles
-      }
-      return span
+      return this._merageStyle(super.get(spanId))
     } else {
       // Returns a typeset only.
       return this._typeSets.get(spanId)
@@ -89,11 +84,30 @@ export default class extends ContainerWithSubContainer {
   }
 
   get topLevel() {
-    return this.all.filter((span) => span.parent === null).sort(spanComparator)
+    const styleOnlySpans = [...this._typeSets.values()].filter(
+      (s) => !this._container.has(s.id)
+    )
+
+    // Merge style to objcet spans before the rendreAllSpan.
+    return super.all
+      .map((span) => this._merageStyle(span))
+      .concat(styleOnlySpans)
+      .filter((span) => span.parent === null)
+      .sort(spanComparator)
+  }
+
+  // Merges a span and a typeset so that it can be rendered as a single DOM element.
+  _merageStyle(span) {
+    if (this._typeSets.has(span.id)) {
+      span.styles = this._typeSets.get(span.id).styles
+    }
+
+    return span
   }
 
   remove(id) {
     const span = super.remove(id)
+
     updateSpanTree(this.all)
     return span
   }
