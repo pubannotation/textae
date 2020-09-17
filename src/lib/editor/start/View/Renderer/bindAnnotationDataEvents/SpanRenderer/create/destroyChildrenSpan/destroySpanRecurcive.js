3182c1e6709489c5aa06873aa7ebf8f6bc3797ef
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-09-17 11:28:27 +0900

    refactor: The order in which to remove the DOM of the span is unified to pre order, the same as when it was created

--- src/lib/editor/start/View/Renderer/bindAnnotationDataEvents/SpanRenderer/create/destroyChildrenSpan/destroySpanRecurcive.js
+++ src/lib/editor/start/View/Renderer/bindAnnotationDataEvents/SpanRenderer/create/destroyChildrenSpan/destroySpanRecurcive.js
@@ -2,9 +2,10 @@ import destroy from '../../destroy'
 
 // Destroy DOM elements of descendant spans.
 export default function destroySpanRecurcive(span) {
+  destroy(span.id)
+
   for (const child of span.children) {
     destroySpanRecurcive(child)
   }
 
-  destroy(span.id)
 }
