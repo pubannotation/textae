625f610b598267ba9b27f3c00d6389f1e5bc8bec
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-09-17 11:44:17 +0900

    refactor: Delete an useless guard condition

--- src/lib/editor/start/View/Renderer/bindAnnotationDataEvents/SpanRenderer/create/index.js
+++ src/lib/editor/start/View/Renderer/bindAnnotationDataEvents/SpanRenderer/create/index.js
@@ -11,15 +11,7 @@ export default function create(editor, span, renderEntityFunc) {
   })
 
   span.traverse(
-    (span) => {
-      const spanElement = document.querySelector(`#${span.id}`)
-      if (spanElement) {
-        spanElement.setAttribute('tabindex', 0)
-        spanElement.classList.add('textae-editor__span')
-      } else {
-        renderSingleSpan(editor, span)
-      }
-    },
+    (span) => renderSingleSpan(editor, span),
     (span) => {
       // When the child spans contain bold style spans, the width of the parent span changes.
       // Render the entity after the child span has been rendered.
