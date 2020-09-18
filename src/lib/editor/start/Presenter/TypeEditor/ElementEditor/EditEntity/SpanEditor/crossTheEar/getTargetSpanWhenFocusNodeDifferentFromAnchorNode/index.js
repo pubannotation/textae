aa4d8472e44e45576e8ffb1896e73b566a5ee931
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-09-18 17:53:46 +0900

    refactor: Delete an useless argument

--- src/lib/editor/start/Presenter/TypeEditor/ElementEditor/EditEntity/SpanEditor/crossTheEar/getTargetSpanWhenFocusNodeDifferentFromAnchorNode/index.js
+++ src/lib/editor/start/Presenter/TypeEditor/ElementEditor/EditEntity/SpanEditor/crossTheEar/getTargetSpanWhenFocusNodeDifferentFromAnchorNode/index.js
@@ -1,7 +1,7 @@
 import isFocusInSelectedSpan from '../../isFocusInSelectedSpan'
 import isForcusOneDownUnderAnchor from './isForcusOneDownUnderAnchor'
 
-export default function(annotationData, selectionModel, selectionWrapper, id) {
+export default function(annotationData, selectionModel, selectionWrapper) {
   if (isFocusInSelectedSpan(annotationData, selectionModel, selectionWrapper)) {
     // If a span is selected, it is able to begin drag out of an outer span of the span and shrink the span.
     // The focus node should be at the selected node.
@@ -15,8 +15,6 @@ export default function(annotationData, selectionModel, selectionWrapper, id) {
     // 1. The anchorNode out of the span and in the parent of the span.
     // 2. The foucusNode is in the span.
     return selectionWrapper.selection.focusNode.parentNode.id
-  } else if (id) {
-    return id
   }
 
   return null
