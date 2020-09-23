c00d0277af9dc34fb255dcf1a5f9e2138f56fc78
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-09-23 15:52:55 +0900

    refactor: Inline the getSpanDom

--- src/lib/editor/start/View/bindSelectionModelEvents/SpanDomSelector.js
+++ src/lib/editor/start/View/bindSelectionModelEvents/SpanDomSelector.js
@@ -1,9 +1,8 @@
-import getSpanDom from './getSpanDom'
-import SELECTED from '../../SELECTED'
+import SELECTED from '../SELECTED'
 
 export default class {
   select(id) {
-    const el = getSpanDom(id)
+    const el = document.querySelector(`#${id}`)
     el.classList.add(SELECTED)
 
     // Set focus to the span element in order to scroll the browser to the position of the element.
@@ -11,7 +10,7 @@ export default class {
   }
 
   deselect(id) {
-    const el = getSpanDom(id)
+    const el = document.querySelector(`#${id}`)
 
     // A dom does not exist when it is deleted.
     if (el) {
