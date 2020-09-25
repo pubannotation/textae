b1c2332833de3df09dc714eaa934e0f102bc779a
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-09-25 16:37:48 +0900

    refactor: If already selected or deselected, the event will not be sent

--- src/lib/editor/SelectionModel/IdContainer/add.js
+++ src/lib/editor/SelectionModel/IdContainer/add.js
@@ -1,6 +1,10 @@
 import triggerChange from './triggerChange'
 
 export default function(selected, emitter, kindName, id) {
+  if (selected.has(id)) {
+    return
+  }
+
   selected.add(id)
   emitter.emit(`textae.selection.${kindName}.select`, id)
   triggerChange(emitter, kindName)
--- src/lib/editor/SelectionModel/IdContainer/remove.js
+++ src/lib/editor/SelectionModel/IdContainer/remove.js
@@ -1,7 +1,9 @@
 import triggerChange from './triggerChange'
 
 export default function(selected, emitter, kindName, id) {
-  selected.delete(id)
-  emitter.emit(`textae.selection.${kindName}.deselect`, id)
-  triggerChange(emitter, kindName)
+  if (selected.has(id)) {
+    selected.delete(id)
+    emitter.emit(`textae.selection.${kindName}.deselect`, id)
+    triggerChange(emitter, kindName)
+  }
 }
