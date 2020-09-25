3d293a15d86b757edacc6475894a94507eb628cb
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-09-25 16:51:48 +0900

    refactor: Inline one line functions

--- src/lib/editor/start/View/bindSelectionModelEvents/RelationDomSelector/deselectRelation.js
+++ src/lib/editor/start/View/bindSelectionModelEvents/RelationDomSelector/deselectRelation.js
@@ -1,9 +1,5 @@
 export default function(domPositionCache, relation) {
   const connect = domPositionCache.getConnect(relation.id)
 
-  removeUiSelectClass(connect)
-}
-
-function removeUiSelectClass(connect) {
   if (connect && connect.deselect) connect.deselect()
 }
--- src/lib/editor/start/View/bindSelectionModelEvents/RelationDomSelector/selectRelation.js
+++ src/lib/editor/start/View/bindSelectionModelEvents/RelationDomSelector/selectRelation.js
@@ -1,9 +1,5 @@
 export default function(domPositionCache, relation) {
   const connect = domPositionCache.getConnect(relation.id)
 
-  addUiSelectClass(connect)
-}
-
-function addUiSelectClass(connect) {
   if (connect && connect.select) connect.select()
 }
