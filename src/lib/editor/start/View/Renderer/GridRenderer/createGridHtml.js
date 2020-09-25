755ed2e4194afca94116de5d567ccb1047d6a0ba
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-09-25 11:53:29 +0900

    refactor: Add a comment about grid rendering

--- src/lib/editor/start/View/Renderer/GridRenderer/createGridHtml.js
+++ src/lib/editor/start/View/Renderer/GridRenderer/createGridHtml.js
@@ -1,5 +1,9 @@
 import Handlebars from 'handlebars'
 
+// When multiple entities are added, the grid positioning is performed together.
+// There is a time difference between drawing and setting the position of the grid.
+// When a new entity is added, the added grid is momentarily displayed at the end of the editor.
+// To prevent this, the grid is hidden when drawing the grid and displayed after the move is completed.
 const source = `
   <div id={{id}} class="textae-editor__grid hidden" style="width: {{spanWidth}}px">
   </div>
