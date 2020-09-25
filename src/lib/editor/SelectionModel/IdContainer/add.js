1b13bbef703983c90b3f7642039f0c6241d72321
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-09-25 16:20:11 +0900

    refactor: Removed the unused multiple selection feature

--- src/lib/editor/SelectionModel/IdContainer/add.js
+++ src/lib/editor/SelectionModel/IdContainer/add.js
@@ -1,15 +1,7 @@
 import triggerChange from './triggerChange'
 
 export default function(selected, emitter, kindName, id) {
-  if (id.forEach) {
-    id.forEach((id) => {
-      selected.add(id)
-      emitter.emit(`textae.selection.${kindName}.select`, id)
-      triggerChange(emitter, kindName)
-    })
-  } else {
-    selected.add(id)
-    emitter.emit(`textae.selection.${kindName}.select`, id)
-    triggerChange(emitter, kindName)
-  }
+  selected.add(id)
+  emitter.emit(`textae.selection.${kindName}.select`, id)
+  triggerChange(emitter, kindName)
 }
