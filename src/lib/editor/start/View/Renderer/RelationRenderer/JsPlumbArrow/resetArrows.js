b371c3338a1937af286f03f3b96e7571a016f5b0
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-09-28 13:36:10 +0900

    refactor: Inline the getArrowIds

--- src/lib/editor/start/View/Renderer/RelationRenderer/JsPlumbArrow/resetArrows.js
+++ src/lib/editor/start/View/Renderer/RelationRenderer/JsPlumbArrow/resetArrows.js
@@ -1,9 +1,11 @@
 import addArrow from './addArrow'
-import getArrowIds from './getArrowIds'
 
 export default function(js_plumb_conncetion) {
-  for (const id of getArrowIds(js_plumb_conncetion)) {
-    js_plumb_conncetion.removeOverlays(id)
-    addArrow(id, js_plumb_conncetion)
+  for (const overlay of js_plumb_conncetion.getOverlays()) {
+    if (overlay.type === 'Arrow') {
+      const id = overlay.id
+      js_plumb_conncetion.removeOverlays(id)
+      addArrow(id, js_plumb_conncetion)
+    }
   }
 }
