10f8f135766c4198da9d1afb353a47cf7584f820
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-10-02 18:13:54 +0900

    refactor: jsPlumb will also fire a click event on a connection when you click on a label

--- src/lib/editor/start/Presenter/TypeEditor/jsPlumbConnectionClicked.js
+++ src/lib/editor/start/Presenter/TypeEditor/jsPlumbConnectionClicked.js
@@ -1,16 +1,9 @@
 // A relation is drawn by a jsPlumbConnection.
 // The EventHandlar for clieck event of jsPlumbConnection.
 export default function(elementEditor, jsPlumbConnection, event) {
-  // Check the event is processed already.
-  // Because the jsPlumb will call the event handler twice
-  // when a label is clicked that of a relation added after the initiation.
-  if (
-    elementEditor.getHandler().jsPlumbConnectionClicked &&
-    !event.processedByTextae
-  ) {
+  if (elementEditor.getHandler().jsPlumbConnectionClicked) {
     elementEditor
       .getHandler()
       .jsPlumbConnectionClicked(jsPlumbConnection, event)
   }
-  event.processedByTextae = true
 }
--- src/lib/editor/start/View/Renderer/RelationRenderer/renderLazy/render/Api.js
+++ src/lib/editor/start/View/Renderer/RelationRenderer/renderLazy/render/Api.js
@@ -1,13 +1,8 @@
-import getLabelOverlay from '../../../../../getLabelOverlay'
-
 // Extend module for jsPlumb.Connection.
 export default function() {
   return {
     bindClickAction(onClick) {
       this.bind('click', onClick)
-      getLabelOverlay(this).bind('click', (label, event) =>
-        onClick(label.component, event)
-      )
     }
   }
 }
