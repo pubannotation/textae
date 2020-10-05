eddbee9b32e624e9425e4b7349740ea893caaa1f
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-10-05 16:53:58 +0900

    blocks: Add the BlockSpanModel

---
+++ src/lib/editor/AnnotationData/SpanContainer/BlockSpanModel.js
@@ -0,0 +1,46 @@
+import { makeBlockSpanDomId } from '../../idFactory'
+import SpanModel from './SpanModel'
+
+export default class BlockSpanModel extends SpanModel {
+  constructor(editor, begin, end, entityContainer, spanContainer) {
+    super(editor, begin, end, spanContainer)
+    this._entityContainer = entityContainer
+  }
+
+  // Utility to distinguish with otehr type spans.
+  get isBlock() {
+    return true
+  }
+
+  get entities() {
+    return this._entityContainer.getAllOfSpan(this)
+  }
+
+  get id() {
+    return makeBlockSpanDomId(this._editor, this._begin, this._end)
+  }
+
+  get rectangle() {
+    const spanElement = this.element
+
+    // An element.offsetTop and element.offsetLeft does not work in the Firefox,
+    // when much spans are loaded like http://pubannotation.org/docs/sourcedb/PMC/sourceid/1315279/divs/10/annotations.json.
+    const rectOfSpan = spanElement.getBoundingClientRect()
+    const rectOfTextBox = spanElement.offsetParent.offsetParent.getBoundingClientRect()
+
+    return {
+      top: rectOfSpan.top - rectOfTextBox.top,
+      left: rectOfSpan.left,
+      width: rectOfSpan.width,
+      height: rectOfSpan.height
+    }
+  }
+
+  get backgroundId() {
+    return `bg_of_${this.id}`
+  }
+
+  get backgroundElement() {
+    return document.querySelector(`#${this.backgroundId}`)
+  }
+}
--- src/lib/editor/idFactory.js
+++ src/lib/editor/idFactory.js
@@ -3,6 +3,10 @@ export function makeDenotationSpanDomId(editor, begin, end) {
   return `${editor.editorId}__S${begin}_${end}`
 }
 
+export function makeBlockSpanDomId(editor, begin, end) {
+  return `${editor.editorId}__B${begin}_${end}`
+}
+
 // Exclude : and . from a dom id to use for ID selector.
 export function makeEntityDomId(editor, id) {
   return `${editor.editorId}__E${id.replace(/[:¥.]/g, '')}`
