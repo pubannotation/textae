ce8940c391b5343779bf05e24d55cb2c36588ed4
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-10-06 11:18:28 +0900

    blocks: Render block span

--- src/lib/editor/start/View/Renderer/bindAnnotationDataEvents/SpanRenderer/create/index.js
+++ src/lib/editor/start/View/Renderer/bindAnnotationDataEvents/SpanRenderer/create/index.js
@@ -1,5 +1,6 @@
 import renderDenotation from './renderDenotation'
 import destroy from '../destroy'
+import renderBlock from './renderBlock'
 
 // Destroy children spans to wrap a TextNode with <span> tag when new span over exists spans.
 export default function(span, entityRenderer) {
@@ -10,7 +11,13 @@ export default function(span, entityRenderer) {
   })
 
   span.traverse(
-    (span) => renderDenotation(span),
+    (span) => {
+      if (span.isBlock) {
+        renderBlock(span)
+      } else {
+        renderDenotation(span)
+      }
+    },
     (span) => {
       // When the child spans contain bold style spans, the width of the parent span changes.
       // Render the entity after the child span has been rendered.
---
+++ src/lib/editor/start/View/Renderer/bindAnnotationDataEvents/SpanRenderer/create/renderBlock.js
@@ -0,0 +1,12 @@
+import createRangeToSpan from './createRangeToSpan'
+
+export default function(span) {
+  const targetRange = createRangeToSpan(span)
+  const spanElement = document.createElement('div')
+
+  spanElement.setAttribute('id', span.id)
+  spanElement.setAttribute('title', span.id)
+  spanElement.classList.add('textae-editor__block')
+
+  targetRange.surroundContents(spanElement)
+}
