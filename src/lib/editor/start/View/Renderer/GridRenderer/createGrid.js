684106928ef0593109618f4f60eaf3252d8c8df2
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-10-06 18:40:48 +0900

    refactor: Use destructuring assignment

--- src/lib/editor/start/View/Renderer/GridRenderer/createGrid.js
+++ src/lib/editor/start/View/Renderer/GridRenderer/createGrid.js
@@ -2,17 +2,17 @@ import getRightGrid from './getRightGrid'
 import createGridHtml from './createGridHtml'
 
 export default function(editorDom, spanPositionCache, container, spanId) {
-  const spanPosition = spanPositionCache.get(spanId)
+  const { width } = spanPositionCache.get(spanId)
   const rightGrid = getRightGrid(editorDom, spanId)
 
   if (rightGrid) {
     // insert before the right grid.
-    const html = createGridHtml(spanId, spanPosition.width)
+    const html = createGridHtml(spanId, width)
     rightGrid.insertAdjacentHTML('beforebegin', html)
     return rightGrid.previousElementSibling
   } else {
     // append to the annotation area.
-    const html = createGridHtml(spanId, spanPosition.width)
+    const html = createGridHtml(spanId, width)
     container.insertAdjacentHTML('beforeend', html)
     return container.lastElementChild
   }
