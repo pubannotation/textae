02cb6f77fc4c6b9d13af4faf512f510c23d23986
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-10-14 16:22:56 +0900

    refactor: Use destructuring assignment syntax

--- src/lib/editor/start/Commander/Factory/ReplicateSpanCommand/getReplicationSpans/index.js
+++ src/lib/editor/start/Commander/Factory/ReplicateSpanCommand/getReplicationSpans/index.js
@@ -4,21 +4,16 @@ import isWord from './isWord'
 // Check replications are word or not if spanConfig is set.
 export default function(annotationData, originSpan, detectBoundaryFunc) {
   const wordFilter = detectBoundaryFunc
-    ? (span) =>
-        isWord(
-          annotationData.sourceDoc,
-          detectBoundaryFunc,
-          span.begin,
-          span.end
-        )
-    : (span) => span
+    ? ({ begin, end }) =>
+        isWord(annotationData.sourceDoc, detectBoundaryFunc, begin, end)
+    : () => true
 
   return getSpansTheirStringIsSameWith(annotationData.sourceDoc, originSpan)
     .filter(
-      (span) =>
+      ({ begin }) =>
         // The candidateSpan is a same span when begin is same.
         // Because string of each others are same. End of them are same too.
-        span.begin !== originSpan.begin
+        begin !== originSpan.begin
     )
     .filter(wordFilter)
     .filter(({ begin, end }) => !annotationData.span.hasObjectSpan(begin, end))
