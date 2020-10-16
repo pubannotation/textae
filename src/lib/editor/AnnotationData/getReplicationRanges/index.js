f7144f14613cea964fd4ce078df6dc10045ad9eb
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-10-16 15:04:37 +0900

    refactor: Rename arguments of the getRelicationRanges

--- src/lib/editor/AnnotationData/getReplicationRanges/index.js
+++ src/lib/editor/AnnotationData/getReplicationRanges/index.js
@@ -2,13 +2,19 @@ import getDuplicateSentenceFromText from './getDuplicateSentenceFromText'
 import isWord from './isWord'
 
 // Check replications are word or not if spanConfig is set.
-export default function(sourceDoc, begin1, end, span, isDelimiterFunc) {
-  return getDuplicateSentenceFromText(sourceDoc, begin1, end)
+export default function(
+  sourceDoc,
+  beginOfSample,
+  endOfSample,
+  span,
+  isDelimiterFunc
+) {
+  return getDuplicateSentenceFromText(sourceDoc, beginOfSample, endOfSample)
     .filter(
       ({ begin }) =>
         // The candidateSpan is a same span when begin is same.
         // Because string of each others are same. End of them are same too.
-        begin !== begin1
+        begin !== beginOfSample
     )
     .filter(({ begin, end }) => isWord(sourceDoc, begin, end, isDelimiterFunc))
     .filter(({ begin, end }) => !span.hasObjectSpan(begin, end))
