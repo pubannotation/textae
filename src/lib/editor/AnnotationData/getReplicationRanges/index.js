a9e05e99367909153f4f386e09d970b288346414
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-10-16 14:58:41 +0900

    refactor: The getDelimiterFunc return a function instead of null

--- src/lib/editor/AnnotationData/getReplicationRanges/index.js
+++ src/lib/editor/AnnotationData/getReplicationRanges/index.js
@@ -3,10 +3,6 @@ import isWord from './isWord'
 
 // Check replications are word or not if spanConfig is set.
 export default function(sourceDoc, begin1, end, span, isDelimiterFunc) {
-  const wordFilter = isDelimiterFunc
-    ? ({ begin, end }) => isWord(sourceDoc, isDelimiterFunc, begin, end)
-    : () => true
-
   return getDuplicateSentenceFromText(sourceDoc, begin1, end)
     .filter(
       ({ begin }) =>
@@ -14,7 +10,7 @@ export default function(sourceDoc, begin1, end, span, isDelimiterFunc) {
         // Because string of each others are same. End of them are same too.
         begin !== begin1
     )
-    .filter(wordFilter)
+    .filter(({ begin, end }) => isWord(sourceDoc, isDelimiterFunc, begin, end))
     .filter(({ begin, end }) => !span.hasObjectSpan(begin, end))
     .filter(
       ({ begin, end }) => !span.isBoundaryCrossingWithOtherSpans(begin, end)
--- src/lib/editor/start/Presenter/getIsDelimiterFunc.js
+++ src/lib/editor/start/Presenter/getIsDelimiterFunc.js
@@ -2,6 +2,6 @@ export default function(buttonController, spanConfig) {
   if (buttonController.valueOf('boundary-detection')) {
     return (char) => spanConfig.isDelimiter(char)
   } else {
-    return null
+    return () => true
   }
 }
