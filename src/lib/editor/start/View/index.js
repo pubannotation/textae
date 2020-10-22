bf5f4d7ec0a875e0b4e396e4bca8b7d9e5957c54
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-10-22 15:08:17 +0900

    bugfix: Relationship labels appear in front of the text in entity edit mode

--- src/lib/editor/start/View/index.js
+++ src/lib/editor/start/View/index.js
@@ -13,15 +13,18 @@ import GridHeight from './GridHeight'
 
 export default class {
   constructor(editor, annotationData, selectionModel, typeGap, typeDefinition) {
+    // Place the text box behind the annotation box to allow you
+    // to select the text behind the relationship label in entity editing mode.
+    const html = `
+    <div class="textae-editor__body">
+      <div class="textae-editor__body__annotation-box"></div>
+      <div class="textae-editor__body__text-box" id="${editor.editorId}_text-box"></div>
+    </div>
+    `
     // The editor itself has a "white-space: pre" style for processing text that contains a series of whitespace.
     // In this case, HTML line breaks are included in the editor's height calculation.
     // Remove CRLF so that it is not included in the height calculation.
-    editor[0].innerHTML = `
-    <div class="textae-editor__body">
-      <div class="textae-editor__body__text-box" id="${editor.editorId}_text-box"></div>
-      <div class="textae-editor__body__annotation-box"></div>
-    </div>
-    `.replace(/[\n\r]+/g, '')
+    editor[0].innerHTML = html.replace(/[\n\r]+/g, '')
 
     bindClipBoardEvents(editor)
     bindSelectionModelEvents(editor)
