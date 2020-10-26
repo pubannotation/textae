e013960e51a8768221c657049bf3c2c8ed5d1e61
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-10-26 17:00:46 +0900

    refactor: Add an input assertion

--- src/lib/editor/AnnotationData/isBoundaryCrossingWithOtherSpans/index.js
+++ src/lib/editor/AnnotationData/isBoundaryCrossingWithOtherSpans/index.js
@@ -3,6 +3,8 @@ import isBoundaryCrossing from '../../isBoundaryCrossing'
 // A span its range is coross over with other spans are not able to rendered.
 // Because spans are renderd with span tag. Html tags can not be cross over.
 export default function(spans, begin, end) {
+  console.assert(end, 'end is necessary.')
+
   return (
     spans.filter((existSpan) => isBoundaryCrossing(begin, end, existSpan))
       .length > 0
