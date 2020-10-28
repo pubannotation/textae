c3a910b1a9a16c7762c28c6106e76b6240dd55c0
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-10-28 14:29:16 +0900

    refactor: Add _ prefix to private fieleds

--- src/lib/editor/start/Commander/Factory/CreateAttributeDefinitionCommand.js
+++ src/lib/editor/start/Commander/Factory/CreateAttributeDefinitionCommand.js
@@ -5,27 +5,27 @@ import ConfigurationCommand from './ConfigurationCommand'
 export default class extends ConfigurationCommand {
   constructor(typeContainer, attrDef, index) {
     super()
-    this.typeContainer = typeContainer
-    this.newAttrDef = attrDef
-    this.index = index
+    this._typeContainer = typeContainer
+    this._newAttrDef = attrDef
+    this._index = index
   }
 
   execute() {
     // Added default properties to newly created numeric attribute.
-    if (this.newAttrDef['value type'] === 'numeric') {
-      this.newAttrDef.min = this.newAttrDef.min || 0
-      this.newAttrDef.max = this.newAttrDef.max || 0
-      this.newAttrDef.step = this.newAttrDef.step || 0
-      this.newAttrDef.default = this.newAttrDef.default || 0
+    if (this._newAttrDef['value type'] === 'numeric') {
+      this._newAttrDef.min = this._newAttrDef.min || 0
+      this._newAttrDef.max = this._newAttrDef.max || 0
+      this._newAttrDef.step = this._newAttrDef.step || 0
+      this._newAttrDef.default = this._newAttrDef.default || 0
     }
 
     // Added default value to newly created selection attribute definition.
     // Except when undoing the deletion of selection attribute definition.
     if (
-      this.newAttrDef['value type'] === 'selection' &&
-      !this.newAttrDef.values
+      this._newAttrDef['value type'] === 'selection' &&
+      !this._newAttrDef.values
     ) {
-      this.newAttrDef.values = [
+      this._newAttrDef.values = [
         {
           id: 'default',
           default: true
@@ -33,19 +33,19 @@ export default class extends ConfigurationCommand {
       ]
     }
 
-    this.typeContainer.createAttribute(this.newAttrDef, this.index)
+    this._typeContainer.createAttribute(this._newAttrDef, this._index)
 
     commandLog(
       `create a new attrribute definition:${JSON.stringify(
-        this.newAttrDef
-      )}, index: ${this.index}`
+        this._newAttrDef
+      )}, index: ${this._index}`
     )
   }
 
   revert() {
     return new DeleteAttributeDefinitionCommand(
-      this.typeContainer,
-      this.newAttrDef
+      this._typeContainer,
+      this._newAttrDef
     )
   }
 }
