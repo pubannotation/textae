efc8fc1c824a1950b299bbc2475e72fc1a331bfd
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-10-29 02:04:03 +0900

    blocks: Add block mode to the MODE.js

--- src/lib/MODE.js
+++ src/lib/MODE.js
@@ -4,5 +4,7 @@ export const MODE = {
   VIEW_WITH_RELATION: 'View Instance',
   EDIT_DENOTATION_WITHOUT_RELATION: 'Term Centric',
   EDIT_DENOTATION_WITH_RELATION: 'Instance / Relation',
+  EDIT_BLOCK_WITHOUT_RELATION: 'Block Centric',
+  EDIT_BLOCK_WITH_RELATION: 'Block / Relation',
   EDIT_RELATION: 'Relation Edit'
 }
