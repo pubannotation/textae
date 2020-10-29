02a762cce93879a60b6d9556ef0505a711f365c4
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-10-30 07:21:07 +0900

    refactor: Make setMode function a method of PushButtons

--- src/lib/ButtonController/PushButtons/index.js
+++ src/lib/ButtonController/PushButtons/index.js
@@ -1,6 +1,6 @@
 import buttonConfig from '../../buttonConfig'
+import { MODE } from '../../MODE'
 import Button from './Button'
-import setMode from './setMode'
 
 export default class {
   constructor(editor) {
@@ -11,7 +11,7 @@ export default class {
 
     // Bind an event.
     editor.eventEmitter.on('textae.editMode.transition', (mode) =>
-      setMode(this, mode)
+      this._setMode(mode)
     )
 
     // default pushed;
@@ -30,5 +30,42 @@ export default class {
 
   get names() {
     return this._buttonMap.keys()
+  }
+
+  _setMode(mode) {
+    switch (mode) {
+      case MODE.VIEW_WITHOUT_RELATION:
+        this.getButton('view').value(true)
+        this.getButton('term').value(false)
+        this.getButton('relation').value(false)
+        this.getButton('simple').value(true)
+        break
+      case MODE.VIEW_WITH_RELATION:
+        this.getButton('view').value(true)
+        this.getButton('term').value(false)
+        this.getButton('relation').value(false)
+        this.getButton('simple').value(false)
+        break
+      case MODE.EDIT_DENOTATION_WITHOUT_RELATION:
+        this.getButton('view').value(false)
+        this.getButton('term').value(true)
+        this.getButton('relation').value(false)
+        this.getButton('simple').value(true)
+        break
+      case MODE.EDIT_DENOTATION_WITH_RELATION:
+        this.getButton('view').value(false)
+        this.getButton('term').value(true)
+        this.getButton('relation').value(false)
+        this.getButton('simple').value(false)
+        break
+      case MODE.EDIT_RELATION:
+        this.getButton('view').value(false)
+        this.getButton('term').value(false)
+        this.getButton('relation').value(true)
+        this.getButton('simple').value(false)
+        break
+      default:
+        throw `unknown edit mode!${mode}`
+    }
   }
 }
