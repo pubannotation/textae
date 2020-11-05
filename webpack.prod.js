6fb181ba58a364fc3ec943595ba7c6e1b52c3ec4
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-11-05 11:04:44 +0900

    misc: Do not generate a license file

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,20 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
   output: {
     path: path.resolve(__dirname, 'src/lib'),
     filename: 'bundle.js'
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false
+      })
+    ]
   }
 })
