555facb6579ac73b9fedbcecc6d1823c1ee5c11c
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-11-06 17:36:31 +0900

    bugfix: The type retrieved from the information of the instance is displayed in the palette. When you change the ID of this type, the ID of the type added is the value before the change

--- src/lib/editor/start/Commander/Factory/ChangeTypeDefinitionAndRefectInstancesCommand/createChangeConfigCommand.js
+++ src/lib/editor/start/Commander/Factory/ChangeTypeDefinitionAndRefectInstancesCommand/createChangeConfigCommand.js
@@ -22,10 +22,18 @@ export default function(
       null
     )
   } else {
+    // The change properties contain only the changed attributes.
+    // When the ID is changed, it does not overwrite the ID with the old ID.
+    // When you add a label, the old ID is used to add the type definition.
     return new CreateTypeDefinitionCommand(
       editor,
       typeDefinition,
-      Object.fromEntries(changedProperties)
+      Object.assign(
+        {
+          id
+        },
+        Object.fromEntries(changedProperties)
+      )
     )
   }
 }
