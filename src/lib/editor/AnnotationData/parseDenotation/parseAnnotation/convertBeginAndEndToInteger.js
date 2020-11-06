d7a10475974c179ef2cdf24094949af28ce8bcd8
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-11-06 14:34:44 +0900

    refactor: Fix comment

--- src/lib/editor/AnnotationData/parseDenotation/parseAnnotation/convertBeginAndEndToInteger.js
+++ src/lib/editor/AnnotationData/parseDenotation/parseAnnotation/convertBeginAndEndToInteger.js
@@ -1,5 +1,7 @@
-// If the begin or end value is a string, the comparison with other numbers cannot be done correctly.
-// You cannot generate a valid value for the ID of Dom of span from a begin or end that contains a decimal point.
+// If the begin or end value is a string,
+// the comparison with other numbers cannot be done correctly.
+// You cannot generate a valid value for the ID of HTML element of span
+// from a begin or end that contains a decimal point.
 export default function (span) {
   return Object.assign({}, span, {
     begin: parseInt(span.begin),
