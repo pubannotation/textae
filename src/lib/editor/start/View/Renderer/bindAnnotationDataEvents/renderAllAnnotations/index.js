030349a0031566c36c62d8ae0128e487c9cc48f2
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-11-06 09:44:41 +0900

    refactor: Use Element.innerHtml instead of jQuery.empty in the renderAllAnnotation

--- src/lib/editor/start/View/Renderer/bindAnnotationDataEvents/renderAllAnnotations/index.js
+++ src/lib/editor/start/View/Renderer/bindAnnotationDataEvents/renderAllAnnotations/index.js
@@ -2,6 +2,6 @@ import getAnnotationBox from '../../getAnnotationBox'
 import renderAllSpan from './renderAllSpan'
 
 export default function (editor, annotationData, spanRenderer) {
-  getAnnotationBox(editor).empty()
+  getAnnotationBox(editor)[0].innerHTML = ''
   renderAllSpan(annotationData, spanRenderer)
 }
