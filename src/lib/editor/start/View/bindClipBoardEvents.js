9d65f8f09e7720c6adf824e62c48b8fe0cc915e7
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-11-11 12:12:04 +0900

    refactor; Use the EntityModel.element in the bindClipBoardEvents

--- src/lib/editor/start/View/bindClipBoardEvents.js
+++ src/lib/editor/start/View/bindClipBoardEvents.js
@@ -1,23 +1,16 @@
-import getEntityHTMLelementFromChild from '../getEntityHTMLelementFromChild'
-import getEntityEndopoint from './getEntityEndopoint'
-
 const cssClass = 'ui-to-be-cut'
 
 export default function (editor) {
   editor.eventEmitter.on('textae.clipBoard.change', (added, removed) => {
-    for (const e of added) {
-      const el = getEntityEndopoint(editor, e.id)
-      if (el) {
-        const entityHTMLElement = getEntityHTMLelementFromChild(el)
-        entityHTMLElement.classList.add(cssClass)
+    for (const entity of added) {
+      if (entity.element) {
+        entity.element.classList.add(cssClass)
       }
     }
 
-    for (const e of removed) {
-      const el = getEntityEndopoint(editor, e.id)
-      if (el) {
-        const entityHTMLElement = getEntityHTMLelementFromChild(el)
-        entityHTMLElement.classList.remove(cssClass)
+    for (const entity of removed) {
+      if (entity.element) {
+        entity.element.classList.remove(cssClass)
       }
     }
   })
