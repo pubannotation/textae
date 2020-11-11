bd14e35344b4633e803ac5007b4963d04adc89c1
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-11-11 12:21:04 +0900

    refactor: Pass an entity model instead of id to the EntityHTMLelementSelector

--- src/lib/editor/start/View/EntityHTMLelementSelector.js
+++ src/lib/editor/start/View/EntityHTMLelementSelector.js
@@ -1,26 +1,15 @@
-import getEntityEndopoint from './getEntityEndopoint'
-import getEntityHTMLelementFromChild from '../getEntityHTMLelementFromChild'
 import SELECTED from '../../SELECTED'
 
 export default class {
-  constructor(editor) {
-    this._editor = editor
-  }
-
-  select(id) {
-    const el = getEntityEndopoint(this._editor, id)
-    const entityHTMLElement = getEntityHTMLelementFromChild(el)
-    entityHTMLElement.classList.add(SELECTED)
+  select(entity) {
+    const el = entity.element
+    el.classList.add(SELECTED)
 
     // Set focus to the label element in order to scroll the browser to the position of the element.
-    entityHTMLElement
-      .querySelector('.textae-editor__entity__type-label')
-      .focus()
+    el.querySelector('.textae-editor__entity__type-label').focus()
   }
 
-  deselect(id) {
-    const el = getEntityEndopoint(this._editor, id)
-    const entityHTMLElement = getEntityHTMLelementFromChild(el)
-    entityHTMLElement.classList.remove(SELECTED)
+  deselect(entity) {
+    entity.element.classList.remove(SELECTED)
   }
 }
--- src/lib/editor/start/View/bindSelectionModelEvents.js
+++ src/lib/editor/start/View/bindSelectionModelEvents.js
@@ -1,16 +1,16 @@
 import EntityHTMLelementSelector from './EntityHTMLelementSelector'
 
 export default function (editor) {
-  const entityHtmlelementSelector = new EntityHTMLelementSelector(editor)
+  const entityHtmlelementSelector = new EntityHTMLelementSelector()
 
   editor.eventEmitter
     .on('textae.selection.span.select', (span) => span.select())
     .on('textae.selection.span.deselect', (span) => span.deselect())
     .on('textae.selection.entity.select', (entity) =>
-      entityHtmlelementSelector.select(entity.id)
+      entityHtmlelementSelector.select(entity)
     )
     .on('textae.selection.entity.deselect', (entity) =>
-      entityHtmlelementSelector.deselect(entity.id)
+      entityHtmlelementSelector.deselect(entity)
     )
     .on('textae.selection.relation.select', (relation) =>
       setTimeout(() => relation.selectJsPlumbConnection(), 150)
