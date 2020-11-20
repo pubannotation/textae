ab8511ffb49637f2a26eb6732c69866f92f9fbfe
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-11-21 07:02:44 +0900

    feature: Create a block when in block mode, mouse down on the text and mouse up on the denotation span

--- src/lib/editor/start/Presenter/EditMode/TypeEditor/ElementEditor/EditBlock/SpanEditor.js
+++ src/lib/editor/start/Presenter/EditMode/TypeEditor/ElementEditor/EditBlock/SpanEditor.js
@@ -24,6 +24,7 @@ export default class SpanEditor {
     if (selectionWrapper.isParentOfAnchorNodeTextBox) {
       if (
         selectionWrapper.isParentOfFocusNodeTextBox ||
+        selectionWrapper.isParentOfFocusNodeDenotationSpan ||
         selectionWrapper.isParentOfFocusNodeStyleSpan
       ) {
         this._create(selectionWrapper)
