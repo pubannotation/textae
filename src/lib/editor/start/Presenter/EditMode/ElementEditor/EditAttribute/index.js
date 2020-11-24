fc836f142ec0c446ada8bf834d21348f5e9edd4d
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-11-25 00:10:26 +0900

    refactor: Entity palette already added, no need to add in the EditAttribute

--- src/lib/editor/start/Presenter/EditMode/ElementEditor/EditAttribute/index.js
+++ src/lib/editor/start/Presenter/EditMode/ElementEditor/EditAttribute/index.js
@@ -7,7 +7,6 @@ export default class {
     this._annotationData = annotationData
     this._selectionModel = selectionModel
     this._pallet = entityPallet
-    editor[0].appendChild(this._pallet.el)
 
     bindTextaeEvents(editor, selectionModel, commander)
   }
