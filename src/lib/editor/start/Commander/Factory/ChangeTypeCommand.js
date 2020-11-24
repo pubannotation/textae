0701c82f56ca4f5dd0184d4110e3c7be2969fb70
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-11-24 18:23:54 +0900

    refactor: Rename modelType of the ChangeTypeCommand to the annotationType

--- src/lib/editor/start/Commander/Factory/ChangeTypeCommand.js
+++ src/lib/editor/start/Commander/Factory/ChangeTypeCommand.js
@@ -2,25 +2,27 @@ import commandLog from './commandLog'
 import AnnotationCommand from './AnnotationCommand'
 
 export default class ChangeTypeCommand extends AnnotationCommand {
-  constructor(editor, annotationData, modelType, id, newType) {
+  constructor(editor, annotationData, annotationType, id, newType) {
     super()
     this._editor = editor
     this._annotationData = annotationData
-    this._modelType = modelType
+    this._annotationType = annotationType
     this._id = id
     this._newType = newType
   }
 
   execute() {
-    this.oldType = this._annotationData[this._modelType].get(this._id).typeName
+    this.oldType = this._annotationData[this._annotationType].get(
+      this._id
+    ).typeName
 
     // Update model
-    const targetModel = this._annotationData[this._modelType].changeType(
+    const targetModel = this._annotationData[this._annotationType].changeType(
       this._id,
       this._newType
     )
     commandLog(
-      `change type of a ${this._modelType}. oldtype:${this.oldType} ${this._modelType}:`,
+      `change type of a ${this._annotationType}. oldtype:${this.oldType} ${this._annotationType}:`,
       targetModel
     )
   }
@@ -29,7 +31,7 @@ export default class ChangeTypeCommand extends AnnotationCommand {
     return new ChangeTypeCommand(
       this._editor,
       this._annotationData,
-      this._modelType,
+      this._annotationType,
       this._id,
       this.oldType
     )
