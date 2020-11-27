883dfe9800ef5e7ed8559da2aeb737aa412aba33
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-11-27 13:35:35 +0900

    refactor: Use the browser's DOM API instead of the jQuery's API in the changeCssClass

--- src/lib/editor/start/Presenter/EditMode/StateMachine/Transition/ViewMode/changeCssClass.js
+++ src/lib/editor/start/Presenter/EditMode/StateMachine/Transition/ViewMode/changeCssClass.js
@@ -1,8 +1,9 @@
 export default function (editor, mode) {
-  editor
-    .removeClass('textae-editor__mode--denotation-without-relation')
-    .removeClass('textae-editor__mode--denotation-with-relation')
-    .removeClass('textae-editor__mode--block-with-relation')
-    .removeClass('textae-editor__mode--relation')
-    .addClass(`textae-editor__mode--${mode}`)
+  for (const cssClass of editor[0].classList) {
+    if (cssClass.startsWith('textae-editor__mode')) {
+      editor[0].classList.remove(cssClass)
+    }
+  }
+
+  editor[0].classList.add(`textae-editor__mode--${mode}`)
 }
