1d031919126e47f57ff40aa681364529db8487e6
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-11-28 06:37:24 +0900

    refactor: Delete the SubjectValidation

--- src/lib/editor/AnnotationData/parseAnnotation/parseTrack/validateAnnotation/SubjectValidation.js
+++
@@ -1,8 +0,0 @@
-import isContains from './isContains'
-import Validation from './Validation'
-
-export default class SubjectValidation extends Validation {
-  constructor(denotations, nodes) {
-    super(nodes, (node) => isContains(denotations, node.subj))
-  }
-}
