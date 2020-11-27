c92f5615b1c358494085265c6aeea1ca83e435c1
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-11-27 18:19:10 +0900

    refactor: Modify the decision conditions and eliminate the extension points of the validation class

--- src/lib/editor/AnnotationData/parseAnnotation/parseTrack/validateAnnotation/UniqueAttributeValidaiton.js
+++ src/lib/editor/AnnotationData/parseAnnotation/parseTrack/validateAnnotation/UniqueAttributeValidaiton.js
@@ -3,14 +3,11 @@ import Validation from './Validation'
 // An entity cannot have two attributes of the same predicate.
 export default class UniqueAttributeValidaiton extends Validation {
   constructor(attributes) {
-    super(attributes)
-  }
-
-  _validate(currentNode, index) {
-    const prevNodes = this._nodes.slice(0, index)
-
-    return !prevNodes.some(
-      (node) => node.subj === currentNode.subj && node.pred === currentNode.pred
+    super(
+      attributes,
+      (node) =>
+        attributes.filter((a) => a.subj === node.subj && a.pred === node.pred)
+          .length === 1
     )
   }
 }
