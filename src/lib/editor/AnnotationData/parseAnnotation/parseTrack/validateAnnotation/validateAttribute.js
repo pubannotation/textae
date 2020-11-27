175debc59f99820ed9c29b82b391987883798ee1
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-11-28 06:36:08 +0900

    refactor: Use the ChainValidation in the validateAttribute

--- src/lib/editor/AnnotationData/parseAnnotation/parseTrack/validateAnnotation/validateAttribute.js
+++ src/lib/editor/AnnotationData/parseAnnotation/parseTrack/validateAnnotation/validateAttribute.js
@@ -1,18 +1,25 @@
-import SubjectValidation from './SubjectValidation'
-import UniqueAttributeValidation from './UniqueAttributeValidaiton'
+import isContains from './isContains'
+import ChainValidation from './ChainValidation'
+
+function areSubjAndPredUniqueIn(attributes, node) {
+  return (
+    attributes.filter((a) => a.subj === node.subj && a.pred === node.pred)
+      .length === 1
+  )
+}
 
 export default function (denotations, attributes) {
-  const subjectValidation = new SubjectValidation(denotations, attributes)
-  const uniqValidation = new UniqueAttributeValidation(
-    subjectValidation.validNodes
-  )
+  const [accepts, errorMap] = new ChainValidation(attributes)
+    .and('subject', (a) => isContains(denotations, a.subj))
+    .and('unique', (node) => areSubjAndPredUniqueIn(attributes, node))
+    .validate()
 
   return {
-    accept: uniqValidation.validNodes,
+    accept: accepts,
     reject: {
-      subj: subjectValidation.invalidNodes,
-      duplicatedAttributes: uniqValidation.invalidNodes
+      subj: errorMap.get('subject') || [],
+      duplicatedAttributes: errorMap.get('unique') || []
     },
-    hasError: subjectValidation.invalid || uniqValidation.invalid
+    hasError: errorMap.size
   }
 }
