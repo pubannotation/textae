e5b43e029ede05b66610433c962997514c496634
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-11-30 17:15:50 +0900

    refactor: Use the ChainValidation in the validateDenotation

--- src/lib/editor/AnnotationData/parseAnnotation/parseTrack/validateAnnotation/validateDenotation.js
+++ src/lib/editor/AnnotationData/parseAnnotation/parseTrack/validateAnnotation/validateDenotation.js
@@ -1,15 +1,26 @@
+import getSpanValidation from './getSpanValidation'
 import UniqueIDValidation from './UniqueIDValidation'
 import validateSpan from './validateSpan'
 
 export default function (text, denotations, spans) {
+  const [accept, errorMap] = getSpanValidation(denotations, text, spans)
+    .and(
+      'uniqueID',
+      (n) => denotations.filter((d) => d.id === n.id).length === 1
+    )
+    .validate()
+
   const result = validateSpan(text, denotations, spans)
   const uniqIDValidation = new UniqueIDValidation(result.accept)
 
   return {
-    accept: uniqIDValidation.validNodes,
-    reject: Object.assign(result.reject, {
-      duplicatedID: uniqIDValidation.invalidNodes
-    }),
-    hasError: result.hasError || uniqIDValidation.invalid
+    accept,
+    reject: {
+      wrongRange: errorMap.get('hasLength'),
+      outOfText: errorMap.get('inText'),
+      boundaryCrossingSpans: errorMap.get('isNotCrossing'),
+      duplicatedID: errorMap.get('uniqueID')
+    },
+    hasError: errorMap.size
   }
 }
