08f94e49bf28c70d5bcc2af6e270cc48318d76f5
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-11-30 17:26:57 +0900

    refactor: Use the ChainValidation in the validateBlock

--- src/lib/editor/AnnotationData/parseAnnotation/parseTrack/validateAnnotation/validateBlock.js
+++ src/lib/editor/AnnotationData/parseAnnotation/parseTrack/validateAnnotation/validateBlock.js
@@ -1,8 +1,21 @@
+import getSpanValidation from './getSpanValidation'
 import UniqueIDValidation from './UniqueIDValidation'
 import validateSpan from './validateSpan'
 import Validation from './Validation'
 
 export default function (text, blocks, spans) {
+  const [accept, errorMap] = getSpanValidation(blocks, text, spans)
+    .and('uniqueID', (n) => blocks.filter((d) => d.id === n.id).length === 1)
+    .and(
+      'uniqueRange',
+      ({ span }) =>
+        blocks.filter(
+          ({ span: otherSpan }) =>
+            (span.begin === otherSpan.begin) & (span.end === otherSpan.end)
+        ).length === 1
+    )
+    .validate()
+
   const result = validateSpan(text, blocks, spans)
   const uniqIDValidation = new UniqueIDValidation(result.accept)
   const uniqRangeValidation = new Validation(
@@ -15,12 +28,14 @@ export default function (text, blocks, spans) {
   )
 
   return {
-    accept: uniqRangeValidation.validNodes,
-    reject: Object.assign(result.reject, {
-      duplicatedID: uniqIDValidation.invalidNodes,
-      duplicatedRange: uniqRangeValidation.invalidNodes
-    }),
-    hasError:
-      result.hasError || uniqIDValidation.invalid || uniqRangeValidation.invalid
+    accept,
+    reject: {
+      wrongRange: errorMap.get('hasLength'),
+      outOfText: errorMap.get('inText'),
+      boundaryCrossingSpans: errorMap.get('isNotCrossing'),
+      duplicatedID: errorMap.get('uniqueID'),
+      duplicatedRange: errorMap.get('uniqueRange')
+    },
+    hasError: errorMap.size
   }
 }
