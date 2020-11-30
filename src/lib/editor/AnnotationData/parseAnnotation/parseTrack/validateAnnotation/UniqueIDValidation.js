7e22d4005b5a76e6b34bf109dd17b249218d5319
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-11-30 17:28:03 +0900

    refactor: Delete the UniqueIDValidation

--- src/lib/editor/AnnotationData/parseAnnotation/parseTrack/validateAnnotation/UniqueIDValidation.js
+++
@@ -1,7 +0,0 @@
-import Validation from './Validation'
-
-export default class UniquIDValidation extends Validation {
-  constructor(nodes) {
-    super(nodes, (node) => nodes.filter((n) => n.id === node.id).length === 1)
-  }
-}
