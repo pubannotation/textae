e12a7f535db67704efde20986f29fbb09f063a7c
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-01 17:41:59 +0900

    refactor: Do not set the textae-editor-editable class

--- src/lib/editor/start/Presenter/EditMode/StateMachine/Transition/ViewMode/index.js
+++ src/lib/editor/start/Presenter/EditMode/StateMachine/Transition/ViewMode/index.js
@@ -1,4 +1,3 @@
-import setEditableStyle from './setEditableStyle'
 import changeCssClass from './changeCssClass'
 
 export default class ViewMode {
@@ -8,36 +7,29 @@ export default class ViewMode {
 
   setViewWithRelation() {
     changeCssClass(this._editor, 'view-with-relation')
-    setEditableStyle(this._editor, false)
   }
 
   setViewWithoutRelation() {
     changeCssClass(this._editor, 'view-without-relation')
-    setEditableStyle(this._editor, false)
   }
 
   setDenotationWithRelation() {
     changeCssClass(this._editor, 'denotation-with-relation')
-    setEditableStyle(this._editor, true)
   }
 
   setDenotationWithoutRelation() {
     changeCssClass(this._editor, 'denotation-without-relation')
-    setEditableStyle(this._editor, true)
   }
 
   setBlockWithRelation() {
     changeCssClass(this._editor, 'block-with-relation')
-    setEditableStyle(this._editor, true)
   }
 
   setBlockWithoutRelation() {
     changeCssClass(this._editor, 'block-without-relation')
-    setEditableStyle(this._editor, true)
   }
 
   setRelation() {
     changeCssClass(this._editor, 'relation')
-    setEditableStyle(this._editor, true)
   }
 }
--- src/lib/editor/start/Presenter/EditMode/StateMachine/Transition/ViewMode/setEditableStyle.js
+++
@@ -1,7 +0,0 @@
-export default function (editor, isEditable) {
-  if (isEditable) {
-    editor.classList.add('textae-editor--editable')
-  } else {
-    editor.classList.remove('textae-editor--editable')
-  }
-}
