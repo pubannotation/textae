4f7e0e679bf21ca9d304f247a891fe992c360ce9
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-01 17:44:43 +0900

    refactor: Delete the changeCssClass

--- src/lib/editor/start/Presenter/EditMode/StateMachine/Transition/ViewMode/changeCssClass.js
+++
@@ -1,9 +0,0 @@
-export default function (editor, mode) {
-  for (const cssClass of editor.classList) {
-    if (cssClass.startsWith('textae-editor__mode')) {
-      editor.classList.remove(cssClass)
-    }
-  }
-
-  editor.classList.add(`textae-editor__mode--${mode}`)
-}
