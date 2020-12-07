82bdab2ef58aa9b9f5e42868be0bf3b5ff3470d6
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-07 18:35:30 +0900

    refactor: Pass the spanContainer to the getCurrentMaxHeight

--- src/lib/editor/AnnotationData/GridRectangle/getCurrentMaxHeight.js
+++ src/lib/editor/AnnotationData/GridRectangle/getCurrentMaxHeight.js
@@ -3,13 +3,10 @@ import getHeightIncludeDescendantGrids from './getHeightIncludeDescendantGrids'
 const TEXT_HEIGHT = 23
 const MARGIN_TOP = 30
 
-export default function (annotationData) {
+export default function (spanContainer) {
   const maxHeight = Math.max(
-    ...annotationData.span.allDenotationSpans.map((span) =>
-      getHeightIncludeDescendantGrids(
-        span,
-        annotationData.span._entityGap.value
-      )
+    ...spanContainer.allDenotationSpans.map((span) =>
+      getHeightIncludeDescendantGrids(span, spanContainer._entityGap.value)
     )
   )
   return maxHeight + TEXT_HEIGHT + MARGIN_TOP
