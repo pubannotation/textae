f0594a16ce3589c6b3242a53bdcfaedd7fb0d073
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-07 23:21:51 +0900

    refactor: Delete an unused file

--- src/lib/editor/start/Presenter/EventMap/SelectHandler/const.js
+++
@@ -1,4 +0,0 @@
-export const SPAN_CLASS = 'textae-editor__span'
-export const GRID_CLASS = 'textae-editor__grid'
-export const TYPE_CLASS = 'textae-editor__type'
-export const ENTINY_CLASS = 'textae-editor__entity'
