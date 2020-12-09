ce82b80e6dee51cca127ef99c1396c97640091cf
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-09 16:21:43 +0900

    bugfix: Erasing a span containing the beginning of the text by shrinking it backwards causes an error. Occurs in 6.1.53

--- src/lib/editor/AnnotationData/isBoundaryCrossingWithOtherSpans.js
+++ src/lib/editor/AnnotationData/isBoundaryCrossingWithOtherSpans.js
@@ -3,7 +3,7 @@ import getBoundaryCrossingSpans from './getBoundaryCrossingSpans'
 // A span its range is coross over with other spans are not able to rendered.
 // Because spans are renderd with span tag. Html tags can not be cross over.
 export default function (spans, begin, end) {
-  console.assert(end, 'end is necessary.')
+  console.assert(end !== undefined, 'end is necessary.')
 
   return (
     getBoundaryCrossingSpans(
