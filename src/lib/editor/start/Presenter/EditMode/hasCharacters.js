e228b90e3d70706b32c23ec2c9314a22f6925aaf
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-10 09:59:55 +0900

    refactor: Inline the Positions.selectedString property into the hasCharacters

--- src/lib/editor/start/Presenter/EditMode/hasCharacters.js
+++ src/lib/editor/start/Presenter/EditMode/hasCharacters.js
@@ -1,8 +1,14 @@
+import OrderedPositions from './OrderedPositions'
 import Positions from './Positions'
 
 // A span cannot be created include nonEdgeCharacters only.
 export default function (annotationData, spanConfig, selectionWrapper) {
   const positions = new Positions(annotationData, selectionWrapper)
+  const orderedPositions = new OrderedPositions(positions)
+  const selectedString = annotationData.sourceDoc.substring(
+    orderedPositions.begin,
+    orderedPositions.end
+  )
 
-  return spanConfig.removeBlankChractors(positions.selectedString).length > 0
+  return spanConfig.removeBlankChractors(selectedString).length > 0
 }
