f8dbaba5efb4afc91cd0d7dcdc03bdf1c2b25c71
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-10 09:56:08 +0900

    refactor: Delete the begin and end properties of the Positions

--- src/lib/editor/start/Presenter/EditMode/Positions/index.js
+++ src/lib/editor/start/Presenter/EditMode/Positions/index.js
@@ -23,24 +23,6 @@ export default class Positions {
     return position + this._selection.focusOffset
   }
 
-  // switch the position when the selection is made from right to left
-  get begin() {
-    if (this.anchor < this.focus) {
-      return this.anchor
-    }
-
-    return this.focus
-  }
-
-  // switch the position when the selection is made from right to left
-  get end() {
-    if (this.anchor < this.focus) {
-      return this.focus
-    }
-
-    return this.anchor
-  }
-
   get selectedString() {
     const orderedPositions = new OrderedPositions(this)
     return this._annotationData.sourceDoc.substring(
