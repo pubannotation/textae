87790e9bb0b71fdb948bf8dfc20e595716e5fc56
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-21 21:54:02 +0900

    refactor: Use the compileHandlebarsTemplate in the CreateAttributeDefinitionDIalog

--- src/lib/component/CreateAttributeDefinitionDialog/index.js
+++ src/lib/component/CreateAttributeDefinitionDialog/index.js
@@ -1,13 +1,54 @@
 import delegate from 'delegate'
 import PromiseDialog from '../PromiseDialog'
-import createContentHtml from './createContentHtml'
 import getInputElementValue from '../getInputElementValue'
+import compileHandlebarsTemplate from './../compileHandlebarsTemplate'
+
+const template = compileHandlebarsTemplate(`
+<div class="textae-editor__create-attribute-definition-dialog__container">
+  <div class="textae-editor__create-attribute-definition-dialog__row">
+    <label>Attribute type:</label>
+    <select class="textae-editor__create-attribute-definition-dialog__value-type">
+      <option value="flag"{{#if flagSelected}} selected{{/if}}>flag</option>
+      <option value="selection"{{#if selectionSelected}} selected{{/if}}>selection</option>
+      <option value="string"{{#if stringSelected}} selected{{/if}}>string</option>
+      <option value="numeric"{{#if numericSelected}} selected{{/if}}>numeric</option>
+    </select>
+  </div>
+  <div class="textae-editor__create-attribute-definition-dialog__row">
+    <div class="textae-editor__create-attribute-definition-dialog__pred">
+      <label>Predicate:</label><br>
+      <input value="{{pred}}">
+    </div>
+    {{#if showDefault}}
+      <div class="textae-editor__create-attribute-definition-dialog__default">
+        <label>Default:</label><br>
+        <input value="{{default}}">
+      </div>
+    {{/if}}
+  </div>
+  {{#if showNumeric}}
+    <div class="textae-editor__create-attribute-definition-dialog__row">
+      <div class="textae-editor__create-attribute-definition-dialog__min">
+        <label>Min:</label><br>
+        <input type="text" value="{{min}}">
+      </div>
+      <div class="textae-editor__create-attribute-definition-dialog__max">
+        <label>Max:</label><br>
+        <input type="text" value="{{max}}">
+      </div>
+      <div class="textae-editor__create-attribute-definition-dialog__step">
+        <label>Step:</label><br>
+        <input type="text" value="{{step}}">
+      </div>
+    </div>
+  {{/if}}
+</div>`)
 
 export default class CreateAttributeDefinitionDialog extends PromiseDialog {
   constructor() {
     super(
       'Please enter new attribute definition',
-      createContentHtml({}),
+      template({}),
       {},
       '.textae-editor__create-attribute-definition-dialog__pred',
       () => {
@@ -97,7 +138,7 @@ export default class CreateAttributeDefinitionDialog extends PromiseDialog {
         }
         state[`${valueType}Selected`] = true
 
-        const html = createContentHtml(state)
+        const html = template(state)
         super.el.closest('.ui-dialog-content').innerHTML = html
       }
     )
