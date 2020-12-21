a3bb9bb8840f53f087a194f2fd7ec16d2c16fa63
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-21 22:43:06 +0900

    refactor: Use the compileHandlebarsTemplate in the TypeDefinitionDialog

--- src/lib/component/TypeDefinitionDialog/index.js
+++ src/lib/component/TypeDefinitionDialog/index.js
@@ -1,6 +1,24 @@
 import PromiseDialog from '../PromiseDialog'
-import createContentHtml from './createContentHtml'
 import setSourceOfAutoComplete from './setSourceOfAutoComplete'
+import compileHandlebarsTemplate from '../compileHandlebarsTemplate'
+
+const template = compileHandlebarsTemplate(`
+<div class="textae-editor__edit-type-definition-dialog__container">
+  <div class="textae-editor__edit-type-definition-dialog__input-box">
+  <label>Id:</label><br>
+  <input class="textae-editor__edit-type-definition-dialog--id" value={{id}}>
+  </div>
+  <div class="textae-editor__edit-type-definition-dialog__input-box">
+  <label>Label:<span></span></label><br>
+  <input value="{{label}}">
+  </div>
+  <div class="textae-editor__edit-type-definition-dialog__color-picker">
+  <label>Color:<input class="textae-editor__edit-type-definition-dialog__color-picker__input" type="color" value="{{color}}"></label>
+  </div>
+  <div class="textae-editor__edit-type-definition-dialog__set-default">
+  <label>Default type:<input class="textae-editor__edit-type-definition-dialog__set-default__input" type="checkbox" {{#if isDefault}}checked="checked" disabled="disabled"{{/if}}></label>
+  </div>
+</div>`)
 
 export default class TypeDefinitionDialog extends PromiseDialog {
   constructor(
@@ -12,7 +30,7 @@ export default class TypeDefinitionDialog extends PromiseDialog {
   ) {
     super(
       title,
-      createContentHtml(content),
+      template(content),
       {
         height: 250
       },
