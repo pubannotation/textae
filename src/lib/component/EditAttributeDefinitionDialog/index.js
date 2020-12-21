2a9fc81981dc53272793937111c3150fc52c28f8
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-21 21:57:32 +0900

    refactor: Use the compileHandlebarsTemplate in the EditAttributeDefinitionDIalog

--- src/lib/component/EditAttributeDefinitionDialog/index.js
+++ src/lib/component/EditAttributeDefinitionDialog/index.js
@@ -1,7 +1,39 @@
 import PromiseDialog from '../PromiseDialog'
-import createContentHtml from './createContentHtml'
 import getInputElementValue from '../getInputElementValue'
 import isChanged from './isChanged'
+import compileHandlebarsTemplate from '../compileHandlebarsTemplate'
+
+const template = compileHandlebarsTemplate(`
+<div class="textae-editor__edit-attribute-definition-dialog__container">
+  <div class="textae-editor__edit-attribute-definition-dialog__row">
+    <div class="textae-editor__edit-attribute-definition-dialog__pred">
+      <label>Predicate:</label><br>
+      <input value="{{pred}}">
+    </div>
+    {{#if showDefault}}
+      <div class="textae-editor__edit-attribute-definition-dialog__default">
+        <label>Default:</label><br>
+        <input value="{{default}}">
+      </div>
+    {{/if}}
+  </div>
+  {{#if showNumeric}}
+    <div class="textae-editor__edit-attribute-definition-dialog__row">
+      <div class="textae-editor__edit-attribute-definition-dialog__min">
+        <label>Min:</label><br>
+        <input type="text" value="{{min}}">
+      </div>
+      <div class="textae-editor__edit-attribute-definition-dialog__max">
+        <label>Max:</label><br>
+        <input type="text" value="{{max}}">
+      </div>
+      <div class="textae-editor__edit-attribute-definition-dialog__step">
+        <label>Step:</label><br>
+        <input type="text" value="{{step}}">
+      </div>
+    </div>
+  {{/if}}
+</div>`)
 
 export default class EditAttributeDefinitionDialog extends PromiseDialog {
   constructor(attrDef) {
@@ -9,7 +41,7 @@ export default class EditAttributeDefinitionDialog extends PromiseDialog {
 
     super(
       'Please enter new values',
-      createContentHtml({
+      template({
         pred: json.pred,
         default: json.default,
         min: json.min,
