158133dff3e0e226bc9b9e105367a88f1cc1dc1e
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-21 22:56:46 +0900

    refactor: Delete the toContentHtml

--- src/lib/control/Control/toContentHtml.js
+++
@@ -1,7 +0,0 @@
-import buttonConfig from '../../buttonConfig'
-import compileHandlebarsTemplate from '../../compileHandlebarsTemplate'
-
-export default function (htmlTemplate) {
-  const template = compileHandlebarsTemplate(htmlTemplate)
-  return template(buttonConfig.mapForControl)
-}
