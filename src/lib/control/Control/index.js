a30da7f52e99b762d97749442fe1e156febb6117
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-21 22:56:23 +0900

    refactor: Inline the toContentHtml

--- src/lib/control/Control/index.js
+++ src/lib/control/Control/index.js
@@ -1,13 +1,15 @@
 import dohtml from 'dohtml'
+import buttonConfig from '../../buttonConfig'
+import compileHandlebarsTemplate from '../../compileHandlebarsTemplate'
 import bindEventHandler from './bindEventHandler'
 import updateAllButtonEnableState from './updateAllButtonEnableState'
 import updateButtonPushState from './updateButtonPushState'
-import toContentHtml from './toContentHtml'
 
 // The control is a control bar in an editor.
 export default class Control {
   constructor(htmlTemplate, editor) {
-    const el = dohtml.create(toContentHtml(htmlTemplate))
+    const template = compileHandlebarsTemplate(htmlTemplate)
+    const el = dohtml.create(template(buttonConfig.mapForControl))
 
     this._el = el
     bindEventHandler(this._el, editor)
