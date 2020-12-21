9a45897e72cf52b60c7c857b92b7ac04eafa1d24
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-21 22:59:11 +0900

    refactor: Use the template literal instead of the Handlebars

--- src/lib/editor/AnnotationData/SpanContainer/SpanModel/createGridHtml.js
+++ src/lib/editor/AnnotationData/SpanContainer/SpanModel/createGridHtml.js
@@ -1,14 +1,9 @@
-import Handlebars from 'handlebars'
+export default function (spanId, top, left, width) {
+  const id = `G${spanId}`
+  const style = `top: ${top}px; left: ${left}px; width: ${width}px;`
 
-const source = `
-  <div id={{id}} class="textae-editor__grid" style="{{style}}">
+  return `
+  <div id=${id} class="textae-editor__grid" style="${style}">
   </div>
   `
-const template = Handlebars.compile(source)
-
-export default function (spanId, top, left, width) {
-  return template({
-    id: `G${spanId}`,
-    style: `top: ${top}px; left: ${left}px; width: ${width}px;`
-  })
 }
