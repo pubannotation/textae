737016bdfb470852a3c6eaadaee1ebb9b6268abc
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-22 11:38:42 +0900

    refactor: Exclude functions

---
+++ src/lib/component/EditEntityDialog/toEntityHTML.js
@@ -0,0 +1,26 @@
+import toAttributeHTML from './toAttributeHTML'
+
+export default function (value, label, attributes) {
+  return `
+  <div class="textae-editor__edit-type-dialog__container">
+    <div class="textae-editor__edit-type-dialog__type">
+      <div class="textae-editor__edit-type-dialog__type__predicate">
+        <label>Predicate:</label><br>
+        <span class="textae-editor__edit-type-dialog__type__predicate__value">type</span>
+      </div>
+      <div class="textae-editor__edit-type-dialog__type__value ui-front">
+        <label>Value:</label><br>
+        <input class="textae-editor__edit-type-dialog__type__value__value textae-editor__promise-daialog__observable-element" value="${value}">
+      </div>
+      <div class="textae-editor__edit-type-dialog__type__label">
+        <label>Label:</label><br>
+        <span class="textae-editor__edit-type-dialog__type__label__value">${label}</span>
+      </div>
+    </div>
+    <div class="textae-editor__edit-type-dialog__attributes">
+      ${attributes
+        .map((a) => toAttributeHTML(a.pred, a.obj, a.editDisabled))
+        .join('')}
+    </div>
+  </div>`
+}
