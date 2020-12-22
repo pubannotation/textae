a1b228e081dc04ae8ba27edf8e24e2e1081ae8ce
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-22 23:48:13 +0900

    refactor: Fix template in the toAttribute

--- src/lib/editor/EntityModel/toAttribute.js
+++ src/lib/editor/EntityModel/toAttribute.js
@@ -2,12 +2,16 @@ import toLabel from './toLabel'
 
 export default function ({ title, pred, obj, color, href, label }) {
   return `
-<div class="textae-editor__entity__attribute" title="${title}" data-pred="${pred}" data-obj="${obj}" ${
-    color ? ` style="background-color: ${color}"` : ''
-  }>
-<span class="textae-editor__entity__attribute-label">
-${toLabel(href, label)}
-</span>
+<div
+  class="textae-editor__entity__attribute"
+  title="${title}"
+  data-pred="${pred}"
+  data-obj="${obj}"
+  ${color ? `style="background-color: ${color};"` : ''}
+  >
+  <span class="textae-editor__entity__attribute-label">
+    ${toLabel(href, label)}
+  </span>
 </div>
 `
 }
