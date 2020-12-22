e4df59417b7fa53c5029e7450e0770529f59598e
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-22 23:40:44 +0900

    refactor: Use the template literal to render the entity HTML element

--- src/lib/editor/EntityModel/createEntityHTMLElement.js
+++ src/lib/editor/EntityModel/createEntityHTMLElement.js
@@ -1,36 +1,45 @@
 import dohtml from 'dohtml'
-import compileHandlebarsTemplate from '../../compileHandlebarsTemplate'
-
-// A Type element has an entity_pane elment that has a label and will have entities.
-// jsPlumb requires the id of the DOM which is the endpoint for drawing relationships.
-// If the endpoint doesn't have an id, jsPlumb will set it,
-// and the id will be lost when redrawing the Entity's DOM.
-// To prevent this from happening, set the id of the endpoint DOM.
-const template = compileHandlebarsTemplate(`
-<div class="textae-editor__entity" id="{{id}}" title="{{title}}">
-  <div class="textae-editor__entity__type-values" id="jsPlumb_{{id}}" style="background-color: {{color}}">
-    <div class="textae-editor__entity__type-label" tabindex="0">
-      {{#if href}}
-        <a target="_blank"/ href="{{href}}">{{label}}</a>
-      {{else}}
-        {{label}}
-      {{/if}}
-    </div>
-    {{#each attributes}}
-    <div class="textae-editor__entity__attribute" title="{{title}}" data-pred="{{pred}}" data-obj="{{obj}}" {{#if color}} style="background-color: {{color}}"{{/if}}>
-      <span class="textae-editor__entity__attribute-label">
-        {{#if href}}
-          <a target="_blank"/ href="{{href}}">{{label}}</a>
-        {{else}}
-          {{label}}
-        {{/if}}
-      </span>
-    </div>
-    {{/each}}
-  </div>
-</div>
-`)
 
 export default function (context) {
-  return dohtml.create(template(context))
+  const { id, title, color, href, label, attributes } = context
+
+  // A Type element has an entity_pane elment that has a label and will have entities.
+  // jsPlumb requires the id of the DOM which is the endpoint for drawing relationships.
+  // If the endpoint doesn't have an id, jsPlumb will set it,
+  // and the id will be lost when redrawing the Entity's DOM.
+  // To prevent this from happening, set the id of the endpoint DOM.
+  return dohtml.create(`
+  <div class="textae-editor__entity" id="${id}" title="${title}">
+    <div class="textae-editor__entity__type-values" id="jsPlumb_${id}" style="background-color: ${color}">
+      <div class="textae-editor__entity__type-label" tabindex="0">
+        ${
+          href
+            ? `
+        <a target="_blank"/ href="${href}">${label}</a>
+        `
+            : label
+        }
+      </div>
+      ${attributes
+        .map(
+          ({ title, pred, obj, color, href, label }) => `
+      <div class="textae-editor__entity__attribute" title="${title}" data-pred="${pred}" data-obj="${obj}" ${
+            color ? ` style="background-color: ${color}"` : ''
+          }>
+        <span class="textae-editor__entity__attribute-label">
+          ${
+            href
+              ? `
+          <a target="_blank"/ href="${href}">${label}</a>
+          `
+              : label
+          }
+        </span>
+      </div>
+      `
+        )
+        .join('\n')}
+    </div>
+  </div>
+  `)
 }
