9f21f549cce4c4311676ba5f49b054977e3f646b
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-23 15:52:05 +0900

    refactor: Format template for the TypeDefinitionDialog

--- src/lib/component/TypeDefinitionDialog/index.js
+++ src/lib/component/TypeDefinitionDialog/index.js
@@ -6,22 +6,29 @@ function template(context) {
   return `
 <div class="textae-editor__edit-type-definition-dialog__container">
   <div class="textae-editor__edit-type-definition-dialog__input-box">
-  <label>Id:</label><br>
-  <input class="textae-editor__edit-type-definition-dialog--id textae-editor__promise-daialog__observable-element" value=${
-    id || ''
-  }>
+    <label>Id:</label><br>
+    <input
+      class="textae-editor__edit-type-definition-dialog--id textae-editor__promise-daialog__observable-element" 
+      value=${id || ''}>
   </div>
   <div class="textae-editor__edit-type-definition-dialog__input-box">
-  <label>Label:<span></span></label><br>
-  <input value="${label}">
+    <label>Label:<span></span></label><br>
+    <input value="${label}">
   </div>
   <div class="textae-editor__edit-type-definition-dialog__color-picker">
-  <label>Color:<input class="textae-editor__edit-type-definition-dialog__color-picker__input" type="color" value="${color}"></label>
+    <label>Color:<input 
+      class="textae-editor__edit-type-definition-dialog__color-picker__input" 
+      type="color" 
+      value="${color}">
+    </label>
   </div>
   <div class="textae-editor__edit-type-definition-dialog__set-default">
-  <label>Default type:<input class="textae-editor__edit-type-definition-dialog__set-default__input" type="checkbox" ${
-    isDefault ? 'checked="checked" disabled="disabled"' : ''
-  }></label>
+    <label>Default type:<input 
+      class="textae-editor__edit-type-definition-dialog__set-default__input" 
+      type="checkbox" ${
+        isDefault ? 'checked="checked" disabled="disabled"' : ''
+      }>
+    </label>
   </div>
 </div>`
 }
