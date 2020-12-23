81e0c3f38afea156b87f419852472aca2f7bcc65
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-23 15:49:22 +0900

    refactor: Use template literal instead of the Handlebars in the TypeDefinitionDialog

--- src/lib/component/TypeDefinitionDialog/index.js
+++ src/lib/component/TypeDefinitionDialog/index.js
@@ -1,24 +1,30 @@
 import PromiseDialog from '../PromiseDialog'
 import setSourceOfAutoComplete from './setSourceOfAutoComplete'
-import compileHandlebarsTemplate from '../../compileHandlebarsTemplate'
 
-const template = compileHandlebarsTemplate(`
+function template(context) {
+  const { id, label, color, isDefault } = context
+  return `
 <div class="textae-editor__edit-type-definition-dialog__container">
   <div class="textae-editor__edit-type-definition-dialog__input-box">
   <label>Id:</label><br>
-  <input class="textae-editor__edit-type-definition-dialog--id textae-editor__promise-daialog__observable-element" value={{id}}>
+  <input class="textae-editor__edit-type-definition-dialog--id textae-editor__promise-daialog__observable-element" value=${
+    id || ''
+  }>
   </div>
   <div class="textae-editor__edit-type-definition-dialog__input-box">
   <label>Label:<span></span></label><br>
-  <input value="{{label}}">
+  <input value="${label}">
   </div>
   <div class="textae-editor__edit-type-definition-dialog__color-picker">
-  <label>Color:<input class="textae-editor__edit-type-definition-dialog__color-picker__input" type="color" value="{{color}}"></label>
+  <label>Color:<input class="textae-editor__edit-type-definition-dialog__color-picker__input" type="color" value="${color}"></label>
   </div>
   <div class="textae-editor__edit-type-definition-dialog__set-default">
-  <label>Default type:<input class="textae-editor__edit-type-definition-dialog__set-default__input" type="checkbox" {{#if isDefault}}checked="checked" disabled="disabled"{{/if}}></label>
+  <label>Default type:<input class="textae-editor__edit-type-definition-dialog__set-default__input" type="checkbox" ${
+    isDefault ? 'checked="checked" disabled="disabled"' : ''
+  }></label>
   </div>
-</div>`)
+</div>`
+}
 
 export default class TypeDefinitionDialog extends PromiseDialog {
   constructor(
