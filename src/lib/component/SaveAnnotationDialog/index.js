c9ac595d58c705647f322aeb691cff52ec6d19f2
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-23 16:09:03 +0900

    refactor: Format template for the SaveAnnotationDialog

--- src/lib/component/SaveAnnotationDialog/index.js
+++ src/lib/component/SaveAnnotationDialog/index.js
@@ -4,23 +4,31 @@ import bind from './bind'
 function template(context) {
   const { url, filename } = context
 
-  return `<div>
-<div class="textae-editor__save-dialog__row">
-  <label class="textae-editor__save-dialog__label">URL</label>
-  <input type="text" value="${url}" class="textae-editor__save-dialog__server-file-name url">
-  <input type="button" class="url" ${
-    url ? '' : `disabled="disabled"`
-  } value="Save">
-</div>
-<div class="textae-editor__save-dialog__row">
-  <label class="textae-editor__save-dialog__label">Local</label>
-  <input type="text" value="${filename}" class="textae-editor__save-dialog__local-file-name local">
-  <a class="download" href="#">Download</a>
-</div>
-<div class="textae-editor__save-dialog__row">
-  <label class="textae-editor__save-dialog__label"></label>
-  <a class="viewsource" href="#">Click to see the json source in a new window.</a>
-</div>
+  return `
+<div>
+  <div class="textae-editor__save-dialog__row">
+    <label class="textae-editor__save-dialog__label">URL</label>
+    <input 
+      type="text"
+      value="${url}"
+      class="textae-editor__save-dialog__server-file-name url">
+    <input 
+      type="button"
+      class="url" ${url ? '' : `disabled="disabled"`}
+      value="Save">
+  </div>
+  <div class="textae-editor__save-dialog__row">
+    <label class="textae-editor__save-dialog__label">Local</label>
+    <input
+      type="text"
+      value="${filename}"
+      class="textae-editor__save-dialog__local-file-name local">
+    <a class="download" href="#">Download</a>
+  </div>
+  <div class="textae-editor__save-dialog__row">
+    <label class="textae-editor__save-dialog__label"></label>
+    <a class="viewsource" href="#">Click to see the json source in a new window.</a>
+  </div>
 </div>
 `
 }
