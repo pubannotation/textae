e510e72bde4db66ab1a851ad1df54a58e9d64069
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-23 15:01:29 +0900

    refactor: Use template literal instead of the Handlebars in the ControlBar

--- src/lib/control/ControlBar/index.js
+++ src/lib/control/ControlBar/index.js
@@ -1,26 +1,39 @@
-import compileHandlebarsTemplate from '../../compileHandlebarsTemplate'
 import Control from '../Control'
 import transitWriteButtonImage from './transitWriteButtonImage'
 
 // Make a group of buttons that is headed by the separator.
-const htmlTemplate = `
+function template(context) {
+  const { buttonGroup } = context
+  return `
 <div class="textae-control textae-control-bar">
   <span class="textae-control__title">
     <a href="http://textae.pubannotation.org/" target="_blank">TextAE</a>
   </span>
-  {{#buttonGroup}}
+  ${buttonGroup
+    .map(
+      ({ list }) => `
   <span class="textae-control__separator"></span>
-    {{#list}}
-  <span class="textae-control__icon textae-control__{{type}}-button" title="{{title}}" data-button-type="{{type}}"></span>
-    {{/list}}
-  {{/buttonGroup}}
+    ${list
+      .map(
+        ({ type, title }) => `
+  <span 
+    class="textae-control__icon textae-control__${type}-button" 
+    title="${title}" 
+    data-button-type="${type}">
+  </span>
+  `
+      )
+      .join('\n')}
+  `
+    )
+    .join('\n')}
 </div>
 `
+}
 
 // The control is a control bar in an editor.
 export default class ControlBar extends Control {
   constructor(editor) {
-    const template = compileHandlebarsTemplate(htmlTemplate)
     super(template, editor)
   }
 
