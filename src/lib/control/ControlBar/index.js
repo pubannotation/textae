d331553505339fcd450a25e75a86bb84d237e8ff
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-23 14:53:31 +0900

    refactor: Inline the htmlTemplate into the ControlBar

--- src/lib/control/ControlBar/index.js
+++ src/lib/control/ControlBar/index.js
@@ -1,7 +1,21 @@
 import compileHandlebarsTemplate from '../../compileHandlebarsTemplate'
 import Control from '../Control'
-import { htmlTemplate } from './htmlTemplate'
 import transitWriteButtonImage from './transitWriteButtonImage'
+
+// Make a group of buttons that is headed by the separator.
+const htmlTemplate = `
+<div class="textae-control textae-control-bar">
+  <span class="textae-control__title">
+    <a href="http://textae.pubannotation.org/" target="_blank">TextAE</a>
+  </span>
+  {{#buttonGroup}}
+  <span class="textae-control__separator"></span>
+    {{#list}}
+  <span class="textae-control__icon textae-control__{{type}}-button" title="{{title}}" data-button-type="{{type}}"></span>
+    {{/list}}
+  {{/buttonGroup}}
+</div>
+`
 
 // The control is a control bar in an editor.
 export default class ControlBar extends Control {
