2e17948bbffcac1584cd3f31d2860ea9e2d8a6cf
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-25 15:17:50 +0900

    refactor: Switch the display of the veil after all editors have been judged

--- src/lib/Tool/setVeilObserver/updateVeil.js
+++ src/lib/Tool/setVeilObserver/updateVeil.js
@@ -15,23 +15,23 @@ const veilClass = 'textae-editor-veil'
 export default function (mutationRecords) {
   mutationRecords.forEach((m) => {
     hasWaitingEditor(m.target)
-
-    if (waitingEditors.size > 0) {
-      const veil = document.querySelector(`.${veilClass}`)
-
-      if (veil) {
-        veil.style.display = 'block'
-      } else {
-        document.body.appendChild(
-          dohtml.create(`<div class="${veilClass}"></div>`)
-        )
-      }
-    } else {
-      const veil = document.querySelector(`.${veilClass}`)
-
-      if (veil) {
-        veil.style.display = 'none'
-      }
-    }
   })
+
+  if (waitingEditors.size > 0) {
+    const veil = document.querySelector(`.${veilClass}`)
+
+    if (veil) {
+      veil.style.display = 'block'
+    } else {
+      document.body.appendChild(
+        dohtml.create(`<div class="${veilClass}"></div>`)
+      )
+    }
+  } else {
+    const veil = document.querySelector(`.${veilClass}`)
+
+    if (veil) {
+      veil.style.display = 'none'
+    }
+  }
 }
