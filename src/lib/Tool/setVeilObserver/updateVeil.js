f05422d783757a1444af20bb1afef0bce7272431
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-25 13:52:13 +0900

    refactor: Inline the switchVeil

--- src/lib/Tool/setVeilObserver/updateVeil.js
+++ src/lib/Tool/setVeilObserver/updateVeil.js
@@ -1,6 +1,27 @@
+import dohtml from 'dohtml'
 import hasWaitingEditor from './hasWaitingEditor'
-import switchVeil from './switchVeil'
 
 export default function (mutationRecords) {
   mutationRecords.forEach((m) => switchVeil(hasWaitingEditor(m.target)))
+}
+
+function switchVeil(hasWaitingEditor) {
+  const veilClass = 'textae-editor-veil'
+  if (hasWaitingEditor) {
+    const veil = document.querySelector(`.${veilClass}`)
+
+    if (veil) {
+      veil.style.display = 'block'
+    } else {
+      document.body.appendChild(
+        dohtml.create(`<div class="${veilClass}"></div>`)
+      )
+    }
+  } else {
+    const veil = document.querySelector(`.${veilClass}`)
+
+    if (veil) {
+      veil.style.display = 'none'
+    }
+  }
 }
