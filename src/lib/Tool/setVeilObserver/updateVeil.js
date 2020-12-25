9546be81cc6c22c616f39407491628b27c3d905b
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-25 13:55:14 +0900

    refactor: Inline the switchVeil function in the updateVeil

--- src/lib/Tool/setVeilObserver/updateVeil.js
+++ src/lib/Tool/setVeilObserver/updateVeil.js
@@ -2,26 +2,25 @@ import dohtml from 'dohtml'
 import hasWaitingEditor from './hasWaitingEditor'
 
 export default function (mutationRecords) {
-  mutationRecords.forEach((m) => switchVeil(hasWaitingEditor(m.target)))
-}
+  mutationRecords.forEach((m) => {
+    const veilClass = 'textae-editor-veil'
 
-function switchVeil(hasWaitingEditor) {
-  const veilClass = 'textae-editor-veil'
-  if (hasWaitingEditor) {
-    const veil = document.querySelector(`.${veilClass}`)
+    if (hasWaitingEditor(m.target)) {
+      const veil = document.querySelector(`.${veilClass}`)
 
-    if (veil) {
-      veil.style.display = 'block'
+      if (veil) {
+        veil.style.display = 'block'
+      } else {
+        document.body.appendChild(
+          dohtml.create(`<div class="${veilClass}"></div>`)
+        )
+      }
     } else {
-      document.body.appendChild(
-        dohtml.create(`<div class="${veilClass}"></div>`)
-      )
-    }
-  } else {
-    const veil = document.querySelector(`.${veilClass}`)
+      const veil = document.querySelector(`.${veilClass}`)
 
-    if (veil) {
-      veil.style.display = 'none'
+      if (veil) {
+        veil.style.display = 'none'
+      }
     }
-  }
+  })
 }
