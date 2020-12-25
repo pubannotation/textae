d88c1db172eec11046809deb0f39a11eb4a34bd0
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-25 13:49:32 +0900

    refactor: Inline the veil

--- src/lib/Tool/setVeilObserver/switchVeil.js
+++ src/lib/Tool/setVeilObserver/switchVeil.js
@@ -1,9 +1,29 @@
-import veil from './veil'
+import dohtml from 'dohtml'
 
 export default function (hasWaitingEditor) {
   if (hasWaitingEditor) {
-    veil.show()
+    show()
   } else {
-    veil.hide()
+    hide()
+  }
+}
+
+const veilClass = 'textae-editor-veil'
+
+function show() {
+  const veil = document.querySelector(`.${veilClass}`)
+
+  if (veil) {
+    veil.style.display = 'block'
+  } else {
+    document.body.appendChild(dohtml.create(`<div class="${veilClass}"></div>`))
+  }
+}
+
+function hide() {
+  const veil = document.querySelector(`.${veilClass}`)
+
+  if (veil) {
+    veil.style.display = 'none'
   }
 }
