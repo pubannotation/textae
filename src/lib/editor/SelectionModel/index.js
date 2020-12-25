d814a967c4530664a8b01df8c9791ed30a8e5382
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-25 17:24:41 +0900

    refactor: Use the method chain

--- src/lib/editor/SelectionModel/index.js
+++ src/lib/editor/SelectionModel/index.js
@@ -28,15 +28,16 @@ export default class SelectionModel {
     // When the annotation is loaded and the mode is determined,
     // the selection state is cleared via TypeEditor.cancelSelect.
     // No need to monitor `textae.annotationData.all.change` events.
-    eventEmitter.on('textae.annotationData.span.remove', (span) =>
-      this.span.removeInstance(span)
-    )
-    eventEmitter.on('textae.annotationData.entity.remove', (entity) =>
-      this.entity.removeInstance(entity)
-    )
-    eventEmitter.on('textae.annotationData.relation.remove', (relation) =>
-      this.relation.removeInstance(relation)
-    )
+    eventEmitter
+      .on('textae.annotationData.span.remove', (span) =>
+        this.span.removeInstance(span)
+      )
+      .on('textae.annotationData.entity.remove', (entity) =>
+        this.entity.removeInstance(entity)
+      )
+      .on('textae.annotationData.relation.remove', (relation) =>
+        this.relation.removeInstance(relation)
+      )
   }
 
   add(modelType, id) {
