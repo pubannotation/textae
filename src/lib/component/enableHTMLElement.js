5cc6272b602926e026b8d186b3d32a8ffafa8fcf
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-25 16:44:56 +0900

    refactor: Rename an argument

--- src/lib/component/enableHTMLElement.js
+++ src/lib/component/enableHTMLElement.js
@@ -1,8 +1,9 @@
-// Since the style is specified by [disabled = "disabled"], set the attribute to disabled without using the disable property.
-export default function (dom, enable) {
+// Since the style is specified by [disabled = "disabled"],
+// set the attribute to disabled without using the disable property.
+export default function (element, enable) {
   if (enable) {
-    dom.removeAttribute('disabled')
+    element.removeAttribute('disabled')
   } else {
-    dom.setAttribute('disabled', 'disabled')
+    element.setAttribute('disabled', 'disabled')
   }
 }
