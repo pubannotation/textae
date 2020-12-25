3a9e2c348993b3937743a5db5b1283c94f48763e
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-25 10:10:28 +0900

    refactor: Use the dohtml in the renderBlock

--- src/lib/editor/AnnotationData/SpanContainer/BlockSpanModel/renderBlock.js
+++ src/lib/editor/AnnotationData/SpanContainer/BlockSpanModel/renderBlock.js
@@ -1,12 +1,11 @@
+import dohtml from 'dohtml'
 import createRangeToSpan from '../createRangeToSpan'
 
 export default function (span) {
   const targetRange = createRangeToSpan(span)
-  const spanElement = document.createElement('div')
-
-  spanElement.setAttribute('id', span.id)
-  spanElement.setAttribute('title', span.id)
-  spanElement.classList.add('textae-editor__block')
+  const spanElement = dohtml.create(`
+    <div id="${span.id}" title="${span.id}" class="textae-editor__block"></div>
+  `)
 
   targetRange.surroundContents(spanElement)
 }
