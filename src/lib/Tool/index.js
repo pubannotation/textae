16f3869557ecb92092c805b30b0e54315b4e10bc
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-12-25 15:36:26 +0900

    refactor: Inline the setVeilObserver function

--- src/lib/Tool/index.js
+++ src/lib/Tool/index.js
@@ -1,7 +1,7 @@
 import EditorContainer from './EditorContainer'
 import selectUnselectEditorOn from './selectUnselectEditorOn'
 import redrawOnResize from './redrawOnResize'
-import setVeilObserver from './setVeilObserver'
+import dohtml from 'dohtml'
 
 // The tool manages interactions between components.
 export default class Tool {
@@ -20,4 +20,43 @@ export default class Tool {
     this._editors.push(editor)
     setVeilObserver(editor)
   }
+}
+
+// Since not all editors will be notified at once, keep the state in a module scope variable.
+const waitingEditors = new Set()
+const veilClass = 'textae-editor-veil'
+
+const config = {
+  attributes: true,
+  attributeFilter: ['class']
+}
+
+function setVeilObserver(editor) {
+  new MutationObserver((mutationRecords) => {
+    mutationRecords.forEach(({ target: element }) => {
+      if (element.classList.contains('textae-editor--wait')) {
+        waitingEditors.add(element)
+      } else {
+        waitingEditors.delete(element)
+      }
+    })
+
+    if (waitingEditors.size > 0) {
+      const veil = document.querySelector(`.${veilClass}`)
+
+      if (veil) {
+        veil.style.display = 'block'
+      } else {
+        document.body.appendChild(
+          dohtml.create(`<div class="${veilClass}"></div>`)
+        )
+      }
+    } else {
+      const veil = document.querySelector(`.${veilClass}`)
+
+      if (veil) {
+        veil.style.display = 'none'
+      }
+    }
+  }).observe(editor[0], config)
 }
