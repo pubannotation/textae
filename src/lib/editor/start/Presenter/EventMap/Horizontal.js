dbdd02dc13d6d76dceda87401624c6c7385a7767
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-05 23:04:26 +0900

    refactor: Inline the SelectionModel.addSpan

--- src/lib/editor/start/Presenter/EventMap/Horizontal.js
+++ src/lib/editor/start/Presenter/EventMap/Horizontal.js
@@ -8,7 +8,7 @@ export default class Horizontal {
     const nextSpan = this._searchLeft('textae-editor__span')
     if (nextSpan) {
       if (shiftKey) {
-        this._selectionModel.addSpan(nextSpan.id)
+        this._selectionModel.span.add(nextSpan.id)
       } else {
         this._selectionModel.selectSpan(nextSpan.id)
       }
@@ -25,7 +25,7 @@ export default class Horizontal {
     const nextSpan = this._searchRight('textae-editor__span')
     if (nextSpan) {
       if (shiftKey) {
-        this._selectionModel.addSpan(nextSpan.id)
+        this._selectionModel.span.add(nextSpan.id)
       } else {
         this._selectionModel.selectSpan(nextSpan.id)
       }
