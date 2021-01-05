310b2d334994a3226e67c5ac8c0dcc732d4981ca
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-05 23:18:25 +0900

    refactor: Refactor the selectSpan

--- src/lib/editor/start/Presenter/EditMode/selectSpan.js
+++ src/lib/editor/start/Presenter/EditMode/selectSpan.js
@@ -1,11 +1,13 @@
 export default function (selectionModel, rangeOfSpans, event, spanID) {
   if (rangeOfSpans.length) {
     selectionModel.selectSpanRange(rangeOfSpans)
-  } else {
-    if (event.ctrlKey || event.metaKey) {
-      selectionModel.span.toggle(spanID)
-    } else {
-      selectionModel.selectSpan(spanID)
-    }
+    return
   }
+
+  if (event.ctrlKey || event.metaKey) {
+    selectionModel.span.toggle(spanID)
+    return
+  }
+
+  selectionModel.selectSpan(spanID)
 }
