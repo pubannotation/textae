abeec31d177cfc7cd72cca50faeaead34cb366c7
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-07 13:50:57 +0900

    refactor: Delete the select.js

--- src/lib/component/setSourceOfAutoComplete/select.js
+++
@@ -1,9 +0,0 @@
-export default function (inputElement, labelSpan, ui) {
-  inputElement.value = ui.item.raw.id
-
-  if (labelSpan) {
-    labelSpan.innerText = ui.item.raw.label
-  }
-
-  return false
-}
