f698e2ef4418a4464bb51254bd1d7e4b5d529055
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-08 23:25:55 +0900

    refactor: Reduce nesting of if statements

--- src/lib/component/EntityAndAttributePallet/createContentHtml/index.js
+++ src/lib/component/EntityAndAttributePallet/createContentHtml/index.js
@@ -15,70 +15,68 @@ export default function (
   const addAttribute = typeContainer.attributes.length < 30
   const attributes = getAttributes(typeContainer, selectedPred)
 
-  if (selectedPred) {
-    const attrDef = typeContainer.attributes.find(
-      (a) => a.pred === selectedPred
-    )
-
-    const values = {
+  if (!selectedPred) {
+    return typeTemplate({
       isLock: typeContainer.isLock,
       attributes,
       hasDiff,
-      attrDef: Object.assign(attrDef.JSON, {
-        hasInstance: typeContainer.hasAttributeInstance(selectedPred)
-      }),
-      selectedPred,
-      lastAttributeSelected:
-        typeContainer.attributes.indexOf(attrDef) ===
-        typeContainer.attributes.length - 1,
+      types: typeContainer.pallet,
       addAttribute,
-      selectedEntityLabel: getSelectedEntityLabel(selectionModelEntity.size),
-      isEntityWithSamePredSelected: selectionModelEntity.isSamePredAttrributeSelected(
-        selectedPred
-      )
-    }
-
-    switch (attrDef.valueType) {
-      case 'flag':
-        return flagAttributeTemplate(values)
-      case 'numeric':
-        return numericAttributeTemplate(
-          Object.assign(values, {
-            addAttributeValueButton: true
-          })
-        )
-      case 'selection':
-        // Disable to press the remove button for the value used in the selection attribute.
-        for (const value of values.attrDef.values) {
-          value.indelible = typeContainer.isSelectionAttributeIndelible(
-            selectedPred,
-            value.id
-          )
-        }
-
-        return selectionAttributeTemplate(
-          Object.assign(values, {
-            addAttributeValueButton: true
-          })
-        )
-      case 'string':
-        return stringAttributeTemplate(
-          Object.assign(values, {
-            addAttributeValueButton: true
-          })
-        )
-      default:
-        throw `attrDef.valueType is unknown attribute`
-    }
+      addTypeButton: true,
+      selectedEntityLabel: getSelectedEntityLabel(selectionModelEntity.size)
+    })
   }
 
-  return typeTemplate({
+  const attrDef = typeContainer.attributes.find((a) => a.pred === selectedPred)
+
+  const values = {
     isLock: typeContainer.isLock,
     attributes,
     hasDiff,
-    types: typeContainer.pallet,
+    attrDef: Object.assign(attrDef.JSON, {
+      hasInstance: typeContainer.hasAttributeInstance(selectedPred)
+    }),
+    selectedPred,
+    lastAttributeSelected:
+      typeContainer.attributes.indexOf(attrDef) ===
+      typeContainer.attributes.length - 1,
     addAttribute,
-    addTypeButton: true,
-    selectedEntityLabel: getSelectedEntityLabel(selectionModelEntity.size)
-  })
+    selectedEntityLabel: getSelectedEntityLabel(selectionModelEntity.size),
+    isEntityWithSamePredSelected: selectionModelEntity.isSamePredAttrributeSelected(
+      selectedPred
+    )
+  }
+
+  switch (attrDef.valueType) {
+    case 'flag':
+      return flagAttributeTemplate(values)
+    case 'numeric':
+      return numericAttributeTemplate(
+        Object.assign(values, {
+          addAttributeValueButton: true
+        })
+      )
+    case 'selection':
+      // Disable to press the remove button for the value used in the selection attribute.
+      for (const value of values.attrDef.values) {
+        value.indelible = typeContainer.isSelectionAttributeIndelible(
+          selectedPred,
+          value.id
+        )
+      }
+
+      return selectionAttributeTemplate(
+        Object.assign(values, {
+          addAttributeValueButton: true
+        })
+      )
+    case 'string':
+      return stringAttributeTemplate(
+        Object.assign(values, {
+          addAttributeValueButton: true
+        })
+      )
+    default:
+      throw `attrDef.valueType is unknown attribute`
+  }
 }
