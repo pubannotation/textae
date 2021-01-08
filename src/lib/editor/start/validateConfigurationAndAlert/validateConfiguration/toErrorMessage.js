411b6190c91501de4daa3c8afe580cee8e4d7360
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-08 23:07:39 +0900

    feature: Validate invalid color format

--- src/lib/editor/start/validateConfigurationAndAlert/validateConfiguration/toErrorMessage.js
+++ src/lib/editor/start/validateConfigurationAndAlert/validateConfiguration/toErrorMessage.js
@@ -1,7 +1,11 @@
+import eskape from 'eskape'
+
 export default function (errors) {
   for (const e of errors) {
     if (e.keyword === 'required') {
       return `Invalid configuration: The attribute type whose predicate is '${e.data.pred}' misses a mandatory property, '${e.params.missingProperty}'.`
+    } else if (e.dataPath.includes('color')) {
+      return eskape`Invalid configuration: '${e.data}' is invalid color format.`
     }
   }
 }
