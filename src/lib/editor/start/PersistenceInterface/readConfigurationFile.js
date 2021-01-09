090b458e14b4b8d0c81e8f8e550f4ab02b1f4f51
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-10 07:47:19 +0900

    refactor: Use destructuring assignment

--- src/lib/editor/start/PersistenceInterface/readConfigurationFile.js
+++ src/lib/editor/start/PersistenceInterface/readConfigurationFile.js
@@ -4,13 +4,13 @@ import isJSON from './isJSON'
 export default function (files, editor) {
   const file = files[0]
 
-  readFile(file).then((event) => {
-    if (isJSON(event.target.result)) {
+  readFile(file).then(({ target }) => {
+    if (isJSON(target.result)) {
       editor.eventEmitter.emit(
         'textae.configuration.load',
         'local file',
         file.name,
-        JSON.parse(event.target.result)
+        JSON.parse(target.result)
       )
     } else {
       editor.eventEmitter.emit(
