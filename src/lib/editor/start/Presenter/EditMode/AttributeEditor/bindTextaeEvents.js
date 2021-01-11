14adae1be04488172d533cb80b3f8d4822dd4555
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-12 01:35:45 +0900

    refactor: Delete the bindTextaeEvents

--- src/lib/editor/start/Presenter/EditMode/AttributeEditor/bindTextaeEvents.js
+++
@@ -1,6 +0,0 @@
-export default function (editor, callback) {
-  editor.eventEmitter.on(
-    'textae.entityAndAttributePallet.attribute.selection-attribute-label.click',
-    (attrDef, newObj) => callback(attrDef, newObj)
-  )
-}
