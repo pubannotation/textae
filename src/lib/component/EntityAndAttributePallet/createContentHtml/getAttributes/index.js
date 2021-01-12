77bfbbd4de22fa1cfbb831c6f55f5e406abccfa7
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-12 22:25:48 +0900

    refactor: Refactor the getAttributes

--- src/lib/component/EntityAndAttributePallet/createContentHtml/getAttributes/index.js
+++ src/lib/component/EntityAndAttributePallet/createContentHtml/getAttributes/index.js
@@ -2,13 +2,11 @@ import getAttributeForSelecetdePred from './getAttributeForSelecetdePred'
 import setShortcutKey from './setShortcutKey'
 
 export default function (attributeContainer, selectedPred) {
-  let attributes
-
   if (selectedPred) {
-    attributes = getAttributeForSelecetdePred(attributeContainer, selectedPred)
+    return setShortcutKey(
+      getAttributeForSelecetdePred(attributeContainer, selectedPred)
+    )
   } else {
-    attributes = attributeContainer.attributes
+    return setShortcutKey(attributeContainer.attributes)
   }
-
-  return setShortcutKey(attributes)
 }
