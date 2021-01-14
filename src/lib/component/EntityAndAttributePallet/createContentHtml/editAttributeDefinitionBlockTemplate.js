eba556e5685023de16dea9bf1ecafad9a9ca70c7
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-14 13:38:53 +0900

    refactor: Use the numberOfItemsUsingSelectedPred instead of the attrDef.hasInstance

--- src/lib/component/EntityAndAttributePallet/createContentHtml/editAttributeDefinitionBlockTemplate.js
+++ src/lib/component/EntityAndAttributePallet/createContentHtml/editAttributeDefinitionBlockTemplate.js
@@ -1,9 +1,9 @@
 export default function (context) {
-  const { hasInstance } = context.attrDef
+  const { numberOfItemsUsingSelectedPred } = context
 
   return `
   ${
-    hasInstance
+    numberOfItemsUsingSelectedPred.size > 0
       ? `<button 
           type="button"
           class="textae-editor__type-pallet__table-button textae-editor__type-pallet__table-button--disabled textae-editor__type-pallet__delete-predicate"
