6cc61b2c408e12dcda768f5b9b3184d3cb64c251
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-15 17:12:25 +0900

    feature: Change the show/hide toggle for the Add Attribute button to enable/disable toggle

--- src/lib/component/EntityAndAttributePallet/createContentHtml/predicateControllerTemplate/addAttributeButtonTemplate.js
+++ src/lib/component/EntityAndAttributePallet/createContentHtml/predicateControllerTemplate/addAttributeButtonTemplate.js
@@ -2,7 +2,13 @@ export default function (context) {
   const { isEntityWithSamePredSelected } = context
 
   return isEntityWithSamePredSelected
-    ? ``
+    ? `
+      <button
+        type="button"
+        class="textae-editor__type-pallet__add-attribute"
+        disabled="disabled"
+        >add to</button>
+      `
     : `
       <button
         type="button"
