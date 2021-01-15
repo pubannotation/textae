9628faa5216540a7d197da80260a44fc59cc4ef4
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-15 11:22:38 +0900

    refactor: Indent template

--- src/lib/component/EntityAndAttributePallet/createContentHtml/editAttributeDefinitionBlockTemplate.js
+++ src/lib/component/EntityAndAttributePallet/createContentHtml/editAttributeDefinitionBlockTemplate.js
@@ -2,24 +2,24 @@ export default function (context) {
   const { numberOfItemsUsingSelectedPred } = context
 
   return `
-  ${
-    numberOfItemsUsingSelectedPred.size > 0
-      ? `<button 
-          type="button"
-          class="textae-editor__type-pallet__table-button textae-editor__type-pallet__table-button--disabled textae-editor__type-pallet__delete-predicate"
-          disabled="disabled"
-          title="It cannot be deleted, as this attribute is used for ${numberOfItemsUsingSelectedPred.size} items.">
-        </button>`
-      : `<button
-          type="button"
-          class="textae-editor__type-pallet__table-button textae-editor__type-pallet__delete-predicate"
-          title="Delet this predicate.">
-        </button>`
-  }
-  <button
-    type="button"
-    class="textae-editor__type-pallet__table-button textae-editor__type-pallet__edit-predicate"
-    title="Edit this predicate.">
-  </button>
-`
+    ${
+      numberOfItemsUsingSelectedPred.size > 0
+        ? `<button 
+            type="button"
+            class="textae-editor__type-pallet__table-button textae-editor__type-pallet__table-button--disabled textae-editor__type-pallet__delete-predicate"
+            disabled="disabled"
+            title="It cannot be deleted, as this attribute is used for ${numberOfItemsUsingSelectedPred.size} items.">
+          </button>`
+        : `<button
+            type="button"
+            class="textae-editor__type-pallet__table-button textae-editor__type-pallet__delete-predicate"
+            title="Delet this predicate.">
+          </button>`
+    }
+    <button
+      type="button"
+      class="textae-editor__type-pallet__table-button textae-editor__type-pallet__edit-predicate"
+      title="Edit this predicate.">
+    </button>
+  `
 }
