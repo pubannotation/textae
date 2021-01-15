2ca41c1ed92465ac77796c27ec9b69ac9aec2335
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-15 17:14:15 +0900

    feature: Change the show/hide toggle for the Edit Attribute button to enable/disable toggle

--- src/lib/component/EntityAndAttributePallet/createContentHtml/predicateControllerTemplate/editAttributeButtonTemplate.js
+++ src/lib/component/EntityAndAttributePallet/createContentHtml/predicateControllerTemplate/editAttributeButtonTemplate.js
@@ -2,13 +2,22 @@ export default function (context) {
   const { attrDef, isEntityWithSamePredSelected } = context
   const valueType = attrDef['value type']
 
-  return (valueType === 'string' || valueType === 'numeric') &&
-    isEntityWithSamePredSelected
-    ? `
-      <button
-        type="button"
-        class="textae-editor__type-pallet__edit-object"
-        >edit object of</button>
-    `
+  return valueType === 'string' || valueType === 'numeric'
+    ? isEntityWithSamePredSelected
+      ? `
+        <button
+          type="button"
+          class="textae-editor__type-pallet__edit-object"
+          >edit object of
+        </button>
+        `
+      : `
+        <button
+          type="button"
+          class="textae-editor__type-pallet__edit-object"
+          disabled="disabled"
+          >edit object of
+        </button>
+      `
     : ``
 }
