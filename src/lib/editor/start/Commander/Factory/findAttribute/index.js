b70cb2c5e021255cf6c0bc3c004bf5931eb0f5f1
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-18 14:34:50 +0900

    refactor: Delete an unused module

--- src/lib/editor/start/Commander/Factory/findAttribute/index.js
+++
@@ -1,6 +0,0 @@
-import sameAttribute from './sameAttribute'
-export default function (annotationData, entityId, pred, obj) {
-  return annotationData.entity
-    .get(entityId)
-    .attributes.find(sameAttribute(pred, obj))
-}
--- src/lib/editor/start/Commander/Factory/findAttribute/sameAttribute.js
+++
@@ -1,3 +0,0 @@
-export default function (oldPred, oldObj) {
-  return (a) => a.pred === oldPred && a.obj === oldObj
-}
