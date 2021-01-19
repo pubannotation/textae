73e45fd1175d6dca52b29639f473bc0fd94607fc
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-19 17:37:19 +0900

    refactor: Delete the resetLineHeight

--- src/lib/editor/AnnotationData/createTextBox/TextBox/resetLineHeight.js
+++
@@ -1,8 +0,0 @@
-import pixelToInt from './pixelToInt'
-
-export default function (textBox) {
-  // The default value of the padding top conforms to the line height style of textae.
-  textBox.style.lineHeight = null
-  const style = window.getComputedStyle(textBox)
-  textBox.style.paddingTop = `${pixelToInt(style.lineHeight) / 2}px`
-}
