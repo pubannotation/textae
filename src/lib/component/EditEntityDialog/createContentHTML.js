3212209959218767891ba9e2b1e7078427431df5
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-20 15:56:43 +0900

    refactor: Indent template

--- src/lib/component/EditEntityDialog/createContentHTML.js
+++ src/lib/component/EditEntityDialog/createContentHTML.js
@@ -3,23 +3,23 @@ import toAttributeHTML from './toAttributeHTML'
 
 export default function (typeValues, entityContainer, attributeContainer) {
   const { typeName, attributes } = typeValues
-  const contentHtml = `
-      <table class="textae-editor__edit-type-dialog__table">
-        <thead>
-          <tr>
-            <th>Predicate</th>
-            <th>Value</th>
-            <th>Label</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          ${toEntityHTML(typeName, entityContainer)}
-          ${attributes
-            .map((a) => toAttributeHTML(a, attributeContainer))
-            .join('')}
-          </tbody>
-      </table>
-      `
-  return contentHtml
+
+  return `
+    <table class="textae-editor__edit-type-dialog__table">
+      <thead>
+        <tr>
+          <th>Predicate</th>
+          <th>Value</th>
+          <th>Label</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        ${toEntityHTML(typeName, entityContainer)}
+        ${attributes
+          .map((a) => toAttributeHTML(a, attributeContainer))
+          .join('')}
+        </tbody>
+    </table>
+  `
 }
