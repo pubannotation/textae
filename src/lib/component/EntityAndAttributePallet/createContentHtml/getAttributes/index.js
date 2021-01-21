54422f5f5be6725d2248260f2f4166765cff8718
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-22 05:00:44 +0900

    refactor: Delete the getAttributes

--- src/lib/component/EntityAndAttributePallet/createContentHtml/getAttributes/index.js
+++
@@ -1,9 +0,0 @@
-import getAttributeForSelecetdePred from './getAttributeForSelecetdePred'
-
-export default function (attributeContainer, selectedPred) {
-  if (selectedPred) {
-    return getAttributeForSelecetdePred(attributeContainer, selectedPred)
-  } else {
-    return attributeContainer.attributes
-  }
-}
