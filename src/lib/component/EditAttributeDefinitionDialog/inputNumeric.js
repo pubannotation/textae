4e38c2227f64d5d02476729e680bb9f98d6cec1d
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-23 03:42:19 +0900

    refactor: Indent tempalte

--- src/lib/component/EditAttributeDefinitionDialog/inputNumeric.js
+++ src/lib/component/EditAttributeDefinitionDialog/inputNumeric.js
@@ -1,18 +1,18 @@
 export default function (min, max, step) {
   return `
-  <div class="textae-editor__edit-attribute-definition-dialog__row">
-  <div class="textae-editor__edit-attribute-definition-dialog__min">
-    <label>Min:</label><br>
-    <input type="text" value="${min || ''}">
-  </div>
-  <div class="textae-editor__edit-attribute-definition-dialog__max">
-    <label>Max:</label><br>
-    <input type="text" value="${max || ''}">
-  </div>
-  <div class="textae-editor__edit-attribute-definition-dialog__step">
-    <label>Step:</label><br>
-    <input type="text" value="${step || ''}">
-  </div>
-</div>
-`
+    <div class="textae-editor__edit-attribute-definition-dialog__row">
+      <div class="textae-editor__edit-attribute-definition-dialog__min">
+        <label>Min:</label><br>
+        <input type="text" value="${min || ''}">
+      </div>
+      <div class="textae-editor__edit-attribute-definition-dialog__max">
+        <label>Max:</label><br>
+        <input type="text" value="${max || ''}">
+      </div>
+      <div class="textae-editor__edit-attribute-definition-dialog__step">
+        <label>Step:</label><br>
+        <input type="text" value="${step || ''}">
+      </div>
+    </div>
+  `
 }
