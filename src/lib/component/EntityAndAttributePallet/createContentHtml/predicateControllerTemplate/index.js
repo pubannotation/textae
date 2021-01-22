398b80e9143f83b2d81ee3e671722c204e6eb9f9
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-22 14:20:52 +0900

    bugfix: Selected item number is not shown in the Pallet; occurs in 6.4.53

--- src/lib/component/EntityAndAttributePallet/createContentHtml/predicateControllerTemplate/index.js
+++ src/lib/component/EntityAndAttributePallet/createContentHtml/predicateControllerTemplate/index.js
@@ -25,6 +25,7 @@ export default function (context) {
             ${addAttributeButtonTempalte(context)}
             ${editAttributeButtonTemplate(context)}
             ${removeAttributeButtonTemplate(context)}
+            the ${numberOfSelectedItems} items selected
             `
           : ``
       }
