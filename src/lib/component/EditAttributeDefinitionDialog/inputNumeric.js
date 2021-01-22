7a8f562bc1e60b1fc4cf00594b74aaf3a64120be
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-23 03:41:48 +0900

    refactor: Show empty string instead of null or undefined

--- src/lib/component/EditAttributeDefinitionDialog/inputNumeric.js
+++ src/lib/component/EditAttributeDefinitionDialog/inputNumeric.js
@@ -3,15 +3,15 @@ export default function (min, max, step) {
   <div class="textae-editor__edit-attribute-definition-dialog__row">
   <div class="textae-editor__edit-attribute-definition-dialog__min">
     <label>Min:</label><br>
-    <input type="text" value="${min}">
+    <input type="text" value="${min || ''}">
   </div>
   <div class="textae-editor__edit-attribute-definition-dialog__max">
     <label>Max:</label><br>
-    <input type="text" value="${max}">
+    <input type="text" value="${max || ''}">
   </div>
   <div class="textae-editor__edit-attribute-definition-dialog__step">
     <label>Step:</label><br>
-    <input type="text" value="${step}">
+    <input type="text" value="${step || ''}">
   </div>
 </div>
 `
