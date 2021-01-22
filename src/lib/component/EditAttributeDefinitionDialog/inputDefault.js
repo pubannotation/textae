18bc73a09001f30ccadffc2b1876a37df34eb5f0
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-23 03:05:34 +0900

    refactor: Show empty string when default is null

--- src/lib/component/EditAttributeDefinitionDialog/inputDefault.js
+++ src/lib/component/EditAttributeDefinitionDialog/inputDefault.js
@@ -2,7 +2,7 @@ export default function (componentClassName, _default) {
   return `
   <div class="${componentClassName}__default">
     <label>Default:</label><br>
-    <input value="${_default}">
+    <input value="${_default || ''}">
   </div>
 `
 }
