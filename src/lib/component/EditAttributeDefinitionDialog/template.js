7d54725c22056179b702d34a761a7229531f0d91
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-23 03:56:58 +0900

    refactor: Define inputAttributeDefinition function

--- src/lib/component/EditAttributeDefinitionDialog/template.js
+++ src/lib/component/EditAttributeDefinitionDialog/template.js
@@ -10,13 +10,24 @@ export default function (context) {
 
   return `
 <div class="${componentClassName}__container">
-  <div class="${componentClassName}__row">
-    <div class="${componentClassName}__pred textae-editor__promise-daialog__observable-element">
-      <label>Predicate:</label><br>
-      <input value="${pred}">
-    </div>
-    ${showDefault ? `${inputDefault(componentClassName, _default)}` : ''}
-  </div>
-  ${showNumeric ? `${inputNumeric(componentClassName, min, max, step)}` : ''}
+  ${inputAttributeDefinition(componentClassName, context)}
 </div>`
+}
+
+function inputAttributeDefinition(componentClassName, context) {
+  const { pred, default: _default, min, max, step, valueType } = context
+
+  const showDefault = valueType === 'numeric' || valueType === 'string'
+  const showNumeric = valueType === 'numeric'
+
+  return `
+    <div class="${componentClassName}__row">
+      <div class="${componentClassName}__pred textae-editor__promise-daialog__observable-element">
+        <label>Predicate:</label><br>
+        <input value="${pred}">
+      </div>
+      ${showDefault ? `${inputDefault(componentClassName, _default)}` : ''}
+    </div>
+    ${showNumeric ? `${inputNumeric(componentClassName, min, max, step)}` : ''}
+  `
 }
