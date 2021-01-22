b50941d4189622ec56e6656a1f30c1099d52d248
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-23 03:05:54 +0900

    refactor: Indent template

--- src/lib/component/EditAttributeDefinitionDialog/inputDefault.js
+++ src/lib/component/EditAttributeDefinitionDialog/inputDefault.js
@@ -1,8 +1,8 @@
 export default function (componentClassName, _default) {
   return `
-  <div class="${componentClassName}__default">
-    <label>Default:</label><br>
-    <input value="${_default || ''}">
-  </div>
-`
+    <div class="${componentClassName}__default">
+      <label>Default:</label><br>
+      <input value="${_default || ''}">
+    </div>
+  `
 }
