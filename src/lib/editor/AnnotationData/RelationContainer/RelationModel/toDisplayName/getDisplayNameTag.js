75c724b79f94203b052a763b6bed8624e7e47fc9
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-24 23:10:45 +0900

    refactor: Use the toLabel module

--- src/lib/editor/AnnotationData/RelationContainer/RelationModel/toDisplayName/getDisplayNameTag.js
+++ src/lib/editor/AnnotationData/RelationContainer/RelationModel/toDisplayName/getDisplayNameTag.js
@@ -1,12 +1,9 @@
+import toLabel from '../../../../EntityModel/createEntityHTMLElement/toLabel'
 import getDisplayName from '../../../../getDisplayName'
 import getUri from '../../../../getUri'
 
 export default function (namespace, typeContainer, value) {
   const label = getDisplayName(namespace, value, typeContainer.getLabel(value))
   const href = getUri(namespace, value, typeContainer.getUri(value))
-  if (href) {
-    return `<a target="_blank"/ href="${href}">${label}</a>`
-  } else {
-    return label
-  }
+  return toLabel(href, label)
 }
