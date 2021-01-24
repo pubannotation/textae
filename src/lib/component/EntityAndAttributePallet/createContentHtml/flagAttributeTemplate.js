48b01a49897dd6ee6509453be31d6ffe67d875cf
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-24 23:43:58 +0900

    feature: Show label and color definition of flag attribute in the pallet

--- src/lib/component/EntityAndAttributePallet/createContentHtml/flagAttributeTemplate.js
+++ src/lib/component/EntityAndAttributePallet/createContentHtml/flagAttributeTemplate.js
@@ -2,11 +2,15 @@ import headerTemplate from './headerTemplate'
 import predicateControllerTemplate from './predicateControllerTemplate'
 
 export default function (context) {
+  const { label, color } = context.attrDef
+
   return `
   ${headerTemplate(context)}
   <div>
     <div class="textae-editor__type-pallet__predicate">
       ${predicateControllerTemplate(context)}
+      label: "${label || ''}"
+      color: "${color || ''}"
     </div>
   </div>
   `
