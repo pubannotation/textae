54ae14abc3cee7049ffe9dc37e775a3abc184cdc
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-25 14:26:43 +0900

    refactor: Use the valueType of the context in the template of the CreateAttributeDefinitionDialog

--- src/lib/component/CreateAttributeDefinitionDialog/template.js
+++ src/lib/component/CreateAttributeDefinitionDialog/template.js
@@ -1,12 +1,7 @@
 import inputAttributeDefinition from '../inputAttributeDefinition'
 
 export default function (context) {
-  const {
-    flagSelected,
-    selectionSelected,
-    stringSelected,
-    numericSelected
-  } = context
+  const { valueType } = context
   const componentClassName = 'textae-editor__create-attribute-definition-dialog'
 
   return `
@@ -14,16 +9,16 @@ export default function (context) {
   <div class="${componentClassName}__row">
     <label>Attribute type:</label>
     <select class="${componentClassName}__value-type">
-      <option value="flag"${flagSelected ? ` selected` : ``}>
+      <option value="flag"${valueType === 'flag' ? ` selected` : ``}>
         flag
         </option>
-      <option value="selection"${selectionSelected ? ` selected` : ``}>
+      <option value="selection"${valueType === 'selection' ? ` selected` : ``}>
         selection
       </option>
-      <option value="string"${stringSelected ? ` selected` : ``}>
+      <option value="string"${valueType === 'string' ? ` selected` : ``}>
         string
       </option>
-      <option value="numeric"${numericSelected ? ` selected` : ``}>
+      <option value="numeric"${valueType === 'numeric' ? ` selected` : ``}>
         numeric
       </option>
     </select>
