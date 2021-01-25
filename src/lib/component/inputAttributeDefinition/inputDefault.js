31a29587477d0781ea844b6706bb42b554f6f4bb
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-25 14:07:41 +0900

    refactor: Rename _default local variable

--- src/lib/component/inputAttributeDefinition/inputDefault.js
+++ src/lib/component/inputAttributeDefinition/inputDefault.js
@@ -1,8 +1,8 @@
-export default function (componentClassName, _default) {
+export default function (componentClassName, defaultValue) {
   return `
     <div class="${componentClassName}__default">
       <label>Default:</label><br>
-      <input value="${_default || ''}">
+      <input value="${defaultValue || ''}">
     </div>
   `
 }
