8d92b97b833b5417b430506d65ac81aab10dcff7
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-26 14:20:02 +0900

    refactor: Pass the attribute definition to the tepmlate

--- src/lib/component/EditAttributeDefinitionDialog/index.js
+++ src/lib/component/EditAttributeDefinitionDialog/index.js
@@ -5,93 +5,79 @@ import template from './template'
 
 export default class EditAttributeDefinitionDialog extends PromiseDialog {
   constructor(attrDef) {
-    super(
-      'Please enter new values',
-      template({
-        pred: attrDef.pred,
-        default: attrDef.default,
-        label: attrDef.label,
-        color: attrDef.color,
-        min: attrDef.min,
-        max: attrDef.max,
-        step: attrDef.step,
-        valueType: attrDef.valueType
-      }),
-      {},
-      () => {
-        const pred = getInputElementValue(
-          super.el,
-          '.textae-editor__edit-attribute-definition-dialog__pred'
-        )
-        const defaultValue = getInputElementValue(
-          super.el,
-          '.textae-editor__edit-attribute-definition-dialog__default-value'
-        )
+    super('Please enter new values', template(attrDef), {}, () => {
+      const pred = getInputElementValue(
+        super.el,
+        '.textae-editor__edit-attribute-definition-dialog__pred'
+      )
+      const defaultValue = getInputElementValue(
+        super.el,
+        '.textae-editor__edit-attribute-definition-dialog__default-value'
+      )
 
-        const diff = new Map()
+      const diff = new Map()
 
-        if (attrDef.pred !== pred) {
-          diff.set('pred', pred)
-        }
-
-        if (attrDef.valueType === 'flag') {
-          const label = getInputElementValue(
-            super.el,
-            '.textae-editor__edit-attribute-definition-dialog__label'
-          )
-          const color = getInputElementValue(
-            super.el,
-            '.textae-editor__edit-attribute-definition-dialog__color'
-          )
-
-          if (attrDef.label !== label) {
-            diff.set('label', label)
-          }
-
-          if (attrDef.color !== color) {
-            diff.set('color', color)
-          }
-        }
-
-        if (attrDef.valueType === 'string') {
-          if (attrDef.default !== defaultValue) {
-            diff.set('default', defaultValue)
-          }
-        }
-
-        if (attrDef.valueType === 'numeric') {
-          if (isChanged(attrDef.default, defaultValue)) {
-            diff.set('default', parseFloat(defaultValue))
-          }
-
-          const min = getInputElementValue(
-            super.el,
-            '.textae-editor__edit-attribute-definition-dialog__min'
-          )
-          const max = getInputElementValue(
-            super.el,
-            '.textae-editor__edit-attribute-definition-dialog__max'
-          )
-          const step = getInputElementValue(
-            super.el,
-            '.textae-editor__edit-attribute-definition-dialog__step'
-          )
-
-          if (isChanged(attrDef.min, min)) {
-            diff.set('min', parseFloat(min))
-          }
-
-          if (isChanged(attrDef.max, max)) {
-            diff.set('max', parseFloat(max))
-          }
-
-          if (isChanged(attrDef.step, step)) {
-            diff.set('step', parseFloat(step))
-          }
-        }
-
-        return diff
+      if (attrDef.pred !== pred) {
+        diff.set('pred', pred)
       }
-    )
+
+      if (attrDef.valueType === 'flag') {
+        const label = getInputElementValue(
+          super.el,
+          '.textae-editor__edit-attribute-definition-dialog__label'
+        )
+        const color = getInputElementValue(
+          super.el,
+          '.textae-editor__edit-attribute-definition-dialog__color'
+        )
+
+        if (attrDef.label !== label) {
+          diff.set('label', label)
+        }
+
+        if (attrDef.color !== color) {
+          diff.set('color', color)
+        }
+      }
+
+      if (attrDef.valueType === 'string') {
+        if (attrDef.default !== defaultValue) {
+          diff.set('default', defaultValue)
+        }
+      }
+
+      if (attrDef.valueType === 'numeric') {
+        if (isChanged(attrDef.default, defaultValue)) {
+          diff.set('default', parseFloat(defaultValue))
+        }
+
+        const min = getInputElementValue(
+          super.el,
+          '.textae-editor__edit-attribute-definition-dialog__min'
+        )
+        const max = getInputElementValue(
+          super.el,
+          '.textae-editor__edit-attribute-definition-dialog__max'
+        )
+        const step = getInputElementValue(
+          super.el,
+          '.textae-editor__edit-attribute-definition-dialog__step'
+        )
+
+        if (isChanged(attrDef.min, min)) {
+          diff.set('min', parseFloat(min))
+        }
+
+        if (isChanged(attrDef.max, max)) {
+          diff.set('max', parseFloat(max))
+        }
+
+        if (isChanged(attrDef.step, step)) {
+          diff.set('step', parseFloat(step))
+        }
+      }
+
+      return diff
+    })
   }
 }
