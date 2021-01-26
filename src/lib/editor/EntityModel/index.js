53b8cf82e49470644e8e95e0e1af1526b0c06af1
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-26 13:58:45 +0900

    refactor: Use the this._typeDefinition.attribute

--- src/lib/editor/EntityModel/index.js
+++ src/lib/editor/EntityModel/index.js
@@ -94,7 +94,7 @@ export default class EntityModel {
       this.typeValues.toHTMLElementContext(
         namespace,
         typeContainer,
-        attributeContainer
+        this._typeDefinition.attribute
       )
     )
   }
