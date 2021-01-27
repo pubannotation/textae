6c8617c08a29c3613e546d5bdb6a7576e56311d7
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-27 16:45:27 +0900

    feature: Add title bar style like jQueryUI dialog to the Pallet

--- src/lib/component/Pallet/updateDisplay/index.js
+++ src/lib/component/Pallet/updateDisplay/index.js
@@ -8,7 +8,13 @@ export default function (editor, pallet, content) {
   // After updating the palette, the click event reaches the body.
   // At that time, if the target of the event is the palette, you can see that it is an event for textae-editor.
   pallet.innerHTML = `
-    <div class="textae-editor__type-pallet__title"></div>
+    <div class="textae-editor__type-pallet__title ui-widget-header ui-corner-all">
+      <span>Configuration</span>
+      <button type="button" class="ui-button ui-corner-all ui-widget ui-button-icon-only ui-dialog-titlebar-close" title="Close">
+        <span class="ui-button-icon ui-icon ui-icon-closethick"></span>
+        <span class="ui-button-icon-space"> </span>Close
+      </button>
+    </div>
     <div class="textae-editor__type-pallet__content">${content}</div>
   `
   setWidthWithinWindow(editor, pallet)
