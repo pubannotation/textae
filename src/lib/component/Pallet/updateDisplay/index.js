7c51f5a93a7f71441f11f9645fbc4bf5eb4fddc3
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-27 16:31:39 +0900

    refactor: Add division of the title of the Pallet

--- src/lib/component/Pallet/updateDisplay/index.js
+++ src/lib/component/Pallet/updateDisplay/index.js
@@ -7,7 +7,10 @@ export default function (editor, pallet, content) {
   // The taxtae-editor deselects itself when a click event to something other than taxtae-editor occurs.
   // After updating the palette, the click event reaches the body.
   // At that time, if the target of the event is the palette, you can see that it is an event for textae-editor.
-  pallet.innerHTML = `<div class="textae-editor__type-pallet__content">${content}</div>`
+  pallet.innerHTML = `
+    <div class="textae-editor__type-pallet__title"></div>
+    <div class="textae-editor__type-pallet__content">${content}</div>
+  `
   setWidthWithinWindow(editor, pallet)
   setHeightWithinWindow(pallet)
 }
