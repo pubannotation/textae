f4aa01e0cc54b326847e874d3a10ea176af82315
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-29 16:49:41 +0900

    refactor: Inline the getExistingSamePredicateAttributes

--- src/lib/editor/start/Commander/Factory/getRemoveAttributesByPredCommands/index.js
+++ src/lib/editor/start/Commander/Factory/getRemoveAttributesByPredCommands/index.js
@@ -1,5 +1,4 @@
 import getRemoveAtributesByPredAndObjCommands from './getRemoveAtributesByPredAndObjCommands'
-import getExistingSamePredicateAttributes from './getExistingSamePredicateAttributes'
 
 export default function (
   editor,
@@ -7,10 +6,10 @@ export default function (
   selectionModel,
   attributeDefinition
 ) {
-  const attrs = getExistingSamePredicateAttributes(
-    selectionModel.entity.all,
-    attributeDefinition
-  )
+  const attrs = selectionModel.entity.all
+    .reduce((attrs, entity) => attrs.concat(entity.attributes), [])
+    .filter((a) => a.pred === attributeDefinition.pred)
+
   return getRemoveAtributesByPredAndObjCommands(
     attrs,
     editor,
