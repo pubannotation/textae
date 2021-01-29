dbba7762c630604663691970ce95233f19bc8fa1
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-29 16:44:45 +0900

    refactor: Use concat

--- src/lib/editor/start/Commander/Factory/getRemoveAttributesByPredCommands/getExistingSamePredicateAttributes.js
+++ src/lib/editor/start/Commander/Factory/getRemoveAttributesByPredCommands/getExistingSamePredicateAttributes.js
@@ -1,8 +1,5 @@
 export default function (entities, { pred }) {
   return entities.reduce((attrs, entity) => {
-    for (const attr of entity.attributes.filter((a) => a.pred === pred)) {
-      attrs.push(attr)
-    }
-    return attrs
+    return attrs.concat(entity.attributes.filter((a) => a.pred === pred))
   }, [])
 }
