0c88a7b52b79628c243b1cebc578ba71ab6425ab
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-29 16:46:13 +0900

    refactor: Separate reduce and filter

--- src/lib/editor/start/Commander/Factory/getRemoveAttributesByPredCommands/getExistingSamePredicateAttributes.js
+++ src/lib/editor/start/Commander/Factory/getRemoveAttributesByPredCommands/getExistingSamePredicateAttributes.js
@@ -1,5 +1,7 @@
 export default function (entities, { pred }) {
-  return entities.reduce((attrs, entity) => {
-    return attrs.concat(entity.attributes.filter((a) => a.pred === pred))
-  }, [])
+  return entities
+    .reduce((attrs, entity) => {
+      return attrs.concat(entity.attributes)
+    }, [])
+    .filter((a) => a.pred === pred)
 }
