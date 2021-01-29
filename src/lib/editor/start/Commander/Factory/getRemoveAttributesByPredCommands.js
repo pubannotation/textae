783025ee838c0a0a5e3e4123751c950df7007c1c
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-29 17:05:51 +0900

    refactor: Extract local variable

--- src/lib/editor/start/Commander/Factory/getRemoveAttributesByPredCommands.js
+++ src/lib/editor/start/Commander/Factory/getRemoveAttributesByPredCommands.js
@@ -1,7 +1,9 @@
 import { RemoveCommand } from './commandTemplate'
 
 export default function (editor, annotationData, selectionModel, pred) {
-  return selectionModel.entity.all
+  const items = selectionModel.entity.all
+
+  return items
     .reduce((attrs, entity) => attrs.concat(entity.attributes), [])
     .filter((a) => a.pred === pred)
     .map(
