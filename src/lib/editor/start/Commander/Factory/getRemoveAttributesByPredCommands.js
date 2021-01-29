e85fa27600f75b8165c548873596874c2493ea9f
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-29 17:03:36 +0900

    refactor: Refactor the getRemoveAttributesByPredCommands

--- src/lib/editor/start/Commander/Factory/getRemoveAttributesByPredCommands.js
+++ src/lib/editor/start/Commander/Factory/getRemoveAttributesByPredCommands.js
@@ -4,13 +4,14 @@ export default function (editor, annotationData, selectionModel, pred) {
   return selectionModel.entity.all
     .reduce((attrs, entity) => attrs.concat(entity.attributes), [])
     .filter((a) => a.pred === pred)
-    .map((attribute) => {
-      return new RemoveCommand(
-        editor,
-        annotationData,
-        selectionModel,
-        'attribute',
-        attribute.id
-      )
-    })
+    .map(
+      (a) =>
+        new RemoveCommand(
+          editor,
+          annotationData,
+          selectionModel,
+          'attribute',
+          a.id
+        )
+    )
 }
