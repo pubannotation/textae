f488e92ba8944c4bc916710810d3530ec6d181d6
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-31 06:55:07 +0900

    refactor: Inline a local valiable

--- src/lib/editor/start/Presenter/openEditStringAttributeDialog/index.js
+++ src/lib/editor/start/Presenter/openEditStringAttributeDialog/index.js
@@ -2,21 +2,24 @@ import EditStringAttributeDialog from '../../../../component/EditStringAttribute
 
 export default function (selectionModelEntity, attribute, commander, attrDef) {
   const dialog = new EditStringAttributeDialog(attribute, attrDef)
+
   dialog.promise.then(({ newObj, newLabel }) => {
-    let command
     if (newObj) {
-      command = commander.factory.changeAttributesOfSelectedEntitiesWithSamePred(
-        attrDef,
-        newObj,
-        newLabel
+      commander.invoke(
+        commander.factory.changeAttributesOfSelectedEntitiesWithSamePred(
+          attrDef,
+          newObj,
+          newLabel
+        )
       )
     } else {
-      command = commander.factory.removeAttributesFromSelectedEntitiesByPredCommand(
-        attrDef
+      commander.invoke(
+        commander.factory.removeAttributesFromSelectedEntitiesByPredCommand(
+          attrDef
+        )
       )
     }
-
-    commander.invoke(command)
   })
+
   dialog.open()
 }
