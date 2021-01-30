e97fa94226247f16e5b391f4fc15e8c3c9b7cc44
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-31 07:03:50 +0900

    refactor: Inline a local variables

--- src/lib/editor/start/Presenter/EditMode/AttributeEditor/toggleFlagAttribute.js
+++ src/lib/editor/start/Presenter/EditMode/AttributeEditor/toggleFlagAttribute.js
@@ -1,6 +1,5 @@
 export default function (attrDef, commander) {
-  const command = commander.factory.toggleFlagAttributeToSelectedEntitiesCommand(
-    attrDef
+  commander.invoke(
+    commander.factory.toggleFlagAttributeToSelectedEntitiesCommand(attrDef)
   )
-  commander.invoke(command)
 }
