910c8256e931d18fe09328bffb71caa0b9a7f96d
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-31 06:52:57 +0900

    refactor: Extract a local variable

--- src/lib/editor/start/Presenter/openEditStringAttributeDialog/index.js
+++ src/lib/editor/start/Presenter/openEditStringAttributeDialog/index.js
@@ -4,9 +4,14 @@ import getChangeOrRemoveCommand from './getChangeOrRemoveCommand'
 export default function (selectionModelEntity, attribute, commander, attrDef) {
   const dialog = new EditStringAttributeDialog(attribute, attrDef)
   dialog.promise.then(({ newObj, newLabel }) => {
-    commander.invoke(
-      getChangeOrRemoveCommand(newObj, commander, attrDef, newLabel)
+    const command = getChangeOrRemoveCommand(
+      newObj,
+      commander,
+      attrDef,
+      newLabel
     )
+
+    commander.invoke(command)
   })
   dialog.open()
 }
