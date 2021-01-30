e623222fccedd14ace64d618a4f40ad14a25e78e
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-01-31 06:54:03 +0900

    refactor:Inline the getChangeOrRemoveCommand

--- src/lib/editor/start/Presenter/openEditStringAttributeDialog/index.js
+++ src/lib/editor/start/Presenter/openEditStringAttributeDialog/index.js
@@ -1,15 +1,20 @@
 import EditStringAttributeDialog from '../../../../component/EditStringAttributeDialog'
-import getChangeOrRemoveCommand from './getChangeOrRemoveCommand'
 
 export default function (selectionModelEntity, attribute, commander, attrDef) {
   const dialog = new EditStringAttributeDialog(attribute, attrDef)
   dialog.promise.then(({ newObj, newLabel }) => {
-    const command = getChangeOrRemoveCommand(
-      newObj,
-      commander,
-      attrDef,
-      newLabel
-    )
+    let command
+    if (newObj) {
+      command = commander.factory.changeAttributesOfSelectedEntitiesWithSamePred(
+        attrDef,
+        newObj,
+        newLabel
+      )
+    } else {
+      command = commander.factory.removeAttributesFromSelectedEntitiesByPredCommand(
+        attrDef
+      )
+    }
 
     commander.invoke(command)
   })
