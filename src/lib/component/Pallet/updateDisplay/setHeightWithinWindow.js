72dcfb456f84d45e97571bd9effe2e00431bfd05
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-02-02 03:08:41 +0900

    bugfix: The height of the pallet will not increase once it is shrunk

--- src/lib/component/Pallet/updateDisplay/setHeightWithinWindow.js
+++ src/lib/component/Pallet/updateDisplay/setHeightWithinWindow.js
@@ -2,5 +2,7 @@ export default function (pallet) {
   if (window.innerHeight - 2 <= pallet.offsetHeight) {
     const height = window.innerHeight - 2
     pallet.style.height = `${height}px`
+  } else {
+    pallet.style.height = null
   }
 }
