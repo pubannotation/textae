4ecf8259322155bcb114e83f736e06cea6cd659a
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-02-02 09:06:18 +0900

    refactor: Inline the updateDisplay

--- src/lib/component/Pallet/index.js
+++ src/lib/component/Pallet/index.js
@@ -1,8 +1,9 @@
 import delegate from 'delegate'
 import enableJqueryDraggable from './enableJqueryDraggable'
-import updateDisplay from './updateDisplay'
 import getMousePoint from './getMousePoint'
 import createPalletElement from './createPalletElement'
+import setWidthWithin from './updateDisplay/setWidthWithin'
+import setHeightWithin from './updateDisplay/setHeightWithin'
 
 export default class Pallet {
   constructor(editor, annotationType) {
@@ -52,7 +53,12 @@ export default class Pallet {
 
   updateDisplay() {
     if (this.visibly) {
-      updateDisplay(this._editor, this._el, this._annotationType, this._content)
+      this._updateDisplay(
+        this._editor,
+        this._el,
+        this._annotationType,
+        this._content
+      )
     }
   }
 
@@ -62,9 +68,40 @@ export default class Pallet {
 
   show() {
     this._el.style.display = 'block'
-    updateDisplay(this._editor, this._el, this._annotationType, this._content)
+    this._updateDisplay(
+      this._editor,
+      this._el,
+      this._annotationType,
+      this._content
+    )
 
     this._moveInto(this._editor, this._el)
+  }
+
+  _updateDisplay(editor, pallet, annotationType, content) {
+    // Wrap the content in a special class so that you can determine if the target of the event is an element of the palette
+    // even after the content has been removed from the DOM tree.
+    // The taxtae-editor deselects itself when a click event to something other than taxtae-editor occurs.
+    // After updating the palette, the click event reaches the body.
+    // At that time, if the target of the event is the palette, you can see that it is an event for textae-editor.
+    pallet.innerHTML = `
+      <div class="textae-editor__type-pallet__title-bar ui-widget-header ui-corner-all">
+        <span>${
+          annotationType.charAt(0).toUpperCase() + annotationType.slice(1)
+        } configuration</span>
+        <button 
+          type="button"
+          class="textae-editor__type-pallet__close-button ui-button ui-corner-all ui-widget ui-button-icon-only ui-dialog-titlebar-close"
+          title="Close">
+          <span class="ui-button-icon ui-icon ui-icon-closethick"></span>
+          <span class="ui-button-icon-space"> </span>Close
+        </button>
+      </div>
+      <div class="textae-editor__type-pallet__content">${content}</div>
+    `
+
+    setWidthWithin(pallet, editor[0].offsetWidth)
+    setHeightWithin(pallet, window.innerHeight)
   }
 
   _moveInto(editor, pallet) {
