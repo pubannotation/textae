c9850a659f0e64f0a62849a05dfc66196cc5d1cc
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-02-02 16:50:34 +0900

    refactor: Use destructuring assignment

--- src/lib/editor/start/Commander/Factory/getRemoveAttributesByPredCommands.js
+++ src/lib/editor/start/Commander/Factory/getRemoveAttributesByPredCommands.js
@@ -2,7 +2,7 @@ import { RemoveCommand } from './commandTemplate'
 
 export default function (editor, annotationData, items, pred) {
   return items
-    .reduce((attrs, entity) => attrs.concat(entity.attributes), [])
+    .reduce((attrs, { attributes }) => attrs.concat(attributes), [])
     .filter((a) => a.pred === pred)
     .map((a) => new RemoveCommand(editor, annotationData, 'attribute', a.id))
 }
