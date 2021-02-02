b926b0d0b86cbb34a8bb63b3b4019938ed9d35b3
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-02-02 14:42:20 +0900

    refactor: Rename a constant variable

--- src/lib/component/Pallet/setHeightWithin.js
+++ src/lib/component/Pallet/setHeightWithin.js
@@ -1,8 +1,8 @@
-const BUFFER = 14
+const BORDER_HEIGHT = 7 * 2
 
 export default function (pallet, height) {
-  if (height - BUFFER <= pallet.offsetHeight) {
-    pallet.style.height = `${height - BUFFER}px`
+  if (height - BORDER_HEIGHT <= pallet.offsetHeight) {
+    pallet.style.height = `${height - BORDER_HEIGHT}px`
   } else {
     pallet.style.height = null
   }
