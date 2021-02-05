5b4546e8dda7c5c74bc7bede3bb04a030d0b1098
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-02-05 13:47:30 +0900

    refactor: Return kind of a command directly

--- src/lib/editor/History/Commands.js
+++ src/lib/editor/History/Commands.js
@@ -6,6 +6,6 @@ export default class Commands {
   // Summarizes whether the included command affects Annotation or Configuration,
   // and determines whether there is content to be saved in Annotation or Configuration.
   get kinds() {
-    return new Set([...this.commands[0].kind])
+    return this.commands[0].kind
   }
 }
