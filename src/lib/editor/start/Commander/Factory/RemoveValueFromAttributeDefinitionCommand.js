6c7b873618f8c50fd24f75c44d5f67976f54351f
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-02-08 21:32:38 +0900

    refactor: Copy only what you need

--- src/lib/editor/start/Commander/Factory/RemoveValueFromAttributeDefinitionCommand.js
+++ src/lib/editor/start/Commander/Factory/RemoveValueFromAttributeDefinitionCommand.js
@@ -12,7 +12,7 @@ export default class RemoveValueFromAttributeDefinitionCommand extends Configura
   }
 
   execute() {
-    const { values } = this._attrDef.JSON
+    const values = [...this._attrDef.values]
     // When removing value with default property.
     if (
       this._attrDef.valueType === 'selection' &&
@@ -27,7 +27,11 @@ export default class RemoveValueFromAttributeDefinitionCommand extends Configura
         }
       })
 
-      values[indexThatAddDefaultTo].default = true
+      values[indexThatAddDefaultTo] = Object.assign(
+        {},
+        values[indexThatAddDefaultTo],
+        { default: true }
+      )
     }
 
     this._deletedValue = values.splice(this._index, 1)[0]
@@ -37,7 +41,11 @@ export default class RemoveValueFromAttributeDefinitionCommand extends Configura
       this._attrDef.valueType === 'selection' &&
       this._indexThatAddDefaultTo !== null
     ) {
-      values[this._indexThatAddDefaultTo].default = true
+      values[this._indexThatAddDefaultTo] = Object.assign(
+        {},
+        values[this._indexThatAddDefaultTo],
+        { default: true }
+      )
     }
 
     this._updatedAttrDef = this._typeContainer.updateValues(
