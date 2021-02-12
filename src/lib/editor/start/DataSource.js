159f845f590be38a21ff231555f3db9c52ba36b9
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-02-12 22:13:29 +0900

    refactor: Add DataSource class

---
+++ src/lib/editor/start/DataSource.js
@@ -0,0 +1,21 @@
+import toSourceStrin from './toSourceString'
+
+export default class DataSource {
+  constructor(type, id, data) {
+    this._type = type
+    this._id = id
+    this._data = data
+  }
+
+  get type() {
+    return this._type
+  }
+
+  get data() {
+    return this._data
+  }
+
+  get displayName() {
+    return toSourceStrin(this._type, this._id)
+  }
+}
