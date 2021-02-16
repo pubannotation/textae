0544025553b94c1c49a0bf382ae872cf02cef3a3
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-02-17 04:50:16 +0900

    refactor: Use a promise from the PromiseDialog.open

--- src/lib/editor/start/Presenter/openEditStringAttributeDialog.js
+++ src/lib/editor/start/Presenter/openEditStringAttributeDialog.js
@@ -1,27 +1,25 @@
 import EditStringAttributeDialog from '../../../component/EditStringAttributeDialog'
 
 export default function (selectionModelEntity, attribute, commander, attrDef) {
-  const dialog = new EditStringAttributeDialog(attribute, attrDef)
-
-  dialog.promise.then(({ newObj, newLabel }) => {
-    if (newObj) {
-      commander.invoke(
-        commander.factory.changeAttributesOfItemsWithSamePred(
-          selectionModelEntity.all,
-          attrDef,
-          newObj,
-          newLabel
+  new EditStringAttributeDialog(attribute, attrDef)
+    .open()
+    .then(({ newObj, newLabel }) => {
+      if (newObj) {
+        commander.invoke(
+          commander.factory.changeAttributesOfItemsWithSamePred(
+            selectionModelEntity.all,
+            attrDef,
+            newObj,
+            newLabel
+          )
         )
-      )
-    } else {
-      commander.invoke(
-        commander.factory.removeAttributesFromItemsByPredCommand(
-          selectionModelEntity.all,
-          attrDef
+      } else {
+        commander.invoke(
+          commander.factory.removeAttributesFromItemsByPredCommand(
+            selectionModelEntity.all,
+            attrDef
+          )
         )
-      )
-    }
-  })
-
-  dialog.open()
+      }
+    })
 }
