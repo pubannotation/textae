296ed2f3c869f86433c3b7c760ffe5bbbc801561
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-02-17 04:49:03 +0900

    refactor: Use a promise from the PromiseDialog.open

--- src/lib/editor/start/Presenter/openEditNumericAttributeDialog.js
+++ src/lib/editor/start/Presenter/openEditNumericAttributeDialog.js
@@ -1,14 +1,14 @@
 import EditNumericAttributeDialog from '../../../component/EditNumericAttributeDialog'
 
 export default function (selectionModelEntity, attrDef, attribute, commander) {
-  const dialog = new EditNumericAttributeDialog(attrDef, attribute)
-  dialog.promise.then(({ newObj }) => {
-    const command = commander.factory.changeAttributesOfItemsWithSamePred(
-      selectionModelEntity.all,
-      attrDef,
-      newObj
-    )
-    commander.invoke(command)
-  })
-  dialog.open()
+  new EditNumericAttributeDialog(attrDef, attribute)
+    .open()
+    .then(({ newObj }) => {
+      const command = commander.factory.changeAttributesOfItemsWithSamePred(
+        selectionModelEntity.all,
+        attrDef,
+        newObj
+      )
+      commander.invoke(command)
+    })
 }
