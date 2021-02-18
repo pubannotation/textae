9983fd8e1aca9af53d7722028127530831e5ff38
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-02-18 13:54:22 +0900

    refactor: Delete the connectorStrokeStyle

--- src/lib/editor/AnnotationData/RelationContainer/RelationModel/JsPlumbConnectionWrapper/connectorStrokeStyle/index.js
+++
@@ -1,7 +0,0 @@
-import converseHEXinotRGBA from './converseHEXinotRGBA'
-
-export default function (definitionContainer, relation) {
-  return {
-    strokeStyle: converseHEXinotRGBA(relation.getColor(definitionContainer))
-  }
-}
