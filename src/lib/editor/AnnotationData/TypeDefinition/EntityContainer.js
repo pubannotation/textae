9630818e88b33f78b3dc61e4596d32c150f2734f
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-02-18 11:41:54 +0900

    refactor: Delete the EntityContainer

--- src/lib/editor/AnnotationData/TypeDefinition/EntityContainer.js
+++
@@ -1,7 +0,0 @@
-import Container from './Container'
-
-export default class EntityContainer extends Container {
-  constructor(editor, getAllInstanceFunc) {
-    super(editor, 'entity', getAllInstanceFunc, '#77DDDD')
-  }
-}
