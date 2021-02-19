1a27b9684f2d2469cdb4c4f5a02dfc89eec29525
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-02-20 00:01:20 +0900

    bugfix: An error occurs when the authentication window fails to appear

--- src/lib/editor/DataAccessObject/AjaxSender/requestAjax/serverAuthHandler/index.js
+++ src/lib/editor/DataAccessObject/AjaxSender/requestAjax/serverAuthHandler/index.js
@@ -6,15 +6,18 @@ export default function (ajaxResponse, errorHandler, retryHandler) {
 
   if (location) {
     const win = openPopUp(location)
-
-    // Watching for cross-domain pop-up windows to close.
-    // https://stackoverflow.com/questions/9388380/capture-the-close-event-of-popup-window-in-javascript/48240128#48240128
-    const timer = setInterval(() => {
-      if (win.closed) {
-        clearInterval(timer)
-        retryHandler()
-      }
-    }, 1000)
+    if (win) {
+      // Watching for cross-domain pop-up windows to close.
+      // https://stackoverflow.com/questions/9388380/capture-the-close-event-of-popup-window-in-javascript/48240128#48240128
+      const timer = setInterval(() => {
+        if (win.closed) {
+          clearInterval(timer)
+          retryHandler()
+        }
+      }, 1000)
+    } else {
+      errorHandler()
+    }
   } else {
     errorHandler()
   }
