dfbeec5171e6d638ebb9fe317af19b4e83321f8a
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-02-22 13:50:20 +0900

    refactor: Rename the DefaultHandler.removeType

--- src/lib/editor/start/Presenter/EditMode/DefaultHandler.js
+++ src/lib/editor/start/Presenter/EditMode/DefaultHandler.js
@@ -29,7 +29,7 @@ export default class DefaultHandler {
     )
   }
 
-  removeType(id, label) {
+  removeTypeDefinition(id, label) {
     const removeType = {
       id,
       label: label || ''
--- src/lib/editor/start/Presenter/EditMode/Edit/bindPalletEvents/index.js
+++ src/lib/editor/start/Presenter/EditMode/Edit/bindPalletEvents/index.js
@@ -71,7 +71,7 @@ export default function (
     }
 
     commander.invoke(
-      handler.removeType(
+      handler.removeTypeDefinition(
         e.delegateTarget.dataset.id,
         e.delegateTarget.dataset.label
       )
