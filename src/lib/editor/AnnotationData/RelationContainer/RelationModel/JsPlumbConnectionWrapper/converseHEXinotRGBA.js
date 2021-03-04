d972b7816bde5fe853b667aa644b4d588e814dc6
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-04 14:29:44 +0900

    refactor: Delete the JsPlumbConnectionWrapper

--- src/lib/editor/AnnotationData/RelationContainer/RelationModel/JsPlumbConnectionWrapper/converseHEXinotRGBA.js
+++
@@ -1,8 +0,0 @@
-export default function (color) {
-  const c = color.slice(1)
-  const r = parseInt(c.substr(0, 2), 16)
-  const g = parseInt(c.substr(2, 2), 16)
-  const b = parseInt(c.substr(4, 2), 16)
-
-  return `rgba(${r}, ${g}, ${b}, 1)`
-}
