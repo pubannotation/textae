c10fbe1a294cfd0c7489c6d274c286e2b377e0df
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-04 14:31:12 +0900

    refactor: Delete id attribute of entity html element for jsPlumb

--- src/lib/editor/EntityModel/createEntityHTMLElement/index.js
+++ src/lib/editor/EntityModel/createEntityHTMLElement/index.js
@@ -6,15 +6,10 @@ export default function (context) {
   const { id, title, color, href, displayName, attributes } = context
 
   // A Type element has an entity_pane elment that has a label and will have entities.
-  // jsPlumb requires the id of the DOM which is the endpoint for drawing relationships.
-  // If the endpoint doesn't have an id, jsPlumb will set it,
-  // and the id will be lost when redrawing the Entity's DOM.
-  // To prevent this from happening, set the id of the endpoint DOM.
   const html = `
 <div class="textae-editor__entity" id="${id}" title="${title}">
   <div
     class="textae-editor__entity__type-values"
-    id="jsPlumb_${id}" 
     style="background-color: ${color};"
     >
     <div
