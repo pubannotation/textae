9440c7fb4d90bb6be2ac83a83eff3bfefb6ba6d3
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-05 16:16:29 +0900

    refactor: Delete the renderAllAnnotations

--- src/lib/editor/start/View/Renderer/bindAnnotationDataEvents/renderAllAnnotations/index.js
+++
@@ -1,8 +0,0 @@
-import getAnnotationBox from '../../../../../getAnnotationBox'
-
-export default function (editor, annotationData, spanRenderer) {
-  getAnnotationBox(editor).innerHTML = ''
-  for (const span of annotationData.span.topLevel) {
-    spanRenderer.render(span)
-  }
-}
