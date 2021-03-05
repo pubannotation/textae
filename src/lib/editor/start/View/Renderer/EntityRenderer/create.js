1896e27bbc12b71e108f1bdf174fc74a8cdab65c
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-05 16:58:49 +0900

    refactor: Delete a old comment

--- src/lib/editor/start/View/Renderer/EntityRenderer/create.js
+++ src/lib/editor/start/View/Renderer/EntityRenderer/create.js
@@ -1,4 +1,3 @@
-// An entity is a circle on Type that is an endpoint of a relation.
 // A span have one grid and a grid can have multi types and a type can have multi entities.
 // A grid is only shown when at least one entity is owned by a correspond span.
 export default function (entity, namespace) {
