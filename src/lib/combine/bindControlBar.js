3c8b5915e7bd69f350f173d6ebd0d88489877e5a
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-05 10:26:33 +0900

    refactor: The stickInParent is not necessary

--- src/lib/combine/bindControlBar.js
+++ src/lib/combine/bindControlBar.js
@@ -1,11 +1,6 @@
-import stickInParent from './stickInParent'
-
 export default function (editor, controlBar) {
   // add control bar
   editor[0].insertBefore(controlBar.el, editor[0].childNodes[0])
-
-  // Stick the control bar in the editor.
-  stickInParent(controlBar.el)
 
   editor.eventEmitter
     .on('textae-event.control.button.push', (data) =>
