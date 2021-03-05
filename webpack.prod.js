af4aba05ec4540d6fe49a7f681bc0e925cb8b7ef
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-05 11:23:09 +0900

    misc: Give the package name and version number of the distribution file with Webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,15 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 const path = require('path')
+const PACKAGE = require('./package.json')
+const { name, version } = PACKAGE
 const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
   output: {
     path: path.resolve(__dirname, 'src/lib'),
-    filename: 'bundle.js'
+    filename: `${name}-${version}.min.js`
   },
   optimization: {
     minimize: true,
