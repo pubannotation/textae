35185204f343fe4f5e4f5db2cc5071ce834aa632
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-05 17:01:19 +0900

    refactor: Inline the create

--- src/lib/editor/start/View/Renderer/EntityRenderer/index.js
+++ src/lib/editor/start/View/Renderer/EntityRenderer/index.js
@@ -1,4 +1,3 @@
-import create from './create'
 import destroy from './destroy'
 
 export default class EntityRenderer {
@@ -8,7 +7,19 @@ export default class EntityRenderer {
   }
 
   render(entity) {
-    create(entity, this._annotationData.namespace)
+    // Don't delete child Span on span moves.
+    // Check if a child span is already present so that it is not drawn twice.
+    if (entity.element) {
+      return
+    }
+
+    // A span have one grid and a grid can have multi types and a type can have multi entities.
+    // A grid is only shown when at least one entity is owned by a correspond span.
+    const grid = entity.span.gridElement || entity.span.renderGridElement()
+
+    // Append a new entity to the type
+    const element = entity.renderElement(this._annotationData.namespace)
+    grid.insertAdjacentElement('beforeend', element)
 
     entity.reflectEntityGapInTheHeight()
   }
