c85b7fff5ff40c6ee23a13585cb98fd3868e9506
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-08 12:11:43 +0900

    refactor: Rename local variables

--- src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/createPath.js
+++ src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/createPath.js
@@ -9,36 +9,39 @@ export default function (
   isBold
 ) {
   const path = document.createElementNS(NS.SVG, 'path')
-  let x1 = sourceEndpoint.left + sourceEndpoint.width / 2 - annotationBox.left
-  const y1 = sourceEndpoint.top - annotationBox.top
-  let x2 = targetEndpoint.left + targetEndpoint.width / 2 - annotationBox.left
-  const y2 = targetEndpoint.top - annotationBox.top
+  let sourceX =
+    sourceEndpoint.left + sourceEndpoint.width / 2 - annotationBox.left
+  const sourceY = sourceEndpoint.top - annotationBox.top
+  let targetX =
+    targetEndpoint.left + targetEndpoint.width / 2 - annotationBox.left
+  const targetY = targetEndpoint.top - annotationBox.top
 
   // Source is left, target is right.
-  if (20 < x2 - x1) {
+  if (sourceX < targetX - 20) {
     // Shift only when the entity has enough width to shift the endpoint.
     if (10 < sourceEndpoint.width / 2) {
-      x1 += 10
+      sourceX += 10
     }
     if (10 < targetEndpoint.width / 2) {
-      x2 -= 10
+      targetX -= 10
     }
   }
 
   // Target is left, source is right.
-  if (20 < x1 - x2) {
+  if (targetX < sourceX - 20) {
     if (10 < sourceEndpoint.width / 2) {
-      x1 -= 10
+      sourceX -= 10
     }
     if (10 < targetEndpoint.width / 2) {
-      x2 += 10
+      targetX += 10
     }
   }
 
-  const controleY = Math.min(y1, y2) - Math.abs(x2 - x1) / 6 - 10
+  const controleY =
+    Math.min(sourceY, targetY) - Math.abs(targetX - sourceX) / 6 - 10
   path.setAttribute(
     'd',
-    `M ${x1}, ${y1} C ${x1} ${controleY}, ${x2} ${controleY}, ${x2} ${y2}`
+    `M ${sourceX}, ${sourceY} C ${sourceX} ${controleY}, ${targetX} ${controleY}, ${targetX} ${targetY}`
   )
 
   path.setAttribute('style', `fill:none; stroke: ${color};`)
