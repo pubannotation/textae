2308aa732914d133ca7c0db73d052d5542ae3d3d
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-08 10:03:26 +0900

    refactor: Delete old comment

--- src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection.js
+++ src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection.js
@@ -131,19 +131,6 @@ export default class SVGConnection {
     const labelX = pathBBox.x + pathBBox.width / 2
     const labelY = pathBBox.y - 2
 
-    // // https://ja.javascript.info/bezier-curve
-    // // (1−t)3P1 + 3(1−t)2tP2 +3(1−t)t2P3 + t3P4
-    // const t = 0.5
-    // const labelX =
-    //   Math.pow(1 - t, 3) * x1 +
-    //   3 * Math.pow(1 - t, 2) * t * x1 +
-    //   3 * (1 - t) * Math.pow(t, 2) * x2 +
-    //   Math.pow(t, 3) * x2
-    // const labelY =
-    //   Math.pow(1 - t, 3) * y1 +
-    //   3 * Math.pow(1 - t, 2) * t * controleY +
-    //   3 * (1 - t) * Math.pow(t, 2) * controleY +
-    //   Math.pow(t, 3) * y2
     const label = document.createElementNS(NS.SVG, 'text')
     label.textContent = `[${this._relation.id}] ${getDisplayName(
       this._namespace,
