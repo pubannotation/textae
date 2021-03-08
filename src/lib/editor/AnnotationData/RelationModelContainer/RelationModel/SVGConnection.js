038c4220071bfcb3d82a27e63225e69402692760
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-08 10:40:43 +0900

    refactor: Extract a createPath function

--- src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection.js
+++ src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection.js
@@ -72,29 +72,16 @@ export default class SVGConnection {
       .getBoundingClientRect()
     const sourceEndpoint = this._relation.sourceEndpoint.getBoundingClientRect()
     const targetEndpoint = this._relation.targetEndpoint.getBoundingClientRect()
+    const color = this._color
 
-    const path = document.createElementNS(NS.SVG, 'path')
-    const x1 =
-      sourceEndpoint.left + sourceEndpoint.width / 2 - annotationBox.left
-    const y1 = sourceEndpoint.top - annotationBox.top
-    const x2 =
-      targetEndpoint.left + targetEndpoint.width / 2 - annotationBox.left
-    const y2 = targetEndpoint.top - annotationBox.top
-    const controleY = Math.min(y1, y2) - Math.abs(x2 - x1) / 6 - 10
-    path.setAttribute(
-      'd',
-      `M ${x1}, ${y1} C ${x1} ${controleY}, ${x2} ${controleY}, ${x2} ${y2}`
+    const path = createPath(
+      sourceEndpoint,
+      annotationBox,
+      targetEndpoint,
+      color,
+      arrow,
+      isBold
     )
-
-    path.setAttribute('style', `fill:none; stroke: ${this._color};`)
-
-    // The ID of the SVG element is global scope in the Window.
-    // If you don't make it unique, it will use another editor's arrow.
-    path.setAttribute('marker-end', `url(#${arrow.id})`)
-
-    if (isBold) {
-      path.classList.add('textae-editor__relation--isBold')
-    }
 
     this._relationBox.appendChild(path)
 
@@ -205,4 +192,35 @@ export default class SVGConnection {
     this._relationBox.removeChild(this._label)
     this._relationBox.removeChild(this._labelBackground)
   }
+}
+
+function createPath(
+  sourceEndpoint,
+  annotationBox,
+  targetEndpoint,
+  color,
+  arrow,
+  isBold
+) {
+  const path = document.createElementNS(NS.SVG, 'path')
+  const x1 = sourceEndpoint.left + sourceEndpoint.width / 2 - annotationBox.left
+  const y1 = sourceEndpoint.top - annotationBox.top
+  const x2 = targetEndpoint.left + targetEndpoint.width / 2 - annotationBox.left
+  const y2 = targetEndpoint.top - annotationBox.top
+  const controleY = Math.min(y1, y2) - Math.abs(x2 - x1) / 6 - 10
+  path.setAttribute(
+    'd',
+    `M ${x1}, ${y1} C ${x1} ${controleY}, ${x2} ${controleY}, ${x2} ${y2}`
+  )
+
+  path.setAttribute('style', `fill:none; stroke: ${color};`)
+
+  // The ID of the SVG element is global scope in the Window.
+  // If you don't make it unique, it will use another editor's arrow.
+  path.setAttribute('marker-end', `url(#${arrow.id})`)
+
+  if (isBold) {
+    path.classList.add('textae-editor__relation--isBold')
+  }
+  return path
 }
