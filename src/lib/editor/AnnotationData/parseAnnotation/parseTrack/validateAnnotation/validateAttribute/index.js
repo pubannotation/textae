39e82d31a99286023ae654ae60ed783a22e4575a
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-09 10:13:58 +0900

    refactor: Rename an argument

--- src/lib/editor/AnnotationData/parseAnnotation/parseTrack/validateAnnotation/validateAttribute/index.js
+++ src/lib/editor/AnnotationData/parseAnnotation/parseTrack/validateAnnotation/validateAttribute/index.js
@@ -2,9 +2,9 @@ import isContains from '../isContains'
 import ChainValidation from '../ChainValidation'
 import isUniqueIn from './isUniqueIn'
 
-export default function (denotations, attributes) {
+export default function (subjects, attributes) {
   return new ChainValidation(attributes)
-    .and('subject', (a) => isContains(denotations, a.subj))
+    .and('subject', (a) => isContains(subjects, a.subj))
     .and('unique', (node) => isUniqueIn(attributes, node))
     .validateAll()
 }
