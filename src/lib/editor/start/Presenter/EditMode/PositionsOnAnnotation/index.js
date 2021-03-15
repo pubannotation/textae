2d91f999e908780d88b2c04dc2ed5bc51ef3da8e
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-15 10:51:58 +0900

    refactor: Rename the _spanContainer property

--- src/lib/editor/start/Presenter/EditMode/PositionsOnAnnotation/index.js
+++ src/lib/editor/start/Presenter/EditMode/PositionsOnAnnotation/index.js
@@ -2,20 +2,23 @@ import getPosition from './getPosition'
 
 export default class PositionsOnAnnotation {
   constructor(spanModelContainer, selectionWrapper) {
-    this._spanContainer = spanModelContainer
+    this._spanModelContainer = spanModelContainer
     this._selection = selectionWrapper.selection
   }
 
   get anchor() {
     const position = getPosition(
-      this._spanContainer,
+      this._spanModelContainer,
       this._selection.anchorNode
     )
     return position + this._selection.anchorOffset
   }
 
   get focus() {
-    const position = getPosition(this._spanContainer, this._selection.focusNode)
+    const position = getPosition(
+      this._spanModelContainer,
+      this._selection.focusNode
+    )
     return position + this._selection.focusOffset
   }
 }
