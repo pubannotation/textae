5f6d973875173b35f0b91c35554f31995848a913
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-17 14:01:10 +0900

    feature: Set the height of the marker to 8 pixels.

--- src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/setMarkerStyle.js
+++ src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/setMarkerStyle.js
@@ -1,13 +1,13 @@
 export default function (marker, weight, color, isTail) {
   marker.setAttribute('markerWidth', 12 * weight)
   marker.setAttribute('markerHeight', 12 * weight)
-  marker.setAttribute('refX', isTail ? 12 * weight : 0)
+  marker.setAttribute('refX', isTail ? 8 * weight : 0)
   marker.setAttribute('refY', 6 * weight)
 
   const polygon = marker.children[0]
   polygon.setAttribute(
     'points',
-    `0 0, ${12 * weight} ${6 * weight}, 0 ${12 * weight}`
+    `0 0, ${8 * weight} ${6 * weight}, 0 ${12 * weight}`
   )
   polygon.setAttribute('stroke', `${color}`)
   polygon.setAttribute('fill', `${color}`)
