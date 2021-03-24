610798e40d8ddddf92a520de7a1244245af6b77f
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-24 15:10:58 +0900

    feature: Make the border color of the endpoints of the relation the same as the border color of the entity

--- src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/setMarkerStyle.js
+++ src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/setMarkerStyle.js
@@ -11,6 +11,6 @@ export default function (marker, weight, color, isTail) {
     'points',
     `0 0, ${MarkerHeight * weight} ${6 * weight}, 0 ${12 * weight}`
   )
-  polygon.setAttribute('stroke', `${color}`)
+  polygon.setAttribute('stroke', 'rgba(100, 100, 215, 0.8)')
   polygon.setAttribute('fill', `${color}`)
 }
