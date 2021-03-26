c8f1d7249b7b411894ba487c149e32f1984b5c64
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-26 11:17:25 +0900

    refactor: Delete a handler of the textae-event.entity-and-attribute-pallet.attribute.remove-attribute-instance-button.click event

--- src/lib/editor/start/Presenter/EditMode/bindAttributeTabEvents.js
+++ src/lib/editor/start/Presenter/EditMode/bindAttributeTabEvents.js
@@ -1,22 +1,11 @@
 export default function (eventEmitter, commander, selectionModelEntity) {
   // Bind events about attributes.
-  eventEmitter
-    .on(
-      'textae-event.entity-and-attribute-pallet.attribute.remove-attribute-instance-button.click',
-      (attrDef) =>
-        commander.invoke(
-          commander.factory.removeAttributesFromItemsByPredCommand(
-            selectionModelEntity.all,
-            attrDef
-          )
-        )
-    )
-    .on(
-      'textae-event.entity-and-attribute-pallet.attribute.tab.drop',
-      (oldIndex, newIndex) => {
-        commander.invoke(
-          commander.factory.moveAttributeDefintionComannd(oldIndex, newIndex)
-        )
-      }
-    )
+  eventEmitter.on(
+    'textae-event.entity-and-attribute-pallet.attribute.tab.drop',
+    (oldIndex, newIndex) => {
+      commander.invoke(
+        commander.factory.moveAttributeDefintionComannd(oldIndex, newIndex)
+      )
+    }
+  )
 }
