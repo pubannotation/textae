adc436e8c2e5a5041c7bbb5f117277b952948488
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-26 13:50:22 +0900

    refactor: Fix unit label in the pallet

--- src/lib/component/EntityAndAttributePallet/createContentHtml/headerTemplate/getSelectedEntityLabel.js
+++ src/lib/component/EntityAndAttributePallet/createContentHtml/headerTemplate/getSelectedEntityLabel.js
@@ -1,7 +1,7 @@
 export default function (numberOfSelectedEntities) {
   return numberOfSelectedEntities === 1
-    ? '1 entity selected'
+    ? '1 item selected'
     : numberOfSelectedEntities > 1
-    ? `${numberOfSelectedEntities} entities selected`
+    ? `${numberOfSelectedEntities} items selected`
     : ''
 }
