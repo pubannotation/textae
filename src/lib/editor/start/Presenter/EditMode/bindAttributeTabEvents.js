fad22dd30353693ac6d6ef5398c193007a43310c
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-26 11:05:31 +0900

    refactor: Delete a handler of the textae-event.entity-and-attribute-pallet.attribute.edit-object-of-attribute-instance-button.click event

--- src/lib/editor/start/Presenter/EditMode/bindAttributeTabEvents.js
+++ src/lib/editor/start/Presenter/EditMode/bindAttributeTabEvents.js
@@ -1,37 +1,6 @@
-import openEditNumericAttributeDialog from '../openEditNumericAttributeDialog'
-import openEditStringAttributeDialog from '../openEditStringAttributeDialog'
-
 export default function (eventEmitter, commander, selectionModelEntity) {
   // Bind events about attributes.
   eventEmitter
-    .on(
-      'textae-event.entity-and-attribute-pallet.attribute.edit-object-of-attribute-instance-button.click',
-      (attrDef) => {
-        const attribute = selectionModelEntity.findSelectedAttributeWithSamePredicate(
-          attrDef.pred
-        )
-        switch (attrDef.valueType) {
-          case 'numeric':
-            openEditNumericAttributeDialog(
-              selectionModelEntity,
-              attrDef,
-              attribute,
-              commander
-            )
-            break
-          case 'string':
-            openEditStringAttributeDialog(
-              selectionModelEntity,
-              attribute,
-              commander,
-              attrDef
-            )
-            break
-          default:
-            throw new Error(`Invalid attribute valueType: ${attrDef.valueType}`)
-        }
-      }
-    )
     .on(
       'textae-event.entity-and-attribute-pallet.attribute.remove-attribute-instance-button.click',
       (attrDef) =>
