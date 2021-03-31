4fd3b0f08984697272b7bb5202e5282450f06db8
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-03-31 18:16:28 +0900

    refactor: Extract a getXPositions file

--- src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/createPath/index.js
+++ src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/createPath/index.js
@@ -1,8 +1,7 @@
 import { NS } from '../../NS'
 import { MarkerHeight } from '../MarkerHeight'
+import getXPositions from './getXPositions'
 
-const DistanceToShift = 8
-const MinimumDistance = DistanceToShift * 3
 export default function (
   sourceEndpoint,
   annotationBox,
@@ -39,45 +38,4 @@ export default function (
   }
 
   return [path, { sourceY, targetY, controleY, sourceX, targetX }]
-}
-
-function getXPositions(isBold, sourceEndpoint, targetEndpoint, annotationBox) {
-  const hasSourceEnoughWidth =
-    isBold || MinimumDistance <= sourceEndpoint.width / 2
-  const hasTaregtEntityWidth =
-    isBold || MinimumDistance <= targetEndpoint.width / 2
-
-  const centerOfSource =
-    sourceEndpoint.left + sourceEndpoint.width / 2 - annotationBox.left
-  const centerOfTarget =
-    targetEndpoint.left + targetEndpoint.width / 2 - annotationBox.left
-
-  // Shift only when the entity has enough width to shift the endpoint.
-  const leftTarget = hasTaregtEntityWidth
-    ? centerOfTarget - DistanceToShift * 3
-    : centerOfTarget
-  const leftSource = hasSourceEnoughWidth
-    ? centerOfSource - DistanceToShift
-    : centerOfSource
-  const rightTarget = hasTaregtEntityWidth
-    ? centerOfTarget + DistanceToShift
-    : centerOfTarget
-  const rightSource = hasSourceEnoughWidth
-    ? centerOfSource + DistanceToShift * 3
-    : centerOfSource
-
-  // Shift if the source and target positions are not swapped.
-  if (rightSource < leftTarget) {
-    return {
-      source: rightSource,
-      target: leftTarget
-    }
-  } else if (rightTarget < leftSource) {
-    return {
-      source: leftSource,
-      target: rightTarget
-    }
-  } else {
-    return { source: centerOfSource, target: centerOfTarget }
-  }
 }
