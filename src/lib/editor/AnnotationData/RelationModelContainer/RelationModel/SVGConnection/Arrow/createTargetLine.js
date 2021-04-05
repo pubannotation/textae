01ba846cec33a40507063d143a80d7a1ea132391
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-04-05 17:44:53 +0900

    refactor: Extract createTargetLine as a file

---
+++ src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/createTargetLine.js
@@ -0,0 +1,18 @@
+import { NS } from '../NS'
+import { MarkerHeight } from './MarkerHeight'
+import { BaseColorStroke } from './BaseColorStroke'
+
+export default function (targetX, targetY, targetEndpoint, annotationBox) {
+  const targetLine = document.createElementNS(NS.SVG, 'polyline')
+  const centerOfTarget =
+    targetEndpoint.left + targetEndpoint.width / 2 - annotationBox.left
+  targetLine.setAttribute(
+    'points',
+    `${targetX} ${targetY + MarkerHeight}, ${centerOfTarget} ${
+      targetY + MarkerHeight
+    }, ${centerOfTarget} ${targetEndpoint.top - annotationBox.top}`
+  )
+  targetLine.setAttribute('style', `${BaseColorStroke}; fill: none;`)
+
+  return targetLine
+}
--- src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/inedx.js
+++ src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/inedx.js
@@ -1,10 +1,8 @@
 import createPath from './createPath'
-import { NS } from '../NS'
-import { MarkerHeight } from './MarkerHeight'
-import { BaseColorStroke } from './BaseColorStroke'
 import createSourceTriangle from './createSourceTriangle'
 import createTargetTriangle from './createTargetTriangle'
 import createSourceLine from './createSourceLine'
+import createTargetLine from './createTargetLine'
 
 export default class Arrow {
   constructor(
@@ -147,17 +145,6 @@ export default class Arrow {
   }
 
   _createTargetLine(targetX, targetY, targetEndpoint, annotationBox) {
-    const targetLine = document.createElementNS(NS.SVG, 'polyline')
-    const centerOfTarget =
-      targetEndpoint.left + targetEndpoint.width / 2 - annotationBox.left
-    targetLine.setAttribute(
-      'points',
-      `${targetX} ${targetY + MarkerHeight}, ${centerOfTarget} ${
-        targetY + MarkerHeight
-      }, ${centerOfTarget} ${targetEndpoint.top - annotationBox.top}`
-    )
-    targetLine.setAttribute('style', `${BaseColorStroke}; fill: none;`)
-
-    return targetLine
+    return createTargetLine(targetX, targetY, targetEndpoint, annotationBox)
   }
 }
