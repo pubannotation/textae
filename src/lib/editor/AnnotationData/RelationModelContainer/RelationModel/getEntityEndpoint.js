6e11d1429cd09dc3d0730862e58e227a97a3c78f
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-04-07 14:22:07 +0900

    refactor: Delete the getEntityEndpoint

--- src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/getEntityEndpoint.js
+++
@@ -1,8 +0,0 @@
-import { makeEntityHTMLElementId } from '../../../idFactory'
-
-export default function (editor, entityId) {
-  const id = makeEntityHTMLElementId(editor, entityId)
-  return editor[0].querySelector(
-    `#${id} .textae-editor__signboard__type-values`
-  )
-}
