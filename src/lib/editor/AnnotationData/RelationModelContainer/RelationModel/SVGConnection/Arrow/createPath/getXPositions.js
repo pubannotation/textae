1af2c1eaad2b0b11e0b3b99af10ba5e3a423bb75
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-04-07 15:11:51 +0900

    refactor: Applying de Morgan's law

--- src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/createPath/getXPositions.js
+++ src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/createPath/getXPositions.js
@@ -8,10 +8,10 @@ export default function (isBold, sourceEntity, targetEntity, annotationBox) {
   // When the entity width is small and the endpoint is displayed in the center of the entity and the entity has only one endpoint,
   // hovering will not move the entity left or right.
   const combineSourceEndpoints =
-    !(isBold && sourceEntity.relations.length > 1) &&
+    (!isBold || sourceEntity.relations.length === 1) &&
     !(MinimumDistance <= sourceEndpoint.width / 2)
   const combineTargetEndpoints =
-    !(isBold && targetEntity.relations.length > 1) &&
+    (!isBold || targetEntity.relations.length === 1) &&
     !(MinimumDistance <= targetEndpoint.width / 2)
 
   const centerOfSource =
