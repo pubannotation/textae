c65daa1e72e424382b9544e9fdd9b135548a618a
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-04-12 14:55:05 +0900

    refactor: Extract guard condition

--- src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/createPath/getControlXs.js
+++ src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/createPath/getControlXs.js
@@ -1,26 +1,18 @@
 export default function (sourceX, sourceY, targetX, targetY) {
+  if (targetY === sourceY || Math.abs(targetX - sourceX) > 42) {
+    return { sourceControlX: sourceX, targetControlX: targetX }
+  }
+
   if (sourceY < targetY) {
     const sourceControlX = sourceX
 
     // When the source endpoint and target endpoint are close,
     // bend the target endpoint side of the relationship significantly.
-    const targetControlX =
-      targetX +
-      (targetY === sourceY || Math.abs(targetX - sourceX) > 42
-        ? 0
-        : sourceX <= targetX + 16
-        ? 150
-        : -150)
+    const targetControlX = targetX + (sourceX <= targetX + 16 ? 150 : -150)
 
     return { sourceControlX, targetControlX }
   } else {
-    const sourceControlX =
-      sourceX +
-      (targetY === sourceY || Math.abs(targetX - sourceX) > 42
-        ? 0
-        : sourceX <= targetX + 16
-        ? 150
-        : -150)
+    const sourceControlX = sourceX + (sourceX <= targetX + 16 ? 150 : -150)
 
     const targetControlX = targetX
 
