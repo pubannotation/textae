3a67214ac9c01814a151554fac502d9deda5b06b
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-04-12 14:59:19 +0900

    refactor: Fix comments

--- src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/createPath/getControlXs.js
+++ src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/createPath/getControlXs.js
@@ -1,11 +1,10 @@
+// When the source endpoint and target endpoint are close, bent the relationship significantly.
 export default function (sourceX, sourceY, targetX, targetY) {
   if (targetY === sourceY || Math.abs(targetX - sourceX) > 42) {
     return { sourceControlX: sourceX, targetControlX: targetX }
   }
 
-  // When the source endpoint and target endpoint are close
   if (sourceY < targetY) {
-    // bend the target endpoint side of the relationship significantly.
     return {
       sourceControlX: sourceX,
       targetControlX: targetX + (sourceX <= targetX + 16 ? 150 : -150)
