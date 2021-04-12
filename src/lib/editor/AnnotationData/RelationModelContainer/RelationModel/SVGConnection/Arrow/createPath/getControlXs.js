da50dd2ad1385897634fe1e41a09b70a1270742f
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-04-12 14:29:20 +0900

    feature: When the source entity is below the target entity, the control point on the source endpoint side is moved outward

--- src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/createPath/getControlXs.js
+++ src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/createPath/getControlXs.js
@@ -1,15 +1,29 @@
 export default function (sourceX, sourceY, targetX, targetY) {
-  const sourceControlX = sourceX
+  if (sourceY < targetY) {
+    const sourceControlX = sourceX
 
-  // When the source endpoint and target endpoint are close,
-  // bend the target endpoint side of the relationship significantly.
-  const targetControlX =
-    targetX +
-    (targetY === sourceY || Math.abs(targetX - sourceX) > 42
-      ? 0
-      : sourceX <= targetX + 16
-      ? 150
-      : -150)
+    // When the source endpoint and target endpoint are close,
+    // bend the target endpoint side of the relationship significantly.
+    const targetControlX =
+      targetX +
+      (targetY === sourceY || Math.abs(targetX - sourceX) > 42
+        ? 0
+        : sourceX <= targetX + 16
+        ? 150
+        : -150)
 
-  return { sourceControlX, targetControlX }
+    return { sourceControlX, targetControlX }
+  } else {
+    const sourceControlX =
+      sourceX +
+      (targetY === sourceY || Math.abs(targetX - sourceX) > 42
+        ? 0
+        : sourceX <= targetX + 16
+        ? 150
+        : -150)
+
+    const targetControlX = targetX
+
+    return { sourceControlX, targetControlX }
+  }
 }
