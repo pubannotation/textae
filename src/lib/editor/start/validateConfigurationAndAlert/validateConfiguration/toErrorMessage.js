c69a3ef4afc7e80cd8120dc85ce86fc43f74ed7c
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-05-07 17:44:37 +0900

    fix: An error occurs when an invalid configuration is loaded
    The Ajv has changed the property name of the error object in version 8.0.0.
    see: https://github.com/ajv-validator/ajv/releases/tag/v8.0.0

--- src/lib/editor/start/validateConfigurationAndAlert/validateConfiguration/toErrorMessage.js
+++ src/lib/editor/start/validateConfigurationAndAlert/validateConfiguration/toErrorMessage.js
@@ -4,7 +4,7 @@ export default function (errors) {
   for (const e of errors) {
     if (e.keyword === 'required') {
       return `Invalid configuration: The attribute type whose predicate is '${e.data.pred}' misses a mandatory property, '${e.params.missingProperty}'.`
-    } else if (e.dataPath.includes('color')) {
+    } else if (e.instancePath.includes('color')) {
       return eskape`Invalid configuration: '${e.data}' is invalid color format.`
     }
   }
