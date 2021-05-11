c7e93de448c4f65c983d40ffb2d5d911771cce14
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-05-11 17:29:46 +0900

    refactor: Format a HTML template

--- src/lib/component/CreateAttributeDefinitionDialog/template.js
+++ src/lib/component/CreateAttributeDefinitionDialog/template.js
@@ -7,18 +7,42 @@ export default function (componentClassName, context) {
 <div class="${componentClassName}__container">
   <div class="${componentClassName}__row">
     <label>Attribute type:</label>
-    <label><input type="radio" name="${componentClassName}__value-type" value="flag"${
-    valueType === 'flag' ? ` checked` : ``
-  }>flag</label>
-    <label><input type="radio" name="${componentClassName}__value-type" value="selection"${
-    valueType === 'selection' ? ` checked` : ``
-  }>selection</label>
-    <label><input type="radio" name="${componentClassName}__value-type" value="string"${
-    valueType === 'string' ? ` checked` : ``
-  }>string</label>
-    <label><input type="radio" name="${componentClassName}__value-type" value="numeric"${
-    valueType === 'numeric' ? ` checked` : ``
-  }>numeric</label>
+    <label>
+      <input
+        type="radio" 
+        name="${componentClassName}__value-type"
+        value="flag"
+        ${valueType === 'flag' ? `checked` : ``}
+        >
+      flag
+    </label>
+    <label>
+      <input
+        type="radio"
+        name="${componentClassName}__value-type"
+        value="selection"
+        ${valueType === 'selection' ? `checked` : ``}
+        >
+      selection
+    </label>
+    <label>
+      <input
+        type="radio"
+        name="${componentClassName}__value-type"
+        value="string"
+        ${valueType === 'string' ? `checked` : ``}
+        >
+      string
+    </label>
+    <label>
+      <input
+        type="radio"
+        name="${componentClassName}__value-type"
+        value="numeric"
+        ${valueType === 'numeric' ? `checked` : ``}
+        >
+      numeric
+    </label>
   </div>
   ${inputAttributeDefinition(componentClassName, context)}
 </div>`
