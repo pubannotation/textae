eb7af8d8f7ad22efb2e6bda9914dc84f0459b35f
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-05-11 17:50:16 +0900

    feature: Display an icon in the Attribute Type column of the Add Attribute Definition dialog

--- src/lib/component/CreateAttributeDefinitionDialog/template.js
+++ src/lib/component/CreateAttributeDefinitionDialog/template.js
@@ -7,42 +7,48 @@ export default function (componentClassName, context) {
 <div class="${componentClassName}__container">
   <div class="${componentClassName}__row">
     <label>Attribute type:</label>
-    <label>
-      <input
-        type="radio" 
-        name="${componentClassName}__value-type"
-        value="flag"
-        ${valueType === 'flag' ? `checked` : ``}
-        >
-      flag
-    </label>
-    <label>
-      <input
-        type="radio"
-        name="${componentClassName}__value-type"
-        value="selection"
-        ${valueType === 'selection' ? `checked` : ``}
-        >
-      selection
-    </label>
-    <label>
-      <input
-        type="radio"
-        name="${componentClassName}__value-type"
-        value="string"
-        ${valueType === 'string' ? `checked` : ``}
-        >
-      string
-    </label>
-    <label>
-      <input
-        type="radio"
-        name="${componentClassName}__value-type"
-        value="numeric"
-        ${valueType === 'numeric' ? `checked` : ``}
-        >
-      numeric
-    </label>
+    <div class="${componentClassName}__value-type-row">
+      <label>
+        <input
+          type="radio" 
+          name="${componentClassName}__value-type"
+          value="flag"
+          ${valueType === 'flag' ? `checked` : ``}
+          >
+        <span class="${componentClassName}__value-type--flag">
+        flag
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="${componentClassName}__value-type"
+          value="selection"
+          ${valueType === 'selection' ? `checked` : ``}
+          >
+        <span class="${componentClassName}__value-type--selection">
+        selection
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="${componentClassName}__value-type"
+          value="string"
+          ${valueType === 'string' ? `checked` : ``}
+          >
+        <span class="${componentClassName}__value-type--string">
+        string
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="${componentClassName}__value-type"
+          value="numeric"
+          ${valueType === 'numeric' ? `checked` : ``}
+          >
+        <span class="${componentClassName}__value-type--numeric">
+        numeric
+      </label>
+    </div>
   </div>
   ${inputAttributeDefinition(componentClassName, context)}
 </div>`
