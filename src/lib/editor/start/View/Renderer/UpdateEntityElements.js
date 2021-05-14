38b4162c44e122d3673466ec8dbcead09298e70c
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-05-14 17:59:32 +0900

    refactor: Delete the updateEntityElements.updateEntityHtmlelement method

--- src/lib/editor/start/View/Renderer/UpdateEntityElements.js
+++ src/lib/editor/start/View/Renderer/UpdateEntityElements.js
@@ -3,19 +3,6 @@ export default class UpdateEntityElements {
     this._annotationData = annotationData
   }
 
-  updateEntityHtmlelement(typeName) {
-    for (const entity of this._annotationData.entity.all) {
-      // If the type name ends in a wildcard, look for the DOMs to update with a forward match.
-      if (
-        entity.typeName === typeName ||
-        (typeName.lastIndexOf('*') === typeName.length - 1 &&
-          entity.typeName.indexOf(typeName.slice(0, -1) === 0))
-      ) {
-        entity.updateElement()
-      }
-    }
-  }
-
   updateAttribute(pred) {
     for (const entity of this._annotationData.entity.all.filter((e) =>
       e.typeValues.hasSpecificPredicateAttribute(pred)
