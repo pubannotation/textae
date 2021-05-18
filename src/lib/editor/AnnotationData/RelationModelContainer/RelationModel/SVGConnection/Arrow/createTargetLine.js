58679ae9066c65d3030ccb16b3c523b84ab078e9
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-05-18 17:02:27 +0900

    refactor: Rename an argument

--- src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/createTargetLine.js
+++ src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/createTargetLine.js
@@ -1,15 +1,14 @@
 import { NS } from '../NS'
 import { MarkerHeight } from './MarkerHeight'
 
-export default function ({ targetX, targetY }, targetEndpoint, annotationBox) {
+export default function ({ targetX, targetY }, entity, annotationBox) {
   const targetLine = document.createElementNS(NS.SVG, 'polyline')
-  const centerOfTarget =
-    targetEndpoint.left + targetEndpoint.width / 2 - annotationBox.left
+  const centerOfTarget = entity.left + entity.width / 2 - annotationBox.left
   targetLine.setAttribute(
     'points',
     `${targetX} ${targetY + MarkerHeight}, ${centerOfTarget} ${
       targetY + MarkerHeight
-    }, ${centerOfTarget} ${targetEndpoint.top - annotationBox.top}`
+    }, ${centerOfTarget} ${entity.top - annotationBox.top}`
   )
 
   return targetLine
