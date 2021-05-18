3e6c891664fe549b33bf187a8cd35eb91c7343d5
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-05-19 03:01:31 +0900

    refactor: Use the EntityModel.center

--- src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/createJetty.js
+++ src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/createJetty.js
@@ -4,8 +4,7 @@ import { MarkerHeight } from './MarkerHeight'
 export default function (x, y, entity, annotationBox) {
   const jetty = document.createElementNS(NS.SVG, 'polyline')
   const entityRect = entity.clientRect
-  const centerOfEntity =
-    entityRect.left + entityRect.width / 2 - annotationBox.left
+  const centerOfEntity = entity.center
   jetty.setAttribute(
     'points',
     `${x} ${y + MarkerHeight}, ${centerOfEntity} ${
