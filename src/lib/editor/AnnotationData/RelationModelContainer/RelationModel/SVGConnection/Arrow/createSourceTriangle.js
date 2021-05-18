ff081936bb0d9cdc2496cd9e35814a37d547ba00
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-05-18 17:56:00 +0900

    refactor: Rename a local variable

--- src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/createSourceTriangle.js
+++ src/lib/editor/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/createSourceTriangle.js
@@ -2,11 +2,8 @@ import { NS } from '../NS'
 import { MarkerHeight } from './MarkerHeight'
 
 export default function () {
-  const sourceTriangle = document.createElementNS(NS.SVG, 'polygon')
-  sourceTriangle.setAttribute(
-    'points',
-    `-4 ${MarkerHeight}, 4 ${MarkerHeight}, 0 0`
-  )
+  const bollard = document.createElementNS(NS.SVG, 'polygon')
+  bollard.setAttribute('points', `-4 ${MarkerHeight}, 4 ${MarkerHeight}, 0 0`)
 
-  return sourceTriangle
+  return bollard
 }
