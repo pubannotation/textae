3d38c4f6f748f26c22cc9f9127a144e3b5b5610d
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-06-02 15:51:34 +0900

    refactor: Rename variables

--- src/lib/editor/SelectionModel/SelectedItemsWithAttributes.js
+++ src/lib/editor/SelectionModel/SelectedItemsWithAttributes.js
@@ -2,46 +2,44 @@ import SelectedItems from './SelectedItems'
 
 export default class SelectedItemsWithAttributes extends SelectedItems {
   selectedWithAttributeOf(pred) {
-    return this.all.some((element) =>
-      element.attributes.some((attribute) => attribute.pred === pred)
+    return this.all.some((item) =>
+      item.attributes.some((attribute) => attribute.pred === pred)
     )
   }
 
   onlySelectedWithJustOneAttributeOf(pred) {
     return this.all.every(
-      (element) =>
-        element.attributes.filter((attribute) => attribute.pred === pred)
+      (item) =>
+        item.attributes.filter((attribute) => attribute.pred === pred)
           .length === 1
     )
   }
 
   selectedWithoutAttributeOf(pred) {
     return this.all.some(
-      (element) =>
-        !element.attributes.some((attribute) => attribute.pred === pred)
+      (item) => !item.attributes.some((attribute) => attribute.pred === pred)
     )
   }
 
   isDupulicatedPredAttrributeSelected(pred) {
     return this.all.some(
-      (element) =>
-        element.attributes.filter((attribute) => attribute.pred === pred)
-          .length > 1
+      (item) =>
+        item.attributes.filter((attribute) => attribute.pred === pred).length >
+        1
     )
   }
 
   findSelectedWithSamePredicateAttribute(pred) {
-    return this.all.find((element) =>
-      element.attributes.find((attribute) => attribute.pred === pred)
+    return this.all.find((item) =>
+      item.attributes.find((attribute) => attribute.pred === pred)
     )
   }
 
   findSelectedAttributeWithSamePredicate(pred) {
-    const selectedElementWithSamePred =
-      this.findSelectedWithSamePredicateAttribute(pred)
+    const itemWithSamePred = this.findSelectedWithSamePredicateAttribute(pred)
 
-    if (selectedElementWithSamePred) {
-      return selectedElementWithSamePred.attributes.find((a) => a.pred === pred)
+    if (itemWithSamePred) {
+      return itemWithSamePred.attributes.find((a) => a.pred === pred)
     }
   }
 }
