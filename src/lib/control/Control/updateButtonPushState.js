1498f32bfbc8d1c0f0c32d87a2870e257dd8ce50
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-06-22 17:40:52 +0900

    refactor: Add a guard condition

--- src/lib/control/Control/updateButtonPushState.js
+++ src/lib/control/Control/updateButtonPushState.js
@@ -1,6 +1,12 @@
 export default function (el, buttonType, isPushed) {
   const button = el.querySelector(`.textae-control__${buttonType}-button`)
 
+  // Touch devices limit the number of buttons displayed in the context menu
+  // due to the limited screen area.
+  if (!button) {
+    return
+  }
+
   if (isPushed) {
     button.classList.add('textae-control__icon--pushed')
   } else {
