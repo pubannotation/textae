9a89c5492b2a8d9a811711254671b313ae55ba8a
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-06-22 11:23:41 +0900

    refactor: Use the SelectionWrapper.getPositionsOnAnnotation method

--- src/lib/editor/start/Presenter/EditMode/expandSpan/getNewSpan/index.js
+++ src/lib/editor/start/Presenter/EditMode/expandSpan/getNewSpan/index.js
@@ -1,5 +1,4 @@
 import getNewExpandSpan from './getNewExpandSpan'
-import PositionsOnAnnotation from '../../PositionsOnAnnotation'
 
 export default function getNewSpan(
   annotationData,
@@ -8,9 +7,8 @@ export default function getNewSpan(
   selectionWrapper,
   spanConfig
 ) {
-  const positionsOnAnnotation = new PositionsOnAnnotation(
-    annotationData.span,
-    selectionWrapper
+  const positionsOnAnnotation = selectionWrapper.getPositionsOnAnnotation(
+    annotationData.span
   )
 
   return getNewExpandSpan(
