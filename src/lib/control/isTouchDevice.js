674dc3836c9480c16abe8a9472415d8caf224939
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-06-22 09:50:49 +0900

    refactor: Delete name of an exported function

--- src/lib/control/isTouchDevice.js
+++ src/lib/control/isTouchDevice.js
@@ -1,5 +1,5 @@
 // https://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript/4819886#4819886
-export default function isTouchDevice() {
+export default function () {
   return (
     'ontouchstart' in window ||
     navigator.maxTouchPoints > 0 ||
