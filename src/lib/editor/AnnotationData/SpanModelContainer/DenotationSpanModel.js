fb00f9f415ce5ea2890e946ac2f23d4b7257da83
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-06-22 16:59:36 +0900

    fix: Toolbar flows when scrolling in Android chrome
    To fix the position of the toolbar, we use position: sticky.
    In Android chrome, if an element is drawn outside the device width, the sticky position will be shifted upward by the width of the outside.
    To prevent this, the grid is not drawn outside the text box.

--- src/lib/editor/AnnotationData/SpanModelContainer/DenotationSpanModel.js
+++ src/lib/editor/AnnotationData/SpanModelContainer/DenotationSpanModel.js
@@ -2,6 +2,7 @@ import SELECTED from './SELECTED'
 import getHeightIncludeDescendantGrids from './getHeightIncludeDescendantGrids'
 import round from './round'
 import SpanModel from './SpanModel'
+import isTouchDevice from '../../../control/isTouchDevice'
 
 const TEXT_HEIGHT = 23
 const MARGIN_TOP = 30
@@ -39,10 +40,20 @@ export default class DenotationSpanModel extends SpanModel {
     const rectOfTextBox =
       spanElement.offsetParent.offsetParent.getBoundingClientRect()
 
+    const left = rectOfSpan.left - rectOfTextBox.left
+
+    // To fix the position of the toolbar, we use position: sticky.
+    // On Android chrome, if an element is drawn at a position that is out of the device width,
+    // the sticky position will be shifted upward by the width of the overhang.
+    // To prevent this, the grid is not drawn outside the text box.
+    const width = isTouchDevice()
+      ? Math.min(rectOfSpan.width, rectOfTextBox.width - left)
+      : rectOfSpan.width
+
     return {
       top: rectOfSpan.top - rectOfTextBox.top,
-      left: rectOfSpan.left - rectOfTextBox.left,
-      width: rectOfSpan.width
+      left,
+      width
     }
   }
 
