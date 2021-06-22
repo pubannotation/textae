a61f262383675d71f2068fef498e726231329608
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-06-22 11:24:24 +0900

    refactor: Use the SelectionWrapper.getPositionsOnAnnotation method

--- src/lib/editor/start/Presenter/EditMode/hasCharacters.js
+++ src/lib/editor/start/Presenter/EditMode/hasCharacters.js
@@ -1,11 +1,9 @@
 import OrderedPositions from './OrderedPositions'
-import PositionsOnAnnotation from './PositionsOnAnnotation'
 
 // A span cannot be created include nonEdgeCharacters only.
 export default function (annotationData, spanConfig, selectionWrapper) {
-  const positionsOnAnnotation = new PositionsOnAnnotation(
-    annotationData.span,
-    selectionWrapper
+  const positionsOnAnnotation = selectionWrapper.getPositionsOnAnnotation(
+    annotationData.span
   )
   const orderedPositions = new OrderedPositions(positionsOnAnnotation)
   const selectedString = annotationData.sourceDoc.substring(
