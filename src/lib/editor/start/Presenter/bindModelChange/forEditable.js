af4ad4e4cf4090e44173f80a8e1326feda536fe2
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-06-29 17:01:55 +0900

    refactor: Delete the forEditable module

--- src/lib/editor/start/Presenter/bindModelChange/forEditable.js
+++
@@ -1,9 +0,0 @@
-import isSimple from '../isSimple'
-
-export default function (editMode, annotationData) {
-  if (isSimple(annotationData)) {
-    editMode.toEditDenotationWithoutRelation()
-  } else {
-    editMode.toEditDenotationWithRelation()
-  }
-}
