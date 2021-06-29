0710901559b44a294b87ef042474deb09620f742
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-06-29 12:16:17 +0900

    refactor: Add input assertion to the forwardMethods

--- src/lib/editor/start/Presenter/forwardMethods.js
+++ src/lib/editor/start/Presenter/forwardMethods.js
@@ -1,6 +1,7 @@
 export default function (self, getTarget, methods) {
   for (const method of methods) {
     const target = getTarget()
+    console.assert(target[method], `No ${method} method to forward`, target)
     self[method] = (...args) => target[method].apply(target, args)
   }
 }
