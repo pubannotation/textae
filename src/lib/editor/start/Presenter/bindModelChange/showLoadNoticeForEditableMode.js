77fed2341258a9140199299346c63af2f57dfc21
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-06-29 17:03:45 +0900

    refactor: Delete the showLoadNoticeForEditableMode

--- src/lib/editor/start/Presenter/bindModelChange/showLoadNoticeForEditableMode.js
+++
@@ -1,9 +0,0 @@
-import alertifyjs from 'alertifyjs'
-
-export default function (multitrack) {
-  if (multitrack) {
-    alertifyjs.success(
-      'track annotations have been merged to root annotations.'
-    )
-  }
-}
