27dad1a44acc41e2be3c0fd0b51500252c6e299b
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-06-29 17:00:44 +0900

    refactor: Use the EditMode.forEditable method

--- src/lib/editor/start/Presenter/bindModelChange/index.js
+++ src/lib/editor/start/Presenter/bindModelChange/index.js
@@ -1,5 +1,4 @@
 import showLoadNoticeForEditableMode from './showLoadNoticeForEditableMode'
-import forEditable from './forEditable'
 
 export default function (editor, editMode, mode) {
   editor.eventEmitter.on(
@@ -9,7 +8,7 @@ export default function (editor, editMode, mode) {
         editMode.forView()
       } else {
         showLoadNoticeForEditableMode(multitrack)
-        forEditable(editMode, annotationData)
+        editMode.forEditable()
       }
     }
   )
