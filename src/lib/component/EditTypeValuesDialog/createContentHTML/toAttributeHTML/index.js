d560ec63793e4d0a4eb9fef3fff6bfd3afa62f14
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-07-29 10:56:05 +0900

    refactor: Extract a getLabelOf module

--- src/lib/component/EditTypeValuesDialog/createContentHTML/toAttributeHTML/index.js
+++ src/lib/component/EditTypeValuesDialog/createContentHTML/toAttributeHTML/index.js
@@ -1,3 +1,5 @@
+import getLabelOf from './getLabelOf'
+
 export default function (
   attribute,
   index,
@@ -59,27 +61,4 @@ export default function (
     </button>
   </td>
 </tr>`
-}
-
-function getLabelOf(attribute, attributeContainer) {
-  const { pred, obj } = attribute
-  const { valueType } = attributeContainer.get(pred)
-
-  switch (valueType) {
-    case 'string':
-      // In the case of String attributes,
-      // Labels completed by autocomplete can be reflected in attribute definitions.
-      // We want to keep the label in the attribute hash until we press the OK button.
-      return attribute.label || attributeContainer.getLabel(pred, obj) || ''
-    case 'selection':
-      // In the case of Selection attributes,
-      // we want to refer only to the label of the attribute definition.
-      return attributeContainer.getLabel(pred, obj) || ''
-    case 'numeric':
-    case 'flag':
-      // No label for numric attributes or for flag attributes.
-      return ''
-    default:
-      throw `unknown attribute type: ${valueType}`
-  }
 }
