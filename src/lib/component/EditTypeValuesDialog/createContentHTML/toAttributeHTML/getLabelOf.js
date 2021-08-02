1ab3dd6b1264c6e63f670d1247d677b22deb8da1
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-08-02 10:36:55 +0900

    feature: Display label of numeric attribute in the Edit Type Value dialog

--- src/lib/component/EditTypeValuesDialog/createContentHTML/toAttributeHTML/getLabelOf.js
+++ src/lib/component/EditTypeValuesDialog/createContentHTML/toAttributeHTML/getLabelOf.js
@@ -9,10 +9,10 @@ export default function (attribute, attributeContainer) {
       // We want to keep the label in the attribute hash until we press the OK button.
       return attribute.label || attributeContainer.getLabel(pred, obj) || ''
     case 'selection':
-      // In the case of Selection attributes,
+    case 'numeric':
+      // In the case of Selection or Numeric attributes,
       // we want to refer only to the label of the attribute definition.
       return attributeContainer.getLabel(pred, obj) || ''
-    case 'numeric':
     case 'flag':
       // No label for numric attributes or for flag attributes.
       return ''
