7772764c2106018950c42b1d5a53001ac8b379ba
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-08-04 17:01:39 +0900

    feature: Change to a vertical form in the Type Definition dialog to make it easier to use on mobile devices

--- src/lib/component/TypeDefinitionDialog/index.js
+++ src/lib/component/TypeDefinitionDialog/index.js
@@ -6,29 +6,29 @@ function template(context) {
   return `
 <div class="textae-editor__edit-type-definition-dialog__container">
   <div class="textae-editor__edit-type-definition-dialog__input-box ui-front">
-    <label>Id:</label><br>
+    <label>Id</label>
     <input
       class="textae-editor__edit-type-definition-dialog--id textae-editor__promise-daialog__observable-element" 
       value=${id || ''}>
   </div>
   <div class="textae-editor__edit-type-definition-dialog__input-box ui-front">
-    <label>Label:<span></span></label><br>
+    <label>Label<span></span></label>
     <input value="${label}">
   </div>
   <div class="textae-editor__edit-type-definition-dialog__color-picker">
-    <label>Color:<input 
+    <label><input 
       class="textae-editor__edit-type-definition-dialog__color-picker__input" 
       type="color" 
       value="${color}">
-    </label>
+    Color</label>
   </div>
   <div class="textae-editor__edit-type-definition-dialog__set-default">
-    <label>Default type:<input 
+    <label><input 
       class="textae-editor__edit-type-definition-dialog__set-default__input" 
       type="checkbox" ${
         isDefault ? 'checked="checked" disabled="disabled"' : ''
       }>
-    </label>
+    Default type</label>
   </div>
 </div>`
 }
