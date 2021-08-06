4a7d691f1e8e3ab64ecf0fde96b620e81d0f3a6f
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-08-06 11:07:23 +0900

    refactor: Rename the CSS class name for the TypeDefinitionDialog

--- src/lib/component/TypeDefinitionDialog/index.js
+++ src/lib/component/TypeDefinitionDialog/index.js
@@ -4,27 +4,27 @@ import setSourceOfAutoComplete from './setSourceOfAutoComplete'
 function template(context) {
   const { id, label, color, isDefault } = context
   return `
-<div class="textae-editor__edit-type-definition-dialog__container">
-  <div class="textae-editor__edit-type-definition-dialog__row ui-front">
+<div class="textae-editor__type-definition-dialog__container">
+  <div class="textae-editor__type-definition-dialog__row ui-front">
     <label>Id</label>
     <input
-      class="textae-editor__edit-type-definition-dialog--id textae-editor__promise-daialog__observable-element" 
+      class="textae-editor__type-definition-dialog--id textae-editor__promise-daialog__observable-element" 
       value=${id || ''}>
   </div>
-  <div class="textae-editor__edit-type-definition-dialog__row ui-front">
+  <div class="textae-editor__type-definition-dialog__row ui-front">
     <label>Label<span></span></label>
     <input value="${label}">
   </div>
-  <div class="textae-editor__edit-type-definition-dialog__color-picker">
+  <div class="textae-editor__type-definition-dialog__color-picker">
     <label><input 
-      class="textae-editor__edit-type-definition-dialog__color-picker__input" 
+      class="textae-editor__type-definition-dialog__color-picker__input" 
       type="color" 
       value="${color}">
     Color</label>
   </div>
-  <div class="textae-editor__edit-type-definition-dialog__set-default">
+  <div class="textae-editor__type-definition-dialog__set-default">
     <label><input 
-      class="textae-editor__edit-type-definition-dialog__set-default__input" 
+      class="textae-editor__type-definition-dialog__set-default__input" 
       type="checkbox" ${
         isDefault ? 'checked="checked" disabled="disabled"' : ''
       }>
