40b8b191de3e8086dfffc00af4818ea88de47111
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-08-06 10:40:39 +0900

    feature: Change to a vertical form in the Edit Value of Attribute Definition dialog to make it easier to use on mobile devices

--- src/lib/component/EditValueOfAttributeDefinitionDialog/inputDefault.js
+++ src/lib/component/EditValueOfAttributeDefinitionDialog/inputDefault.js
@@ -1,9 +1,15 @@
 export default function (showDefault, defaultValue) {
   return showDefault
     ? `
-    <div class="textae-editor__add-value-to-attribute-dialog__default">
-      <label>default:</label><br>
-      <input type="checkbox" ${defaultValue ? `checked="checked"` : ``}>
+    <div class="textae-editor__add-value-to-attribute-dialog__row">
+      <label>
+        <input
+          type="checkbox"
+          ${defaultValue ? `checked="checked"` : ``}
+          class="textae-editor__add-value-to-attribute-dialog__default"
+        >
+        default
+      </label>
     </div>
     `
     : ``
--- src/lib/component/EditValueOfAttributeDefinitionDialog/template.js
+++ src/lib/component/EditValueOfAttributeDefinitionDialog/template.js
@@ -13,19 +13,29 @@ export default function (context) {
   return `
 <div class="textae-editor__add-value-to-attribute-dialog__container">
   <div class="textae-editor__add-value-to-attribute-dialog__row">
-    <div class="textae-editor__add-value-to-attribute-dialog__range_or_id_or_pattern textae-editor__promise-daialog__observable-element">
-      <label>${labelForRangeOrIdOrPattern}:</label><br>
-      <input value="${rangeOrIdOrPattern || ''}">
-    </div>
-    ${inputDefault(showDefault, defaultValue)}
-    <div class="textae-editor__add-value-to-attribute-dialog__label textae-editor__promise-daialog__observable-element">
-      <label>label:</label><br>
-      <input value="${label || ''}">
-    </div>
-    <div class="textae-editor__add-value-to-attribute-dialog__color">
-      <label>color:</label><br>
-      <input type="color" value="${color || ''}">
-    </div>
+    <label>${labelForRangeOrIdOrPattern}</label>
+    <input
+      value="${rangeOrIdOrPattern || ''}"
+      class="textae-editor__add-value-to-attribute-dialog__range_or_id_or_pattern textae-editor__promise-daialog__observable-element"
+    >
+  </div>
+  ${inputDefault(showDefault, defaultValue)}
+  <div class="textae-editor__add-value-to-attribute-dialog__row">
+    <label>label</label>
+    <input
+      value="${label || ''}"
+      class="textae-editor__add-value-to-attribute-dialog__label textae-editor__promise-daialog__observable-element"
+    >
+  </div>
+  <div class="textae-editor__add-value-to-attribute-dialog__row">
+    <label>
+      <input
+        type="color"
+        value="${color || ''}"
+        class="textae-editor__add-value-to-attribute-dialog__color"
+      >
+      color
+    </label>
   </div>
 </div>`
 }
