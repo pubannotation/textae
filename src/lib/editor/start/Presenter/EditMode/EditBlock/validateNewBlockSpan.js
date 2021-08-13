e024d7ef5fb6eb4341e3b3edfa59d4c3b64fc949
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-08-13 12:32:51 +0900

    feature: Show alert message when creating a boundary crossing span is failed

--- src/lib/editor/start/Presenter/EditMode/EditBlock/validateNewBlockSpan.js
+++ src/lib/editor/start/Presenter/EditMode/EditBlock/validateNewBlockSpan.js
@@ -1,6 +1,9 @@
+import alertifyjs from 'alertifyjs'
+
 export default function (annotationData, begin, end) {
   // The span cross exists spans.
   if (annotationData.span.isBoundaryCrossingWithOtherSpans(begin, end)) {
+    alertifyjs.warning('A span cannot be modifyed to make a boundary crossing.')
     return false
   }
 
