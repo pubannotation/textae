6fc85e465e5e3f5e2bec14f46acee30b75076613
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-08-13 12:22:06 +0900

    refactor: Extract a validateNewSpan function

--- src/lib/editor/start/Presenter/EditMode/EditDenotation/SpanEditor/create/index.js
+++ src/lib/editor/start/Presenter/EditMode/EditDenotation/SpanEditor/create/index.js
@@ -18,28 +18,34 @@ export default function (
     spanConfig
   )
 
+  if (validateNewSpan(annotationData, begin, end)) {
+    const command = createCommand(
+      commander,
+      { begin, end },
+      isReplicateAuto,
+      isDelimiterFunc
+    )
+
+    commander.invoke(command)
+  }
+}
+
+function validateNewSpan(annotationData, begin, end) {
   // The span cross exists spans.
   if (annotationData.span.isBoundaryCrossingWithOtherSpans(begin, end)) {
     alertifyjs.warning('A span cannot be modifyed to make a boundary crossing.')
-    return
+    return false
   }
 
   // The span exists already.
   if (annotationData.span.hasDenotationSpan(begin, end)) {
-    return
+    return false
   }
 
   // There is a BlockSpan that is a child.
   if (annotationData.span.hasBlockSpanBetween(begin, end)) {
-    return
+    return false
   }
 
-  const command = createCommand(
-    commander,
-    { begin, end },
-    isReplicateAuto,
-    isDelimiterFunc
-  )
-
-  commander.invoke(command)
+  return true
 }
