cb5bddbe7223b02a63ad596f51ddaf36dcd828a7
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-08-13 11:48:56 +0900

    feature: Show message by alertify instead of the window.alert

--- src/lib/editor/start/Presenter/EditMode/clearTextSelectionAndAlert.js
+++ src/lib/editor/start/Presenter/EditMode/clearTextSelectionAndAlert.js
@@ -1,9 +1,7 @@
 import clearTextSelection from './clearTextSelection'
+import alertifyjs from 'alertifyjs'
 
 export default function (message) {
-  // Wait 10 milliseconds to show alert after deselecting text.
-  // 10 milliseconds was decided by experimenting with Google Chrome.
-  // Changes may be required in your environment.
   clearTextSelection()
-  setTimeout(() => alert(message), 10)
+  alertifyjs.warning(message)
 }
