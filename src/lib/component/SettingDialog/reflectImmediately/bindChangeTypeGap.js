7a27651079bc4130a45cbe05d6836243d0fadb6e
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-08-17 16:42:54 +0900

    refactor: Rename an argument

--- src/lib/component/SettingDialog/reflectImmediately/bindChangeTypeGap.js
+++ src/lib/component/SettingDialog/reflectImmediately/bindChangeTypeGap.js
@@ -1,13 +1,13 @@
 import delgate from 'delegate'
 import debounce300 from './debounce300'
 
-export default function (content, entityGap, textBox) {
+export default function (content, typeGap, textBox) {
   delgate(
     content,
     '.textae-editor__setting-dialog__type-gap-text',
     'change',
     debounce300((e) => {
-      entityGap.value = Number(e.target.value)
+      typeGap.value = Number(e.target.value)
       content.querySelector(
         '.textae-editor__setting-dialog__line-height-text'
       ).value = textBox.lineHeight
