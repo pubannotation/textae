e5edc6f9414ee125560f385407e0b101969fcc25
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-08-31 17:45:56 +0900

    feature: Reloading when trying to scroll further when you are at the top on an Android device. Show a confirmation dialog to prevent this.

--- src/lib/editor/start/observe/observeHistoryChange.js
+++ src/lib/editor/start/observe/observeHistoryChange.js
@@ -1,8 +1,11 @@
+import isTouchDevice from '../../../isTouchDevice'
+
 export default function (editor) {
   editor.eventEmitter.on('textae-event.history.change', (history) => {
     // change leaveMessage show
-    window.onbeforeunload = history.hasAnythingToSaveAnnotation
-      ? () => true
-      : null
+    // Reloading when trying to scroll further when you are at the top on an Android device.
+    // Show a confirmation dialog to prevent this.
+    window.onbeforeunload =
+      isTouchDevice() || history.hasAnythingToSaveAnnotation ? () => true : null
   })
 }
