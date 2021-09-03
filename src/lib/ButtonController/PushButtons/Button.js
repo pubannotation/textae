17516f778440b19066730f7cb79fc7e39144fdf9
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-09-03 10:11:52 +0900

    refactor: Make fields private

--- src/lib/ButtonController/PushButtons/Button.js
+++ src/lib/ButtonController/PushButtons/Button.js
@@ -2,31 +2,31 @@
 export default class Button {
   constructor(eventEmitter, buttonName) {
     this._eventEmitter = eventEmitter
-    this.name = buttonName
-    this.state = false
+    this._name = buttonName
+    this._state = false
   }
 
   value(newValue) {
     if (newValue !== undefined) {
-      this.state = newValue
+      this._state = newValue
       this.propagate()
     } else {
-      return this.state
+      return this._state
     }
 
     return null
   }
 
   toggle() {
-    this.state = !this.state
+    this._state = !this._state
     this.propagate()
   }
 
   // Propagate button state to the tool.
   propagate() {
     this._eventEmitter.emit('textae-event.control.button.push', {
-      buttonName: this.name,
-      state: this.state
+      buttonName: this._name,
+      state: this._state
     })
   }
 }
