5e4a06891a7bcac31997908ed3f40dd71439e5e6
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-09-03 11:08:53 +0900

    refactor: Remove logic for ContextMenu from the updateButtonPushState

--- src/lib/control/ControlBar/updateButtonPushState.js
+++ src/lib/control/ControlBar/updateButtonPushState.js
@@ -1,12 +1,6 @@
 export default function (el, buttonType, isPushed) {
   const button = el.querySelector(`.textae-control-${buttonType}-button`)
 
-  // Touch devices limit the number of buttons displayed in the context menu
-  // due to the limited screen area.
-  if (!button) {
-    return
-  }
-
   if (isPushed) {
     button.classList.add('textae-control-icon--pushed')
   } else {
