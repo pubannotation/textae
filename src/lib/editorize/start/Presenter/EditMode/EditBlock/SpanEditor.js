f1025d4c0e0b861f783aade300f7c477553fe4e6
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-09-16 12:27:23 +0900

    feature: On mobile devices, do not shrink the block span when the selected text fits into one block span

--- src/lib/editorize/start/Presenter/EditMode/EditBlock/SpanEditor.js
+++ src/lib/editorize/start/Presenter/EditMode/EditBlock/SpanEditor.js
@@ -303,6 +303,14 @@ export default class SpanEditor {
       return null
     }
 
+    // On mobile devices,
+    // do not shrink the block span when the selected text fits into one block span.
+    if (
+      selectionWrapper.parentOfAnchorNode === selectionWrapper.parentOfFocusNode
+    ) {
+      return null
+    }
+
     // When you select text by mouse operation,
     // the anchor node of the selected string is always inside the span to be extended,
     // and the focus node is outside.
