23e3ad039571f4d9b08218ce274fcfa6b914bc71
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-09-21 17:05:21 +0900

    refactor: Delete default export function name

--- src/lib/component/Pallet/enableJqueryDraggable.js
+++ src/lib/component/Pallet/enableJqueryDraggable.js
@@ -1,7 +1,7 @@
 import $ from 'jquery'
 import 'jquery-ui/ui/widgets/draggable'
 
-export default function enableJqueryDraggable(element, editor) {
+export default function (element, editor) {
   $(element).draggable({
     containment: editor[0],
     distance: 10
