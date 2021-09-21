8f5f038ed8169337cbb3eeb5177a6b4b5f2997fc
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-09-21 16:42:22 +0900

    refactor: Rename arguments

--- src/lib/editorize/idFactory.js
+++ src/lib/editorize/idFactory.js
@@ -1,6 +1,6 @@
 // The ID of spans has editorId and begin and end, like 'editor1__S0_15'.
-export function makeDenotationSpanHTMLElementID(editorId, begin, end) {
-  return `${editorId}__S${begin}_${end}`
+export function makeDenotationSpanHTMLElementID(editorID, begin, end) {
+  return `${editorID}__S${begin}_${end}`
 }
 
 export function makeBlockSpanHTMLElementID(editorID, begin, end) {
