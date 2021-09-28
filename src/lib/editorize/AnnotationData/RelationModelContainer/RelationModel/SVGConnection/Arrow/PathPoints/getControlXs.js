f6956fc4fddedf1cedb5a3e17406d7a224114c23
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-09-28 15:13:35 +0900

    refactor: Pass the source and the target to the bentSignificantly

--- src/lib/editorize/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/PathPoints/getControlXs.js
+++ src/lib/editorize/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/PathPoints/getControlXs.js
@@ -19,12 +19,12 @@ function bentSignificantly(source, target) {
   if (source.y < target.y) {
     return {
       sourceControlX: source.x,
-      targetControlX: target.x + (source.x <= target.xF + 16 ? 150 : -150)
+      targetControlX: target.x + (source.x <= target.x + 16 ? 150 : -150)
     }
   } else {
     return {
-      sourceControlX: source.x + (source.x <= target.xF + 16 ? 150 : -150),
-      targetControlX: target.xF
+      sourceControlX: source.x + (source.x <= target.x + 16 ? 150 : -150),
+      targetControlX: target.x
     }
   }
 }
