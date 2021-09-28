68104563ff0d283fc2f46c7ba5a187fbcb5bb572
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-09-28 15:22:53 +0900

    feature: Decide whether to bend to the left or right depending on the anchor position of the entity above when making large bends in the relation

--- src/lib/editorize/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/PathPoints/getControlXs/bentSignificantly.js
+++ src/lib/editorize/AnnotationData/RelationModelContainer/RelationModel/SVGConnection/Arrow/PathPoints/getControlXs/bentSignificantly.js
@@ -2,11 +2,11 @@ export default function (source, target) {
   if (source.y < target.y) {
     return {
       sourceControlX: source.x,
-      targetControlX: target.x + (source.x <= target.x + 16 ? 150 : -150)
+      targetControlX: target.x + (source.anchor === 'right' ? 150 : -150)
     }
   } else {
     return {
-      sourceControlX: source.x + (source.x <= target.x + 16 ? 150 : -150),
+      sourceControlX: source.x + (target.anchor === 'right' ? 150 : -150),
       targetControlX: target.x
     }
   }
