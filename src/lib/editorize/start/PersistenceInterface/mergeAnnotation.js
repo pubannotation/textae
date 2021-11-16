b2410007db150a98cd43c9364cbd444dfb93cda1
ledsun <shigeru.nakajima@gmail.com>
2021-11-16 15:21:54 +0900

    refactor: Delete the mergeAnnotation

--- src/lib/editorize/start/PersistenceInterface/mergeAnnotation.js
+++
@@ -1,4 +0,0 @@
-// Merge with the original annotation and save the value unchanged in the editor.
-export default function (originalAnnotation, annotationData, config) {
-  return Object.assign(originalAnnotation, annotationData.JSON, { config })
-}
