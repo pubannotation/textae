fe83741c9c5b8fadd0e573140854679593b6e435
ledsun <shigeru.nakajima@gmail.com>
2021-11-16 17:23:59 +0900

    refactor: Use spread operator

--- src/lib/editorize/start/patchConfiguration/AttributeConfigurationGenerator/fillInferDefinitionFromAnnotation/fill.js
+++ src/lib/editorize/start/patchConfiguration/AttributeConfigurationGenerator/fillInferDefinitionFromAnnotation/fill.js
@@ -4,7 +4,7 @@ export default function (config, newAttributeDefinitions) {
   for (const newDef of newAttributeDefinitions) {
     const index = config.findIndex((a) => a.pred === newDef.pred)
     if (index !== -1) {
-      config[index] = Object.assign(newDef, config[index])
+      config[index] = { ...newDef, ...config[index] }
     } else {
       config.push(newDef)
     }
