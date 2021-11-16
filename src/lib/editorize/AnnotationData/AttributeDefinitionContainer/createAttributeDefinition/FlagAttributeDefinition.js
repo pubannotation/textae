73f293e74a249873415a674d85241d0759149a64
ledsun <shigeru.nakajima@gmail.com>
2021-11-16 17:19:50 +0900

    refactor: Use spread operator

--- src/lib/editorize/AnnotationData/AttributeDefinitionContainer/createAttributeDefinition/FlagAttributeDefinition.js
+++ src/lib/editorize/AnnotationData/AttributeDefinitionContainer/createAttributeDefinition/FlagAttributeDefinition.js
@@ -25,11 +25,14 @@ export default class FlagAttributeDefinition extends AttributeDefinition {
   }
 
   get JSON() {
-    return Object.assign(super.JSON, {
-      'value type': 'flag',
-      default: this.default,
-      label: this.label,
-      color: this.color
-    })
+    return {
+      ...super.JSON,
+      ...{
+        'value type': 'flag',
+        default: this.default,
+        label: this.label,
+        color: this.color
+      }
+    }
   }
 }
