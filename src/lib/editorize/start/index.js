ba6e5f08e30885d79e3c8122dfe159f46deed993
ledsun <shigeru.nakajima@gmail.com>
2021-11-17 14:59:16 +0900

    refactor: Inline the observe

--- src/lib/editorize/start/index.js
+++ src/lib/editorize/start/index.js
@@ -1,4 +1,6 @@
 import alertifyjs from 'alertifyjs'
+import delegate from 'delegate'
+import debounce from 'debounce'
 import SpanConfig from './SpanConfig'
 import Commander from './Commander'
 import View from './View'
@@ -13,12 +15,16 @@ import OriginalData from './OriginalData'
 import ButtonController from './ButtonController'
 import ClipBoard from './ClipBoard'
 import AnnotationAutoSaver from './AnnotationAutoSaver'
-import observe from './observe'
 import ControlBar from '../control/ControlBar'
 import ContextMenu from '../control/ContextMenu'
 import KeyEventMap from './KeyEventMap'
 import IconEventMap from './IconEventMap'
 import AnnotationDataEventsObserver from '../AnnotationDataEventsObserver'
+import warningIfBeginEndOfSpanAreNotInteger from './warningIfBeginEndOfSpanAreNotInteger'
+import validateConfigurationAndAlert from './validateConfigurationAndAlert'
+import setAnnotationAndConfiguration from './setAnnotationAndConfiguration'
+import DataSource from '../DataSource'
+import isTouchDevice from '../isTouchDevice'
 
 export default function (
   editor,
@@ -128,15 +134,149 @@ export default function (
     view
   )
 
-  observe(
-    editor,
-    history,
-    annotationData,
-    spanConfig,
-    params,
-    originalData,
-    dataAccessObject,
-    buttonController
+  editor.eventEmitter
+    .on('textae-event.type-definition.reset', () =>
+      history.resetConfiguration()
+    )
+    .on('textae-event.annotation-data.events-observer.change', (hasChange) => {
+      // change leaveMessage show
+      // Reloading when trying to scroll further when you are at the top on an Android device.
+      // Show a confirmation dialog to prevent this.
+      window.onbeforeunload = isTouchDevice() || hasChange ? () => true : null
+    })
+    .on(
+      'textae-event.data-access-object.annotation.load.success',
+      (dataSource) => {
+        if (!dataSource.data.config && params.get('config')) {
+          dataAccessObject.loadConfigulation(params.get('config'), dataSource)
+        } else {
+          warningIfBeginEndOfSpanAreNotInteger(dataSource.data)
+
+          if (dataSource.data.config) {
+            // When config is specified, it must be JSON.
+            // For example, when we load an HTML file, we treat it as text here.
+            if (typeof dataSource.data.config !== 'object') {
+              alertifyjs.error(`configuration in anntotaion file is invalid.`)
+              return
+            }
+          }
+
+          const validConfig = validateConfigurationAndAlert(
+            dataSource.data,
+            dataSource.data.config
+          )
+
+          if (validConfig) {
+            setAnnotationAndConfiguration(
+              validConfig,
+              buttonController,
+              spanConfig,
+              annotationData,
+              dataSource.data
+            )
+
+            originalData.annotation = dataSource
+          }
+        }
+      }
+    )
+    .on('textae-event.data-access-object.annotation.load.error', (url) =>
+      alertifyjs.error(
+        `Could not load the file from the location you specified.: ${url}`
+      )
+    )
+    .on(
+      'textae-event.data-access-object.annotation.format.error',
+      ({ displayName }) =>
+        alertifyjs.error(
+          `${displayName} is not a annotation file or its format is invalid.`
+        )
+    )
+    .on(
+      'textae-event.data-access-object.configuration.load.success',
+      (dataSource, loadedAnnotation = null) => {
+        // When config is specified, it must be JSON.
+        // For example, when we load an HTML file, we treat it as text here.
+        if (typeof dataSource.data !== 'object') {
+          alertifyjs.error(
+            `${dataSource.displayName} is not a configuration file or its format is invalid.`
+          )
+          return
+        }
+
+        if (loadedAnnotation) {
+          warningIfBeginEndOfSpanAreNotInteger(loadedAnnotation.data)
+        }
+
+        // If an annotation that does not contain a configuration is loaded
+        // and a configuration is loaded from a taxtae attribute value,
+        // both the loaded configuration and the annotation are passed.
+        // If only the configuration is read, the annotation is null.
+        const annotation = (loadedAnnotation && loadedAnnotation.data) || {
+          ...originalData.annotation,
+          ...annotationData.JSON
+        }
+
+        const validConfig = validateConfigurationAndAlert(
+          annotation,
+          dataSource.data
+        )
+
+        if (!validConfig) {
+          return
+        }
+
+        setAnnotationAndConfiguration(
+          validConfig,
+          buttonController,
+          spanConfig,
+          annotationData,
+          annotation
+        )
+
+        if (loadedAnnotation) {
+          originalData.annotation = loadedAnnotation
+        }
+
+        originalData.configuration = dataSource
+      }
+    )
+    .on('textae-event.data-access-object.configuration.load.error', (url) =>
+      alertifyjs.error(
+        `Could not load the file from the location you specified.: ${url}`
+      )
+    )
+    .on(
+      'textae-event.data-access-object.configuration.format.error',
+      ({ displayName }) =>
+        alertifyjs.error(
+          `${displayName} is not a configuration file or its format is invalid.!`
+        )
+    )
+    .on('textae-event.data-access-object.configuration.save', (editedData) => {
+      originalData.configuration = new DataSource(null, null, editedData)
+    })
+
+  const dom = editor[0]
+
+  // Prevent a selection text with shift keies.
+  dom.addEventListener('mousedown', (e) => {
+    if (e.shiftKey) {
+      e.preventDefault()
+    }
+  })
+
+  // Prevent a selection of an entity by the double-click.
+  delegate(dom, '.textae-editor__signboard', 'mousedown', (e) =>
+    e.preventDefault()
+  )
+
+  document.addEventListener(
+    'selectionchange',
+    debounce(() => editor.instanceMethods.applyTextSelection(), 100)
+  )
+  document.addEventListener('contextmenu', () =>
+    editor.instanceMethods.applyTextSelection()
   )
 
   editor.eventEmitter
