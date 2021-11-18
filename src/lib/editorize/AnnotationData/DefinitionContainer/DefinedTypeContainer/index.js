3f188ddc723ac3b089c1d37ea0be7c0248596233
ledsun <shigeru.nakajima@gmail.com>
2021-11-18 15:46:59 +0900

    feature: Preserve the order of type definitions

--- src/lib/editorize/AnnotationData/DefinitionContainer/DefinedTypeContainer/index.js
+++ src/lib/editorize/AnnotationData/DefinitionContainer/DefinedTypeContainer/index.js
@@ -1,33 +1,38 @@
 import getConfig from './getConfig'
 
 export default class DefinedTypeContainer {
+  // Expected values is an array of object.
+  // An example of object is {"id": "Regulation","color": "#FFFF66","default": true}.
   constructor(values) {
-    // expected values is an array of object. example of object is {"id": "Regulation","color": "#FFFF66","default": true}.
-    this._map = values.reduce((acc, cur) => acc.set(cur.id, cur), new Map())
+    // If the order of the type definitions changes,
+    // it will be treated as a change, so preserve the order.
+    this._list = values
   }
 
   has(id) {
-    return this._map.has(id)
+    return this.map.has(id)
   }
 
   get(id) {
-    return { ...this._map.get(id) }
+    return { ...this.map.get(id) }
   }
 
   replace(id, newType) {
-    // Delete old ID when changing ID.
-    if (id !== newType.id) {
-      this._map.delete(id)
+    const index = this._list.findIndex((elem) => elem.id === id)
+
+    if (index !== -1) {
+      this._list.splice(index, 1, newType)
+    } else {
+      this._list.push(newType)
     }
-    this._map.set(newType.id, newType)
   }
 
   delete(id) {
-    this._map.delete(id)
+    this._list = this._list.filter((elem) => elem.id !== id)
   }
 
   ids() {
-    return this._map.keys()
+    return this.map.keys()
   }
 
   getConfig(id) {
@@ -35,12 +40,12 @@ export default class DefinedTypeContainer {
   }
 
   labelIncludes(term) {
-    return [...this._map.values()]
+    return this._list
       .filter((t) => t.label)
       .filter((t) => t.label.includes(term))
   }
 
   get map() {
-    return this._map
+    return this._list.reduce((acc, cur) => acc.set(cur.id, cur), new Map())
   }
 }
