f497dcf1ff73882da374f467160f017c9458d9a8
ledsun <shigeru.nakajima@gmail.com>
2021-12-14 16:02:06 +0900

    feature: Applying Styles to the Tips Dialog

--- src/lib/component/TipsDialog.js
+++ src/lib/component/TipsDialog.js
@@ -3,20 +3,21 @@ import Dialog from './Dialog'
 export default class TipsDialog extends Dialog {
   constructor() {
     super(
-      'Tips for Keyboard short-cuts about copy and paste.',
-      `<div >
-        <span>For security reason, the shortcut keys for cut/copy/paste are changed as follows:</span>
-        <table>
+      'For security reason, the shortcut keys for cut/copy/paste are changed as follows:',
+      `<table class="textae-editor__tips-dialog__table">
+        <thead>
           <tr><th></th><th>before</th><th>after</th></tr>
+        </thead>
+        <tbody>
           <tr><td>cut</td><td>x</td><td>Ctrl-x</td></tr>
           <tr><td>copy</td><td>c</td><td>Ctrl-c</td></tr>
           <tr><td>paste</td><td>v</td><td>Ctrl-v</td></tr>
-        </table>
-      </div>`,
+        </tbody>
+      </table>
+      `,
 
       {
-        height: 313,
-        maxWidth: 523
+        maxWidth: 785
       }
     )
   }
