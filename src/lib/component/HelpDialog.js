e325e60adfcfb6cdb228b667cdde7fea932281a0
ledsun <shigeru.nakajima@gmail.com>
2021-12-14 16:58:28 +0900

    feature: Update the contents of the help dialog to reflect the change in the copy and paste shortcut key

--- src/lib/component/HelpDialog.js
+++ src/lib/component/HelpDialog.js
@@ -4,10 +4,33 @@ export default class HelpDialog extends Dialog {
   constructor() {
     super(
       'Help (Keyboard short-cuts)',
-      '<div class="textae-tool__key-help"></div>',
+      `<div class="textae-tool__key-help"></div>
+       <ul>
+         <li>
+          cut
+          <image
+           class="textae-editor__help-dialog__icon"
+           src="http://localhost:8000/src/lib/css/images/btn_cut_16.png">
+          : Ctrl+x
+        </li>
+         <li>
+          copy
+          <image
+           class="textae-editor__help-dialog__icon"
+           src="http://localhost:8000/src/lib/css/images/btn_copy_16.png">
+          : Ctrl+c
+        </li>
+         <li>
+          paste
+          <image
+           class="textae-editor__help-dialog__icon"
+           src="http://localhost:8000/src/lib/css/images/btn_paste_16.png">
+          : Ctrl+v
+        </li>
+       </ul>
+      `,
 
       {
-        height: 313,
         maxWidth: 523
       }
     )
