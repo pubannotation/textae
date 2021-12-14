847bc65bccc4185bf8844c235071408f148ae434
ledsun <shigeru.nakajima@gmail.com>
2021-12-14 14:24:01 +0900

    refactor: Make it possible to call the editor's copy and paste API in order to handle copy and paste events in the EditorContainer

--- src/lib/editorize/start/InstanceMethods.js
+++ src/lib/editorize/start/InstanceMethods.js
@@ -1,9 +1,17 @@
+import forwardMethods from './Presenter/forwardMethods'
+
 export default class InstanceMethods {
   constructor(presenter, buttonController, view) {
     this._view = view
     this._buttonController = buttonController
     this._presenter = presenter
     this._isActive = false
+
+    forwardMethods(this, () => presenter, [
+      'copyEntities',
+      'cutEntities',
+      'pasteEntities'
+    ])
   }
 
   get isActive() {
