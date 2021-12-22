a1859ef712fce54384454975953a39b0b3ede184
ledsun <shigeru.nakajima@gmail.com>
2021-12-22 17:55:23 +0900

    refactor: Write the type information in comments so that VSCode can jump to the code

--- src/lib/component/TypeValuesPallet/index.js
+++ src/lib/component/TypeValuesPallet/index.js
@@ -18,6 +18,7 @@ export default class TypeValuesPallet extends Pallet {
     super(editorHTMLElement, title)
 
     this._eventEmitter = eventEmitter
+    /** @type {import('../../editorize/AnnotationData').default} */
     this._annotationData = annotationData
     this._definitionContainer = definitionContainer
     this._selectionModelItems = selectionModelEntity
