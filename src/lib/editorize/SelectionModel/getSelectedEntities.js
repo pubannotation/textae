39695073daea868d92435b769f70fcda4f458b69
ledsun <shigeru.nakajima@gmail.com>
2022-01-05 10:54:19 +0900

    refactor: Inline the getSelectedSpansEntities

--- src/lib/editorize/SelectionModel/getSelectedEntities.js
+++ src/lib/editorize/SelectionModel/getSelectedEntities.js
@@ -1,8 +1,9 @@
-import getSelectedSpansEntities from './getSelectedSpansEntities'
-
 // Unique Entities. Because a entity is deplicate When a span and thats entity is selected.
 export default function (selectionModel) {
   return new Set(
-    getSelectedSpansEntities(selectionModel).concat(selectionModel.entity.all)
+    selectionModel.span.all
+      .map((span) => span.entities)
+      .flat()
+      .concat(selectionModel.entity.all)
   )
 }
