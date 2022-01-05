4af430500505d1433027020f9adb6f2b7866b0fc
ledsun <shigeru.nakajima@gmail.com>
2022-01-05 11:00:59 +0900

    refactor: Delete the getSelectedEntities

--- src/lib/editorize/SelectionModel/getSelectedEntities.js
+++
@@ -1,9 +0,0 @@
-// Unique Entities. Because a entity is deplicate When a span and thats entity is selected.
-export default function (selectionModel) {
-  return new Set(
-    selectionModel.span.all
-      .map((span) => span.entities)
-      .flat()
-      .concat(selectionModel.entity.all)
-  )
-}
