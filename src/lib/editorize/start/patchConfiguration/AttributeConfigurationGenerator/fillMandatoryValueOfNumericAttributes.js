085a4bab0ab0b91e7cedc65daa6821f0e01110f7
ledsun <shigeru.nakajima@gmail.com>
2022-01-12 14:54:03 +0900

    feature: Eliminate the ability to fill in numeric attribute definitions with 0 when min and max are missing

--- src/lib/editorize/start/patchConfiguration/AttributeConfigurationGenerator/fillMandatoryValueOfNumericAttributes.js
+++ src/lib/editorize/start/patchConfiguration/AttributeConfigurationGenerator/fillMandatoryValueOfNumericAttributes.js
@@ -1,8 +1,6 @@
 import clone from '../clone'
 import {
   DEFAULT,
-  MAX,
-  MIN,
   STEP
 } from '../../../AttributeDefinitionContainer/createAttributeDefinition/NumericAttributeDefinition'
 
@@ -12,12 +10,6 @@ export default function (config) {
   for (const a of config.filter((a) => a['value type'] === 'numeric')) {
     if (!Object.prototype.hasOwnProperty.call(a, 'default')) {
       a.default = DEFAULT
-    }
-    if (!Object.prototype.hasOwnProperty.call(a, 'min')) {
-      a.min = MIN
-    }
-    if (!Object.prototype.hasOwnProperty.call(a, 'max')) {
-      a.max = MAX
     }
     if (!Object.prototype.hasOwnProperty.call(a, 'step')) {
       a.step = STEP
