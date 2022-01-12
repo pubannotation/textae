bf9a66214301c87f42352e57738d37455a261cce
ledsun <shigeru.nakajima@gmail.com>
2022-01-12 17:41:15 +0900

    refactor: Delete old comments

--- src/lib/editorize/AnnotationData/SpanModelContainer/getHeightIncludeDescendantGrids/getGridHeight/getAttributesHeight.js
+++ src/lib/editorize/AnnotationData/SpanModelContainer/getHeightIncludeDescendantGrids/getGridHeight/getAttributesHeight.js
@@ -1,5 +1,4 @@
 export default function getAttributesHeight(entities) {
-  // The number of attributes for all entities of the same type is the same, as different attributes have different types.
   return entities
     .map((entity) => entity.attributesHeight)
     .reduce((sum, heght) => sum + heght, 0)
