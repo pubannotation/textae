29cd480593d26684b58a803c623e1af2550441d3
ledsun <shigeru.nakajima@gmail.com>
2022-01-13 10:28:27 +0900

    refactor: Show media attribute definitions in the pallet

--- src/lib/component/TypeValuesPallet/createContentHtml/index.js
+++ src/lib/component/TypeValuesPallet/createContentHtml/index.js
@@ -3,6 +3,7 @@ import flagAttributeTemplate from './flagAttributeTemplate'
 import numericAttributeTemplate from './numericAttributeTemplate'
 import selectionAttributeTemplate from './selectionAttributeTemplate'
 import stringAttributeTemplate from './stringAttributeTemplate'
+import mediaAttributeTemplate from './mediaAttributeTemplate'
 
 export default function (
   types,
@@ -62,6 +63,8 @@ export default function (
       return selectionAttributeTemplate(context, attributeContainer)
     case 'string':
       return stringAttributeTemplate(context)
+    case 'media':
+      return mediaAttributeTemplate(context)
     default:
       throw `attrDef.valueType is unknown attribute`
   }
