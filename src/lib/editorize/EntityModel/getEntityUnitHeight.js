aefadabbad4e7e8c42e32a5875f270335c162c82
ledsun <shigeru.nakajima@gmail.com>
2022-01-18 11:57:15 +0900

    refactor: Add a JSDoc comment

--- src/lib/editorize/EntityModel/getEntityUnitHeight.js
+++ src/lib/editorize/EntityModel/getEntityUnitHeight.js
@@ -2,6 +2,11 @@ import typeGapUnitHeight from '../typeGapUnitHeight'
 
 const labelUnitHegiht = 18
 
+/**
+ *
+ * @param {number} typeGap
+ * @returns
+ */
 export default function (typeGap) {
   return typeGap * typeGapUnitHeight + labelUnitHegiht
 }
