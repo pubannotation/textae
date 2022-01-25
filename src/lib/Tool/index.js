4f162538cefdd8a23dfa10997dc8de4dc96d34be
ledsun <shigeru.nakajima@gmail.com>
2022-01-25 17:00:07 +0900

    refactor: Inline the redrawOnResize

--- src/lib/Tool/index.js
+++ src/lib/Tool/index.js
@@ -1,20 +1,25 @@
 import EditorContainer from './EditorContainer'
 import selectUnselectEditorOn from './selectUnselectEditorOn'
-import redrawOnResize from './redrawOnResize'
 import Veil from './Veil'
+import throttle from 'throttleit'
 
 // The tool manages interactions between components.
 export default class Tool {
   constructor() {
     this._editors = new EditorContainer()
     this._veil = new Veil()
-    redrawOnResize(this._editors)
 
     // When the DOMContentLoaded event occurs, document.body may not have been initialized yet.
     // When the load event occurs, bind the event handler of document.body.
     window.addEventListener('load', () => {
       selectUnselectEditorOn(this._editors)
     })
+
+    // Observe window-resize event and redraw all editors.
+    window.addEventListener(
+      'resize',
+      throttle(() => this._editors.redraw(), 500)
+    )
   }
 
   registerEditor(editor) {
