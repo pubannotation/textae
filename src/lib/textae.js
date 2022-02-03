9eb93dda8956dff083cc06750ab7d57be626dcb5
ledsun <shigeru.nakajima@gmail.com>
2022-02-03 22:22:02 +0900

    refactor: Generate editor id befere initiate it

--- src/lib/textae.js
+++ src/lib/textae.js
@@ -6,11 +6,11 @@ const tool = new Tool()
 
 export default function () {
   for (const self of document.querySelectorAll('.textae-editor')) {
-    // Create an editor
     const $this = $(self)
-    const $e = editorize($this)
     // Register an editor
-    tool.registerEditor($e)
+    tool.registerEditor($this)
+    // Create an editor
+    const $e = editorize($this)
     // Start an editor
     $e.start($e)
   }
