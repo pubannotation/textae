81a693a90ebbf951300f57a8769ee885329a4dee
ledsun <shigeru.nakajima@gmail.com>
2022-02-04 00:08:58 +0900

    refactor: Delete an unused module

--- src/lib/editorize/start/getStatusBar.js
+++
@@ -1,5 +0,0 @@
-import StatusBar from '../../component/StatusBar'
-
-export default function (editorHTMLElement, parameterValue) {
-  return new StatusBar(editorHTMLElement, parameterValue === 'on')
-}
