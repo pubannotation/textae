9bb67ab3371678438cba10b633c303051252e6ca
ledsun <shigeru.nakajima@gmail.com>
2022-02-04 17:58:29 +0900

    refactor: Rename variables

--- src/lib/textae.js
+++ src/lib/textae.js
@@ -5,12 +5,12 @@ import editorize from './editorize'
 const tool = new Tool()
 
 export default function () {
-  for (const self of document.querySelectorAll('.textae-editor')) {
+  for (const element of document.querySelectorAll('.textae-editor')) {
     // Create an editor
-    const $this = $(self)
+    const $this = $(element)
     $this.editorID = tool.nextID
     editorize($this)
     // Register an editor
-    tool.registerEditor($this, self)
+    tool.registerEditor($this, element)
   }
 }
