2ece1d4f0fdbe83d2379e6d23e40e47f9de45778
ledsun <shigeru.nakajima@gmail.com>
2022-02-04 17:44:44 +0900

    refactor: Delete the selectEditor

--- src/lib/Tool/selectEditor.js
+++
@@ -1,8 +0,0 @@
-export default function (editors, editor) {
-  if (editor) {
-    if (editors.selected && editor[0] !== editors.selected[0]) {
-      editors.unselect(editors.selected)
-    }
-    editors.selected = editor
-  }
-}
