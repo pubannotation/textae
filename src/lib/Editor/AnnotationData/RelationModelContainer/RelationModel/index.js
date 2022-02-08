5ade28c4c434c5f8ff3ac7d3add2872db9223acb
ledsun <shigeru.nakajima@gmail.com>
2022-02-08 12:13:33 +0900

    refactor: Add a comment

--- src/lib/Editor/AnnotationData/RelationModelContainer/RelationModel/index.js
+++ src/lib/Editor/AnnotationData/RelationModelContainer/RelationModel/index.js
@@ -95,6 +95,9 @@ export default class RelationModel {
   select() {
     if (this._connection && !this._isSelected) {
       this._isSelected = true
+      // When we select a relation,
+      // it is hovering and we have already highlighted the line,
+      // so we only need to update the label.
       this._connection.updateLabelHighlighting()
     }
   }
