4f96d23afa73b77bb0bfa3b0b96d6f75a4bfb74d
ledsun <shigeru.nakajima@gmail.com>
2022-02-22 14:41:36 +0900

    refactor: Add JSDoc

--- src/lib/Editor/AnnotationData/RelationModelContainer/RelationModel/Connection/Arrow/moveJetty.js
+++ src/lib/Editor/AnnotationData/RelationModelContainer/RelationModel/Connection/Arrow/moveJetty.js
@@ -1,5 +1,12 @@
 import { MarkerHeight } from './MarkerHeight'
 
+/**
+ *
+ * @param {Element} jetty
+ * @param {number} x
+ * @param {number} y
+ * @param {import('../../../../../EntityModel').default} entity
+ */
 export default function (jetty, x, y, entity) {
   jetty.setAttribute(
     'points',
