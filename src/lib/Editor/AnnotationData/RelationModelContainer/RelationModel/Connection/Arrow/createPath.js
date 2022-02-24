44f78af23c22ce66888afb6f4da48720d8b68a6b
ledsun <shigeru.nakajima@gmail.com>
2022-02-24 17:43:41 +0900

    refactor: Add JSDoc

--- src/lib/Editor/AnnotationData/RelationModelContainer/RelationModel/Connection/Arrow/createPath.js
+++ src/lib/Editor/AnnotationData/RelationModelContainer/RelationModel/Connection/Arrow/createPath.js
@@ -1,5 +1,9 @@
 import { NS } from '../NS'
 
+/**
+ *
+ * @returns {SVGGraphicsElement} path
+ */
 export default function () {
   const path = document.createElementNS(NS.SVG, 'path')
   path.classList.add('textae-editor__relation')
