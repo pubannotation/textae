f3f92427732ca6461d3360e0bff0d08ae3ed680b
ledsun <shigeru.nakajima@gmail.com>
2022-03-03 17:47:21 +0900

    refactor: Use PathPoints._isBentOnTargetSide property

--- src/lib/Editor/AnnotationData/RelationModelContainer/RelationModel/Connection/Arrow/PathPoints/index.js
+++ src/lib/Editor/AnnotationData/RelationModelContainer/RelationModel/Connection/Arrow/PathPoints/index.js
@@ -57,7 +57,7 @@ export default class PathPoints {
   }
 
   get pathCommands() {
-    if (this.targetControlX !== this.targetX) {
+    if (this._isBentOnTargetSide) {
       return `M ${this.sourceX}, ${this.sourceY}
               C ${this.sourceControlX} ${this.controlY}, ${this.targetControlX} ${this.controlY}, ${this._junctionPointX} ${this._junctionPointY}
               Q ${this.targetX} ${this._additionalControlY}, ${this.targetX} ${this.targetY}`
@@ -88,7 +88,7 @@ export default class PathPoints {
     // (1−t)3P1 + 3(1−t)2tP2 +3(1−t)t2P3 + t3P4
     const { sourceY, targetY, controlY } = this
 
-    if (this.targetControlX !== this.targetX) {
+    if (this._isBentOnTargetSide) {
       return [...Array(sample).keys()]
         .map((i) => (i * 1) / sample)
         .find((t) => {
@@ -130,7 +130,7 @@ export default class PathPoints {
   getXOnT(_t) {
     const { sourceX, targetX, sourceControlX, targetControlX } = this
 
-    if (this.targetControlX !== this.targetX) {
+    if (this._isBentOnTargetSide) {
       return (
         Math.pow(1 - _t, 3) * sourceX +
         3 * Math.pow(1 - _t, 2) * _t * sourceControlX +
@@ -148,7 +148,7 @@ export default class PathPoints {
   }
 
   get _additionalControlY() {
-    if (this.targetControlX !== this.targetX) {
+    if (this._isBentOnTargetSide) {
       return this.sourceY * 0.3 + this.targetY * 0.7
     }
 
@@ -160,7 +160,7 @@ export default class PathPoints {
   }
 
   get _junctionPointX() {
-    if (this.targetControlX !== this.targetX) {
+    if (this._isBentOnTargetSide) {
       return this.targetControlX * 0.25 + this.targetX * 0.75
     }
 
@@ -172,7 +172,7 @@ export default class PathPoints {
   }
 
   get _junctionPointY() {
-    if (this.targetControlX !== this.targetX) {
+    if (this._isBentOnTargetSide) {
       return this.controlY * 0.25 + this._additionalControlY * 0.75
     }
 
