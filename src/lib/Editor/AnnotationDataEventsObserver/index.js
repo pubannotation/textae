4a0563ddc0ba7194be3cf7feae1f3c202163e1ea
ledsun <shigeru.nakajima@gmail.com>
2022-04-06 18:08:51 +0900

    feature: When multi-track or broken annotations are read, they are not treated as unsaved annotations.

--- src/lib/Editor/AnnotationDataEventsObserver/index.js
+++ src/lib/Editor/AnnotationDataEventsObserver/index.js
@@ -13,27 +13,13 @@ export default class AnnotationDataEventsObserver {
     this._annotationData = annotationData
     this._observable = new Observable(false)
 
-    // Fix loading annotation automatically when loading multitrack or broken annotation.
-    // That is differnt with data on the serever.
-    // So even if no changes at the editor, there is something to save to the server.
-    this._loadedAnnotationIsModified = false
-
     eventEmitter
       .on('textae-event.resource.annotation.save', () => {
         this._observable.set(false)
         this._loadedAnnotationIsModified = false
       })
-      .on(
-        'textae-event.annotation-data.all.change',
-        (_, multitrack, hasError) => {
-          if (multitrack || hasError) {
-            this._observable.set(true)
-            this._loadedAnnotationIsModified = true
-          } else {
-            this._observable.set(false)
-            this._loadedAnnotationIsModified = false
-          }
-        }
+      .on('textae-event.annotation-data.all.change', (_) =>
+        this._observable.set(false)
       )
       .on('textae-event.annotation-data.span.add', () => this._updateState())
       .on('textae-event.annotation-data.span.change', () => this._updateState())
@@ -76,8 +62,7 @@ export default class AnnotationDataEventsObserver {
 
   _updateState() {
     this._observable.set(
-      this._loadedAnnotationIsModified ||
-        diffOfAnnotation(this._originalData, this._annotationData)
+      diffOfAnnotation(this._originalData, this._annotationData)
     )
   }
 }
