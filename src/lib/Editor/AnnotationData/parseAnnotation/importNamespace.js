d6b665382dd7c0858dad657a88af45b5681c1565
ledsun <shigeru.nakajima@gmail.com>
2022-04-28 13:45:13 +0900

    refactor: Inline importSource module.

--- src/lib/Editor/AnnotationData/parseAnnotation/importNamespace.js
+++ src/lib/Editor/AnnotationData/parseAnnotation/importNamespace.js
@@ -1,5 +1,3 @@
-import importSource from './importSource'
-
 export default function (destination, source) {
   // Clone source to prevet changing orignal data.
   importSource(
@@ -7,4 +5,14 @@ export default function (destination, source) {
     (namespace) => ({ id: namespace.prefix, ...namespace }),
     source
   )
+}
+
+function importSource(targets, translater, source, type) {
+  if (source) {
+    source = source.map(translater)
+  }
+
+  for (const target of targets) {
+    target.addSource(source, type)
+  }
 }
