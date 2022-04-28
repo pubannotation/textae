484dd4de7738edc154864f308561d681981afe59
ledsun <shigeru.nakajima@gmail.com>
2022-04-28 15:12:15 +0900

    refactor: Use destructuring assignment

--- src/lib/Editor/AnnotationData/parseAnnotation/parseTrack/index.js
+++ src/lib/Editor/AnnotationData/parseAnnotation/parseTrack/index.js
@@ -14,35 +14,33 @@ export default function (
   rowData,
   trackNumber = ''
 ) {
-  const result = validateAnnotation(text, spans, rowData)
+  const { accept, reject } = validateAnnotation(text, spans, rowData)
 
-  const denotations = result.accept.denotation.map((src) =>
+  const denotations = accept.denotation.map((src) =>
     translateSpan(src, trackNumber)
   )
   spanContainer.addSource(denotations, 'denotation')
   entityContainer.addSource(denotations, 'denotation')
 
-  const attributes = result.accept.attribute.map((src) =>
+  const attributes = accept.attribute.map((src) =>
     translateAttribute(src, trackNumber)
   )
   attributeContainer.addSource(attributes)
 
-  const relations = result.accept.relation.map((src) =>
+  const relations = accept.relation.map((src) =>
     translateRelation(src, trackNumber)
   )
   relationContainer.addSource(relations)
 
-  const typesettings = result.accept.typeSetting.map((src) => ({
+  const typesettings = accept.typeSetting.map((src) => ({
     ...src,
     span: convertBeginAndEndToInteger(src.span)
   }))
   spanContainer.addSource(typesettings, 'typesetting')
 
-  const blocks = result.accept.block.map((src) =>
-    translateSpan(src, trackNumber)
-  )
+  const blocks = accept.block.map((src) => translateSpan(src, trackNumber))
   spanContainer.addSource(blocks, 'block')
   entityContainer.addSource(blocks, 'block')
 
-  return result.reject
+  return reject
 }
