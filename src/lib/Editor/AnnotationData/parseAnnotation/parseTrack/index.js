cddb7c0941aef111dbdb3789c0e50274fb966328
ledsun <shigeru.nakajima@gmail.com>
2022-04-28 15:14:22 +0900

    refactor: Rearrange import order

--- src/lib/Editor/AnnotationData/parseAnnotation/parseTrack/index.js
+++ src/lib/Editor/AnnotationData/parseAnnotation/parseTrack/index.js
@@ -16,31 +16,31 @@ export default function (
 ) {
   const { accept, reject } = validateAnnotation(text, spans, rowData)
 
-  const denotations = accept.denotation.map((src) =>
-    translateSpan(src, trackNumber)
-  )
-  spanContainer.addSource(denotations, 'denotation')
-  entityContainer.addSource(denotations, 'denotation')
-
-  const attributes = accept.attribute.map((src) =>
-    translateAttribute(src, trackNumber)
-  )
-  attributeContainer.addSource(attributes)
-
-  const relations = accept.relation.map((src) =>
-    translateRelation(src, trackNumber)
-  )
-  relationContainer.addSource(relations)
-
   const typesettings = accept.typeSetting.map((src) => ({
     ...src,
     span: convertBeginAndEndToInteger(src.span)
   }))
   spanContainer.addSource(typesettings, 'typesetting')
 
+  const denotations = accept.denotation.map((src) =>
+    translateSpan(src, trackNumber)
+  )
+  spanContainer.addSource(denotations, 'denotation')
+  entityContainer.addSource(denotations, 'denotation')
+
   const blocks = accept.block.map((src) => translateSpan(src, trackNumber))
   spanContainer.addSource(blocks, 'block')
   entityContainer.addSource(blocks, 'block')
+
+  const relations = accept.relation.map((src) =>
+    translateRelation(src, trackNumber)
+  )
+  relationContainer.addSource(relations)
+
+  const attributes = accept.attribute.map((src) =>
+    translateAttribute(src, trackNumber)
+  )
+  attributeContainer.addSource(attributes)
 
   return reject
 }
