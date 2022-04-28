306f51a29812d04e8cf3c9936263947f6930894a
ledsun <shigeru.nakajima@gmail.com>
2022-04-28 15:05:22 +0900

    refactor: Delete the ImportSource module

--- src/lib/Editor/AnnotationData/parseAnnotation/importSource.js
+++
@@ -1,9 +0,0 @@
-export default function (targets, translater, source, type) {
-  if (source) {
-    source = source.map(translater)
-  }
-
-  for (const target of targets) {
-    target.addSource(source, type)
-  }
-}
