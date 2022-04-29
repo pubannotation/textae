3d0095b0d5cd41f09bbd66c16022a5032485a01f
ledsun <shigeru.nakajima@gmail.com>
2022-04-30 05:32:55 +0900

    refactor: Inline convertBeginAndEndToInteger module

--- src/lib/Editor/AnnotationData/parseAnnotation/parseTrack/convertBeginAndEndToInteger.js
+++
@@ -1,7 +0,0 @@
-// If the begin or end value is a string,
-// the comparison with other numbers cannot be done correctly.
-// You cannot generate a valid value for the ID of HTML element of span
-// from a begin or end that contains a decimal point.
-export default function (span) {
-  return { ...span, begin: parseInt(span.begin), end: parseInt(span.end) }
-}
--- src/lib/Editor/AnnotationData/parseAnnotation/parseTrack/index.js
+++ src/lib/Editor/AnnotationData/parseAnnotation/parseTrack/index.js
@@ -1,5 +1,4 @@
 import validateAnnotation from './validateAnnotation'
-import convertBeginAndEndToInteger from './convertBeginAndEndToInteger'
 
 export default function (
   spanContainer,
@@ -57,4 +56,12 @@ export default function (
   attributeContainer.addSource(attributes)
 
   return reject
+}
+
+// If the begin or end value is a string,
+// the comparison with other numbers cannot be done correctly.
+// You cannot generate a valid value for the ID of HTML element of span
+// from a begin or end that contains a decimal point.
+function convertBeginAndEndToInteger(span) {
+  return { ...span, begin: parseInt(span.begin), end: parseInt(span.end) }
 }
