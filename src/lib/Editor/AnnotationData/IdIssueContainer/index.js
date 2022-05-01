465beb8ed51e9298edca46ccb43fe4dc9968678f
ledsun <shigeru.nakajima@gmail.com>
2022-05-01 23:05:18 +0900

    refactor: Make getNexID function a function

--- src/lib/Editor/AnnotationData/IdIssueContainer/index.js
+++ src/lib/Editor/AnnotationData/IdIssueContainer/index.js
@@ -31,29 +31,29 @@ export default class IdIssueContainer extends ModelContainer {
 
   _assignID(instance) {
     if (!instance.id) {
-      // Overwrite to revert
-      const existingIDs = this._container.keys()
-      const newId = getNextID(this._prefixFunc(instance), existingIDs)
-      instance.id = newId
+      instance.id = this._getNextID(instance)
     }
+
     return instance
   }
-}
 
-function getNextID(prefix, existingIDs) {
-  // The format of id is a prefix and a number, for exapmle 'T1'.
-  const wellFormattedIDs = new Set()
-  for (const id of existingIDs) {
-    if (new RegExp(`^${prefix}\\d+$`).test(id)) {
-      wellFormattedIDs.add(id.slice(1))
+  _getNextID(instance) {
+    const prefix = this._prefixFunc(instance)
+
+    // The format of id is a prefix and a number, for exapmle 'T1'.
+    const wellFormattedIDs = new Set()
+    for (const id of this._container.keys()) {
+      if (new RegExp(`^${prefix}\\d+$`).test(id)) {
+        wellFormattedIDs.add(id.slice(1))
+      }
     }
-  }
 
-  // The Math.max retrun -Infinity when the second argument array is empty.
-  if (wellFormattedIDs.size === 0) {
-    return `${prefix}1`
-  }
+    // The Math.max retrun -Infinity when the second argument array is empty.
+    if (wellFormattedIDs.size === 0) {
+      return `${prefix}1`
+    }
 
-  const max = Math.max(...wellFormattedIDs.values())
-  return `${prefix}${max + 1}`
+    const max = Math.max(...wellFormattedIDs.values())
+    return `${prefix}${max + 1}`
+  }
 }
