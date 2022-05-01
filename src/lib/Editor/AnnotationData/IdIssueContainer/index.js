a9e49735e4d74b26c89c3b0913752c5b0f914ac2
ledsun <shigeru.nakajima@gmail.com>
2022-05-01 22:44:41 +0900

    refactor: Inline the getNextId module

--- src/lib/Editor/AnnotationData/IdIssueContainer/index.js
+++ src/lib/Editor/AnnotationData/IdIssueContainer/index.js
@@ -1,5 +1,4 @@
 import ModelContainer from '../ModelContainer'
-import getNextId from './getNextId'
 
 export default class IdIssueContainer extends ModelContainer {
   constructor(emitter, name, prefixFunc) {
@@ -39,4 +38,16 @@ export default class IdIssueContainer extends ModelContainer {
     }
     return instance
   }
+}
+
+function getNextId(prefix, existsIds) {
+  // The format of id is a prefix and a number, for exapmle 'T1'.
+  const numbers = existsIds
+    .filter((id) => new RegExp(`^${prefix}\\d+$`).test(id))
+    .map((id) => id.slice(1))
+
+  // The Math.max retrun -Infinity when the second argument array is empty.
+  const max = numbers.length === 0 ? 0 : Math.max(...numbers)
+
+  return prefix + (max + 1)
 }
