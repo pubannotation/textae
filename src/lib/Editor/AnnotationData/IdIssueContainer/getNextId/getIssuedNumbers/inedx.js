7a224e17cf735226afa7218552994b66de0afe26
ledsun <shigeru.nakajima@gmail.com>
2022-05-01 22:41:48 +0900

    refactor: Inline the getIssuedNumbers module

--- src/lib/Editor/AnnotationData/IdIssueContainer/getNextId/getIssuedNumbers/inedx.js
+++
@@ -1,6 +0,0 @@
-export default function (ids, prefix) {
-  // The format of id is a prefix and a number, for exapmle 'T1'.
-  return ids
-    .filter((id) => new RegExp(`^${prefix}\\d+$`).test(id))
-    .map((id) => id.slice(1))
-}
--- src/lib/Editor/AnnotationData/IdIssueContainer/getNextId/index.js
+++ src/lib/Editor/AnnotationData/IdIssueContainer/getNextId/index.js
@@ -1,8 +1,11 @@
-import getIssuedNumbers from './getIssuedNumbers/inedx'
 import getNextNumber from './getNextNumber'
 
 export default function (prefix, existsIds) {
-  const numbers = getIssuedNumbers(existsIds, prefix)
+  // The format of id is a prefix and a number, for exapmle 'T1'.
+  const numbers = existsIds
+    .filter((id) => new RegExp(`^${prefix}\\d+$`).test(id))
+    .map((id) => id.slice(1))
+
   const nextNumber = getNextNumber(numbers)
 
   return prefix + nextNumber
