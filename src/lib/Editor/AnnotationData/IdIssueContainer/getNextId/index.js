e9cab6e81ddf2e41da43b008d49045dccecdf1a8
ledsun <shigeru.nakajima@gmail.com>
2022-05-01 22:43:14 +0900

    refactor: Inline the getNextNumber module

--- src/lib/Editor/AnnotationData/IdIssueContainer/getNextId/index.js
+++ src/lib/Editor/AnnotationData/IdIssueContainer/getNextId/index.js
@@ -1,12 +1,11 @@
-import getNextNumber from './getNextNumber'
-
 export default function (prefix, existsIds) {
   // The format of id is a prefix and a number, for exapmle 'T1'.
   const numbers = existsIds
     .filter((id) => new RegExp(`^${prefix}\\d+$`).test(id))
     .map((id) => id.slice(1))
 
-  const nextNumber = getNextNumber(numbers)
+  // The Math.max retrun -Infinity when the second argument array is empty.
+  const max = numbers.length === 0 ? 0 : Math.max(...numbers)
 
-  return prefix + nextNumber
+  return prefix + (max + 1)
 }
