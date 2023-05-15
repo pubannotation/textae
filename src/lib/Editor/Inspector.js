9fc9f423a57384ae5c8d921d8c504215a77c710b
ledsun <shigeru.nakajima@gmail.com>
2023-05-15 14:23:09 +0900

    feature: Do not generate HTML elements for the write target of the inspect attribute.
    BREAKING CHANGE:
    If HTML elements are generated dynamically in TextAE, it becomes difficult to set up event handlers for those HTML elements.
    Create the HTML element to which the inspect attribute is output outside of TextAE.
    The easiest way is to predefine it in the HTML file.

--- src/lib/Editor/Inspector.js
+++ src/lib/Editor/Inspector.js
@@ -1,20 +1,16 @@
-import dohtml from 'dohtml'
-
 export default class Inspector {
   constructor(sourceElement, id, eventEmitter) {
-    const destinationElement = dohtml.create(
-      `<div id="${id}" style="display: none;"></div>`
-    )
-    sourceElement.insertAdjacentElement('afterend', destinationElement)
-
     eventEmitter.on(
       'textae-event.annotation-data.events-observer.change',
       (annotationData) => {
-        destinationElement.textContent = JSON.stringify(
-          annotationData.JSON,
-          null,
-          2
-        )
+        const destinationElement = document.querySelector(`#${id}`)
+        if (destinationElement) {
+          destinationElement.textContent = JSON.stringify(
+            annotationData.JSON,
+            null,
+            2
+          )
+        }
       }
     )
   }
