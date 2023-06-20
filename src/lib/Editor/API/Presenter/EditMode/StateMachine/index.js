95503792c3cdc7df36e64ff5aa5a2872926c925f
ledsun <shigeru.nakajima@gmail.com>
2023-06-20 17:46:20 +0900

    refactor: Remove state machine

--- src/lib/Editor/API/Presenter/EditMode/StateMachine/index.js
+++ src/lib/Editor/API/Presenter/EditMode/StateMachine/index.js
@@ -1,6 +1,4 @@
-import Machine from 'emitter-fsm'
 import { MODE } from '../../../../../MODE'
-import bindTransition from './bindTransition'
 
 export default class StateMachine {
   /**
@@ -9,82 +7,6 @@ export default class StateMachine {
    * @param {import('../Transition').default} transition
    */
   constructor(relationContainer, transition) {
-    const m = new Machine({
-      states: [
-        MODE.INIT,
-        MODE.EDIT_DENOTATION_WITHOUT_RELATION,
-        MODE.EDIT_DENOTATION_WITH_RELATION,
-        MODE.EDIT_BLOCK_WITHOUT_RELATION,
-        MODE.EDIT_BLOCK_WITH_RELATION,
-        MODE.EDIT_RELATION,
-        MODE.VIEW_WITHOUT_RELATION,
-        MODE.VIEW_WITH_RELATION
-      ]
-    })
-
-    m.config(MODE.EDIT_DENOTATION_WITHOUT_RELATION, {
-      from: {
-        exclude: [MODE.VIEW_WITH_RELATION]
-      },
-      to: {
-        exclude: [MODE.INIT, MODE.VIEW_WITH_RELATION]
-      }
-    })
-
-    m.config(MODE.EDIT_DENOTATION_WITH_RELATION, {
-      from: {
-        exclude: [MODE.VIEW_WITHOUT_RELATION]
-      },
-      to: {
-        exclude: [MODE.INIT, MODE.VIEW_WITHOUT_RELATION]
-      }
-    })
-
-    m.config(MODE.EDIT_BLOCK_WITHOUT_RELATION, {
-      from: {
-        exclude: [MODE.VIEW_WITH_RELATION]
-      },
-      to: {
-        exclude: [MODE.INIT, MODE.VIEW_WITH_RELATION]
-      }
-    })
-
-    m.config(MODE.EDIT_BLOCK_WITH_RELATION, {
-      from: {
-        exclude: [MODE.VIEW_WITHOUT_RELATION]
-      },
-      to: {
-        exclude: [MODE.INIT, MODE.VIEW_WITHOUT_RELATION]
-      }
-    })
-
-    m.config(MODE.EDIT_RELATION, {
-      to: {
-        exclude: [MODE.INIT]
-      }
-    })
-
-    m.config(MODE.VIEW_WITHOUT_RELATION, {
-      from: {
-        exclude: [MODE.EDIT_DENOTATION_WITH_RELATION]
-      },
-      to: {
-        exclude: [MODE.INIT, MODE.EDIT_DENOTATION_WITH_RELATION]
-      }
-    })
-
-    m.config(MODE.VIEW_WITH_RELATION, {
-      from: {
-        exclude: [MODE.EDIT_DENOTATION_WITHOUT_RELATION]
-      },
-      to: {
-        exclude: [MODE.INIT, MODE.EDIT_DENOTATION_WITHOUT_RELATION]
-      }
-    })
-
-    bindTransition(m, transition)
-
-    this._m = m
     this._relationContainer = relationContainer
     this._showRelation = false
     this._transition = transition
