40a50d5cf07d7e7d60d011115e0d4fb18c2db4e5
ledsun <shigeru.nakajima@gmail.com>
2023-06-21 13:40:43 +0900

    refactor: Inline Transition

--- src/lib/Editor/API/Presenter/EditMode/StateMachine.js
+++ src/lib/Editor/API/Presenter/EditMode/StateMachine.js
@@ -1,5 +1,5 @@
 import { MODE } from '../../../../MODE'
-import Transition from './Transition'
+import EditorCSSClassForMode from './Transition/EditorCSSClassForMode'
 
 export default class StateMachine {
   /**
@@ -19,15 +19,13 @@ export default class StateMachine {
   ) {
     this._relationContainer = relationContainer
     this._currentShowRelation = false
-    this._transition = new Transition(
-      eventEmitter,
-      editorHTMLElement,
-      typeGap,
-      noEdit,
-      editEntity,
-      editBlock,
-      editRelation
-    )
+    this._eventEmitter = eventEmitter
+    this._editorCSSClassForMode = new EditorCSSClassForMode(editorHTMLElement)
+    this._typeGap = typeGap
+    this._noEdit = noEdit
+    this._editEntity = editEntity
+    this._editBlock = editBlock
+    this._editRelation = editRelation
     this._currentState = MODE.INIT
   }
 
@@ -38,24 +36,72 @@ export default class StateMachine {
   toViewMode(showRelation) {
     this._currentShowRelation = showRelation
     this._currentState = MODE.VIEW
-    this._transition.toView(showRelation)
+    this._typeGap.show = showRelation
+    this._eventEmitter.emit(
+      'textae-event.edit-mode.transition',
+      MODE.VIEW,
+      showRelation
+    )
+
+    this._noEdit()
+    if (showRelation) {
+      this._editorCSSClassForMode.setViewWithRelation()
+    } else {
+      this._editorCSSClassForMode.setViewWithoutRelation()
+    }
   }
 
   toTermMode(showRelation) {
     this._currentShowRelation = showRelation
     this._currentState = MODE.EDIT_DENOTATION
-    this._transition.toEditDenotation(showRelation)
+    this._typeGap.show = showRelation
+    this._eventEmitter.emit(
+      'textae-event.edit-mode.transition',
+      MODE.EDIT_DENOTATION,
+      showRelation
+    )
+
+    this._noEdit()
+    this._editEntity()
+    if (showRelation) {
+      this._editorCSSClassForMode.setDenotationWithRelation()
+    } else {
+      this._editorCSSClassForMode.setDenotationWithoutRelation()
+    }
   }
 
   toBlockMode(showRelation) {
     this._currentShowRelation = showRelation
     this._currentState = MODE.EDIT_BLOCK
-    this._transition.toEditBlock(showRelation)
+    this._typeGap.show = showRelation
+    this._eventEmitter.emit(
+      'textae-event.edit-mode.transition',
+      MODE.EDIT_BLOCK,
+      showRelation
+    )
+
+    this._noEdit()
+    this._editBlock()
+
+    if (showRelation) {
+      this._editorCSSClassForMode.setBlockWithRelation()
+    } else {
+      this._editorCSSClassForMode.setBlockWithoutRelation()
+    }
   }
 
   toRelationMode() {
     this._currentState = MODE.EDIT_RELATION
-    this._transition.toEditRelation()
+    this._typeGap.show = true
+    this._eventEmitter.emit(
+      'textae-event.edit-mode.transition',
+      MODE.EDIT_RELATION,
+      true
+    )
+
+    this._noEdit()
+    this._editRelation()
+    this._editorCSSClassForMode.setRelation()
   }
 
   toggleSimpleMode() {
