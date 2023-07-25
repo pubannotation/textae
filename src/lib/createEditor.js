365a7e6ea02019b79dddfa2bb30d9589d636d51f
ledsun <shigeru.nakajima@gmail.com>
2023-07-25 15:13:32 +0900

    refactor: Extract a module

---
+++ src/lib/createEditor.js
@@ -0,0 +1,25 @@
+import Editor from './Editor'
+
+export default function createEditor(element, tool) {
+  return new Editor(
+    element,
+    tool.nextID,
+    () => {
+      // jQuery Ui dialogs are not in the editor.
+      for (const dialog of document.querySelectorAll('.ui-dialog')) {
+        dialog.classList.add('textae-editor--wait')
+      }
+      for (const dialog of document.querySelectorAll('.ui-widget-overlay')) {
+        dialog.classList.add('textae-editor--wait')
+      }
+    },
+    () => {
+      for (const dialog of document.querySelectorAll('.ui-dialog')) {
+        dialog.classList.remove('textae-editor--wait')
+      }
+      for (const dialog of document.querySelectorAll('.ui-widget-overlay')) {
+        dialog.classList.remove('textae-editor--wait')
+      }
+    }
+  )
+}
