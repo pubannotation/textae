d1e955a0e2365a0f2b75e2628952726ff28a7a02
ledsun <shigeru.nakajima@gmail.com>
2023-07-25 17:47:45 +0900

    feature: Add an API to set up a callback function to detect changes in JSON data.
    The name of this API is tentative.

--- src/lib/textae.js
+++ src/lib/textae.js
@@ -25,6 +25,9 @@ export default function () {
       // We plan to add information here that we would like to make available to the outside world.
       set annotation(annotation) {
         editor.load(annotation)
+      },
+      set inspectCallback(callback) {
+        editor.setInspector(callback)
       }
     }))
 }
