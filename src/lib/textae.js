7b863476398550cfbc402d72065b98d63a379f43
ledsun <shigeru.nakajima@gmail.com>
2023-07-25 15:07:24 +0900

    refactor: Use forEach
    It will make it easier to do the "replace the forEach function with a map function and return a return value" modification.

--- src/lib/textae.js
+++ src/lib/textae.js
@@ -8,35 +8,39 @@ export default function () {
   // Set position of toast messages.
   alertifyjs.set('notifier', 'position', 'top-right')
 
-  for (const element of document.querySelectorAll('.textae-editor')) {
-    if (element.dataset.textaeInitialized) continue
+  Array.from(document.querySelectorAll('.textae-editor'))
+    .filter((element) => !element.dataset.textaeInitialized)
+    .forEach((element) => {
+      // Create an editor
+      const editor = new Editor(
+        element,
+        tool.nextID,
+        () => {
+          // jQuery Ui dialogs are not in the editor.
+          for (const dialog of document.querySelectorAll('.ui-dialog')) {
+            dialog.classList.add('textae-editor--wait')
+          }
+          for (const dialog of document.querySelectorAll(
+            '.ui-widget-overlay'
+          )) {
+            dialog.classList.add('textae-editor--wait')
+          }
+        },
+        () => {
+          for (const dialog of document.querySelectorAll('.ui-dialog')) {
+            dialog.classList.remove('textae-editor--wait')
+          }
+          for (const dialog of document.querySelectorAll(
+            '.ui-widget-overlay'
+          )) {
+            dialog.classList.remove('textae-editor--wait')
+          }
+        }
+      )
+      // Register an editor
+      tool.registerEditor(element, editor)
 
-    // Create an editor
-    const editor = new Editor(
-      element,
-      tool.nextID,
-      () => {
-        // jQuery Ui dialogs are not in the editor.
-        for (const dialog of document.querySelectorAll('.ui-dialog')) {
-          dialog.classList.add('textae-editor--wait')
-        }
-        for (const dialog of document.querySelectorAll('.ui-widget-overlay')) {
-          dialog.classList.add('textae-editor--wait')
-        }
-      },
-      () => {
-        for (const dialog of document.querySelectorAll('.ui-dialog')) {
-          dialog.classList.remove('textae-editor--wait')
-        }
-        for (const dialog of document.querySelectorAll('.ui-widget-overlay')) {
-          dialog.classList.remove('textae-editor--wait')
-        }
-      }
-    )
-    // Register an editor
-    tool.registerEditor(element, editor)
-
-    // Mark as initiated.
-    element.dataset.textaeInitialized = true
-  }
+      // Mark as initiated.
+      element.dataset.textaeInitialized = true
+    })
 }
