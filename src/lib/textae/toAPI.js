d0db603ce8995f2f805a6b793265f45a439f2bd3
ledsun <shigeru.nakajima@gmail.com>
2023-07-26 15:48:37 +0900

    refactor: Add JSDoc

--- src/lib/textae/toAPI.js
+++ src/lib/textae/toAPI.js
@@ -1,3 +1,7 @@
+/**
+ * Convert the editor object to an object that can be used from the outside.
+ * @param {import('../Editor').default} editor
+ */
 export default function toAPI(editor) {
   return {
     // We plan to add information here that we would like to make available to the outside world.
