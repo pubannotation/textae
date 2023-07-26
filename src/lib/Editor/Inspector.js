ac9ba34498ca3b96f40aa8679beeb125e393be2e
ledsun <shigeru.nakajima@gmail.com>
2023-07-26 16:01:56 +0900

    refactor: Add Inspector:die method

--- src/lib/Editor/Inspector.js
+++ src/lib/Editor/Inspector.js
@@ -1,8 +1,18 @@
 export default class Inspector {
   constructor(eventEmitter, callback) {
+    this._eventEmitter = eventEmitter
+    this._listener = (annotationData) => callback(annotationData.JSON)
+
     eventEmitter.on(
       'textae-event.annotation-data.events-observer.change',
-      (annotationData) => callback(annotationData.JSON)
+      this._listener
+    )
+  }
+
+  die() {
+    this._eventEmitter.off(
+      'textae-event.annotation-data.events-observer.change',
+      this._listener
     )
   }
 }
