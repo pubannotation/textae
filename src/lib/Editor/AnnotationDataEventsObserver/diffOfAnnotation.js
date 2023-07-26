0879e5ee23ec9300c511b0fa6fe8c11ba963c19b
ledsun <shigeru.nakajima@gmail.com>
2023-07-26 17:19:39 +0900

    refactor: Add a function name

--- src/lib/Editor/AnnotationDataEventsObserver/diffOfAnnotation.js
+++ src/lib/Editor/AnnotationDataEventsObserver/diffOfAnnotation.js
@@ -1,6 +1,6 @@
 import { diff } from 'jsondiffpatch'
 
-export default function (originalData, annotationData) {
+export default function diffOfAnnotation(originalData, annotationData) {
   return diff(
     prepareDiff(originalData.annotation),
     prepareDiff(annotationData.JSON)
