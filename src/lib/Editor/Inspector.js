c1cc00f3265d5ae2d3023464e2950666032a9537
ledsun <shigeru.nakajima@gmail.com>
2023-07-27 11:34:39 +0900

    refactor: Rename a private field

--- src/lib/Editor/Inspector.js
+++ src/lib/Editor/Inspector.js
@@ -3,7 +3,7 @@ import diffOfAnnotation from './diffOfAnnotation'
 export default class Inspector {
   constructor(eventEmitter, callback, annotationData) {
     this._eventEmitter = eventEmitter
-    this._previousJSON = annotationData.externalFormat
+    this._previous = annotationData.externalFormat
     this._listener = (annotationData) => {
       // TextAE's internal data treats spans and entities separately.
       // On the other hand, in the external data, they are treated together as denotation.
@@ -12,8 +12,8 @@ export default class Inspector {
       // There is no change in denotation between these two events;
       // we want to be notified only when there is a change in denotation.
       // Notify only when there is a change by comparing with external data format.
-      if (diffOfAnnotation(this._previousJSON, annotationData.externalFormat)) {
-        this._previousJSON = annotationData.externalFormat
+      if (diffOfAnnotation(this._previous, annotationData.externalFormat)) {
+        this._previous = annotationData.externalFormat
         callback(annotationData.externalFormat)
       }
     }
