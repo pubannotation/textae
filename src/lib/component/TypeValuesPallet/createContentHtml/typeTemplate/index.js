30dd543bd41fc23f0ac23cee38917a07dace750b
ledsun <shigeru.nakajima@gmail.com>
2023-12-06 17:20:54 +0900

    refactor: Split file

---
+++ src/lib/component/TypeValuesPallet/createContentHtml/typeTemplate/index.js
@@ -0,0 +1,47 @@
+import headerTemplate from '../headerTemplate'
+import anemone from '../../../anemone'
+import toTypeRow from './toTypeRow'
+
+export default function (context) {
+  const { types, isLock } = context
+
+  return anemone`
+  ${() => headerTemplate(context)}
+  <table>
+    <tbody>
+      <tr>
+        <th>id</th>
+        <th>label</th>
+        <th title="Number of annotations.">#</th>
+        <th>
+          ${() =>
+            isLock
+              ? ''
+              : '<span class="textae-editor__pallet__add-button" title="Add new type"></span>'}
+        </th>
+      </tr>
+      ${() =>
+        types
+          ? types
+              .map(
+                ({ color = '', id, uri, defaultType, label = '', useNumber }) =>
+                  toTypeRow(
+                    color,
+                    id,
+                    uri,
+                    defaultType,
+                    label,
+                    useNumber,
+                    isLock
+                  )
+              )
+              .join('\n')
+          : `
+      <tr class="textae-editor__pallet__row">
+        <td class="textae-editor__pallet__no-config" colspan="4">There is no Entity definition.</td>
+      </tr>
+      `}
+    </tbody>
+  </table>
+  `
+}
