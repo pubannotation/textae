0b3e344a8a0ec9e333d75b549c9b4dfc97f04158
ledsun <shigeru.nakajima@gmail.com>
2023-12-06 11:11:23 +0900

    refactor: Use private field

--- src/lib/Editor/Inspector.js
+++ src/lib/Editor/Inspector.js
@@ -1,10 +1,14 @@
 import diffOfAnnotation from './diffOfAnnotation'
 
 export default class Inspector {
+  #eventEmitter
+  #previous
+  #listener
+
   constructor(eventEmitter, callback, annotationModel) {
-    this._eventEmitter = eventEmitter
-    this._previous = annotationModel.externalFormat
-    this._listener = (annotationModel) => {
+    this.#eventEmitter = eventEmitter
+    this.#previous = annotationModel.externalFormat
+    this.#listener = (annotationModel) => {
       // TextAE's internal data treats spans and entities separately.
       // On the other hand, in the external data, they are treated together as denotation.
       // For example, when a Span is added, an event is fired twice,
@@ -12,21 +16,21 @@ export default class Inspector {
       // There is no change in denotation between these two events;
       // we want to be notified only when there is a change in denotation.
       // Notify only when there is a change by comparing with external data format.
-      if (diffOfAnnotation(this._previous, annotationModel.externalFormat)) {
-        this._previous = annotationModel.externalFormat
+      if (diffOfAnnotation(this.#previous, annotationModel.externalFormat)) {
+        this.#previous = annotationModel.externalFormat
         callback(annotationModel.externalFormat)
       }
     }
     eventEmitter.on(
       'textae-event.annotation-data.events-observer.change',
-      this._listener
+      this.#listener
     )
   }
 
   die() {
-    this._eventEmitter.off(
+    this.#eventEmitter.off(
       'textae-event.annotation-data.events-observer.change',
-      this._listener
+      this.#listener
     )
   }
 }
