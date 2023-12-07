4ad3b690e17afb60bc753bc88737e66f7b3b9c77
ledsun <shigeru.nakajima@gmail.com>
2023-12-07 14:38:28 +0900

    refactor: Use private field and method

--- src/lib/Editor/HTMLInlineOptions/index.js
+++ src/lib/Editor/HTMLInlineOptions/index.js
@@ -1,55 +1,58 @@
 import AnnotationParameter from './AnnotationParameter'
 
 export default class HTMLInlineOptions {
+  #element
+  #annotationParameter
+
   constructor(element) {
-    this._element = element
+    this.#element = element
 
     // Reading inline annotations is a destructive operation, so it is done in the constructor.
-    this._annotationParameter = new AnnotationParameter(
-      this._element,
-      this._source
+    this.#annotationParameter = new AnnotationParameter(
+      this.#element,
+      this.#source
     )
   }
 
   get annotation() {
-    return this._annotationParameter
+    return this.#annotationParameter
   }
 
   get autocompletionWS() {
-    return this._readURLAttribute('autocompletion_ws')
+    return this.#readURLAttribute('autocompletion_ws')
   }
 
   get config() {
-    return this._readURLAttribute('config')
+    return this.#readURLAttribute('config')
   }
 
   get configLock() {
     // Over write editor-div's config lock state by url's.
     // Url's default is 'unlock', so its default is also 'unlock'.
-    if (this._source) {
-      const searchParams = new URLSearchParams(this._source.split('?')[1])
+    if (this.#source) {
+      const searchParams = new URLSearchParams(this.#source.split('?')[1])
 
       if (searchParams.has('config_lock')) {
         return searchParams.get('config_lock')
       }
     }
 
-    return this._element.getAttribute('config_lock')
+    return this.#element.getAttribute('config_lock')
   }
 
   get control() {
-    return this._element.getAttribute('control')
+    return this.#element.getAttribute('control')
   }
 
   get inspect() {
-    return this._element.getAttribute('inspect')
+    return this.#element.getAttribute('inspect')
   }
 
   /**
    * @returns {boolean}
    */
   get isEditMode() {
-    switch (this._element.getAttribute('mode')) {
+    switch (this.#element.getAttribute('mode')) {
       case 'edit':
       case 'term-edit':
       case 'block-edit':
@@ -64,37 +67,37 @@ export default class HTMLInlineOptions {
   get isTermEditMode() {
     // Same as edit mode and term-edit mode for compatibility.
     return (
-      this._element.getAttribute('mode') === 'edit' ||
-      this._element.getAttribute('mode') === 'term-edit'
+      this.#element.getAttribute('mode') === 'edit' ||
+      this.#element.getAttribute('mode') === 'term-edit'
     )
   }
 
   get isBlockEditMode() {
-    return this._element.getAttribute('mode') === 'block-edit'
+    return this.#element.getAttribute('mode') === 'block-edit'
   }
 
   get isRelationEditMode() {
-    return this._element.getAttribute('mode') === 'relation-edit'
+    return this.#element.getAttribute('mode') === 'relation-edit'
   }
 
   get statusBar() {
-    return this._element.getAttribute('status_bar')
+    return this.#element.getAttribute('status_bar')
   }
 
   get saveTo() {
-    return this._readURLAttribute('save_to')
+    return this.#readURLAttribute('save_to')
   }
 
-  get _source() {
+  get #source() {
     return (
-      this._element.getAttribute('source') ||
-      this._element.getAttribute('target')
+      this.#element.getAttribute('source') ||
+      this.#element.getAttribute('target')
     )
   }
 
-  _readURLAttribute(name) {
-    if (this._element.hasAttribute(name)) {
-      return decodeURIComponent(this._element.getAttribute(name))
+  #readURLAttribute(name) {
+    if (this.#element.hasAttribute(name)) {
+      return decodeURIComponent(this.#element.getAttribute(name))
     }
 
     return null
