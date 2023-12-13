19582a63a213a5082e901c5a89b8a21d2ef916c0
ledsun <shigeru.nakajima@gmail.com>
2023-12-13 12:36:07 +0900

    refactor: Split file

--- src/lib/component/TypeValuesPallet/createContentHtml/numericAttributeTemplate/index.js
+++ src/lib/component/TypeValuesPallet/createContentHtml/numericAttributeTemplate/index.js
@@ -1,8 +1,8 @@
-import headerTemplate from './headerTemplate'
-import valueButtonsTemplate from './valueButtonsTemplate'
-import showAddAttributeValueButton from './showAddAttributeValueButton'
-import predicateControllerTemplate from './predicateControllerTemplate'
-import anemone from '../../anemone'
+import headerTemplate from '../headerTemplate'
+import showAddAttributeValueButton from '../showAddAttributeValueButton'
+import predicateControllerTemplate from '../predicateControllerTemplate'
+import anemone from '../../../anemone'
+import toBodyRow from './toBodyRow'
 
 export default function (context) {
   const { min, max, step, default: defaultValue, values } = context.attrDef
@@ -35,21 +35,5 @@ export default function (context) {
       </tbody>
     </table>
   </div>
-  `
-}
-function toBodyRow(color, range, label, isLock, index, indelible) {
-  return () => anemone`
-    <tr class="textae-editor__pallet__row" style="background-color: ${color};">
-      <td class="textae-editor__pallet__attribute-label">
-        ${range}
-      </td>
-      <td class="textae-editor__pallet__short-label">
-        ${label}
-      </td>
-      <td class="textae-editor__pallet__short-label">
-        ${color}
-      </td>
-      ${() => valueButtonsTemplate(isLock, index, indelible)}
-    </tr>
   `
 }
---
+++ src/lib/component/TypeValuesPallet/createContentHtml/numericAttributeTemplate/toBodyRow.js
@@ -0,0 +1,26 @@
+import valueButtonsTemplate from '../valueButtonsTemplate'
+import anemone from '../../../anemone'
+
+export default function toBodyRow(
+  color,
+  range,
+  label,
+  isLock,
+  index,
+  indelible
+) {
+  return () => anemone`
+    <tr class="textae-editor__pallet__row" style="background-color: ${color};">
+      <td class="textae-editor__pallet__attribute-label">
+        ${range}
+      </td>
+      <td class="textae-editor__pallet__short-label">
+        ${label}
+      </td>
+      <td class="textae-editor__pallet__short-label">
+        ${color}
+      </td>
+      ${() => valueButtonsTemplate(isLock, index, indelible)}
+    </tr>
+  `
+}
