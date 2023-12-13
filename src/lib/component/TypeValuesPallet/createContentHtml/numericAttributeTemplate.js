0825808ca7e858926638781ee0f3fe3a4ee20144
ledsun <shigeru.nakajima@gmail.com>
2023-12-13 12:29:20 +0900

    refactor: Refactor template

--- src/lib/component/TypeValuesPallet/createContentHtml/numericAttributeTemplate.js
+++ src/lib/component/TypeValuesPallet/createContentHtml/numericAttributeTemplate.js
@@ -20,32 +20,36 @@ export default function (context) {
     </div>
 
     <table>
-      <tbody>
+      <thead>
         <tr>
           <th>range</th>
           <th>label</th>
           <th>color</th>
           ${showAddAttributeValueButton(isLock)}
         </tr>
-        ${() =>
-          values.map(({ color = '', range, label = '', indelible }, index) => {
-            return `
-        <tr class="textae-editor__pallet__row" style="background-color: ${color};">
-          <td class="textae-editor__pallet__attribute-label">
-            ${range}
-          </td>
-          <td class="textae-editor__pallet__short-label">
-            ${label}
-          </td>
-          <td class="textae-editor__pallet__short-label">
-            ${color}
-          </td>
-          ${valueButtonsTemplate(isLock, index, indelible)}
-        </tr>
-      `
-          })}
+      </thead>
+      <tbody>
+        ${values.map(({ color = '', range, label = '', indelible }, index) =>
+          toBodyRow(color, range, label, isLock, index, indelible)
+        )}
       </tbody>
     </table>
   </div>
+  `
+}
+function toBodyRow(color, range, label, isLock, index, indelible) {
+  return () => anemone`
+    <tr class="textae-editor__pallet__row" style="background-color: ${color};">
+      <td class="textae-editor__pallet__attribute-label">
+        ${range}
+      </td>
+      <td class="textae-editor__pallet__short-label">
+        ${label}
+      </td>
+      <td class="textae-editor__pallet__short-label">
+        ${color}
+      </td>
+      ${() => valueButtonsTemplate(isLock, index, indelible)}
+    </tr>
   `
 }
