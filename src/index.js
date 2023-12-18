bb6525797d181606a09ed959ed6fff1295274923
ledsun <shigeru.nakajima@gmail.com>
2023-12-18 16:49:17 +0900

    feature: Show version on console at loaded

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import textae from './lib/textae'
+import packageJson from '../package.json'
 
 document.addEventListener('DOMContentLoaded', textae)
 
 // This function is experimental.
 // It is being released to verify its feasibility for integration into React.
 window.initializeTextAEEditor = textae
+
+console.log(`TextAE Version ${packageJson.version} loaded.`)
