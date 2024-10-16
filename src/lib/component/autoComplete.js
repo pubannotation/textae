e2e9e76f7af568764920a80254585e5d9d190a5d
yush-nh <yushin.nihei@luxiar.com>
2024-10-16 17:32:38 +0900

    feature: Add keyboard selection to autocomplete

--- src/lib/component/autoComplete.js
+++ src/lib/component/autoComplete.js
@@ -8,8 +8,11 @@ export default class Autocomplete {
     this.resultsList.setAttribute('popover', 'auto')
     this.resultsList.style.margin = '0'
 
+    this.selectedIndex = -1
+
     inputElement.parentElement.appendChild(this.resultsList)
     this.inputElement.addEventListener('input', this.handleInput.bind(this))
+    this.inputElement.addEventListener('keydown', this.handleKeyDown.bind(this))
   }
 
   handleInput(event) {
@@ -24,13 +27,14 @@ export default class Autocomplete {
 
   onResults(results) {
     this.resultsList.innerHTML = ''
+    this.selectedIndex = -1
 
     if (results.length === 0) {
       this.resultsList.hidePopover()
       return
     }
 
-    for (const result of results) {
+    results.forEach((result, i) => {
       const listItem = document.createElement('li')
       listItem.innerHTML = `
         <div>
@@ -38,13 +42,16 @@ export default class Autocomplete {
         </div>
       `
 
+      listItem.result = result
+
       listItem.addEventListener('click', () => {
         this.onSelect(result.id, result.label)
         this.resultsList.hidePopover()
       })
 
+      listItem.setAttribute('data-index', i)
       this.resultsList.appendChild(listItem)
-    }
+    })
 
     this.showPopoverUnderInputElement()
   }
@@ -57,5 +64,47 @@ export default class Autocomplete {
     this.resultsList.style.left = `${rect.left + window.scrollX}px`
 
     this.resultsList.showPopover()
+  }
+
+  handleKeyDown(event) {
+    const items = this.resultsList.querySelectorAll('li')
+    if (items.length === 0) return
+
+    switch (event.key) {
+      case 'ArrowDown':
+        this.selectedIndex = (this.selectedIndex + 1) % items.length
+        this.updateSelection(items)
+        break
+
+      case 'ArrowUp':
+        this.selectedIndex =
+          (this.selectedIndex - 1 + items.length) % items.length
+        this.updateSelection(items)
+        break
+
+      case 'Enter':
+        if (this.selectedIndex >= 0) {
+          const selectedItem = items[this.selectedIndex]
+          this.onSelect(selectedItem.result.id, selectedItem.result.label)
+          this.resultsList.hidePopover()
+        }
+        break
+
+      default:
+        return
+    }
+
+    event.preventDefault()
+  }
+
+  updateSelection(items) {
+    items.forEach((item, i) => {
+      if (this.selectedIndex === i) {
+        item.classList.add('selected')
+        item.scrollIntoView({ block: 'nearest' })
+      } else {
+        item.classList.remove('selected')
+      }
+    })
   }
 }
