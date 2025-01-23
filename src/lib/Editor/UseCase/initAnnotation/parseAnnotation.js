ed0f70d13d36566925e1117909a54dcaeff72fbc
yush-nh <yushin.nihei@luxiar.com>
2025-01-23 13:57:25 +0900

    fix: Remove unnecessary error message

--- src/lib/Editor/UseCase/initAnnotation/parseAnnotation.js
+++ src/lib/Editor/UseCase/initAnnotation/parseAnnotation.js
@@ -4,9 +4,7 @@ export default async function parseAnnotation(startUpOptions, resourceType) {
   try {
     return await startUpOptions.annotation()
   } catch (e) {
-    alertifyjs.error(
-      `failed to load annotation from ${resourceType}.<br>${e.message}`
-    )
+    alertifyjs.error(`failed to load annotation from ${resourceType}.`)
     return null
   }
 }
