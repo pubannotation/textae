ee99e6e0c3ae61c005cfb4531441e790c30ec467
yush-nh <yushin.nihei@luxiar.com>
2025-02-10 13:35:01 +0900

    refactor: Move explanation comment

--- src/lib/component/SettingDialog/bindAddCharacter/addCharacterRow.js
+++ src/lib/component/SettingDialog/bindAddCharacter/addCharacterRow.js
@@ -7,13 +7,13 @@ export default function addCharacterRow(content, type) {
   )
   const newValue = input.value
 
-  // Return with alert when new character already exists.
   const currentCharacters = Array.from(
     content.querySelectorAll(
       `.textae-editor__setting-dialog__${type}-character-input`
     )
   ).map((input) => input.value)
 
+  // Return with alert when new character already exists.
   if (currentCharacters.includes(newValue)) {
     alertifyjs.warning(`${newValue} is already added.`)
     return
