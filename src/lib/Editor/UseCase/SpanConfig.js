7f58bc3087a8eda3da0f0aea2056422101ac7663
yush-nh <yushin.nihei@luxiar.com>
2025-02-12 17:53:59 +0900

    refactor: Use # for private property

--- src/lib/Editor/UseCase/SpanConfig.js
+++ src/lib/Editor/UseCase/SpanConfig.js
@@ -28,41 +28,44 @@ const defaults = {
 }
 
 export default class SpanConfig {
+  #delimiterCharacters
+  #blankCharacters
+
   constructor() {
-    this._delimiterCharacters = []
-    this._blankCharacters = []
+    this.#delimiterCharacters = []
+    this.#blankCharacters = []
   }
 
   get delimiterCharacters() {
-    return this._delimiterCharacters
+    return this.#delimiterCharacters
   }
 
   get blankCharacters() {
-    return this._blankCharacters
+    return this.#blankCharacters
   }
 
   set(config) {
     const settings = { ...defaults, ...config }
 
-    this._delimiterCharacters = settings['delimiter characters']
-    this._blankCharacters = settings['non-edge characters']
+    this.#delimiterCharacters = settings['delimiter characters']
+    this.#blankCharacters = settings['non-edge characters']
     return config
   }
 
   isDelimiter(char) {
-    if (this._delimiterCharacters.indexOf('ANY') >= 0) {
+    if (this.#delimiterCharacters.indexOf('ANY') >= 0) {
       return 1
     }
 
-    return this._delimiterCharacters.indexOf(char) >= 0
+    return this.#delimiterCharacters.indexOf(char) >= 0
   }
 
   isBlankCharacter(char) {
-    return this._blankCharacters.indexOf(char) >= 0
+    return this.#blankCharacters.indexOf(char) >= 0
   }
 
   removeBlankCharacters(str) {
-    for (const char of this._blankCharacters) {
+    for (const char of this.#blankCharacters) {
       str = str.replaceAll(char, '')
     }
 
