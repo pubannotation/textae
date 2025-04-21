a4cfb4c8aa282a80aba1e4f1710bac408e433de4
xaiBUh29wX <arino.tamada@luxiar.com>
2025-04-21 17:02:20 +0900

    Removed async from parseFileContent

--- src/lib/Editor/UseCase/PersistenceInterface/readAnnotationFile/parseFileContent.js
+++ src/lib/Editor/UseCase/PersistenceInterface/readAnnotationFile/parseFileContent.js
@@ -1,6 +1,6 @@
 import SimpleInlineTextAnnotation from 'simple-inline-text-annotation'
 
-export default async function parseFileContent(fileContent) {
+export default function parseFileContent(fileContent) {
   try {
     const annotation = SimpleInlineTextAnnotation.parse(fileContent)
 
