29b65bf12ae5db985d42d01ecfbf379aab5da928
xaiBUh29wX <arino.tamada@luxiar.com>
2025-05-15 14:02:26 +0900

    feature: Enable autocomplete by embedding autocompletionCallback in development.html

--- src/lib/textae/API.js
+++ src/lib/textae/API.js
@@ -19,6 +19,10 @@ export default class API {
     this._editor.setLastSelectedDenotationIDCallback(callback)
   }
 
+  set autocompletionCallback(callback) {
+    this._editor.setAutocompletionFunction(callback)
+  }
+
   get id() {
     return this._editor.HTMLElementID
   }
