43f0c586ed1f729e93673cb2ebb9a21a36daae6f
xaiBUh29wX <arino.tamada@luxiar.com>
2025-05-16 17:28:44 +0900

    Remove local data merging from fetchAutocompleteFromWs

--- src/lib/component/fetchAutocompleteFromWs.js
+++ src/lib/component/fetchAutocompleteFromWs.js
@@ -1,30 +1,19 @@
-export default function fetchAutocompleteFromWs(
-  term,
-  done,
-  autocompletionWs,
-  localData = []
-) {
-  if (!autocompletionWs) {
-    done(localData)
+export default function fetchAutocompleteFromWs(term, done, autocompletionWs) {
+  if (autocompletionWs) {
+    const url = new URL(autocompletionWs, location)
+    url.searchParams.append('term', term)
+
+    fetch(url.href)
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        }
+      })
+      .then((data) => {
+        done(Array.isArray(data) ? data : [])
+      })
     return
   }
 
-  const url = new URL(autocompletionWs, location)
-  url.searchParams.append('term', term)
-
-  fetch(url.href)
-    .then((response) => {
-      if (response.ok) {
-        return response.json()
-      }
-    })
-    .then((data) => {
-      // Prior local data if duplicated
-      const filteredData = data.filter(
-        (newDatum) =>
-          !localData.some((localDatum) => newDatum.id === localDatum.id)
-      )
-
-      done(localData.concat(filteredData))
-    })
+  done([])
 }
