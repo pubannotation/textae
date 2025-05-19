d91a6fb08d598494d27ed87f51a289a484475b44
xaiBUh29wX <arino.tamada@luxiar.com>
2025-05-19 14:03:05 +0900

    Replace the existing action with fetchAutocompleteCandidates

--- src/lib/component/fetchAutocompleteFromWs.js
+++ src/lib/component/fetchAutocompleteFromWs.js
@@ -1,15 +1,8 @@
+import fetchAutocompleteCandidates from './fetchAutocompleteCandidates'
+
 export default function fetchAutocompleteFromWs(term, done, autocompletionWs) {
   if (autocompletionWs) {
-    const url = new URL(autocompletionWs, location)
-    url.searchParams.append('term', term)
-
-    fetch(url.href)
-      .then((response) => {
-        if (response.ok) {
-          return response.json()
-        }
-      })
-      .then(done)
+    fetchAutocompleteCandidates(autocompletionWs, term).then(done)
     return
   }
 
