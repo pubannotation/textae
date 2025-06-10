ccb411fe4baf70a77e29ce76aa0d794ef26e7115
shigeru.nakajima <shigeru.nakajima@gmail.com>
2025-06-10 18:36:25 +0900

    misc: ESMize the webpack.prod.js

---
+++ webpack.prod.mjs
@@ -0,0 +1,32 @@
+import path from 'path';
+import { readFileSync } from 'fs';
+import TerserPlugin from 'terser-webpack-plugin';
+
+const PACKAGE = JSON.parse(readFileSync(new URL('./package.json', import.meta.url)));
+const { name, version } = PACKAGE;
+
+const entry = {};
+entry[`${name}-${version}.min`] = './src/index.js';
+entry[`${name}-${version}`] = './src/index.js';
+
+export default {
+  mode: 'production',
+  entry,
+  output: {
+    path: path.resolve(path.dirname(new URL(import.meta.url).pathname), 'tmp'),
+    filename: '[name].js'
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        include: /\.min\.js$/
+      })
+    ]
+  },
+  performance: {
+    maxAssetSize: 4 * 1024 * 1024,
+    maxEntrypointSize: 24 * 1024 * 1024
+  }
+};
