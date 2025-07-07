e89aab3925bc60384fd764ced000b37d1725bbfc
shigeru.nakajima <shigeru.nakajima@gmail.com>
2025-07-07 10:26:24 +0900

    refactor: Add comment

--- src/lib/Editor/forwardMethods.js
+++ src/lib/Editor/forwardMethods.js
@@ -1,6 +1,7 @@
 export default function (delegator, getTargetFunction, methods) {
   for (const method of methods) {
     delegator[method] = (...args) => {
+      // Determine the target when executing the method
       const target = getTargetFunction()
       console.assert(target[method], `No ${method} method to forward`, target)
 
