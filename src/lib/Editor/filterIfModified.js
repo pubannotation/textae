1c1405de48485dc981fc0f5ea78936003b450091
xaiBUh29wX <arino.tamada@luxiar.com>
2025-07-08 17:35:58 +0900

    Add condition to handle selectedText status in filterIfModified

--- src/lib/Editor/filterIfModified.js
+++ src/lib/Editor/filterIfModified.js
@@ -4,7 +4,10 @@ export default function filterIfModified(initialAnnotation) {
   let previous = initialAnnotation
 
   return function (currentAnnotation, callback) {
-    if (diffOfAnnotation(previous, currentAnnotation)) {
+    if (
+      diffOfAnnotation(previous, currentAnnotation) ||
+      currentAnnotation.selectedText.status !== 'unselected'
+    ) {
       previous = currentAnnotation
       callback(currentAnnotation)
     }
