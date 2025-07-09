dc505b189056a80aaabdd5858fe90d97d3fe0b83
shigeru.nakajima <shigeru.nakajima@gmail.com>
2025-07-09 16:08:55 +0900

    refactor: Use AnnotationModel.getSpan method

--- src/lib/Editor/UseCase/Presenter/EditModeSwitch/BlockEditMode/SpanEditor.js
+++ src/lib/Editor/UseCase/Presenter/EditModeSwitch/BlockEditMode/SpanEditor.js
@@ -382,8 +382,8 @@ export default class SpanEditor {
 
       return {
         spanID,
-        ...this.#annotationModel.spanInstanceContainer
-          .get(spanID)
+        ...this.#annotationModel
+          .getSpan(spanID)
           .getShortenInAnchorNodeToFocusNodeDirection(
             this.#menuState.textSelectionAdjuster,
             this.#annotationModel.sourceDoc,
