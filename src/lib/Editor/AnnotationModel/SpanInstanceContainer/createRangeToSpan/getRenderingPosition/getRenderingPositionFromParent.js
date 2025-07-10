28e7de1e6d88b362b6efd0e88568e8850beea0e8
shigeru.nakajima <shigeru.nakajima@gmail.com>
2025-07-10 11:25:12 +0900

    refactor: Use spread syntax

--- src/lib/Editor/AnnotationModel/SpanInstanceContainer/createRangeToSpan/getRenderingPosition/getRenderingPositionFromParent.js
+++ src/lib/Editor/AnnotationModel/SpanInstanceContainer/createRangeToSpan/getRenderingPosition/getRenderingPositionFromParent.js
@@ -1,11 +1,8 @@
 import getOffset from './getOffset'
 
 export default function getRenderingPositionFromParent(span, parent) {
-  const { start, end } = getOffset(span.begin, span.end, parent.begin)
-
   return {
     textNode: parent.element.firstChild,
-    start,
-    end
+    ...getOffset(span.begin, span.end, parent.begin)
   }
 }
