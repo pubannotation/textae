4abe6f98c526f840fcc0b46a7297111f248d5f9c
shigeru.nakajima <shigeru.nakajima@gmail.com>
2025-07-10 11:29:03 +0900

    refactor: Remove getRenderingPositionFromParent.js

--- src/lib/Editor/AnnotationModel/SpanInstanceContainer/createRangeToSpan/getRenderingPosition/getRenderingPositionFromParent.js
+++
@@ -1,8 +0,0 @@
-import getOffset from './getOffset'
-
-export default function getRenderingPositionFromParent(begin, end, parent) {
-  return {
-    textNode: parent.element.firstChild,
-    ...getOffset(begin, end, parent.begin)
-  }
-}
